dc16dfac8e081b73070d3f93ece3b24b492014fe
Robert Danielsson <robert.danielsson@soprasteria.com>
2020-09-24 10:55:40 +0200

    component-refactoring: Created a standardized logo for the header

---
+++ packages/common/src/components/AppHeader/Logo.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { makeStyles, Box } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  logo: {
+    maxWidth: 121,
+    margin: 20,
+    objectFit: 'cover'
+  },
+  title: {
+    backgroundColor: '#292f4f',
+  },
+})
+
+interface Props {
+  imgSrc: string
+  imgAlt?: string
+  additionalImgStyles?: string
+  additionalWrapperStyles?: string
+}
+
+const Logo: React.FC<Props> = ({ imgSrc, imgAlt, additionalImgStyles, additionalWrapperStyles }) => {
+  const classes = useStyles()
+
+  return (
+    <Box className={`${classes.title} ${additionalWrapperStyles}`}>
+      <img src={imgSrc} alt={imgAlt} className={`${classes.logo} ${additionalImgStyles}`} />
+    </Box>
+  )
+}
+
+export default Logo
