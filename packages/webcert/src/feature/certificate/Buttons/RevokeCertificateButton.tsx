1004bd2de6c603c281a992bd0cf46c6f0f4bd02e
Robert Danielsson <robert.danielsson@soprasteria.com>
2020-09-30 14:38:07 +0200

    Created revoke certificate button which uses the ButtonWithConfirmModal component

---
+++ packages/webcert/src/feature/certificate/Buttons/RevokeCertificateButton.tsx
@@ -0,0 +1,36 @@
+import { ButtonWithConfirmModal } from '@frontend/common'
+import React, { useState } from 'react'
+import { revokeCertificate, RevokeCertificateReason } from '../../../store/certificate/certificateActions'
+import { RevokeCertificateModalContent } from './RevokeCertificateModalContent'
+import DeleteIcon from '@material-ui/icons/Delete'
+import { useDispatch } from 'react-redux'
+
+const RevokeCertificateButton = () => {
+  const [dispatchObject, setDispatchObject] = useState<null | RevokeCertificateReason>(null)
+  const dispatch = useDispatch()
+
+  const handleRevokeForm = (obj: RevokeCertificateReason) => {
+    setDispatchObject(obj)
+  }
+
+  const handleDispatch = () => {
+    if (dispatchObject) {
+      dispatch(revokeCertificate(dispatchObject))
+    }
+  }
+
+  const disabled = dispatchObject === null || dispatchObject.message.length < 1
+
+  return (
+    <ButtonWithConfirmModal
+      confirmButtonDisabled={disabled}
+      buttonText="Makulera"
+      startIcon={<DeleteIcon />}
+      modalTitle="Makulera intyg"
+      onConfirm={handleDispatch}
+      modalContent={<RevokeCertificateModalContent onChange={handleRevokeForm} />}
+      confirmButtonText="Makulera"></ButtonWithConfirmModal>
+  )
+}
+
+export default RevokeCertificateButton
