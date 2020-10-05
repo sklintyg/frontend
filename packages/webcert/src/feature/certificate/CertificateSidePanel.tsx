078a9446f31b49727d1b08d45d1e6ed10a3e3c53
Robert Danielsson <robert.danielsson@soprasteria.com>
2020-10-05 09:24:00 +0200

    (wip) Started adjusting the certificate side panel to display formatted text instead of hard coded text

--- packages/webcert/src/feature/certificate/CertificateSidePanel.tsx
+++ packages/webcert/src/feature/certificate/CertificateSidePanel.tsx
@@ -1,61 +1,85 @@
 import React from 'react'
-import { Paper, Tabs, Tab, Box, Typography, Link } from '@material-ui/core'
+import { Paper, Tabs, Tab, Typography, Link } from '@material-ui/core'
 import { getIsShowSpinner } from '../../store/certificate/certificateSelectors'
 import { useSelector } from 'react-redux'
 import SchoolIcon from '@material-ui/icons/School'
 import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = makeStyles(() => ({
-  flexContainer: {
-    alignItems: 'center',
-    paddingTop: '20px',
+const useStyles = makeStyles((theme) => ({
+  root: {
+    overflowY: 'auto',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  contentWrapper: {
+    padding: theme.spacing(2),
+    flexGrow: 1,
+  },
+  contentText: {
+    whiteSpace: 'pre-line',
+    marginTop: theme.spacing(1),
+    fontSize: theme.typography.body2.fontSize,
+  },
+  border: {
+    border: '1px solid #cdced6',
+  },
+  certificateType: {
+    fontWeight: theme.typography.fontWeightMedium,
+  },
+  certificateVersion: {
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.subtitle2.fontSize,
+    marginLeft: theme.spacing(1),
+  },
+  helpWrapper: {
+    color: '#00a9a7',
+    position: 'sticky',
+    bottom: '0',
+    padding: theme.spacing(2.5),
+  },
+  tabs: {
+    borderBottom: 0,
   },
 }))
 
-interface CertificateSidePanelProps {}
+interface Props {}
 
-const CertificateSidePanel: React.FC<CertificateSidePanelProps> = (props) => {
+const CertificateSidePanel: React.FC<Props> = (props) => {
   const showSpinner = useSelector(getIsShowSpinner)
-  const styles = useStyles()
+  const classes = useStyles()
 
   if (showSpinner) return null
 
+  // TODO: This string should be fetched from some source
+  const string = `Arbetsförmedlingen behöver ett medicinskt utlåtande för en arbetssökande som har ett behov av fördjupat stöd.
+
+  Vi behöver ett utlåtande för att kunna:
+
+  • utreda och bedöma om den arbetssökande har en funktionsnedsättning som medför nedsatt arbetsförmåga
+  • bedöma om vi behöver göra anpassningar i program eller insatser
+  • erbjuda lämpliga utredande, vägledande, rehabiliterande eller arbetsförberedande insatser.`
+
   return (
     <>
-      <Paper style={{ overflowY: 'auto', height: '100%', display: 'flex', flexDirection: 'column' }}>
-        <Tabs
-          classes={{ flexContainer: styles.flexContainer }}
-          value={0}
-          indicatorColor="primary"
-          style={{ border: '1px solid #cdced6', alignItems: 'center' }}>
+      <Paper className={classes.root} square elevation={0}>
+        <Tabs className={`${classes.border} ${classes.tabs}`} value={0} indicatorColor="primary">
           <Tab label="Om intyget" />
         </Tabs>
-        <Box padding="10px" border={'1px solid #cdced6'} borderBottom={0} borderTop={0} flex={1} overflow={'auto'}>
-          <Typography component={'div'} variant="body1">
-            <Box component="span" fontWeight="fontWeightBold">
-              Arbetsförmedlingens medicinska utlåtande
-            </Box>
-            <Box component="span" marginLeft="5px">
-              AF00213 1.0
-            </Box>
-            <Box component="p">
-              Arbetsförmedlingen behöver ett medicinskt utlåtande för en arbetssökande som har ett behov av fördjupat stöd.
-            </Box>
-            <Box component="p">Vi behöver ett utlåtande för att kunna:</Box>
-            <Box component="p"> • utreda och bedöma om den arbetssökande har en funktionsnedsättning som medför nedsatt arbetsförmåga</Box>
-            <Box component="p">• bedöma om vi behöver göra anpassningar i program eller insatser</Box>
-            <Box component="p">• erbjuda lämpliga utredande, vägledande, rehabiliterande eller arbetsförberedande insatser.</Box>
+        <div className={`${classes.contentWrapper} ${classes.border}`}>
+          <Typography className={classes.certificateType}>
+            Arbetsförmedlingens medicinska utlåtande <span className={classes.certificateVersion}>AF00213 1.0</span>
           </Typography>
-        </Box>
-        <Box color={'#00a9a7'} border={'1px solid #cdced6'} position={'sticky'} bottom={'0'} padding={'20px'}>
+          <Typography className={classes.contentText}>{string}</Typography>
+        </div>
+        <div className={`${classes.helpWrapper} ${classes.border}`}>
           <Typography style={{ display: 'flex', alignItems: 'center' }} color={'inherit'}>
             <SchoolIcon style={{ marginRight: '5px' }} />
             <Link style={{ fontSize: '14px', display: 'inline-block' }} href={'#'} color={'inherit'}>
-              {' '}
               Hitta svar på dina frågor i Ineras intygsskola
             </Link>
           </Typography>
-        </Box>
+        </div>
       </Paper>
     </>
   )
