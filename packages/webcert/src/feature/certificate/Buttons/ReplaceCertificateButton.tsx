fefc3c29558fa9e5baa619574fd3394b8cd99e02
Robert Danielsson <robert.danielsson@soprasteria.com>
2020-10-07 17:06:28 +0200

    Added logic to replace cert button to forward the user to the new certificate if the current one is replaced

--- packages/webcert/src/feature/certificate/Buttons/ReplaceCertificateButton.tsx
+++ packages/webcert/src/feature/certificate/Buttons/ReplaceCertificateButton.tsx
@@ -2,22 +2,35 @@ import React from 'react'
 import SyncAltIcon from '@material-ui/icons/SyncAlt'
 import { useHistory } from 'react-router-dom'
 import { ButtonWithConfirmModal } from '@frontend/common'
-import { InfoBox } from '@frontend/common'
-import { useDispatch } from 'react-redux'
+import { InfoBox, isReplaced } from '@frontend/common'
+import { useDispatch, useSelector } from 'react-redux'
 import { replaceCertificate } from '../../../store/certificate/certificateActions'
 import { Typography } from '@material-ui/core'
+import { getCertificateMetaData } from '../../../store/certificate/certificateSelectors'
 
 const ReplaceCertificateButton = () => {
   const history = useHistory()
   const dispatch = useDispatch()
+  const certificateMetadata = useSelector(getCertificateMetaData)
+  if (!certificateMetadata) return null
+
+  const isCertReplaced = isReplaced(certificateMetadata)
+
+  const handleConfirm = () => {
+    if (isCertReplaced) {
+      return () => history.push(`/certificate/${certificateMetadata.relations.children[0].certificateId}`)
+    } else {
+      return () => dispatch(replaceCertificate(history))
+    }
+  }
 
   return (
     <ButtonWithConfirmModal
       buttonText="Ersätt"
       startIcon={<SyncAltIcon />}
       modalTitle="Ersätt intyg"
-      onConfirm={() => dispatch(replaceCertificate(history))}
-      confirmButtonText="Ersätt">
+      onConfirm={handleConfirm()}
+      confirmButtonText={isCertReplaced ? 'Fortsätt på utkast' : 'Ersätt'}>
       <>
         <InfoBox type="info">
           Om intyget innehåller ett allvarligt fel, till exemepel om det är utfärdat på fel patient, bör du istället makulera intyget.
