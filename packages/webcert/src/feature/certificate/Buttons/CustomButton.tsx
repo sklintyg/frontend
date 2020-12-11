a85aa1a2fa9eb436635e47191a4e5b762047f274
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2020-12-11 10:08:15 +0100

    INTYGFV-13633: Updated button styling

--- packages/webcert/src/feature/certificate/Buttons/CustomButton.tsx
+++ packages/webcert/src/feature/certificate/Buttons/CustomButton.tsx
@@ -1,57 +1,5 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core'
-import colors from '../../../components/styles/colors'
 import { ButtonTooltip } from '@frontend/common/src'
-
-/**
-const defaultHover = {
-  backgroundColor: colors.WC_COLOR_21,
-  shadow: '( 0, 0, 0, 0.24)',
-  color: colors.WC_COLOR_00,
-}
-
-const defaultDisabled = {
-  backgroundColor: colors.WC_COLOR_13,
-  color: colors.WC_COLOR_22,
-}
-
-const useStyles = makeStyles((theme) => ({
-  primary: {
-    backgroundColor: colors.WC_COLOR_14,
-    color: colors.WC_COLOR_00,
-    borderRadius: '4px',
-    '&:hover': defaultHover,
-    '&:disabled': defaultDisabled,
-  },
-  secondary: {
-    backgroundColor: colors.WC_COLOR_15,
-    color: colors.WC_COLOR_08,
-    borderRadius: '4px',
-    '&:hover': defaultHover,
-    '&:disabled': defaultDisabled,
-  },
-  success: {
-    backgroundColor: colors.WC_COLOR_16,
-    color: colors.WC_COLOR_00,
-    borderRadius: '4px',
-    '&:hover': defaultHover,
-    '&:disabled': defaultDisabled,
-  },
-  default: {
-    backgroundColor: colors.WC_COLOR_00,
-    color: colors.WC_COLOR_17,
-    borderColor: colors.WC_COLOR_17,
-    borderRadius: '4px',
-    '&:hover': {
-      backgroundColor: colors.WC_COLOR_21,
-      shadow: '( 0, 0, 0, 0.24)',
-      color: colors.WC_COLOR_00,
-      borderColor: 'inherit',
-    },
-    '&:disabled': defaultDisabled,
-  },
-}))
- **/
 
 interface Props {
   style: 'primary' | 'secondary' | 'success' | 'default'
@@ -63,26 +11,30 @@ interface Props {
   startIcon?: React.ReactNode
   text?: string
   tooltip?: string
+  squared?: boolean
 }
 
 const CustomButton: React.FC<Props> = (props) => {
-  //const classes = useStyles()
-
   let addedClass
+  if (!props.squared) {
+    addedClass = 'ic-button--rounded '
+  }
   switch (props.style) {
     case 'success':
+      addedClass += 'ic-btn--success iu-bg-success'
+      break
     case 'primary':
-      addedClass = 'ic-button--primary'
+      addedClass += 'ic-button--primary'
       break
     case 'secondary':
     case 'default':
     default:
-      addedClass = 'ic-button--secondary'
+      addedClass += 'ic-button--secondary'
   }
 
   return (
     <ButtonTooltip description={props.tooltip ? props.tooltip : ''}>
-      <button className={'ic-button ic-button--rounded ' + addedClass} type="button" disabled={props.disabled} onClick={props.onClick}>
+      <button className={'ic-button ' + addedClass} type="button" disabled={props.disabled} onClick={props.onClick}>
         <span className="iu-mr-200" style={{ display: 'flex' }}>
           {props.startIcon}
         </span>{' '}
