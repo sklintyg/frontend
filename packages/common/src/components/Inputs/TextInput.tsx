876636dd72b3580600e1293ba348debe06854263
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2021-02-15 11:39:09 +0100

    INTYGFV-13552: Updated text input to common CSS for Inera

--- packages/common/src/components/Inputs/TextInput.tsx
+++ packages/common/src/components/Inputs/TextInput.tsx
@@ -1,31 +1,31 @@
 import React, { ChangeEvent } from 'react'
-import { TextField } from '@material-ui/core'
-import useTheme from '@material-ui/core/styles/useTheme'
 
 interface Props {
-  label: string
+  label?: string
   name?: string
   value?: string
-  onChange: (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => void
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void
   hasValidationError?: boolean
-  additionalStyles?: string
   disabled?: boolean
+  placeholder?: string
+  additionalStyles?: string
 }
 
 const TextInput: React.FC<Props> = (props) => {
-  const theme = useTheme()
-  const { label, disabled, name, onChange, value, additionalStyles, hasValidationError } = props
-
+  const { label, disabled, name, onChange, value, additionalStyles, hasValidationError, placeholder } = props
   return (
-    <TextField
-      disabled={disabled}
-      className={additionalStyles}
-      style={{ color: `${hasValidationError ? `${theme.palette.error.main}` : ''}` }}
-      name={name ?? ''}
-      onChange={(e) => onChange(e)}
-      label={label}
-      variant="outlined"
-    />
+    <>
+      {label ? <p>{label}</p> : ''}
+      <input
+        type="text"
+        disabled={disabled}
+        className={`ic-textfield ${additionalStyles} ${hasValidationError ? 'iu-color-error iu-border-error' : ''}`}
+        name={name ?? ''}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e)}
+      />
+    </>
   )
 }
 
