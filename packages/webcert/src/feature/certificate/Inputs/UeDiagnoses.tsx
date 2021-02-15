b7d260c75790e44a90506f2313bbf85b4dba7e85
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2021-02-15 11:42:27 +0100

    INTYGFV-13552: Updated diagnosis components

--- packages/webcert/src/feature/certificate/Inputs/UeDiagnoses.tsx
+++ packages/webcert/src/feature/certificate/Inputs/UeDiagnoses.tsx
@@ -1,22 +1,24 @@
 import * as React from 'react'
 import { useSelector } from 'react-redux'
 import { useAppDispatch } from '../../../store/store'
-import {
-  CertificateDataElement,
-  CertificateDataValueType,
-  ConfigUeDiagnoses,
-  ValueDiagnosisList,
-  ValueDiagnosis,
-  DiagnosisTypeahead,
-  Diagnosis,
-} from '@frontend/common'
+import { CertificateDataElement, CertificateDataValueType, ConfigUeDiagnoses, ValueDiagnosisList, ValueDiagnosis } from '@frontend/common'
 import { updateCertificateDataElement } from '../../../store/certificate/certificateActions'
 import { getQuestionHasValidationError, getShowValidationErrors } from '../../../store/certificate/certificateSelectors'
 import { QuestionValidationTexts, RadioButton } from '@frontend/common'
-import TextInput from '@frontend/common/src/components/Inputs/TextInput'
 import { useEffect, useState } from 'react'
-import { getDiagnosisTypeahead } from '../../../store/utils/utilsActions'
 import { getDiagnosisTypeaheadResult } from '../../../store/utils/utilsSelectors'
+import styled from 'styled-components'
+import UeDiagnosis from './UeDiagnosis'
+
+const RadioWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-bottom: 10px;
+
+  div {
+    padding-right: 10px;
+  }
+`
 
 interface Props {
   question: CertificateDataElement
@@ -28,12 +30,8 @@ const UeDiagnoses: React.FC<Props> = ({ question, disabled }) => {
   const [selectedCodeSystem, setSelectedCodeSystem] = useState(questionConfig.terminology[0].id)
   const [currentCodeValue, setCurrentCodeValue] = useState('')
   const isShowValidationError = useSelector(getShowValidationErrors)
-  const shouldDisplayValidationError = useSelector(getQuestionHasValidationError(question.id))
   const diagnosisTypeaheadResult = useSelector(getDiagnosisTypeaheadResult())
-  const diagnosisListValue = getDiagnosisListValue(question)
   const dispatch = useAppDispatch()
-  const MAX_NUMBER_OF_TYPEAHEAD_RESULTS = 18
-  const MIN_CODE_LENGTH = 2
 
   useEffect(() => {
     if (enteredCodeExists()) {
@@ -63,7 +61,7 @@ const UeDiagnoses: React.FC<Props> = ({ question, disabled }) => {
   function enteredCodeExists(): boolean {
     if (diagnosisTypeaheadResult !== null && diagnosisTypeaheadResult.resultat === 'OK') {
       for (const index in diagnosisTypeaheadResult.diagnoser) {
-        if (diagnosisTypeaheadResult.diagnoser[index].kod === currentCodeValue) {
+        if (diagnosisTypeaheadResult.diagnoser[index].kod.toLowerCase() === currentCodeValue) {
           return true
         }
       }
@@ -75,56 +73,14 @@ const UeDiagnoses: React.FC<Props> = ({ question, disabled }) => {
     setSelectedCodeSystem(event.currentTarget.name)
   }
 
-  const handleCodeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log('handleCodeChange: ' + event.currentTarget.value)
-    setCurrentCodeValue(event.currentTarget.value)
-
-    if (event.currentTarget.value !== undefined && event.currentTarget.value.length > MIN_CODE_LENGTH) {
-      dispatch(
-        getDiagnosisTypeahead({
-          codeSystem: selectedCodeSystem,
-          codeFragment: event.currentTarget.value.toUpperCase(),
-          maxNumberOfResults: MAX_NUMBER_OF_TYPEAHEAD_RESULTS,
-        })
-      )
-    }
-  }
-
-  const handleDescriptionChange: React.ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement> = (event) => {
-    // const valueDiagnosis: ValueDiagnosis = {
-    //   type: CertificateDataValueType.DIAGNOSIS,
-    //   id: event.currentTarget.name,
-    //   terminology: selectedCodeSystem,
-    //   code: 'F500',
-    //   description: 'Anorexia nervosa',
-    // }
-    // const updatedValue = getUpdatedValue(question, valueDiagnosis)
-    // dispatch(updateCertificateDataElement(updatedValue))
-  }
-
-  const getOptions = () => {
-    if (diagnosisTypeaheadResult === null || diagnosisTypeaheadResult.resultat !== 'OK' || currentCodeValue.length <= MIN_CODE_LENGTH) {
-      return
-    }
-
-    return diagnosisTypeaheadResult.diagnoser.map((diagnosis) => {
-      return <option key={diagnosis.kod + diagnosis.beskrivning} value={diagnosis.kod} label={diagnosis.beskrivning} />
-    })
-  }
-
-  if (!diagnosisListValue) {
-    return <div>Value not supported!</div>
-  }
-
   //TODO: F50 får tydligen inte väljas enligt nuvarande diagnos-komponent
-  //TODO: Labels i input (kod, diagnos)
   //TODO: Texterna ovanför radioknapparna och inputarna
   //TODO: Validering under varje input
 
   return (
     <>
-      <div>Välj kodverk:</div>
-      <div>
+      <div className="iu-pt-300">Välj kodverk:</div>
+      <RadioWrapper>
         {questionConfig.terminology.map((terminology) => {
           return (
             <RadioButton
@@ -132,28 +88,23 @@ const UeDiagnoses: React.FC<Props> = ({ question, disabled }) => {
               disabled={disabled}
               label={terminology.label}
               name={terminology.id}
+              id={terminology.id}
               value={terminology.id}
               checked={selectedCodeSystem === terminology.id}
               onChange={handleCodeSystemChange}
             />
           )
         })}
-      </div>
-      <div>Diagnoskod enligt ICD-10 SE</div>
+      </RadioWrapper>
+      <div className="iu-pt-300">Diagnoskod enligt ICD-10 SE</div>
       {questionConfig.list.map((diagnosis) => {
         return (
-          <div key={diagnosis.id + 'div'}>
-            <input key={diagnosis.id + 'code'} disabled={disabled} type="text" list="typeaheadList" onChange={handleCodeChange} />
-            <datalist id="typeaheadList">{getOptions()}</datalist>
-            <TextInput
-              key={diagnosis.id + 'description'}
-              disabled={disabled}
-              hasValidationError={shouldDisplayValidationError}
-              label="Diagnos"
-              name={diagnosis.id}
-              onChange={handleDescriptionChange}
-            />
-          </div>
+          <UeDiagnosis
+            key={diagnosis.id + '-diagnosis'}
+            question={question}
+            disabled={disabled}
+            id={diagnosis.id}
+            selectedCodeSystem={selectedCodeSystem}></UeDiagnosis>
         )
       })}
       {isShowValidationError && <QuestionValidationTexts validationErrors={question.validationErrors} />}
@@ -161,20 +112,16 @@ const UeDiagnoses: React.FC<Props> = ({ question, disabled }) => {
   )
 }
 
-function getDiagnosisListValue(question: CertificateDataElement): ValueDiagnosisList | null {
-  if (question.value?.type !== CertificateDataValueType.DIAGNOSIS_LIST) {
-    return null
-  }
-  return question.value as ValueDiagnosisList
-}
-
 function getUpdatedValue(question: CertificateDataElement, valueDiagnosis: ValueDiagnosis): CertificateDataElement {
-  //TODO: Jag måste kolla om id redan finns i listan
-
   const updatedQuestion: CertificateDataElement = { ...question }
-  updatedQuestion.value = { ...(updatedQuestion.value as ValueDiagnosisList) }
-  ;(updatedQuestion.value as ValueDiagnosisList).list = []
-  ;(updatedQuestion.value as ValueDiagnosisList).list.push(valueDiagnosis)
+  const updatedQuestionValue = { ...(updatedQuestion.value as ValueDiagnosisList) }
+  let updatedValueList = [...(updatedQuestionValue.list as ValueDiagnosis[])]
+  const updatedValueIndex = updatedValueList.findIndex((val) => val.id === valueDiagnosis.id)
+  if (updatedValueIndex === -1) {
+    updatedValueList = [...updatedValueList, valueDiagnosis as ValueDiagnosis]
+  }
+  updatedQuestionValue.list = updatedValueList
+  updatedQuestion.value = updatedQuestionValue
   return updatedQuestion
 }
 
---
+++ packages/webcert/src/feature/certificate/Inputs/UeDiagnosis.tsx
@@ -0,0 +1,159 @@
+import Typeahead from '@frontend/common/src/components/Inputs/Typeahead'
+import * as React from 'react'
+import styled from 'styled-components'
+import { css } from 'styled-components'
+import { CertificateDataElement, CertificateDataValueType, Diagnosis, ValueDiagnosis, ValueDiagnosisList } from '@frontend/common/src'
+import { useSelector } from 'react-redux'
+import { getQuestionHasValidationError } from '../../../store/certificate/certificateSelectors'
+import { getDiagnosisTypeahead } from '../../../store/utils/utilsActions'
+import { useAppDispatch } from '../../../store/store'
+import { updateCertificateDataElement } from '../../../store/certificate/certificateActions'
+import { getDiagnosisTypeaheadResult } from '../../../store/utils/utilsSelectors'
+
+interface Props {
+  question: CertificateDataElement
+  disabled: boolean
+  id: string
+  selectedCodeSystem: string
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-top: 10px;
+`
+
+const codeAdditionalStyles = css`
+  max-width: 150px !important;
+  padding-right: 10px;
+`
+
+const UeDiagnosis: React.FC<Props> = ({ disabled, id, selectedCodeSystem, question }) => {
+  const shouldDisplayValidationError = useSelector(getQuestionHasValidationError(question.id))
+  const savedDiagnosis = (question.value as ValueDiagnosisList).list.find((item) => item.id === id)
+  const [description, setDescription] = React.useState(savedDiagnosis !== undefined ? savedDiagnosis.description : undefined)
+  const [code, setCode] = React.useState(savedDiagnosis !== undefined ? savedDiagnosis.code : undefined)
+  const [openDescription, setOpenDescription] = React.useState(false)
+  const [openCode, setOpenCode] = React.useState(false)
+  const typeaheadResult = useSelector(getDiagnosisTypeaheadResult())
+  const dispatch = useAppDispatch()
+  const MAX_NUMBER_OF_TYPEAHEAD_RESULTS = 18
+  const MIN_CODE_LENGTH = 2
+
+  const handleCodeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCode(event.currentTarget.value)
+    if (event.currentTarget.value !== undefined && event.currentTarget.value.length > MIN_CODE_LENGTH) {
+      dispatch(
+        getDiagnosisTypeahead({
+          codeSystem: selectedCodeSystem,
+          codeFragment: event.currentTarget.value.toUpperCase(),
+          maxNumberOfResults: MAX_NUMBER_OF_TYPEAHEAD_RESULTS,
+        })
+      )
+    }
+    setOpenCode(true)
+    saveDiagnosis()
+  }
+
+  const handleDescriptionChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setDescription(event.currentTarget.value)
+    setOpenDescription(true)
+    saveDiagnosis()
+  }
+
+  function enteredCodeExists(): boolean {
+    if (typeaheadResult !== undefined && typeaheadResult !== null && typeaheadResult.resultat === 'OK') {
+      for (const index in typeaheadResult.diagnoser) {
+        if (typeaheadResult.diagnoser[index].kod.toLowerCase() === code) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  const getSuggestions = () => {
+    if (
+      typeaheadResult === undefined ||
+      typeaheadResult === null ||
+      typeaheadResult.resultat !== 'OK' ||
+      !code ||
+      code.length <= MIN_CODE_LENGTH
+    ) {
+      return []
+    }
+    return typeaheadResult.diagnoser.map((diagnosis: Diagnosis) => {
+      return diagnosis.kod + ' - ' + diagnosis.beskrivning
+    })
+  }
+
+  const onDiagnosisSelected = (value: string) => {
+    const newCode = value.split('-')[0]
+    const newDesc = value.split('-')[1]
+    setCode(newCode)
+    setDescription(newDesc)
+    setOpenCode(false)
+    setOpenDescription(false)
+    saveDiagnosis()
+  }
+
+  // TODO: Does description&code need to have a value?
+  const saveDiagnosis = () => {
+    if (enteredCodeExists()) {
+      const diagnosisValue: ValueDiagnosis = {
+        type: CertificateDataValueType.DIAGNOSIS,
+        id: id,
+        terminology: selectedCodeSystem,
+        code: code as string,
+        description: description as string,
+      }
+      const updatedValue = getUpdatedValue(question, diagnosisValue)
+      dispatch(updateCertificateDataElement(updatedValue))
+    }
+  }
+
+  return (
+    <Wrapper key={id + '-wrapper'}>
+      <Typeahead
+        suggestions={getSuggestions()}
+        additionalStyles={codeAdditionalStyles}
+        placeholder="Kod"
+        key={id + '-code'}
+        id={id}
+        disabled={disabled}
+        hasValidationError={shouldDisplayValidationError}
+        onSuggestionSelected={onDiagnosisSelected}
+        value={code}
+        open={openCode}
+        onChange={handleCodeChange}
+      />
+      <Typeahead
+        suggestions={getSuggestions()}
+        key={id + '-description'}
+        placeholder="Diagnos"
+        disabled={disabled}
+        hasValidationError={shouldDisplayValidationError}
+        onSuggestionSelected={onDiagnosisSelected}
+        id={id}
+        value={description}
+        onChange={handleDescriptionChange}
+        open={openDescription}
+      />
+    </Wrapper>
+  )
+}
+
+function getUpdatedValue(question: CertificateDataElement, valueDiagnosis: ValueDiagnosis): CertificateDataElement {
+  const updatedQuestion: CertificateDataElement = { ...question }
+  const updatedQuestionValue = { ...(updatedQuestion.value as ValueDiagnosisList) }
+  let updatedValueList = [...(updatedQuestionValue.list as ValueDiagnosis[])]
+  const updatedValueIndex = updatedValueList.findIndex((val) => val.id === valueDiagnosis.id)
+  if (updatedValueIndex === -1) {
+    updatedValueList = [...updatedValueList, valueDiagnosis as ValueDiagnosis]
+  }
+  updatedQuestionValue.list = updatedValueList
+  updatedQuestion.value = updatedQuestionValue
+  return updatedQuestion
+}
+
+export default UeDiagnosis
