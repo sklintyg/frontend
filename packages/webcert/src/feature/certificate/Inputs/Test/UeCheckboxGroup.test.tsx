be75f4cd70064b295285ac3fb63c5b7eca53d3d2
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2021-03-18 13:17:41 +0100

    INTYGFV-13827: Added tests for UeCheckboxGroup

---
+++ packages/webcert/src/feature/certificate/Inputs/Test/UeCheckboxGroup.test.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import UeCheckboxGroup from '../UeCheckboxGroup'
+import { CertificateDataElement, CertificateDataValueType, ConfigTypes } from '@frontend/common/src/types/certificate'
+import * as redux from 'react-redux'
+
+const CHECKBOXES = [
+  { label: 'Checkbox1', id: 'Checkbox_1' },
+  { label: 'Checkbox-2', id: 'Checkbox_2' },
+  { label: 'Checkbox 3', id: 'Checkbox_3' },
+]
+
+const question: CertificateDataElement = {
+  id: 'checkbox',
+  mandatory: true,
+  index: 0,
+  parent: '',
+  visible: true,
+  readOnly: false,
+  validation: [],
+  validationErrors: [],
+  value: { type: CertificateDataValueType.CODE_LIST, list: [] },
+  config: {
+    text: '',
+    description: '',
+    type: ConfigTypes.UE_CHECKBOX_MULTIPLE_CODE,
+    list: CHECKBOXES,
+  },
+}
+
+const renderDefaultComponent = () => {
+  render(
+    <>
+      <UeCheckboxGroup question={question} disabled={false} />
+    </>
+  )
+}
+
+const useSelectorSpy = jest.spyOn(redux, 'useSelector')
+const useDispatchSpy = jest.spyOn(redux, 'useDispatch')
+useDispatchSpy.mockReturnValue(jest.fn())
+useSelectorSpy.mockReturnValue(jest.fn())
+
+describe('Checkbox group component', () => {
+  it('renders without crashing', () => {
+    renderDefaultComponent()
+  })
+
+  it('disables correctly all checkboxes', () => {
+    render(
+      <>
+        <UeCheckboxGroup question={question} disabled={true} />
+      </>
+    )
+    const checkboxes = screen.queryAllByRole('checkbox')
+    expect(checkboxes).toHaveLength(CHECKBOXES.length)
+    checkboxes.forEach((c: any) => {
+      expect(c).toBeDisabled()
+    })
+  })
+
+  it('allows user to check and uncheck checkboxes', () => {
+    renderDefaultComponent()
+    const checkboxes = screen.queryAllByRole('checkbox')
+    expect(checkboxes).toHaveLength(CHECKBOXES.length)
+    checkboxes.forEach((c: any) => {
+      expect(c).not.toBeDisabled()
+      expect(c).not.toBeChecked()
+      userEvent.click(c)
+      expect(c).toBeChecked()
+    })
+  })
+})
