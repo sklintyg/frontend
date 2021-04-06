5ed389b90b5c4d9be9dfc3944ea603986994e4cd
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2021-04-06 07:41:44 +0200

    INTYGFV-13883: Change to default port while experimenting with dockerization.

--- packages/webcert/src/store/api/apiMiddleware.ts
+++ packages/webcert/src/store/api/apiMiddleware.ts
@@ -18,7 +18,7 @@ const apiMiddleware: Middleware = ({ dispatch }: MiddlewareAPI) => (next: Dispat
 
   try {
     const response = await axios.request({
-      baseURL: 'http://localhost:3000',
+      baseURL: 'http://localhost',
       url,
       method,
       data,
