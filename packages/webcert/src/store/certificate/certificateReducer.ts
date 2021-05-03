96b9b4aecc5a9fb57efca18d45877ab42fa5e52e
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2021-05-03 13:55:20 +0200

    INTYGFV-13859: Adjusted mockserver to respond with new response dto.

--- packages/webcert/src/store/certificate/certificateReducer.ts
+++ packages/webcert/src/store/certificate/certificateReducer.ts
@@ -74,18 +74,20 @@ const certificateReducer = createReducer(initialState, (builder) =>
           continue
         }
 
-        switch (question.value!.type) {
-          case CertificateDataValueType.TEXT:
-            const textValue = question.value as ValueText
-            if (textValue.text === undefined) {
-              textValue['text'] = ''
-            }
-            break
-          case CertificateDataValueType.BOOLEAN:
-            const booleanValue = question.value as ValueBoolean
-            if (booleanValue.selected === undefined) {
-              booleanValue['selected'] = null
-            }
+        if (question.value) {
+          switch (question.value!.type) {
+            case CertificateDataValueType.TEXT:
+              const textValue = question.value as ValueText
+              if (textValue.text === undefined) {
+                textValue['text'] = ''
+              }
+              break
+            case CertificateDataValueType.BOOLEAN:
+              const booleanValue = question.value as ValueBoolean
+              if (booleanValue.selected === undefined) {
+                booleanValue['selected'] = null
+              }
+          }
         }
       }
     })
