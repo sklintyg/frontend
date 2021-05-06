e97330ce4d31e23d75499063f65b14d21e6e81f9
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2021-05-06 10:19:29 +0200

    INTYGFV-13569: Updated statuses in certificate header

--- packages/webcert/src/feature/certificate/CertificateHeader/Status/SentStatus.tsx
+++ packages/webcert/src/feature/certificate/CertificateHeader/Status/SentStatus.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { CertificateMetadata, isReplaced, isSigned, StatusWithIcon } from '@frontend/common'
+import { CertificateEvent, CertificateEventType } from '@frontend/common/src'
 
 interface Props {
   certificateMetadata: CertificateMetadata
+  certificateEvents: CertificateEvent[]
 }
 
-const SentStatus: React.FC<Props> = ({ certificateMetadata }) => {
-  if (!isSigned(certificateMetadata) || isReplaced(certificateMetadata)) return null
+const SentStatus: React.FC<Props> = ({ certificateMetadata, certificateEvents }) => {
+  const isSent = certificateEvents.some((e) => e.type === CertificateEventType.SENT)
+  if (!isSigned(certificateMetadata) || isReplaced(certificateMetadata) || !isSent) return null
 
-  return <StatusWithIcon icon={'CheckIcon'}>Intyget är skickat till Arbetsförmedlingen</StatusWithIcon>
+  //TODO: Replace this with recipient from backend
+  return (
+    <StatusWithIcon icon={'CheckIcon'}>
+      Intyget är skickat till {certificateMetadata.type === 'lisjp' ? 'Försäkringskassan' : 'Arbetsförmedlingen'}
+    </StatusWithIcon>
+  )
 }
 
 export default SentStatus
