5c9384fab28ed93e824e8991630294f37e86af4f
Kristina Gullman <kristina.gullman@nordicmedtest.se>
2021-07-01 15:34:06 +0200

    INTYGFV-14015:Added info for AF00213 intyg

--- E2ETest/cypress/fixtures/AF_intyg/maxAF00213Data.js
+++ E2ETest/cypress/fixtures/AF_intyg/maxAF00213Data.js
@@ -1,88 +1,24 @@
 af = {
-    "grundFörMedicinsktUnderlag": {
-        "intygetÄrBaseratPå": {
-            "minUndersökning": true,
-            "minTelefonkontakt": true,
-            "journaluppgifter": true,
-            "annat": true,
-            "annatText": "Utlåtande från barnmorska"
+    "funktionsnedsättning": {
+        "finnsBesvärPågrundAvSjukdom": true,
+        "närPlanerasBehandlingKlar": "behandlingen planeras vara klar om ett tag"
+    },
+
+    "utredning och behandling": {
+        "finnsPågåendeEllerPplaneradeUtredningar": true,
+        "hurPåverkarUtredningarna": {
+            "text": "Allt beror på hur det kommer att bli"
         }
     },
 
-    "sysselsättning": {
-        "iRelationTillVilkenSysselsättningBedömerDuArbetsförmågan": {
-            "nuvarandeArbete": true,
-            "yrkeOchArbetsuppgifter": "Snickare. Tunga lyft, arbete på stege, hantera maskinverktyg, mycket gående",
-            "arbetssökande": true,
-            "föräldraledighet": true,
-            "studier": true
-        }
-    },
-
-    "diagnos": {
-        "rad1": {
-            "kod": "O267",
-            "text": "Symfyseolys under graviditet, förlossning och barnsängstid"
-        },
-        "rad2": {
-            "kod": "O470A",
-            "text": "Förvärkar utan cervixpåverkan före utgången av graviditetsvecka 37"
-        },
-        "rad3": {
-            "kod": "O210",
-            "text": "Lätta graviditetskräkningar"
-        }
-    },
-
-    "sjukdomensKonsekvenserFörPatienten": {
-        "funktionsnedsättning": "Uttalade besvär från bäcken, svår smärta vid lägesförändring, hälta, instabilitet bäcken",
-        "aktivitetsbegränsning": "starka bäckensmärtor som ger uttalad aktivitetsbegränsning vid fysisk aktivitet och kontorsarbete samt koncentrationssvårigheter"
-    },
-
-    "medicinskBehandling": {
-        "pågåendeBehandling": "Sjukgymnastik och rörelseprogram för att lindra bäckenuppluckring",
-        "planeradeBehandling": "Specialist, ultraljudskontroller"
-    },
-
-    "bedömning": {
-        "minBedömningAvPatientensNedsättningAvArbetsförmågan": {
-            "enFjärdedel": true,
-            "hälften": true,
-            "treFjärdedel": true,
-            "hel": true
-        },
-        "längreNedsattArbetsförmåga": "Haft tidigare bäckeninsufficens vid graviditet, haft tidigare missfall och kräver nu fler kontroller och avlastning",
-        "förläggaArbetstidOlika": {
-            "ja": true,
-            "arbetstidsförläggningstext": "Har lättare kontorsarbete på måndagar som hon klarar av med nuvarande besvär om ingen försämring sker."
-        },
-        "resorTillOchFrånArbete": true,
-        "prognosFörArbetsförmågaUtifrånAktuelltUndersökningstillfälle": {
-            "återgåHeltEfterSjukskrivning": true
-        }
-    },
-
-    "åtgärder": {
-        "inteAktuellt": false,
-        "arbetsträning": true,
-        "arbetsanspassning": true,
-        "sökaNyttArbete": true,
-        "besökPåArbetsplatsen": true,
-        "ergonomiskBedömning": true,
-        "hjälpmedel": true,
-        "konflikthantering": true,
-        "kontaktMedFöretagshälsovård": true,
-        "omfördelningAvArbetsuppgifter": true,
-        "övrigt": true,
-        "flerÅtgärder": "Om anpassningar till lättare uppgifter utan tunga lyft och mycket gående kan ordnas kvarstår arbetsförmåga under en tid framöver."
-    },
+    "arbetetsPåverkanPåSjukdom": {
+        "kanSjukdomenFörvärras": true,
+        "vilkenTypAvArbetsuppgifter":{
+            "text": "Det är helt omöjligt att böja sig ned för att hjälpa ett barn på med skorna"
+        } 
+     },
 
     "övrigt": {
-        "text": "Planerad partus 2019-09-11"
-    },
-
-    "kontakt": {
-        "ja": true,
-        "text": "För att diskutera sjukskrivningsperiod och tydliggörande kring besvärsbilden samt framtida arbetsförmåga."
+        "text": "Planerat avstämning 2021-08-19"
     }
 }
