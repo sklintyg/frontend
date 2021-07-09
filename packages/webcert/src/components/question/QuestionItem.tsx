08fcf91d2c9ab551e8470a7e6bc51611e4acfc8a
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2021-07-09 09:34:33 +0200

    INTYGFV-13873: Added image for FK and fixed layout for question items

--- packages/webcert/src/components/question/QuestionItem.tsx
+++ packages/webcert/src/components/question/QuestionItem.tsx
@@ -2,17 +2,24 @@ import React from 'react'
 import styled from 'styled-components'
 import { Question } from '@frontend/common'
 import { format } from 'date-fns'
+import fkImg from './fk.png'
 
 // TODO: Replace color with var(--color-grey-400)
 const QuestionHeader = styled.div`
   border-bottom: 1px solid #8d8d8d;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `
 
 const Card = styled.div`
   margin: 10px 0 10px 0;
   padding: 10px;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
 `
 
 interface Props {
@@ -23,12 +30,15 @@ const QuestionItem: React.FC<Props> = ({ question }) => {
   return (
     <Card className={'ic-card'}>
       <QuestionHeader>
-        <div>
-          <p>{question.author}</p>
-          <p className={'iu-fw-heading'}>{question.subject}</p>
-        </div>
-        <div>
-          <p className={'iu-color-grey-400'}>{format(new Date(question.sent), 'yyyy-MM-dd HH:mm')}</p>
+        <img src={fkImg} className={'iu-mr-200'} />
+        <div className={'iu-fullwidth'}>
+          <Wrapper>
+            <p className={'iu-fw-heading'}>{question.author}</p>
+          </Wrapper>
+          <Wrapper>
+            <p className={'iu-fw-heading'}>{question.subject}</p>
+            <p className={'iu-color-grey-400 iu-m-none'}>{format(new Date(question.sent), 'yyyy-MM-dd HH:mm')}</p>
+          </Wrapper>
         </div>
       </QuestionHeader>
       <p>{question.message}</p>
