e6a8e16075d41875e20dca087218c48eb3b4ea11
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2021-08-13 15:40:36 +0200

    INTYGFV-13557: Infobox should not be visible if there is no warning

--- packages/webcert/src/feature/certificate/Inputs/UeSickLeavePeriod/SickLeavePeriodWarning.tsx
+++ packages/webcert/src/feature/certificate/Inputs/UeSickLeavePeriod/SickLeavePeriodWarning.tsx
@@ -6,6 +6,8 @@ import { InfoBox } from '@frontend/common'
 export const SickLeavePeriodWarning: React.FC = () => {
   const warning = useSelector(getSickLeavePeriodWarning)
 
+  if (warning === '' || warning == null) return null
+
   return (
     <InfoBox type={'info'}>
       <p>{warning}</p>
