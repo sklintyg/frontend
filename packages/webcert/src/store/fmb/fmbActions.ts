e414c945fb079440795c91907f54eaa9529a9f72
Robert Danielsson <42959759+RobertDanielsson@users.noreply.github.com>
2021-09-08 09:29:38 +0200

    INTYGFV-13557: Added FMB warning for sick leave period (#123)
    Co-authored-by: mhornfeldt <michaela.hornfeldt@soprasteria.com>
    Co-authored-by: Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>

--- packages/webcert/src/store/fmb/fmbActions.ts
+++ packages/webcert/src/store/fmb/fmbActions.ts
@@ -1,10 +1,21 @@
 import { createAction } from '@reduxjs/toolkit'
-import { FMBDiagnosisCodeInfo } from '@frontend/common'
+import { FMBDiagnosisCodeInfo, ValueDateRangeList } from '@frontend/common'
+import { ValueDiagnosisList } from '@frontend/common/src'
 
 export interface FMBDiagnoseRequest {
   icd10Code: string
   icd10Description: string
   index: number
+}
+
+export interface FMBValidateSickLeavePeriodRequest {
+  icd10Codes: string[]
+  dateRangeList: ValueDateRangeList
+  personId: string
+}
+
+export interface ValidateSickLeavePeriodResponse {
+  message: string
 }
 
 const FMB = '[FMB]'
@@ -22,3 +33,16 @@ export const updateFMBDiagnosisCodeInfo = createAction<FMBDiagnosisCodeInfo>(`${
 export const updateFMBPanelActive = createAction<boolean>(`${FMB} Update panel active`)
 
 export const removeFMBDiagnosisCodes = createAction<FMBDiagnosisCodeInfo>(`${FMB} Remove diagnosis code info`)
+
+export const setSickLeavePeriodWarning = createAction<string>(`${FMB} Set sick leave period warning`)
+
+export const setPatientId = createAction<string>(`${FMB} Set patient id`)
+
+export const setSickLeavePeriodValue = createAction<ValueDateRangeList>(`${FMB} Set sick leave period value`)
+
+export const setDiagnosisListValue = createAction<ValueDiagnosisList>(`${FMB} Set diagnosis list value`)
+
+export const validateSickLeavePeriod = createAction<FMBValidateSickLeavePeriodRequest>(`${FMB} Validate sick leave period`)
+export const validateSickLeavePeriodStarted = createAction(`${FMB} Validate sick leave period started`)
+export const validateSickLeavePeriodSuccess = createAction<ValidateSickLeavePeriodResponse>(`${FMB} Validate sick leave period success`)
+export const validateSickLeavePeriodError = createAction(`${FMB} Validate sick leave period error`)
