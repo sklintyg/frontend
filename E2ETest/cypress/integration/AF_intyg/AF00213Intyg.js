10cccc1c6436d946b8dff6465cb4e34cd9499f6d
Kristina Gullman <kristina.gullman@nordicmedtest.se>
2021-10-18 14:04:03 +0200

    INTYGFV-14015: E2E-Test for React-frontend Webcert. Includes FK7804 and AF00213. (#136)

---
+++ E2ETest/cypress/integration/AF_intyg/AF00213Intyg.js
@@ -0,0 +1,81 @@
+/* globals context cy */
+/// <reference types="Cypress" />
+//import * as intyg from '../../support/FK_intyg/fk_helpers'
+import * as intyg from '../../support/AF_intyg/af00213Intyg'
+
+// LISJP = Läkarintyg för sjukpenning, FK 7804
+describe('AF00213-intyg minimalt ifyllt', function() {
+
+    before(function() {
+        cy.fixture('AF_intyg/maxAF00213Data').as('intygsdata');
+        cy.fixture('vEnheter/alfaVC').as('vårdenhet');
+        cy.fixture('vPatienter/tolvanTolvansson').as('vårdtagare');
+        cy.fixture('vPersonal/ajlaDoktor').as('vårdpersonal');
+        
+    });
+  
+    context('Användare har möjlighet att uföra följande med AF00213 Intyg ',function() {
+      beforeEach(function() {
+           /*   cy.fixture('AF_intyg/maxAF00213Data').as('intygsdata');
+        cy.fixture('vEnheter/alfaVC').as('vårdenhet');
+        cy.fixture('vPatienter/tolvanTolvansson').as('vårdtagare');
+        cy.fixture('vPersonal/ajlaDoktor').as('vårdpersonal');*/
+        //function skapaIntygViaApi(fx,status, typ, fillType){
+            cy.skapaIntygViaApi(this,1,true,true).then((utkastId) => {
+                cy.wrap(utkastId).as('utkastId');
+                cy.log("af00213-utkast med id " + utkastId + " skapat och används i testfallet");
+            });
+
+        });
+        describe('Funktioner på ett AF20013 intyg', () =>{
+
+            
+           
+           /* it('Skriva ut minimalt AF00213 intyg', function () {
+                //cy.visit('https://wc2.wc.localtest.me/welcome');
+                cy.loggaInVårdpersonalIntegrerat(this.vårdpersonal, this.vårdenhet, this.utkastId);
+
+                const önskadUrl = "/certificate/" + this.utkastId ;
+                cy.visit(önskadUrl);
+                intyg.signeraSkicka();
+
+                intyg.skrivUtUtkast();
+
+            });*/
+             
+            it('Skriva ut ett signerat AF20013 intyg', function () {
+                //cy.visit('https://wc2.wc.localtest.me/welcome');
+                cy.loggaInVårdpersonalIntegrerat(this.vårdpersonal, this.vårdenhet, this.utkastId);
+                const önskadUrl = "/certificate/" + this.utkastId ;
+                cy.visit(önskadUrl);
+                intyg.signeraSkicka();
+                intyg.skrivUt("utkast", this.utkastId, "af00213");//skriver ut via request
+            });
+            it('Ersätta ett AF00213 intyg', function (){
+                cy.loggaInVårdpersonalIntegrerat(this.vårdpersonal, this.vårdenhet, this.utkastId);
+                const önskadUrl = "/certificate/" + this.utkastId ;
+                cy.visit(önskadUrl);
+                intyg.signeraSkicka();
+                intyg.ersatta();
+                //cy.get('button').contains("Skicka till Arbetsförmedlingen").should('not.exist');
+                cy.contains(this.utkastId).should('not.exist')
+                //cy.get('.CertificateFooter__RightWrapper-sc-1mdu7r7-1 > .iu-fs-200').contains(this.utkastId).should('not.exist');
+               
+            });
+            it('Makulerar ett ifyllt AF00213 intyg', function () {
+                //cy.visit('https://wc2.wc.localtest.me/welcome');
+                cy.loggaInVårdpersonalIntegrerat(this.vårdpersonal, this.vårdenhet, this.utkastId);
+
+                const önskadUrl = "/certificate/" + this.utkastId ;
+                cy.visit(önskadUrl);
+                cy.wait(100);
+                intyg.signeraSkicka();
+
+                intyg.makulera();
+                cy.contains(this.utkastId).should('not.exist')
+
+        });
+
+        });
+    });
+});
