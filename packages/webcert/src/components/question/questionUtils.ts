70e3630f5c52aeae7566d1e82348ea812dc912bb
Robert Danielsson <robert.danielsson@soprasteria.com>
2021-10-21 15:38:24 +0200

    INTYGFV-13873: Removed unused cherry picked code

--- packages/webcert/src/components/question/questionUtils.ts
+++ packages/webcert/src/components/question/questionUtils.ts
@@ -5,17 +5,6 @@ export const getNumberOfUnhandledQuestions = (questions: Question[]): number => 
   return unhandledQuestions.length > 0 ? unhandledQuestions.length : 0
 }
 
-export const getQuestionsOrderedByLastUpdatedAndHandled = (questions: Question[]): Question[] => {
-  return questions.sort((questionA, questionB) => orderByLastUpdated(questionA, questionB)).sort((question) => (question.handled ? -1 : 1))
-}
-
-const orderByLastUpdated = (questionA: Question, questionB: Question) => {
-  const dateA = new Date(questionA.lastUpdate).getTime()
-  const dateB = new Date(questionB.lastUpdate).getTime()
-
-  return dateA > dateB ? 1 : -1
-}
-
 export const getShouldComplementedBeActive = (administrativeQuestions: Question[], complementQuestions: Question[]): boolean => {
   if (areComplementAndAdministrativeEmpty(administrativeQuestions, complementQuestions)) return true
   if (doesComplementAndAdministrativeHaveUnhandledQuestions(administrativeQuestions, complementQuestions)) return true
