d23fa61a6f03962cb628b6af06b8e8a23ed32dc8
Kristina Gullman <kristina.gullman@nordicmedtest.se>
2021-10-22 07:41:11 +0200

    INTYGFV-14187:Added some E2E-test for certifictate AG7804 (#141)

--- E2ETest/cypress/integration/Behorigheter/intygSJF.js
+++ E2ETest/cypress/integration/Behorigheter/intygSJF.js
@@ -19,7 +19,7 @@ describe('LISJP-intyg tomt', function() {
     context('Använadare har möjlighet att uföra följande med ett tomt utkast ',function() {
         beforeEach(function() {
             //UNSIGNED LISJP EMPTY
-            cy.skapaIntygViaApi(this,0,false,true,false).then((utkastId) => {
+            cy.skapaIntygViaApi(this,0,1,true,false).then((utkastId) => {
                 cy.wrap(utkastId).as('utkastId');
                 cy.log("LISJP-utkast med id " + utkastId + " skapat och används i testfallet");
             });
--- E2ETest/cypress/integration/SKR_intyg/AG7804Intyg.js
+++ E2ETest/cypress/integration/SKR_intyg/AG7804Intyg.js
@@ -1,10 +1,10 @@
 /* globals context cy */
 /// <reference types="Cypress" />
 //import * as intyg from '../../support/FK_intyg/fk_helpers'
-import * as intyg from '../../support/FK_intyg/lisjpIntyg'
+import * as intyg from '../../support/SKR_intyg/AG7804Intyg'
 
-// LISJP = Läkarintyg för sjukpenning, FK 7804
-describe('FK7804-intyg minimalt ifyllt', function() {
+// AG7804 = Läkarintyg om arbetsförmåga – arbetsgivaren, AG 7804
+describe('AG7804-intyg minimalt ifyllt', function() {
 
     before(function() {
         cy.fixture('FK_intyg/maxLisjpData').as('intygsdata');
@@ -17,15 +17,15 @@ describe('FK7804-intyg minimalt ifyllt', function() {
     context('Användare har möjlighet att uföra följande med FK7804 Intyg ',function() {
       beforeEach(function() {
              //UNSIGNED LISJP FILLED
-            cy.skapaIntygViaApi(this,1,1,true).then((utkastId) => {
+            cy.skapaIntygViaApi(this,1,2,true).then((utkastId) => {
                 cy.wrap(utkastId).as('utkastId');
                 cy.log("Lisjp-utkast med id " + utkastId + " skapat och används i testfallet");
             });
 
         });
-        describe('Funktioner på ett Lisjp intyg', () =>{
+        describe('Funktioner på ett AG7804-intyg', () =>{
 
-            it('Makulerar ett signerat LISJP intyg', function () {
+            it('Makulerar ett signerat AG7804-intyg', function () {
                     cy.loggaInVårdpersonalIntegrerat(this.vårdpersonal, this.vårdenhet, this.utkastId);
                     const önskadUrl = "/certificate/" + this.utkastId ;
                     cy.visit(önskadUrl);
@@ -35,7 +35,7 @@ describe('FK7804-intyg minimalt ifyllt', function() {
                     cy.contains(this.utkastId).should('not.exist')
 
             });
-            it('Skicka ett signerat LISJP intyg', function () {
+            it('Skicka ett signerat AG7804-intyg', function () {
                 cy.loggaInVårdpersonalIntegrerat(this.vårdpersonal, this.vårdenhet, this.utkastId);
 
                 const önskadUrl = "/certificate/" + this.utkastId ;
@@ -44,48 +44,38 @@ describe('FK7804-intyg minimalt ifyllt', function() {
                 //intyg.signeraSkicka();
                 //expect(cy.contains("Obligatoriska uppgifter saknas")).to.exist;
                 intyg.signera();
-              /*  cy.get('button').contains("Signera intyget").click();
-                expect(cy.contains("Intyget är tillgängligt för patienten")).to.exist;*/
-               
-                cy.get('button').contains("Skicka till Försäkringskassan").click();
-               
-                cy.get('.ic-button-group > :nth-child(1) > .ic-button').click(); //detta behöver jag hjälp med
-                
+                                           
                 expect(cy.contains("Intyget är tillgängligt för patienten")).to.exist;
 
             });
-            it('Skriva ut ett signerat LISJP intyg', function () {
+            it('Skriva ut ett signerat AG7804-intyg', function () {
                 //cy.visit('https://wc2.wc.localtest.me/welcome');
                 cy.loggaInVårdpersonalIntegrerat(this.vårdpersonal, this.vårdenhet, this.utkastId);
                 const önskadUrl = "/certificate/" + this.utkastId ;
                 cy.visit(önskadUrl);
                 intyg.signera();
-                intyg.skrivUt("utkast", this.utkastId, "lisjp");//skriver ut via request
+                intyg.skrivUt("utkast", this.utkastId, "ag7804");//skriver ut via request
             });
-            it('Förnya ett LISJP intyg', function (){
+            it('Förnya ett AG7804-intyg', function (){
                 cy.loggaInVårdpersonalIntegrerat(this.vårdpersonal, this.vårdenhet, this.utkastId);
                 const önskadUrl = "/certificate/" + this.utkastId ;
                 cy.visit(önskadUrl);
                 intyg.signera();
                 intyg.fornya();
-                cy.get('button').contains("Skicka till Försäkringskassan").should('not.exist');
+                cy.get('button').contains("Intyget är tillgängligt för patienten").should('not.exist');
                
                 cy.contains(this.utkastId).should('not.exist')
-               // expect(cy.contains(this.utkastId)).to.not.exist;
-                
-               // cy.get('.CertificateFooter__RightWrapper-sc-1mdu7r7-1 > .iu-fs-200').contains(this.utkastId).should('not.exist');
+         
                
             });
-            it('Ersätta ett LISJP intyg', function (){
+            it('Ersätta ett AG7804-intyg', function (){
                 cy.loggaInVårdpersonalIntegrerat(this.vårdpersonal, this.vårdenhet, this.utkastId);
                 const önskadUrl = "/certificate/" + this.utkastId ;
                 cy.visit(önskadUrl);
                 intyg.signera();
-                intyg.ersatta();
-                cy.get('button').contains("Skicka till Försäkringskassan").should('not.exist');
+                intyg.ersatta();                
                 cy.contains(this.utkastId).should('not.exist')
-                //cy.get('.CertificateFooter__RightWrapper-sc-1mdu7r7-1 > .iu-fs-200').contains(this.utkastId).should('not.exist');
-               
+                               
             });
             
 
