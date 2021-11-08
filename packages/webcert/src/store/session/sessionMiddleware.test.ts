daa5fd5c464140ff5726db12a54093f2fdd2708a
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2021-11-08 06:08:36 +0100

    INTYGFV-14133: Implemented middleware logic for session polling.

---
+++ packages/webcert/src/store/session/sessionMiddleware.test.ts
@@ -0,0 +1,114 @@
+import MockAdapter from 'axios-mock-adapter'
+import { configureStore, EnhancedStore } from '@reduxjs/toolkit'
+import axios from 'axios'
+import reducer from '../reducers'
+import dispatchHelperMiddleware, { clearDispatchedActions } from '../test/dispatchHelperMiddleware'
+import apiMiddleware from '../api/apiMiddleware'
+import { sessionMiddleware } from './sessionMiddleware'
+import { getSessionStatus, getSessionStatusSuccess, setPollHandle, setSessionStatusPending, startPoll, stopPoll } from './sessionActions'
+
+// https://stackoverflow.com/questions/53009324/how-to-wait-for-request-to-be-finished-with-axios-mock-adapter-like-its-possibl
+const flushPromises = () => new Promise((resolve) => setTimeout(resolve))
+
+describe('Test session middleware', () => {
+  let fakeAxios: MockAdapter
+  let testStore: EnhancedStore
+
+  beforeEach(() => {
+    fakeAxios = new MockAdapter(axios)
+    testStore = configureStore({
+      reducer,
+      middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(dispatchHelperMiddleware, apiMiddleware, ...sessionMiddleware),
+    })
+  })
+
+  afterEach(() => {
+    clearDispatchedActions()
+  })
+
+  describe('Handle StartPolling', () => {
+    it('shall start polling if not active', async () => {
+      testStore.dispatch(startPoll())
+      await flushPromises()
+      expect(testStore.getState().ui.uiSession.pollHandle).toBeTruthy()
+    })
+
+    it('shall ignore start polling if already active', async () => {
+      const activePollHandle = setInterval(() => console.log('active poll'), 30000)
+      testStore.dispatch(setPollHandle(activePollHandle))
+
+      testStore.dispatch(startPoll())
+
+      await flushPromises()
+      expect(testStore.getState().ui.uiSession.pollHandle).toEqual(activePollHandle)
+    })
+  })
+
+  describe('Handle StopPolling', () => {
+    it('shall stop polling if active', async () => {
+      const activePollHandle = setInterval(() => console.log('active poll'), 30000)
+      testStore.dispatch(setPollHandle(activePollHandle))
+
+      testStore.dispatch(stopPoll())
+
+      await flushPromises()
+      expect(testStore.getState().ui.uiSession.pollHandle).toBeFalsy()
+    })
+
+    it('shall ignore stop polling if none active', async () => {
+      testStore.dispatch(stopPoll())
+
+      await flushPromises()
+      expect(testStore.getState().ui.uiSession.pollHandle).toBeFalsy()
+    })
+  })
+
+  describe('Handle Get Session Status', () => {
+    it('shall call api to get session status', async () => {
+      testStore.dispatch(getSessionStatus())
+
+      await flushPromises()
+      expect(fakeAxios.history.get.length).toBe(1)
+      expect(fakeAxios.history.get[0].url).toBe('/api/session-auth-check/ping')
+    })
+
+    it('shall not call api again if a request is pending', async () => {
+      testStore.dispatch(setSessionStatusPending(true))
+
+      testStore.dispatch(getSessionStatus())
+
+      await flushPromises()
+      expect(fakeAxios.history.get.length).toBe(0)
+    })
+  })
+
+  describe('Handle Get Session Status Success', () => {
+    beforeEach(() => {
+      testStore.dispatch(setSessionStatusPending(true))
+    })
+
+    it('shall store session status', async () => {
+      const sessionStatus = { authenticated: true, hasSession: true, secondsUntilExpire: 999 }
+      testStore.dispatch(getSessionStatusSuccess(sessionStatus))
+
+      await flushPromises()
+      expect(testStore.getState().ui.uiSession.sessionStatus).toEqual(sessionStatus)
+    })
+
+    it('shall update session status pending to false', async () => {
+      const sessionStatus = { authenticated: true, hasSession: true, secondsUntilExpire: 999 }
+      testStore.dispatch(getSessionStatusSuccess(sessionStatus))
+
+      await flushPromises()
+      expect(testStore.getState().ui.uiSession.pending).toEqual(false)
+    })
+
+    it('shall set session to logged out is not authenticated - THIS IS TEMPORARY UNTIL ERROR-HANDLING IS IMPLEMENTED -', async () => {
+      const sessionStatus = { authenticated: false, hasSession: true, secondsUntilExpire: 999 }
+      testStore.dispatch(getSessionStatusSuccess(sessionStatus))
+
+      await flushPromises()
+      expect(testStore.getState().ui.uiSession.loggedOut).toEqual(true)
+    })
+  })
+})
---
+++ packages/webcert/src/store/session/sessionMiddleware.ts
@@ -0,0 +1,87 @@
+import { Dispatch, Middleware } from 'redux'
+import { AnyAction } from '@reduxjs/toolkit'
+import {
+  clearPollHandle,
+  getSessionStatus,
+  getSessionStatusError,
+  getSessionStatusStarted,
+  getSessionStatusSuccess, setLoggedOut,
+  setPollHandle,
+  setSessionStatus,
+  setSessionStatusPending,
+  startPoll,
+  stopPoll
+} from "./sessionActions";
+import { apiCallBegan } from '../api/apiActions'
+
+export const handleStartPoll: Middleware<Dispatch> = ({ dispatch, getState }) => (next) => (action: AnyAction): void => {
+  next(action)
+
+  if (!startPoll.match(action)) {
+    return
+  }
+
+  if (getState().ui.uiSession.pollHandle) {
+    return
+  }
+
+  const handlePoll = setInterval(() => {
+    dispatch(getSessionStatus())
+  }, 30000)
+
+  dispatch(setPollHandle(handlePoll))
+}
+
+export const handleStopPoll: Middleware<Dispatch> = ({ dispatch, getState }) => (next) => (action: AnyAction): void => {
+  next(action)
+
+  if (!stopPoll.match(action)) {
+    return
+  }
+
+  if (getState().ui.uiSession.pollHandle) {
+    clearInterval(getState().ui.uiSession.pollHandle)
+    dispatch(clearPollHandle())
+  }
+}
+
+export const handleGetSessionStatus: Middleware<Dispatch> = ({ dispatch, getState }) => (next) => (action: AnyAction): void => {
+  next(action)
+
+  if (!getSessionStatus.match(action)) {
+    return
+  }
+
+  if (getState().ui.uiSession.pending) {
+    return
+  }
+
+  dispatch(setSessionStatusPending(true))
+
+  dispatch(
+    apiCallBegan({
+      url: '/api/session-auth-check/ping',
+      method: 'GET',
+      onStart: getSessionStatusStarted.type,
+      onSuccess: getSessionStatusSuccess.type,
+      onError: getSessionStatusError.type,
+    })
+  )
+}
+
+export const handleGetSessionStatusSuccess: Middleware<Dispatch> = ({ dispatch, getState }) => (next) => (action: AnyAction): void => {
+  next(action)
+
+  if (!getSessionStatusSuccess.match(action)) {
+    return
+  }
+
+  dispatch(setSessionStatusPending(false))
+  dispatch(setSessionStatus(action.payload))
+
+  if (!action.payload.authenticated) {
+    dispatch(setLoggedOut(true))
+  }
+}
+
+export const sessionMiddleware = [handleStartPoll, handleStopPoll, handleGetSessionStatus, handleGetSessionStatusSuccess]
