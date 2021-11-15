8063f8222d9e4f49427a9c46423bc4e1b1756772
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2021-11-15 10:47:16 +0100

    INTYGFV-14207: Changed text in modal for administrators and added external link symbol (#186)

--- packages/webcert/src/feature/certificate/Modals/ProtectedPersonDoctorModal.tsx
+++ packages/webcert/src/feature/certificate/Modals/ProtectedPersonDoctorModal.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import { InfoBox, TextWithInfoModal } from '@frontend/common/src'
+import { useSelector } from 'react-redux'
+import { isDoctor } from '../../../store/user/userSelectors'
 
 const ItalicLink = styled.span`
   a {
@@ -10,6 +12,8 @@ const ItalicLink = styled.span`
 `
 
 const ProtectedPersonDoctorModal: React.FC = ({}) => {
+  const isUserDoctor = useSelector(isDoctor)
+
   return (
     <ItalicLink>
       {' '}
@@ -20,10 +24,12 @@ const ProtectedPersonDoctorModal: React.FC = ({}) => {
         </InfoBox>
         <p className={'iu-fw-bold iu-pt-300'}>Att använda Webcert med skyddade personuppgifter innebär:</p>
         <ul className={'iu-flex-column'}>
-          <li className={'iu-py-300'}>
-            När du signerar ett intyg kommer ditt namn och information om den vårdgivare och vårdenhet intyget är utfärdat på att vara
-            synligt.
-          </li>
+          {isUserDoctor && (
+            <li className={'iu-py-300'}>
+              När du signerar ett intyg kommer ditt namn och information om den vårdgivare och vårdenhet intyget är utfärdat på att vara
+              synligt.
+            </li>
+          )}
           <li>Vid kommunikation med Försäkringskassan kring ett intyg kommer ditt namn att vara synligt.</li>
         </ul>{' '}
         <p className={'iu-py-300'}>Detta kan göra att information om dig och var du arbetar kan spridas.</p>
--- packages/webcert/src/store/user/userSelectors.ts
+++ packages/webcert/src/store/user/userSelectors.ts
@@ -4,3 +4,6 @@ import { User } from '@frontend/common'
 export const getUser = (state: RootState): User | null => state.ui.uiUser.user
 
 export const getUserPreference = (key: string) => (state: RootState): string | undefined => state.ui.uiUser.user?.preferences?.[key]
+
+export const isDoctor = (state: RootState): boolean | null =>
+  state.ui.uiUser.user && state.ui.uiUser.user.role.toLowerCase().includes('läkare')
