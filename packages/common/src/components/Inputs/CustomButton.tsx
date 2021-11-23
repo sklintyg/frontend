038249788f2bfc7abc72c99a7636fc9ba7d96d49
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2021-11-23 12:51:36 +0100

    INTYGFV-14183: Updated spacing for header buttons (#217)

--- packages/common/src/components/Inputs/CustomButton.tsx
+++ packages/common/src/components/Inputs/CustomButton.tsx
@@ -51,7 +51,7 @@ export const CustomButton: React.FC<Props & { ref?: React.Ref<HTMLButtonElement>
   }
 
   return (
-    <Wrapper data-tip={props.tooltip}>
+    <Wrapper data-tip={props.tooltip} className={'custom-button'}>
       <button
         ref={ref as React.RefObject<HTMLButtonElement>}
         type={props.type ?? 'button'}
