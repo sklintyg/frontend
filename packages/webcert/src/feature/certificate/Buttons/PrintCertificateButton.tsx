a62fd0413208b1de8a648cf3467ceac74a4b89da
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2021-12-03 14:56:26 +0100

    INTYGFV-14288: Added modal for printing certificate if patient is protected

--- packages/webcert/src/feature/certificate/Buttons/PrintCertificateButton.tsx
+++ packages/webcert/src/feature/certificate/Buttons/PrintCertificateButton.tsx
@@ -4,26 +4,44 @@ import { printCertificate } from '../../../store/certificate/certificateActions'
 import { CertificateMetadata, CustomButton } from '@frontend/common'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPrint } from '@fortawesome/free-solid-svg-icons'
+import { ButtonWithConfirmModal } from '@frontend/common/src'
 
 interface Props {
   name: string
   description: string
   enabled: boolean
+  body?: string
   certificateMetadata: CertificateMetadata
 }
 
-const PrintCertificateButton: React.FC<Props> = ({ name, description, enabled, certificateMetadata }) => {
+const PrintCertificateButton: React.FC<Props> = ({ name, description, enabled, certificateMetadata, body }) => {
   const dispatch = useDispatch()
 
   return (
-    <CustomButton
-      tooltip={description}
-      disabled={!enabled}
-      buttonStyle="primary"
-      text={name}
-      startIcon={<FontAwesomeIcon icon={faPrint} size="lg"></FontAwesomeIcon>}
-      onClick={() => dispatch(printCertificate(certificateMetadata))}
-    />
+    <>
+      {body ? (
+        <ButtonWithConfirmModal
+          description={description}
+          disabled={!enabled}
+          buttonStyle="primary"
+          name={name}
+          modalTitle={'Skriv ut utkast'}
+          startIcon={<FontAwesomeIcon icon={faPrint} size="lg"></FontAwesomeIcon>}
+          onConfirm={() => dispatch(printCertificate(certificateMetadata))}
+          confirmButtonText={'Skriv ut'}>
+          <div dangerouslySetInnerHTML={{ __html: body }}></div>
+        </ButtonWithConfirmModal>
+      ) : (
+        <CustomButton
+          tooltip={description}
+          disabled={!enabled}
+          buttonStyle="primary"
+          text={name}
+          startIcon={<FontAwesomeIcon icon={faPrint} size="lg"></FontAwesomeIcon>}
+          onClick={() => dispatch(printCertificate(certificateMetadata))}
+        />
+      )}
+    </>
   )
 }
 
