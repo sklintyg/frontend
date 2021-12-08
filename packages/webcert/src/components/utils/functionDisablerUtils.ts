5d00fc2397e59618ea9c2717d1b11a140c41657d
Robert Danielsson <robert.danielsson@soprasteria.com>
2021-12-08 11:03:15 +0100

    INTYGFV-14134: Simplified id generator for generateFunctionDisabler

--- packages/webcert/src/components/utils/functionDisablerUtils.ts
+++ packages/webcert/src/components/utils/functionDisablerUtils.ts
@@ -41,11 +41,6 @@ export const generateFunctionDisabler = (): FunctionDisabler => {
   return { id: uuidv4() }
 }
 
-// https://stackoverflow.com/questions/105034/how-to-create-guid-uuid
 function uuidv4(): string {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    const r = (Math.random() * 16) | 0,
-      v = c === 'x' ? r : (r & 0x3) | 0x8
-    return v.toString(16)
-  })
+  return (Date.now() * Math.random()).toString()
 }
