908c87e24b151c71c3e109f6096b56d0397a9c9d
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-01-05 14:59:58 +0100

    INTYGFV-14292: Updated info box type

--- packages/webcert/src/feature/certificate/Modals/ProtectedUserApprovalModal.tsx
+++ packages/webcert/src/feature/certificate/Modals/ProtectedUserApprovalModal.tsx
@@ -47,7 +47,7 @@ const ProtectedUserApprovalModal: React.FC<Props> = ({ showModal, preferenceKey 
       }
       content={
         <>
-          <InfoBox type={'observe'}>
+          <InfoBox type={'info'}>
             <p>Du har skyddade personuppgifter.</p>
           </InfoBox>
           <p className="iu-fw-bold iu-py-300"> Att använda Webcert med en skyddade personuppgifter innebär:</p>
@@ -67,7 +67,7 @@ const ProtectedUserApprovalModal: React.FC<Props> = ({ showModal, preferenceKey 
             Vill du ändå gå vidare och använda Webcert kan du göra det, efter att du godkänt att du tagit del av denna information. Detta
             godkännande behöver du bara göra en gång.
           </p>
-          <p>Vill du inte använda Webcert, eller av annan anledning väljer att avbryta, så kommer Webcert att avslutas.</p>"
+          <p>Vill du inte använda Webcert, eller av annan anledning väljer att avbryta, så kommer Webcert att avslutas.</p>
           <Checkbox onChange={onCheckboxChange} label={'Jag förstår och godkänner.'} checked={approved} />
         </>
       }
