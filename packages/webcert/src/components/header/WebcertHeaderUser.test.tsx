370262a10af0305104bbef8af92d63c06b12688f
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-01-11 16:41:13 +0100

    INTYGFV-14292: Updated tests

--- packages/webcert/src/components/header/WebcertHeaderUser.test.tsx
+++ packages/webcert/src/components/header/WebcertHeaderUser.test.tsx
@@ -4,47 +4,81 @@ import WebcertHeaderUser from './WebcertHeaderUser'
 import * as redux from 'react-redux'
 import { User } from '@frontend/common/src'
 import userEvent from '@testing-library/user-event'
+import { Provider } from 'react-redux'
+import store from '../../store/store'
 
-const setup = (protectedPerson: boolean) => {
+const setup = (protectedPerson: boolean, approvedTerms?: string) => {
   const spy = jest.spyOn(redux, 'useSelector')
 
   const mockUser: User = {
     name: 'Test Testsson',
     role: 'Läkare',
     protectedPerson: protectedPerson,
+    preferences: { 'wc.vardperson.sekretess.approved': approvedTerms ? approvedTerms : '' },
   }
   spy.mockReturnValue(mockUser)
+}
+
+const renderComponent = () => {
+  render(
+    <Provider store={store}>
+      <WebcertHeaderUser />
+    </Provider>
+  )
 }
 
 describe('WebcertHeaderUser', () => {
   it('displays user role', () => {
     setup(false)
-    render(<WebcertHeaderUser />)
+    renderComponent()
     expect(screen.getByText(/Läkare/i)).toBeInTheDocument()
   })
 
   it('displays users name and role', (): void => {
     setup(false)
-    render(<WebcertHeaderUser />)
+    renderComponent()
     expect(screen.getByText(/Test Testsson/i)).toBeInTheDocument()
     expect(screen.getByText(/Läkare/i)).toBeInTheDocument()
   })
 
   it('should not show protected person link', (): void => {
     setup(false)
-    render(<WebcertHeaderUser />)
+    renderComponent()
     expect(screen.queryByText(/Skyddade personuppgifter/i)).not.toBeInTheDocument()
   })
 
-  it('should show protected person link', (): void => {
+  it('should show protected person modal if approval is not saved in preferences', (): void => {
     setup(true)
-    render(<WebcertHeaderUser />)
+    renderComponent()
+    expect(screen.getByRole('dialog')).toBeInTheDocument()
+    expect(screen.getByText(/Du har skyddade personuppgifter/i)).toBeInTheDocument()
+  })
+
+  it('should show protected person link when approval modal is closed', (): void => {
+    setup(true)
+    renderComponent()
+    userEvent.click(screen.getByRole('checkbox'))
+    userEvent.click(screen.getByText('Till Webcert'))
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
     expect(screen.getByText(/Skyddade personuppgifter/i)).toBeInTheDocument()
   })
 
-  it('should open protected person modal', (): void => {
-    setup(true)
-    render(<WebcertHeaderUser />)
+  it('should not show protected person modal if approval is saved in preferences', (): void => {
+    setup(true, 'true')
+    renderComponent()
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
+    expect(screen.queryByText(/Du har skyddade personuppgifter/i)).not.toBeInTheDocument()
+  })
+
+  it('should show protected person link when approval modal is closed', (): void => {
+    setup(true, 'true')
+    renderComponent()
+    expect(screen.getByText(/Skyddade personuppgifter/i)).toBeInTheDocument()
+  })
+
+  it('should open protected person modal when clicking on link', (): void => {
+    setup(true, 'true')
+    renderComponent()
     userEvent.click(screen.getByText(/Skyddade personuppgifter/i))
     expect(screen.getByText('Användning av Webcert med skyddade personuppgifter')).toBeInTheDocument()
   })
