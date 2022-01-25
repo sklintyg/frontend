8d759cf1050e514e6148c9d9a52efddcf37a0ee6
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-01-25 15:48:55 +0100

    INTYGFV-14330: Added test for testing if client validation errors are included in validation summary

--- packages/common/src/utils/validationUtils.test.ts
+++ packages/common/src/utils/validationUtils.test.ts
@@ -1089,7 +1089,7 @@ describe('Set initial values to a certificate', () => {
   })
 
   it('should return empty validation error summary', () => {
-    const result = getSortedValidationErrorSummary(certificate)
+    const result = getSortedValidationErrorSummary(certificate, [])
 
     expect(result.length).toBe(0)
   })
@@ -1102,7 +1102,7 @@ describe('Set initial values to a certificate', () => {
     certificate.metadata.careUnitValidationErrors = []
     certificate.metadata.careUnitValidationErrors.push(validationError)
 
-    const result = getSortedValidationErrorSummary(certificate)
+    const result = getSortedValidationErrorSummary(certificate, [])
 
     expect(result.length).toBe(3)
     expect(result[0].id).toBe('sysselsattning')
@@ -1113,5 +1113,11 @@ describe('Set initial values to a certificate', () => {
     expect(result[1].index).toBe(11)
     expect(result[2].id).toBe(CARE_UNIT_ADDRESS_CATEGORY_TITLE_ID)
     expect(result[2].text).toBe(CARE_UNIT_ADDRESS_CATEGORY_TITLE)
+  })
+
+  it('should include client validation errors in result', () => {
+    const certificate = getCertificate()
+    const result = getSortedValidationErrorSummary(certificate, [{ id: '1.2', category: '', field: '', type: '', text: '' }])
+    expect(result.length).toBe(1)
   })
 })
