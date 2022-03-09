449708a2e22b108ebda5eafb57fa5065d7e7ed6d
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-03-09 13:30:00 +0100

    INTYGFV-14460: Refactored header

--- packages/webcert/src/page/CreateCertificatePage.tsx
+++ packages/webcert/src/page/CreateCertificatePage.tsx
@@ -1,12 +1,8 @@
 import React, { useState } from 'react'
 import PatientSearch from '../components/patient/PatientSearch'
-import logo from '../components/header/webcert_logo.png'
-import WebcertHeaderUser from '../components/header/WebcertHeaderUser'
-import WebcertHeaderUnit from '../components/header/WebcertHeaderUnit'
-import SystemBanners from '../components/notification/SystemBanners'
-import { AppHeader } from '@frontend/common'
 import { useDispatch } from 'react-redux'
 import { searchPatient } from '../store/patient/patientActions'
+import WebcertHeader from '../components/header/WebcertHeader'
 
 const CreateCertificatePage: React.FC = () => {
   const [patientId, setPatientId] = useState('')
@@ -22,14 +18,10 @@ const CreateCertificatePage: React.FC = () => {
 
   return (
     <>
-      <AppHeader
-        logo={logo}
-        alt={'Logo Webcert'}
-        primaryItems={[<WebcertHeaderUser />, <WebcertHeaderUnit />]}
-        secondaryItems={[]}
-        banners={[<SystemBanners />]}
-      />
-      <PatientSearch patientId={patientId} onChange={onTextInputChange} onSubmit={onSubmit} />
+      <WebcertHeader />
+      <div className={'ic-container'}>
+        <PatientSearch patientId={patientId} onChange={onTextInputChange} onSubmit={onSubmit} />
+      </div>
     </>
   )
 }
