1211513e5972804b45a895f772d1d445d412c820
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-03-15 16:17:08 +0100

    INTYGFV-14460: Updated test in middleware

--- packages/webcert/src/store/patient/patientMiddleware.test.ts
+++ packages/webcert/src/store/patient/patientMiddleware.test.ts
@@ -2,7 +2,7 @@ import { configureStore, EnhancedStore } from '@reduxjs/toolkit'
 import reducer from '../reducers'
 import dispatchHelperMiddleware, { clearDispatchedActions } from '../test/dispatchHelperMiddleware'
 import { patientMiddleware } from './patientMiddleware'
-import { getPatient } from './patientActions'
+import { getPatient, GetPatientResponse } from './patientActions'
 import MockAdapter from 'axios-mock-adapter'
 import axios from 'axios'
 import { PatientStatus } from '@frontend/common/src/types/patient'
@@ -10,6 +10,10 @@ import apiMiddleware from '../api/apiMiddleware'
 import { createPatient } from '../../components/patient/patientTestUtils'
 
 const flushPromises = () => new Promise((resolve) => setTimeout(resolve))
+
+const mockHistory = {
+  push: jest.fn(),
+}
 
 describe('Test patient middleware', () => {
   let fakeAxios: MockAdapter
@@ -29,19 +33,21 @@ describe('Test patient middleware', () => {
 
   describe('Handle get patient', () => {
     it('shall call api to get patient', async () => {
-      testStore.dispatch(getPatient('patientId'))
+      // @ts-expect-error mocking history
+      testStore.dispatch(getPatient({ patientId: 'patientId', history: mockHistory }))
 
       await flushPromises()
       expect(fakeAxios.history.get.length).toBe(1)
       expect(fakeAxios.history.get[0].url).toEqual('/api/patient/patientId')
     })
 
-    it('shall save patient', async () => {
+    it('shall save patient if response includes status FOUND', async () => {
       const expectedPatient = createPatient('patientId')
-      const getPatientSuccess = { patient: expectedPatient, status: PatientStatus.FOUND } as GetPatientSuccess
-      fakeAxios.onGet('/api/certificate/patientId').reply(200, getPatientSuccess)
+      const getPatientSuccess = { patient: expectedPatient, status: PatientStatus.FOUND } as GetPatientResponse
+      fakeAxios.onGet('/api/patient/patientId').reply(200, getPatientSuccess)
 
-      testStore.dispatch(getPatient('patientId'))
+      // @ts-expect-error mocking history
+      testStore.dispatch(getPatient({ patientId: 'patientId', history: mockHistory }))
 
       await flushPromises()
       expect(testStore.getState().ui.uiPatient.patient).toEqual(expectedPatient)
