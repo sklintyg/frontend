7aa08fe6913ad8b104b305a69491ef1ba84398a0
HelenaEdblad <85883491+HelenaEdblad@users.noreply.github.com>
2022-03-18 14:25:53 +0100

    INTYGFV-14203: Make links in banner open in new tab and add icon (#327)

--- packages/common/src/utils/sanitizeText.ts
+++ packages/common/src/utils/sanitizeText.ts
@@ -1,5 +1,5 @@
 import DOMPurify from 'dompurify'
 
 export const sanitizeText = (text: string) => {
-  return { __html: DOMPurify.sanitize(text) }
+  return { __html: DOMPurify.sanitize(text, { ADD_ATTR: ['target'] }) }
 }
