664c08e01cbbdc71184fcd3a46def4e6ee40892a
HelenaEdblad <helena.edblad@soprasteria.com>
2022-03-18 13:17:53 +0100

    INTYGFV-14452: SAML settings

--- packages/webcert/src/setupProxy.js
+++ packages/webcert/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function(app) {
   app.use(
-    ['/fake', '/api', '/moduleapi', '/testability', '/visa'],
+    ['/fake', '/api', '/moduleapi', '/testability', '/visa', '/saml'],
     createProxyMiddleware({
       target: 'http://localhost:8020',
       changeOrigin: true,
