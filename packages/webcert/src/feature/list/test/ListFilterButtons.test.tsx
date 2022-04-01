5b2a2a1b53e2cd8d2ba529a39bef703645d49768
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-04-01 10:09:43 +0200

    INTYGFV-14465: Added tests for filter buttons

---
+++ packages/webcert/src/feature/list/test/ListFilterButtons.test.tsx
@@ -0,0 +1,58 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import ListFilterButtons from '../ListFilterButtons'
+import userEvent from '@testing-library/user-event'
+import { CustomTooltip } from '@frontend/common/src'
+
+const TOOLTIP_SEARCH = 'Tooltip for search button'
+const TOOLTIP_RESET = 'Rensa sökfiltret.'
+const onSearch = jest.fn()
+const onReset = jest.fn()
+
+const renderComponent = (isSearchEnabled: boolean) => {
+  render(
+    <>
+      <CustomTooltip />
+      <ListFilterButtons searchTooltip={TOOLTIP_SEARCH} isSearchEnabled={isSearchEnabled} onSearch={onSearch} onReset={onReset} />
+    </>
+  )
+}
+
+describe('ListFilterButtons', () => {
+  it('should show search button', () => {
+    renderComponent(true)
+    expect(screen.getByText('Sök')).toBeInTheDocument()
+  })
+
+  it('should show search button', () => {
+    renderComponent(true)
+    expect(screen.getByText('Återställ sökfiltret', { exact: false })).toBeInTheDocument()
+  })
+
+  it('should show search tooltip', () => {
+    renderComponent(true)
+    userEvent.hover(screen.getByText('Sök'))
+    expect(screen.getByText(TOOLTIP_SEARCH)).toBeInTheDocument()
+  })
+
+  it('should show reset tooltip', () => {
+    renderComponent(true)
+    userEvent.hover(screen.getByText('Återställ sökfiltret'))
+    expect(screen.getByText(TOOLTIP_RESET)).toBeInTheDocument()
+  })
+
+  it('should have search enabled if filter validates', () => {
+    renderComponent(true)
+    expect(screen.getByText('Sök')).toBeEnabled()
+  })
+
+  it('should have search disabled if filter does not validate', () => {
+    renderComponent(false)
+    expect(screen.getByText('Sök')).toBeDisabled()
+  })
+
+  it('should have search disabled if filter does not validate', () => {
+    renderComponent(false)
+    expect(screen.getByText('Sök')).toBeDisabled()
+  })
+})
