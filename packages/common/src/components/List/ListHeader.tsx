5d4a3e44bad38ed46a01dbb326583012a620bd97
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-04-14 12:56:57 +0200

    INTYGFV-14465: Updated tests

--- packages/common/src/components/List/ListHeader.tsx
+++ packages/common/src/components/List/ListHeader.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import styled from 'styled-components/macro'
+import styled from 'styled-components'
 
 const Wrapper = styled.div`
   box-shadow: 0 2px 4px 0 rgb(0 0 0 / 12%);
--- packages/common/src/components/List/PatientListInfoContent.tsx
+++ packages/common/src/components/List/PatientListInfoContent.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import styled from 'styled-components/macro'
+import styled from 'styled-components'
 import infoImage from '../../images/info-image.svg'
 import alertImage from '../../images/alert-image.svg'
 import lockClosed from '../../images/lock-closed.svg'
