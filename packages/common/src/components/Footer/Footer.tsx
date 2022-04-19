03963cffde563a11f1054deed3b98d686a74d41a
HelenaEdblad <helena.edblad@soprasteria.com>
2022-04-19 10:27:49 +0200

    INTYGFV-14452: Bugfixes footer and header

--- packages/common/src/components/Footer/Footer.tsx
+++ packages/common/src/components/Footer/Footer.tsx
@@ -44,7 +44,7 @@ export const Footer: React.FC = () => (
               </a>
             </li>
             <li>
-              <TextWithInfoModal additionalStyles="ic-link-chevron" modalTitle="Om kakor (cookies)" text="Cookiepolicy">
+              <TextWithInfoModal additionalStyles="ic-link-chevron" modalTitle="Om kakor (cookies)" text="Om kakor">
                 <p>
                   Vi använder kakor (cookies) för att den här webbplatsen ska fungera på ett bra sätt för dig. Genom att logga in accepterar
                   du vår användning av kakor.
@@ -95,19 +95,65 @@ export const Footer: React.FC = () => (
       <nav className="ic-nav-list" id="mobile-nav" aria-label="Sidfot meny mobil">
         <ul className="ic-nav-list__list">
           <li>
-            <a className="ic-link ic-link--external" target="_blank" href="https://www.inera.se/">
-              Inera AB <ExternalLinkIconImg src={externalLinkIcon} />
-            </a>
+            <div className="ic-container--narrow-md iu-px-none">
+              <a className="ic-link ic-link--external" target="_blank" href="https://www.inera.se/">
+                Inera AB <ExternalLinkIconImg src={externalLinkIcon} />
+              </a>
+            </div>
           </li>
           <li>
-            <a className="ic-link ic-link--external" target="_blank" href="https://www.inera.se/kontakta-oss/felanmalan-och-anvandarstod/">
-              Inera Support <ExternalLinkIconImg src={externalLinkIcon} />
-            </a>
+            <div className="ic-container--narrow-md iu-px-none">
+              <a
+                className="ic-link ic-link--external"
+                target="_blank"
+                href="https://www.inera.se/kontakta-oss/felanmalan-och-anvandarstod/">
+                Inera Support <ExternalLinkIconImg src={externalLinkIcon} />
+              </a>
+            </div>
           </li>
           <li>
-            <a className="ic-link ic-link--external" target="_blank" href="https://inera.atlassian.net/wiki/x/SwhmFQ">
-              Webcerts informationsyta <ExternalLinkIconImg src={externalLinkIcon} />
-            </a>
+            <div className="ic-container--narrow-md iu-px-none">
+              <a className="ic-link ic-link--external" target="_blank" href="https://inera.atlassian.net/wiki/x/SwhmFQ">
+                Webcerts informationsyta <ExternalLinkIconImg src={externalLinkIcon} />
+              </a>
+            </div>
+          </li>
+          <li>
+            <div className="ic-container--narrow-md iu-px-none">
+              <TextWithInfoModal modalTitle="Om kakor (cookies)" text="Om kakor">
+                <p>
+                  Vi använder kakor (cookies) för att den här webbplatsen ska fungera på ett bra sätt för dig. Genom att logga in accepterar
+                  du vår användning av kakor.
+                </p>
+                <h3>Så här använder vi kakor</h3>
+                <p>
+                  Den typ av kakor som används på den här webbplatsen kallas för sessionskakor. De lagras temporärt i din dators minne under
+                  tiden du är inne på webbplatsen. Sessionskakor sparar ingen personlig information om dig, och de försvinner när du stänger
+                  din webbläsare.
+                </p>
+                <p>
+                  I Webcert används sessionskakor för att du ska kunna navigera i tjänsten utan att behöva logga in på nytt varje gång du
+                  går till en ny sida. De används också för att de filterinställningar du gör ska finnas kvar under hela tiden du är
+                  inloggad. För att vara säker på att kakorna inte sparas i din dator efter avslutad session måste du stänga webbläsaren när
+                  du har loggat ut.
+                </p>
+                <h3>Undvika kakor</h3>
+                <p>
+                  Vill du inte acceptera kakor kan din webbläsare ställas in så att du automatiskt nekar till lagring av kakor eller
+                  informeras varje gång en webbplats begär att få lagra en kaka. Genom webbläsaren kan också tidigare lagrade kakor raderas.
+                  Se webbläsarens hjälpsidor för mer information.
+                </p>
+                <p>Väljer du att inte acceptera kakor så kan du inte identifiera dig med e-legitimation i denna e-tjänst.</p>
+                <p>
+                  Mer information om kakor kan du finna på{' '}
+                  <a href="https://pts.se/sv/privat/internet/integritet/kakor-cookies/" target="_blank">
+                    Kommunikationsmyndigheten PTS sida om kakor launch
+                    <ExternalLinkIconImg src={externalLinkIcon} />
+                  </a>
+                  .
+                </p>
+              </TextWithInfoModal>
+            </div>
           </li>
         </ul>
       </nav>
