90d6eea6671f6e9e0dcd07c90d0f396c07ada8ad
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-05-10 11:04:08 +0200

    INTYGFV-14621: Fixed test

--- packages/common/src/components/AppHeader/AppHeader.test.tsx
+++ packages/common/src/components/AppHeader/AppHeader.test.tsx
@@ -2,50 +2,74 @@ import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import AppHeader from './AppHeader'
+import store from '@frontend/webcert/src/store/store'
+import { Provider } from 'react-redux'
+import { MemoryRouter, Route } from 'react-router-dom'
+
+const renderComponent = (primaryItems: React.ReactNode[], secondaryItems: React.ReactNode[]) => {
+  render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={['/create']}>
+        <Route path="/create">
+          <AppHeader primaryItems={primaryItems} secondaryItems={secondaryItems} />
+        </Route>
+      </MemoryRouter>
+    </Provider>
+  )
+}
+
+const renderComponentWithLogo = (logo: string, alt: string) => {
+  render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={['/create']}>
+        <Route path="/create">
+          <AppHeader logo={logo} alt={alt} />
+        </Route>
+      </MemoryRouter>
+    </Provider>
+  )
+}
 
 describe('App header', () => {
   it('displays primary items', (): void => {
-    render(<AppHeader primaryItems={[<p>Test</p>]} />)
+    renderComponent([<p>Test</p>], [])
     expect(screen.getByText(/Test/i)).toBeInTheDocument()
   })
 
   it('displays secondary items', (): void => {
-    render(<AppHeader secondaryItems={[<p>Test test</p>]} />)
+    renderComponent([], [<p>Test test</p>])
     expect(screen.getByText(/Test test/i)).toBeInTheDocument()
   })
 
   it('displays several primary items', (): void => {
-    render(
-      <AppHeader
-        primaryItems={[
-          <div>
-            <p>Test</p>
-            <span>My text</span>
-          </div>,
-        ]}
-      />
+    renderComponent(
+      [
+        <div>
+          <p>Test</p>
+          <span>My text</span>
+        </div>,
+      ],
+      []
     )
     expect(screen.getByText(/Test/i)).toBeInTheDocument()
     expect(screen.getByText(/My text/i)).toBeInTheDocument()
   })
 
   it('displays several secondary items', (): void => {
-    render(<AppHeader secondaryItems={['Testing', 'Test test']} />)
+    renderComponent([], ['Testing', 'Test test'])
     expect(screen.getByText(/Test test/i)).toBeInTheDocument()
     expect(screen.getByText(/Testing/i)).toBeInTheDocument()
   })
 
   it('displays primary and secondary items', (): void => {
-    render(
-      <AppHeader
-        primaryItems={[
-          <div>
-            <p>Primary items</p>
-            <span>My text</span>
-          </div>,
-        ]}
-        secondaryItems={['Testing', 'Test test']}
-      />
+    renderComponent(
+      [
+        <div>
+          <p>Primary items</p>
+          <span>My text</span>
+        </div>,
+      ],
+      ['Testing', 'Test test']
     )
 
     expect(screen.getByText(/Test test/i)).toBeInTheDocument()
@@ -55,7 +79,7 @@ describe('App header', () => {
   })
 
   it('displays logo', (): void => {
-    render(<AppHeader logo={'src'} alt={'alt text'} />)
+    renderComponentWithLogo('src', 'alt text')
     expect(screen.getByAltText(/alt text/i)).toBeInTheDocument()
   })
 })
