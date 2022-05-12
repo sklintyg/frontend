57f679379b50b42db52a8ffe345ebb62c798dfa6
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-05-12 14:43:22 +0200

    INTYGFV-14453: Example structure for test to check if modal renders

--- packages/webcert/src/components/modal/CareProviderModal.test.tsx
+++ packages/webcert/src/components/modal/CareProviderModal.test.tsx
@@ -4,19 +4,49 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import store from '../../store/store'
 import CareProviderModal from './CareProviderModal'
+import { updateUser } from '../../store/user/userActions'
+import { LoginMethod, SigningMethod, Unit, User } from '@frontend/common'
 
 describe('Care provider modal', () => {
   const renderComponent = () => {
     render(
       <Provider store={store}>
         <BrowserRouter>
-          <CareProviderModal title="Modaltext" open={false} />
+          <CareProviderModal title="Välj vårdenhet" open={false} />
         </BrowserRouter>
       </Provider>
     )
   }
 
-  it('Shall show care provider modal if loggedInCareProvider is not set', () => {
+  //test will not work unless functionality to render CareProviderModal is extracted from App to another component. Either a wrapper or the CareProviderModal
+  it('should show care provider modal if loggedInCareProvider is not set', () => {
+    store.dispatch(updateUser(getUserWithEmptyUnit()))
     renderComponent()
+    expect(screen.getByText('Välj vårdenhet')).toBeInTheDocument()
   })
 })
+
+const getUserWithEmptyUnit = (): User => {
+  const unit: Unit = {
+    unitId: '',
+    unitName: '',
+    address: '',
+    zipCode: '',
+    city: '',
+    phoneNumber: '',
+    email: '',
+    isInactive: false,
+  }
+  return {
+    hsaId: '',
+    name: '',
+    role: 'doctor',
+    loggedInUnit: unit,
+    loggedInCareUnit: unit,
+    loggedInCareProvider: unit,
+    preferences: null,
+    loginMethod: LoginMethod.BANK_ID,
+    signingMethod: SigningMethod.FAKE,
+    protectedPerson: false,
+  }
+}
