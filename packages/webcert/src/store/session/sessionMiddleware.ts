6c8d2ea0438c1cf9dbcb59749f6690b17fba6e0b
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-05-12 11:18:23 +0200

    INTYGFV-14621: Tried fixing test

--- packages/webcert/src/store/session/sessionMiddleware.ts
+++ packages/webcert/src/store/session/sessionMiddleware.ts
@@ -22,7 +22,7 @@ const handleStartPoll: Middleware<Dispatch> = ({ dispatch, getState }) => () => 
     return
   }
 
-  const handlePoll = setInterval(() => {
+  const handlePoll: NodeJS.Timeout = setInterval(() => {
     dispatch(getSessionStatus())
   }, 30000)
 
