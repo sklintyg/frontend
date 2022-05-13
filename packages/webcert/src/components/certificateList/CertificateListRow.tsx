7d01ccc3f0ebf5376320524abc21c7abd178b66f
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-05-13 11:32:12 +0200

    INTYGFV-14461: Fixed formatting of links in certificate info

--- packages/webcert/src/components/certificateList/CertificateListRow.tsx
+++ packages/webcert/src/components/certificateList/CertificateListRow.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
-import { CustomButton, ResourceLink, TextWithInfoModal } from '@frontend/common'
+import { CustomButton, ResourceLink, sanitizeText, TextWithInfoModal } from '@frontend/common'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar as star } from '@fortawesome/free-regular-svg-icons'
 import { faStar as starChecked } from '@fortawesome/free-solid-svg-icons'
 import classnames from 'classnames'
+import WCDynamicLink from '../../utils/WCDynamicLink'
 
 interface Props {
   certificateName: string
@@ -14,7 +15,7 @@ interface Props {
   preferenceClick: (...args: string[]) => void
   favorite: boolean
   createCertificate: (...args: string[]) => void
-  link: ResourceLink
+  link?: ResourceLink
 }
 
 const Row = styled.div`
@@ -54,6 +55,29 @@ const CertificateListRow: React.FC<Props> = ({
     createCertificate(id)
   }
 
+  const formatText = (text: string) => {
+    const splitText = text.split('<LINK:')
+    if (splitText.length > 1) {
+      const dynamicLinkKey = splitText[1].split('>')[0]
+      const textAfterLink = splitText[1].split('>')[1]
+      return (
+        <>
+          <p>
+            {splitText[0]}
+            <WCDynamicLink linkKey={dynamicLinkKey} />
+            {textAfterLink}
+          </p>
+        </>
+      )
+    }
+    return text
+  }
+
+  const hasDynamicLink = (text: string) => {
+    const splitText = text.split('<LINK:')
+    return splitText.length > 1
+  }
+
   return (
     <Row className="iu-flex iu-flex-center iu-p-400">
       <Star className="iu-mr-1rem" onClick={onPreferenceClick} data-tip={favoriteText} aria-label={favoriteText}>
@@ -66,14 +90,18 @@ const CertificateListRow: React.FC<Props> = ({
         <span className="iu-fw-bold">{certificateName}</span> {issuerTypeId}
       </CertificateName>
       <TextWithInfoModal text="Om intyget" modalTitle={`Om ${certificateName}`} className="iu-mr-1rem">
-        <ModalContent>{certificateInfo}</ModalContent>
+        {hasDynamicLink(certificateInfo) ? (
+          <ModalContent>{formatText(certificateInfo)}</ModalContent>
+        ) : (
+          <ModalContent dangerouslySetInnerHTML={sanitizeText(certificateInfo)} />
+        )}
       </TextWithInfoModal>
       <CustomButton
         buttonStyle="primary"
         onClick={onCreateCertificateClick}
-        disabled={!link.enabled}
-        text={link.name}
-        tooltip={link.description}
+        disabled={!link?.enabled}
+        text={link?.name}
+        tooltip={link?.description}
       />
     </Row>
   )
