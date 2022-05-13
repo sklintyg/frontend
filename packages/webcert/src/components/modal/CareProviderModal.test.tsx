68261cb8d030a9d921c52e43f480782566ba3533
HelenaEdblad <helena.edblad@soprasteria.com>
2022-05-13 14:45:03 +0200

    INTYGFV-14453: Add more tests

--- packages/webcert/src/components/modal/CareProviderModal.test.tsx
+++ packages/webcert/src/components/modal/CareProviderModal.test.tsx
@@ -6,29 +6,89 @@ import store from '../../store/store'
 import CareProviderModal from './CareProviderModal'
 import { updateUser } from '../../store/user/userActions'
 import { LoginMethod, SigningMethod, Unit, User } from '@frontend/common'
+import userEvent from '@testing-library/user-event'
+
+const renderComponent = () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <CareProviderModal title="Välj vårdenhet" />
+      </BrowserRouter>
+    </Provider>
+  )
+}
 
 describe('Care provider modal', () => {
-  const renderComponent = () => {
-    render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <CareProviderModal title="Välj vårdenhet" />
-        </BrowserRouter>
-      </Provider>
-    )
-  }
-
-  it('should show care provider modal if loggedInCareProvider is not set', () => {
+  it('should show care provider modal if logged in unit is not set', () => {
     store.dispatch(updateUser(getUserWithEmptyUnit()))
     renderComponent()
     expect(screen.getByText('Välj vårdenhet')).toBeInTheDocument()
   })
+
+  it('should not show modal if logged in unit is set', () => {
+    store.dispatch(updateUser(getUser()))
+    renderComponent()
+    expect(screen.queryByText('Välj vårdenhet')).not.toBeInTheDocument()
+  })
+
+  it('should show care units when care provider modal is open', () => {
+    store.dispatch(updateUser(getUserWithEmptyUnit()))
+    renderComponent()
+    expect(screen.getByText('Care unit')).toBeInTheDocument()
+  })
 })
 
 const getUserWithEmptyUnit = (): User => {
+  const emptyUnit = {} as Unit
+
+  return {
+    hsaId: '',
+    name: '',
+    role: 'doctor',
+    loggedInUnit: emptyUnit,
+    loggedInCareUnit: emptyUnit,
+    loggedInCareProvider: emptyUnit,
+    preferences: null,
+    loginMethod: LoginMethod.BANK_ID,
+    signingMethod: SigningMethod.FAKE,
+    protectedPerson: false,
+    careProviders: [
+      {
+        id: '',
+        name: 'Care Provider',
+        careUnits: [
+          {
+            unitId: '',
+            unitName: 'Care unit',
+            address: '',
+            zipCode: '',
+            city: '',
+            phoneNumber: '',
+            email: '',
+            isInactive: false,
+            units: [
+              {
+                unitId: '',
+                unitName: 'Unit',
+                address: '',
+                zipCode: '',
+                city: '',
+                phoneNumber: '',
+                email: '',
+                isInactive: false,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  }
+}
+
+const getUser = (): User => {
   const unit: Unit = {
-    unitId: '',
-    unitName: '',
+    unitId: 'unitId',
+    unitName: 'unit name',
     address: '',
     zipCode: '',
     city: '',
@@ -37,15 +97,13 @@ const getUserWithEmptyUnit = (): User => {
     isInactive: false,
   }
 
-  const emptyUnit = {} as Unit
-
   return {
     hsaId: '',
     name: '',
     role: 'doctor',
     loggedInUnit: unit,
     loggedInCareUnit: unit,
-    loggedInCareProvider: emptyUnit,
+    loggedInCareProvider: unit,
     preferences: null,
     loginMethod: LoginMethod.BANK_ID,
     signingMethod: SigningMethod.FAKE,
