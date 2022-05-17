6e0a1ad69fbed3f46c085a660a02d8ca232dfddc
HelenaEdblad <helena.edblad@soprasteria.com>
2022-05-17 10:04:25 +0200

    INTYGFV-14453: Add table to care provider modal

--- packages/webcert/src/components/modal/CareProviderModal.tsx
+++ packages/webcert/src/components/modal/CareProviderModal.tsx
@@ -4,8 +4,13 @@ import FocusTrap from 'focus-trap-react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getUser } from '../../store/user/userSelectors'
 import { setUnit } from '../../store/user/userActions'
-import { CareProviders } from './CareProviders'
-import { User } from '@frontend/common'
+import { Unit, User } from '@frontend/common'
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const StyledArrow = styled(FontAwesomeIcon)`
+  cursor: pointer;
+`
 
 const ModalContentWrapper = styled.div`
   p + p {
@@ -15,7 +20,65 @@ const ModalContentWrapper = styled.div`
 
 const WrapText = styled.div`
   white-space: normal;
+  transform: none;
+  margin-left: -19.375rem;
+  margin-top: -40vh;
 `
+
+interface ExpandableTableRowProps {
+  careUnit: string
+  careUnitId: string
+  units: Unit[]
+  handleChooseUnit: (event: React.MouseEvent) => void
+}
+
+const ExpandableTableRow: React.FC<ExpandableTableRowProps> = ({ careUnit, careUnitId, units, handleChooseUnit }) => {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const toggleOpen = () => {
+    setIsExpanded(!isExpanded)
+  }
+
+  return (
+    <>
+      <tr>
+        <td>
+          <StyledArrow icon={isExpanded ? faAngleUp : faAngleDown} className="iu-color-cta-dark iu-mr-300" onClick={toggleOpen} />
+          <button className="ic-link iu-text-left" type="button" id={careUnitId} onClick={handleChooseUnit}>
+            {careUnit}
+          </button>
+        </td>
+        <td>0</td>
+        <td>0</td>
+      </tr>
+      {units && units.map((unit) => <ExpandedUnit isExpanded={isExpanded} unit={unit} handleChooseUnit={handleChooseUnit} />)}
+    </>
+  )
+}
+
+interface ExpandedUnitProps {
+  isExpanded: boolean
+  unit: Unit
+  handleChooseUnit: (event: React.MouseEvent) => void
+}
+
+const ExpandedUnit: React.FC<ExpandedUnitProps> = ({ unit, isExpanded, handleChooseUnit }) => {
+  if (!isExpanded) {
+    return null
+  }
+
+  return (
+    <tr>
+      <td>
+        <button className="ic-link iu-ml-700 iu-text-left" type="button" id={unit.unitId} onClick={handleChooseUnit}>
+          {unit.unitName}
+        </button>
+      </td>
+      <td>0</td>
+      <td>0</td>
+    </tr>
+  )
+}
 
 const CareProviderModal: React.FC = () => {
   const dispatch = useDispatch()
@@ -54,7 +117,31 @@ const CareProviderModal: React.FC = () => {
               <h3 id="dialog-title">Välj vårdenhet</h3>
             </div>
             <ModalContentWrapper className="ic-modal__body ic-text">
-              <CareProviders chooseUnit={handleChooseUnit} careProviders={careProviders} />
+              {careProviders.map((careProvider) => {
+                return (
+                  <table className="ic-table iu-fullwidth">
+                    <thead>
+                      <tr>
+                        <th>{careProvider.name}</th>
+                        <th>Ej hanterade ärenden</th>
+                        <th>Ej signerade utkast</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {careProvider.careUnits.map((careUnit) => {
+                        return (
+                          <ExpandableTableRow
+                            careUnit={careUnit.unitName}
+                            careUnitId={careUnit.unitId}
+                            units={careUnit.units}
+                            handleChooseUnit={handleChooseUnit}
+                          />
+                        )
+                      })}
+                    </tbody>
+                  </table>
+                )
+              })}
             </ModalContentWrapper>
           </WrapText>
         </div>
