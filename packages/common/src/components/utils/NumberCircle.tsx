e6ce101e7131d913cf7c97eb5d62b910044c65dd
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-05-18 09:09:49 +0200

    INTYGFV-14621: Renamed component

--- packages/common/src/components/utils/NumberCircle.tsx
+++ packages/common/src/components/utils/NumberCircle.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const NumberCircle = styled.span`
+const Wrapper = styled.span`
   min-width: 10px;
   padding: 3px 7px !important;
   white-space: nowrap;
@@ -16,12 +16,12 @@ interface Props {
   className?: string
 }
 
-export const Divider: React.FC<Props> = ({ number, style, className }) => {
+export const NumberCircle: React.FC<Props> = ({ number, style, className }) => {
   return (
-    <NumberCircle className={`${className} ${style === 'secondary' ? 'ic-button iu-bg-main iu-color-white' : 'iu-bg-white iu-color-main'}`}>
+    <Wrapper className={`${className} ${style === 'secondary' ? 'ic-button iu-bg-main iu-color-white' : 'iu-bg-white iu-color-main'}`}>
       {number}
-    </NumberCircle>
+    </Wrapper>
   )
 }
 
-export default Divider
+export default NumberCircle
