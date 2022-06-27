97d3a8d10e74260e25c0f16838452ae0453f24e5
HelenaEdblad <helena.edblad@soprasteria.com>
2022-06-27 08:56:58 +0200

    INTYGFV-14657: Add locale string function

--- packages/common/src/utils/dateUtils.ts
+++ packages/common/src/utils/dateUtils.ts
@@ -122,13 +122,17 @@ const getIsIntervalsCorrect = (leftStartTime: Date, leftEndTime: Date, rightStar
   return leftStartTime <= leftEndTime && rightStartTime <= rightEndTime
 }
 
+const getLocaleString = (hoursWorkingPerWeek: number, percentage: number) => {
+  return (hoursWorkingPerWeek *= percentage).toLocaleString('sv-SE')
+}
+
 export const getPeriodWorkHours = (hoursWorkingPerWeek: number, sickLeavePercentage: string) => {
   if (sickLeavePercentage === SickLeavePeriods.EN_FJARDEDEL) {
-    return (hoursWorkingPerWeek *= 0.75).toLocaleString('sv-SE')
+    return getLocaleString(hoursWorkingPerWeek, 0.75)
   } else if (sickLeavePercentage === SickLeavePeriods.HALFTEN) {
-    return (hoursWorkingPerWeek *= 0.5).toLocaleString('sv-SE')
+    return getLocaleString(hoursWorkingPerWeek, 0.5)
   } else if (sickLeavePercentage === SickLeavePeriods.TRE_FJARDEDEL) {
-    return (hoursWorkingPerWeek *= 0.25).toLocaleString('sv-SE')
+    return getLocaleString(hoursWorkingPerWeek, 0.25)
   } else if (sickLeavePercentage === SickLeavePeriods.HELT_NEDSATT) {
     return 0
   }
