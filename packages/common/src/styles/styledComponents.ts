835ba5258d3f4d5a06497dd2e9e30a8e4e707c04
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2022-08-15 13:28:05 +0200

    INTYGFV-14649: Fixed styling of go back button (#441)

--- packages/common/src/styles/styledComponents.ts
+++ packages/common/src/styles/styledComponents.ts
@@ -3,3 +3,11 @@ import styled from 'styled-components'
 export const BoxShadowContainer = styled.div`
   box-shadow: 0 2px 4px 0 rgb(0 0 0 / 12%);
 `
+
+export const LinkButton = styled.button`
+  background: none;
+  border: none;
+  font-size: inherit;
+  line-height: inherit;
+  font-family: inherit;
+`
