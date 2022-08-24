1c8decd5a15f488749bef5dc448b1c3ed1cb8fc4
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-08-24 15:13:30 +0200

    INTYGFV-14720: Fixed tests

--- packages/webcert/src/feature/list/test/List.test.tsx
+++ packages/webcert/src/feature/list/test/List.test.tsx
@@ -25,17 +25,17 @@ describe('List', () => {
   })
 
   it('should display title', () => {
-    renderComponent([], {}, 10)
+    renderComponent([], { type: ListType.UNKOWN }, 10)
     expect(screen.getByText('TITLE')).toBeInTheDocument()
   })
 
   it('should no results if filter results in empty list', () => {
-    renderComponent([], {}, 10)
+    renderComponent([], { type: ListType.UNKOWN }, 10)
     expect(screen.getByText('Inga resultat att visa.')).toBeInTheDocument()
   })
 
   it('should show reset button', () => {
-    renderComponent(getDefaultList(), {})
+    renderComponent(getDefaultList(), { type: ListType.UNKOWN })
     expect(screen.getByText('Återställ sökfilter', { exact: false })).toBeInTheDocument()
   })
 
@@ -45,19 +45,19 @@ describe('List', () => {
   })
 
   it('should not display pagination if filtered list is empty', () => {
-    renderComponent([], {}, 0)
+    renderComponent([], { type: ListType.UNKOWN }, 0)
     expect(screen.queryByText('Föregående', { exact: false })).not.toBeInTheDocument()
     expect(screen.queryByText('Visa antal träffar', { exact: false })).not.toBeInTheDocument()
   })
 
   it('should not display pagination if list is loading', () => {
-    renderComponent(getDefaultList(), {}, 1, true)
+    renderComponent(getDefaultList(), { type: ListType.UNKOWN }, 1, true)
     expect(screen.queryByText('Föregående', { exact: false })).not.toBeInTheDocument()
     expect(screen.queryByText('Visa antal träffar', { exact: false })).not.toBeInTheDocument()
   })
 
   it('should display pagination if list is done loading', () => {
-    renderComponent(getDefaultList(), {}, 20)
+    renderComponent(getDefaultList(), { type: ListType.UNKOWN }, 20)
     expect(screen.getByText('Föregående', { exact: false })).toBeInTheDocument()
   })
 })
