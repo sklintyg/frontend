7b40237206425353f79d2f97d028eba339b8b6b3
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2022-08-24 14:51:37 +0200

    INTYGFV-14476: Updated error when certificate cannot be loaded (#459)

---
+++ packages/webcert/src/components/error/errorPageContent/GetCertificateProblem.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import WCDynamicLink from '../../../utils/WCDynamicLink'
+
+export const GET_CERTIFICATE_PROBLEM_TITLE = 'Intyget kunde inte visas'
+export const GET_CERTIFICATE_PROBLEM_TEXT = 'Om problemet kvarstår, kontakta i första hand din lokala IT-avdelning och i andra hand '
+
+const InternalProblem: React.FC = () => {
+  return (
+    <>
+      <p>
+        <strong>{GET_CERTIFICATE_PROBLEM_TITLE}</strong>
+      </p>
+      <p>
+        {GET_CERTIFICATE_PROBLEM_TEXT} <WCDynamicLink linkKey={'ineraKundserviceAnmalFel'} />
+      </p>
+    </>
+  )
+}
+
+export default InternalProblem
