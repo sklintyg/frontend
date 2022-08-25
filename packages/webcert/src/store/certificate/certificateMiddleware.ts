52d3a0fa8216ed560ef0d63e632931efbca4e2ba
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-08-25 11:42:59 +0200

    INTYGFV-14476: Fixed error handling for getting certificate

--- packages/webcert/src/store/certificate/certificateMiddleware.ts
+++ packages/webcert/src/store/certificate/certificateMiddleware.ts
@@ -165,6 +165,21 @@ const handleGetCertificateError: Middleware<Dispatch> = ({ dispatch }) => () => 
   )
 }
 
+const handleGetCertificateError: Middleware<Dispatch> = ({ dispatch }) => () => (action: AnyAction): void => {
+  if (action.payload.error.errorCode === ErrorCode.UNKNOWN_INTERNAL_PROBLEM.toString()) {
+    dispatch(
+      throwError({
+        type: ErrorType.ROUTE,
+        errorCode: ErrorCode.GET_CERTIFICATE_PROBLEM,
+        message: action.payload.error.message,
+        certificateId: action.payload.certificateId,
+      })
+    )
+  } else {
+    dispatch(apiGenericError(action.payload))
+  }
+}
+
 const handleGetCertificateEvents: Middleware<Dispatch> = ({ dispatch }: MiddlewareAPI) => () => (action: AnyAction): void => {
   dispatch(
     apiCallBegan({
