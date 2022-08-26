e5dc9e303cbd0a9a5447cbb41fe59c40e5bf3753
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2022-08-26 11:40:48 +0200

    INTYGFV-14757: Removed dot in titles for error messages (#464)

--- packages/webcert/src/components/error/errorPageContent/DataNotFound.tsx
+++ packages/webcert/src/components/error/errorPageContent/DataNotFound.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const DATA_NOT_FOUND_TITLE = 'Intyget kunde inte hittas.'
+export const DATA_NOT_FOUND_TITLE = 'Intyget kunde inte hittas'
 export const DATA_NOT_FOUND_MESSAGE = 'Intyget är borttaget eller så saknas behörighet.'
 
 const DataNotFound: React.FC = () => {
