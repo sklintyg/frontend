17595487928ee5b91d481aef9557b5e4a5d2147f
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-08-26 09:15:48 +0200

    INTYGFV-14757: Removed dot in titles for error messages

--- packages/webcert/src/components/error/errorPageContent/AuthorizationProblemConfidentialityMarking.tsx
+++ packages/webcert/src/components/error/errorPageContent/AuthorizationProblemConfidentialityMarking.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const AUTHORIZATION_PROBLEM_CONFIDENTIALITY_MARKING_TITLE = 'Behörighet saknas.'
+export const AUTHORIZATION_PROBLEM_CONFIDENTIALITY_MARKING_TITLE = 'Behörighet saknas'
 export const AUTHORIZATION_PROBLEM_CONFIDENTIALITY_MARKING_MESSAGE =
   'För att hantera intyg för patienter med skyddade personuppgifter krävs att du har befattningen läkare eller tandläkare. ' +
   'Vissa intygstyper får inte hanteras alls för patienter med skyddade personuppgifter, även om du har befattningen som krävs.'
