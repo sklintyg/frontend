c039cfeed0f4a3013bb19ee6c2a48594bd2ebbe5
HelenaEdblad <helena.edblad@soprasteria.com>
2022-09-01 11:30:32 +0200

    INTYGFV-14753: Change title and message to concurrent modification

--- packages/webcert/src/components/error/modals/ConcurrentModification.tsx
+++ packages/webcert/src/components/error/modals/ConcurrentModification.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import WCDynamicLink from '../../../utils/WCDynamicLink'
 import { ModalProps } from './errorUtils'
 import ReloadModal from './ReloadModal'
 
+export const CONCURRENT_MODIFICATION_ERROR_TITLE = 'Tekniskt fel'
 export const CONCURRENT_MODIFICATION_ERROR_MESSAGE =
-  'Utkastet har samtidigt ändrats av en annan användare och kunde därför inte sparas. Ladda om sidan och försök igen.'
+  'Prova att ladda om sidan. Om problemet kvarstår, kontakta i första hand din lokala IT-avdelning och i andra hand'
 
 const ConcurrentModification: React.FC<ModalProps> = ({ errorData }) => {
   return (
     <ReloadModal errorData={errorData}>
-      <p>{CONCURRENT_MODIFICATION_ERROR_MESSAGE}</p>
+      <p>
+        <strong>{CONCURRENT_MODIFICATION_ERROR_TITLE}</strong>
+      </p>
+      <p>
+        {CONCURRENT_MODIFICATION_ERROR_MESSAGE} <WCDynamicLink linkKey={'ineraKundserviceAnmalFel'} />
+      </p>
     </ReloadModal>
   )
 }
