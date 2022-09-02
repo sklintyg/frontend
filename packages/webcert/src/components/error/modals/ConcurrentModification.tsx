9d9fd577169e8c971618a431e7193d48a85c7040
HelenaEdblad <helena.edblad@soprasteria.com>
2022-09-02 10:35:44 +0200

    INTYGFV-14756: Fix test error

--- packages/webcert/src/components/error/modals/ConcurrentModification.tsx
+++ packages/webcert/src/components/error/modals/ConcurrentModification.tsx
@@ -3,7 +3,7 @@ import { ModalProps } from './errorUtils'
 import ReloadModal from './ReloadModal'
 
 export const CONCURRENT_MODIFICATION_ERROR_TITLE = 'Utkastet har ändrats av en annan användare'
-export const CONCURRENT_MODIFICATION_ERROR_MESSAGE = 'Öppna intyget på nytt och försök igen. Utkastet ändrades av: '
+export const CONCURRENT_MODIFICATION_ERROR_MESSAGE = 'Öppna intyget på nytt och försök igen. Utkastet ändrades av:'
 
 const ConcurrentModification: React.FC<ModalProps> = ({ errorData }) => {
   const NAME = errorData.message?.substring(0, errorData.message.indexOf(' -'))
@@ -14,8 +14,7 @@ const ConcurrentModification: React.FC<ModalProps> = ({ errorData }) => {
         <strong>{CONCURRENT_MODIFICATION_ERROR_TITLE}</strong>
       </p>
       <p>
-        {CONCURRENT_MODIFICATION_ERROR_MESSAGE}
-        {NAME}
+        {CONCURRENT_MODIFICATION_ERROR_MESSAGE} {NAME}
       </p>
     </ReloadModal>
   )
