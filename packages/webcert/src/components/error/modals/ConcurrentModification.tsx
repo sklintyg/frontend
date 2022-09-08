7b91b0c45207889a05e720ae09ef5542590da0c6
HelenaEdblad <85883491+HelenaEdblad@users.noreply.github.com>
2022-09-08 14:55:32 +0200

    INTYGFV-14753: Return correct error modal when network error (#482)

--- packages/webcert/src/components/error/modals/ConcurrentModification.tsx
+++ packages/webcert/src/components/error/modals/ConcurrentModification.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { ModalProps } from './errorUtils'
+import { ModalProps, messageSubstring } from './errorUtils'
 import ReloadModal from './ReloadModal'
 
 export const CONCURRENT_MODIFICATION_ERROR_TITLE = 'Utkastet har ändrats av en annan användare'
 export const CONCURRENT_MODIFICATION_ERROR_MESSAGE = 'Öppna intyget på nytt och försök igen. Utkastet ändrades av:'
 
 const ConcurrentModification: React.FC<ModalProps> = ({ errorData }) => {
-  const NAME = errorData.message?.substring(0, errorData.message.indexOf(' -'))
+  const NAME = messageSubstring(errorData)
 
   return (
     <ReloadModal errorData={errorData}>
