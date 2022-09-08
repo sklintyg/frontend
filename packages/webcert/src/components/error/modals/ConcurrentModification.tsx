71a3f722509b6cf51e5c9e78b14aa01bf7e4c2b1
HelenaEdblad <helena.edblad@soprasteria.com>
2022-09-08 14:44:29 +0200

    bugfix/INTYGFV-14756: Change reload confirm button text and current modification text

--- packages/webcert/src/components/error/modals/ConcurrentModification.tsx
+++ packages/webcert/src/components/error/modals/ConcurrentModification.tsx
@@ -3,7 +3,7 @@ import { ModalProps, messageSubstring } from './errorUtils'
 import ReloadModal from './ReloadModal'
 
 export const CONCURRENT_MODIFICATION_ERROR_TITLE = 'Utkastet har ändrats av en annan användare'
-export const CONCURRENT_MODIFICATION_ERROR_MESSAGE = 'Öppna intyget på nytt och försök igen. Utkastet ändrades av:'
+export const CONCURRENT_MODIFICATION_ERROR_MESSAGE = 'Prova att ladda om sidan och försök igen. Utkastet ändrades av:'
 
 const ConcurrentModification: React.FC<ModalProps> = ({ errorData }) => {
   const NAME = messageSubstring(errorData)
