51db63c832f85e2e0c31698a1834b66736b2684b
Samuel Tilly <samuel.tilly@gmail.com>
2022-09-22 09:22:28 +0200

    simplified fake validation functions

--- packages/common/src/utils/faker/fakeCertificateDataValidation.ts
+++ packages/common/src/utils/faker/fakeCertificateDataValidation.ts
@@ -5,7 +5,7 @@ import { ValidationError } from '../../types/certificate'
 export const fakeCertificateDataValidation = (data?: Partial<CertificateDataValidation>): CertificateDataValidation => {
   return {
     type: CertificateDataValidationType.TEXT_VALIDATION,
-    questionId: '',
+    questionId: faker.random.alpha({ count: 5 }),
     expression: '',
     ...data,
   }
@@ -13,11 +13,11 @@ export const fakeCertificateDataValidation = (data?: Partial<CertificateDataVali
 
 export const fakeCertificateValidationError = (data?: Partial<ValidationError>): ValidationError => {
   return {
-    id: data?.id ?? faker.random.alpha({ count: 5 }),
-    category: data?.category ?? faker.random.alpha({ count: 5 }),
-    field: data?.field ?? faker.random.alpha({ count: 5 }),
-    type: data?.field ?? faker.random.alpha({ count: 5 }),
-    text: data?.field ?? faker.lorem.sentence(),
+    id: faker.random.alpha({ count: 5 }),
+    category: faker.random.alpha({ count: 5 }),
+    field: faker.random.alpha({ count: 5 }),
+    type: faker.random.alpha({ count: 5 }),
+    text: faker.lorem.sentence(),
     ...data,
   }
 }
