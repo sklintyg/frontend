cfb2bd4a4d2a4e49508b9fe8109bd75ef6dfcc4e
Helena Edblad <helena.edblad@soprasteria.com>
2022-10-12 09:54:23 +0200

    INTYGFV-14796: Add possibility to show info message in certificate list

--- packages/webcert/src/components/certificateList/CertificateListRow.tsx
+++ packages/webcert/src/components/certificateList/CertificateListRow.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { CustomButton, ResourceLink, sanitizeText, TextWithInfoModal } from '@frontend/common'
+import { CustomButton, InfoBox, ResourceLink, sanitizeText, TextWithInfoModal } from '@frontend/common'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar as star } from '@fortawesome/free-regular-svg-icons'
 import { faStar as starChecked } from '@fortawesome/free-solid-svg-icons'
@@ -17,6 +17,7 @@ interface Props {
   favorite: boolean
   createCertificate: (...args: string[]) => void
   link?: ResourceLink
+  message?: string
 }
 
 const Row = styled.div`
@@ -46,6 +47,7 @@ const CertificateListRow: React.FC<Props> = ({
   favorite,
   createCertificate,
   link,
+  message,
 }) => {
   const favoriteText = favorite ? 'Ta bort som favoritmarkerat intyg.' : 'Markera intyget som favorit och fäst högst upp i listan.'
   const onPreferenceClick = () => {
@@ -86,32 +88,39 @@ const CertificateListRow: React.FC<Props> = ({
   }
 
   return (
-    <Row className="iu-flex iu-flex-center iu-p-400">
-      <Star className="iu-mr-1rem" onClick={onPreferenceClick} data-tip={favoriteText} aria-label={favoriteText}>
-        <FontAwesomeIcon
-          icon={favorite ? starChecked : star}
-          className={classnames({ 'iu-color-information': favorite, 'iu-color-muted': !favorite })}
-        />
-      </Star>
-      <CertificateName>
-        <span className="iu-fw-bold">{certificateName}</span> {issuerTypeId}
-      </CertificateName>
-      <TextWithInfoModal text="Om intyget" modalTitle={`Om ${certificateName}`} className="iu-mr-1rem">
-        {hasDynamicLink(certificateInfo) ? (
-          <ModalContent>{formatText(certificateInfo)}</ModalContent>
-        ) : (
-          <ModalContent dangerouslySetInnerHTML={sanitizeText(certificateInfo)} />
+    <Row className="iu-flex iu-flex-column iu-p-400">
+      <div className="iu-flex iu-flex-center">
+        <Star className="iu-mr-1rem" onClick={onPreferenceClick} data-tip={favoriteText} aria-label={favoriteText}>
+          <FontAwesomeIcon
+            icon={favorite ? starChecked : star}
+            className={classnames({ 'iu-color-information': favorite, 'iu-color-muted': !favorite })}
+          />
+        </Star>
+        <CertificateName>
+          <span className="iu-fw-bold">{certificateName}</span> {issuerTypeId}
+        </CertificateName>
+        <TextWithInfoModal text="Om intyget" modalTitle={`Om ${certificateName}`} className="iu-mr-1rem">
+          {hasDynamicLink(certificateInfo) ? (
+            <ModalContent>{formatText(certificateInfo)}</ModalContent>
+          ) : (
+            <ModalContent dangerouslySetInnerHTML={sanitizeText(certificateInfo)} />
+          )}
+        </TextWithInfoModal>
+        {link && (
+          <CustomButton
+            buttonStyle="primary"
+            onClick={onCreateCertificateClick}
+            startIcon={<img src={file} alt={link?.description} />}
+            disabled={!link?.enabled}
+            text={link?.name}
+            tooltip={link?.description}
+          />
         )}
-      </TextWithInfoModal>
-      {link && (
-        <CustomButton
-          buttonStyle="primary"
-          onClick={onCreateCertificateClick}
-          startIcon={<img src={file} alt={link?.description} />}
-          disabled={!link?.enabled}
-          text={link?.name}
-          tooltip={link?.description}
-        />
+      </div>
+      {message && (
+        <div className="iu-pt-200">
+          <InfoBox type={'info'}>{message}</InfoBox>
+        </div>
       )}
     </Row>
   )
