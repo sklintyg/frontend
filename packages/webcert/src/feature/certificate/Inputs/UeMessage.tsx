b8af67e1c04c8528366bf30b92115157a4b64382
Erik Sylwan <erik.sylwan@soprasteria.com>
2022-10-14 09:28:44 +0200

    wrapping infobox in styled div with negativ top margin to adjust for too large spacing between question and message

--- packages/webcert/src/feature/certificate/Inputs/UeMessage.tsx
+++ packages/webcert/src/feature/certificate/Inputs/UeMessage.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import { CertificateDataElement, ConfigUeMessage, InfoBox } from '@frontend/common'
 import { MessageLevel } from '@frontend/common/src/types/certificate'
+import styled from 'styled-components'
 
 interface Props {
   question: CertificateDataElement
   disabled: boolean
 }
+
+const Wrapper = styled.div`
+  margin-top: -32px;
+`
 
 const messageLevelToInfoBoxLevel = (level: MessageLevel): 'info' | 'error' | 'observe' => {
   switch (level) {
@@ -22,9 +27,11 @@ const UeMessage: React.FC<Props> = ({ question }) => {
   const questionConfig = question.config as ConfigUeMessage
 
   return (
-    <InfoBox type={messageLevelToInfoBoxLevel(questionConfig.level)}>
-      <p>{questionConfig.message}</p>
-    </InfoBox>
+    <Wrapper>
+      <InfoBox type={messageLevelToInfoBoxLevel(questionConfig.level)}>
+        <p>{questionConfig.message}</p>
+      </InfoBox>
+    </Wrapper>
   )
 }
 
