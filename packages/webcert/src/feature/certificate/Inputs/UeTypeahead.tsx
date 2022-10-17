27bf926e0e0cecf9064fd8545d656ef5e6cf98ae
Helena Edblad <helena.edblad@soprasteria.com>
2022-10-17 13:07:20 +0200

    INTYGFV-14790: Use typeahead component

--- packages/webcert/src/feature/certificate/Inputs/UeTypeahead.tsx
+++ packages/webcert/src/feature/certificate/Inputs/UeTypeahead.tsx
@@ -3,24 +3,27 @@ import {
   CertificateDataValidationType,
   ConfigUeTypeahead,
   QuestionValidationTexts,
-  TextInput,
   TextValidation,
   ValueText,
 } from '@frontend/common'
 import _ from 'lodash'
 import * as React from 'react'
-import { useRef, useState, useEffect } from 'react'
+import { useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { updateCertificateDataElement } from '../../../store/certificate/certificateActions'
 import { getQuestionHasValidationError, getShowValidationErrors } from '../../../store/certificate/certificateSelectors'
-import styled, { css } from 'styled-components'
-import { useKeyPress } from '@frontend/common/src/utils/userFunctionUtils'
-import { Element, scroller } from 'react-scroll'
+import Typeahead, { Suggestion } from '@frontend/common/src/components/Inputs/Typeahead'
+
+import { css } from 'styled-components'
 
 export interface Props {
   question: CertificateDataElement
   disabled?: boolean
 }
+
+const wholeRowGrid = css`
+  position: relative;
+`
 
 const UeTypeahead: React.FC<Props> = ({ question, disabled }) => {
   const isShowValidationError = useSelector(getShowValidationErrors)
@@ -41,173 +44,58 @@ const UeTypeahead: React.FC<Props> = ({ question, disabled }) => {
     }, 1000)
   ).current
 
-  const SuggestionsList = styled.ul`
-    list-style-type: none;
-    text-align: left;
-    position: absolute;
-    grid-column: span 6;
-    padding: 5px 0;
-    border-top: 1px solid gray;
-    box-shadow: 0 0 1px rgba(0, 0, 0, 0.1), 0 2px 4px 1px rgba(0, 0, 0, 0.18);
-    left: 16px;
-    right: 0;
-    z-index: 100000;
-    background-color: white;
-    max-height: 350px;
-    overflow-y: auto;
-    flex: 1;
-    grid-area: ul;
-    display: ${open ? 'block' : 'none'};
-    width: 48%;
-  `
-
-  const SuggestionsListItem = styled.li`
-    padding: 3px 20px;
-    cursor: pointer;
-
-    &:hover {
-      background-color: #01a5a3;
-      color: white;
-    }
-  `
-
-  const downPress = useKeyPress('ArrowDown')
-  const upPress = useKeyPress('ArrowUp')
-  const enterPress = useKeyPress('Enter')
-  const escPress = useKeyPress('Escape')
-  const tabPress = useKeyPress('Tab')
-  const [cursor, setCursor] = useState(suggestions.length > 0 ? 0 : -1)
-  const [hovered, setHovered] = useState<number>(-1)
-  const typeaheadList = useRef<null | HTMLUListElement>(null)
-
-  useEffect(() => {
-    setCursor(suggestions.length > 0 && open ? 0 : -1)
-  }, [open, suggestions])
-  useEffect(() => {
-    if (hovered >= 0) {
-      setCursor(hovered)
-    }
-  }, [hovered])
-  useEffect(() => {
-    if (suggestions.length > 0 && downPress && open) {
-      setCursor((prevState) => (prevState < suggestions.length - 1 ? prevState + 1 : 0))
-    }
-  }, [downPress, open, suggestions.length])
-  useEffect(() => {
-    if (suggestions.length > 0 && upPress && open) {
-      setCursor((prevState) => (prevState > 0 ? prevState - 1 : suggestions.length - 1))
-    }
-  }, [open, suggestions.length, upPress])
-  useEffect(() => {
-    if ((enterPress || tabPress) && suggestions.length >= cursor && cursor >= 0 && open) {
-      handleEnter(suggestions[cursor])
-    }
-  }, [cursor, enterPress, open, suggestions, tabPress])
-  useEffect(() => {
-    if (escPress && open) {
-      handleClose()
-    }
-  }, [escPress, open])
-  useEffect(() => {
-    if (suggestions.length > 0 && cursor >= 0 && suggestions[cursor].length > 0 && cursor !== hovered) {
-      const element = typeaheadList.current
-      if (element !== null && element !== undefined) {
-        scroller.scrollTo('typeahead-item-' + cursor, {
-          duration: 0,
-          delay: 0,
-          smooth: false,
-          containerId: 'typeahead-list',
-          offset: -10,
-        })
-      }
-    }
-  }, [cursor, hovered, suggestions])
-
-  const updateHovered = (i: number) => {
-    setHovered(i)
-    setCursor(i)
-  }
-
   const handleClose = () => {
     setOpen(false)
-  }
-
-  const handleEnter = (selectedValue: string) => {
-    setText(selectedValue)
-    setOpen(false)
-  }
-
-  const onClick = (suggestion: string) => {
-    if (suggestion) {
-      setText(suggestion)
-      setOpen(false)
-    }
-  }
-
-  const wholeRowGrid = css`
-    position: relative;
-  `
-
-  const getItemClassName = (item: string, index: number) => {
-    const isCursor = index === cursor
-    const isHoverDifferentFromCursor = cursor >= 0 && hovered === index && hovered !== cursor
-    if (isHoverDifferentFromCursor) {
-      return 'iu-bg-secondary-light iu-color-grey-500'
-    } else if (isCursor) {
-      return 'iu-bg-main iu-color-white'
-    }
   }
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const newText = event.currentTarget.value
     setText(newText)
-    if (!open) {
-      setCursor(0)
-    }
+
     setOpen(true)
     dispatchEditDraft(question, newText)
 
     if (newText === undefined || newText === null) {
       return []
     }
+
+    if (newText.length === 0) {
+      setOpen(false)
+    }
     setSuggestions(questionConfig.typeahead.filter((suggestion: string) => suggestion.toLowerCase().indexOf(newText.toLowerCase()) >= 0))
   }
 
-  const renderSuggestions = () => {
-    if (suggestions.length === 0) {
-      return null
-    }
-    return (
-      <SuggestionsList id={'typeahead-list'} ref={typeaheadList}>
-        {suggestions.map((item, i) => (
-          <SuggestionsListItem
-            id={'typeahead-list-option-' + i}
-            key={item}
-            role="option"
-            title={item}
-            className={getItemClassName(item, i)}
-            onMouseDown={(e) => onClick(item)}
-            onMouseEnter={() => updateHovered(i)}
-            onMouseLeave={() => setHovered(-1)}>
-            <Element name={item}>{item}</Element>
-          </SuggestionsListItem>
-        ))}
-      </SuggestionsList>
-    )
+  const getSuggestions = (): Suggestion[] => {
+    return suggestions.map((suggestion) => {
+      return {
+        label: suggestion,
+        disabled: false,
+        title: suggestion,
+      }
+    })
+  }
+
+  const onSuggestionSelected = (value: string) => {
+    setText(value)
+    setOpen(false)
+    dispatchEditDraft(question, value)
   }
 
   return (
     <div className="iu-pt-200 iu-grid-cols iu-grid-cols-12">
       <div className="iu-grid-span-6">
-        <TextInput
+        <Typeahead
           disabled={disabled}
           hasValidationError={questionHasValidationError}
           onChange={handleChange}
           value={text === null ? '' : text}
           limit={textValidation ? textValidation.limit : 100}
+          suggestions={getSuggestions()}
+          onSuggestionSelected={onSuggestionSelected}
+          open={open}
+          onClose={handleClose}
+          listStyles={wholeRowGrid}
         />
-        <div css={wholeRowGrid}>{open ? renderSuggestions() : ''}</div>
-
         {isShowValidationError && <QuestionValidationTexts validationErrors={question.validationErrors}></QuestionValidationTexts>}
       </div>
     </div>
