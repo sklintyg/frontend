fafdb33c53204f654d02cdef8e01f68e3a89a733
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2022-10-27 08:57:59 +0200

    INTYGFV-14815: simplify and add tests for structureCertificate

--- packages/webcert/src/utils/structureCertificate.ts
+++ packages/webcert/src/utils/structureCertificate.ts
@@ -1,36 +1,28 @@
-import { Certificate, CertificateDataElement, ConfigTypes, sortByIndex } from '@frontend/common'
+import { CertificateData, CertificateDataElement, ConfigTypes, sortByIndex } from '@frontend/common'
 import { CertificateStructure } from '../store/certificate/certificateSelectors'
 
 const hasSubQuestionId = (id: string) => ({ subQuestionIds }: CertificateStructure) => subQuestionIds.includes(id)
 
-export const structureCertificate = (certificate?: Certificate): CertificateStructure[] => {
-  if (certificate == null) {
-    return []
-  }
+const filterSubQuestions = (id: string) => ({ config, parent }: CertificateDataElement) =>
+  config.type !== ConfigTypes.CATEGORY && parent === id
 
-  const elements = Object.values(certificate.data)
+export const structureCertificate = (data: CertificateData): CertificateStructure[] => {
+  const elements = Object.values(data).sort(sortByIndex)
 
-  return elements
-    .sort(sortByIndex)
-    .reduce((structure: CertificateStructure[], element: CertificateDataElement) => {
-      if (structure.some(hasSubQuestionId(element.id))) {
-        return structure
-      }
+  return elements.reduce((structure: CertificateStructure[], element: CertificateDataElement) => {
+    if (structure.some(hasSubQuestionId(element.id))) {
+      return structure
+    }
 
-      const subQuestionIds = elements
-        .filter(() => element.config.type !== ConfigTypes.CATEGORY)
-        .filter((el) => el.config.type !== ConfigTypes.CATEGORY)
-        .filter((el) => el.parent === element.id)
-        .sort(sortByIndex)
-        .map((el) => el.id)
+    const subQuestionIds =
+      element.config.type === ConfigTypes.CATEGORY ? [] : elements.filter(filterSubQuestions(element.id)).map((el) => el.id)
 
-      return structure.concat({
-        id: element.id,
-        subQuestionIds,
-        component: element.config.type,
-        index: element.index,
-        style: element.style,
-      })
-    }, [])
-    .sort(sortByIndex)
+    return structure.concat({
+      id: element.id,
+      subQuestionIds,
+      component: element.config.type,
+      index: element.index,
+      style: element.style,
+    })
+  }, [])
 }
