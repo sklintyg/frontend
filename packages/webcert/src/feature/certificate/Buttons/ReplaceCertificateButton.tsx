05b49e6e67223d1d0033b54faa2e8a1aa7683112
Helena Edblad <helena.edblad@soprasteria.com>
2022-10-28 11:36:50 +0200

    INTYGFV-14910: Add text to replace modal for DB

--- packages/webcert/src/feature/certificate/Buttons/ReplaceCertificateButton.tsx
+++ packages/webcert/src/feature/certificate/Buttons/ReplaceCertificateButton.tsx
@@ -3,9 +3,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons'
 import { useHistory } from 'react-router-dom'
 import { ButtonWithConfirmModal, InfoBox } from '@frontend/common'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { replaceCertificate } from '../../../store/certificate/certificateActions'
 import { FunctionDisabled } from '../../../utils/functionDisablerUtils'
+import { getCertificateMetaData } from '../../../store/certificate/certificateSelectors'
 
 interface Props extends FunctionDisabled {
   name: string
@@ -16,6 +17,8 @@ interface Props extends FunctionDisabled {
 const ReplaceCertificateButton: React.FC<Props> = ({ name, description, enabled, functionDisabled }) => {
   const history = useHistory()
   const dispatch = useDispatch()
+  const certificateMetadata = useSelector(getCertificateMetaData)
+  const isDB = certificateMetadata?.type === 'db'
 
   const handleConfirm = () => {
     dispatch(replaceCertificate(history))
@@ -33,13 +36,32 @@ const ReplaceCertificateButton: React.FC<Props> = ({ name, description, enabled,
       confirmButtonDisabled={functionDisabled}>
       <>
         <InfoBox type="observe" activateIconWrap>
-          <p>Om intyget innehåller ett allvarligt fel, till exempel om det är utfärdat på fel patient, bör du istället makulera intyget.</p>
+          <p>
+            {!isDB
+              ? 'Om intyget innehåller ett allvarligt fel, till exempel om det är utfärdat på fel patient, bör du istället makulera intyget.'
+              : 'Om dödsbeviset är utfärdat på fel patient ska du istället makulera dödsbeviset.'}
+          </p>
         </InfoBox>
-        <p>
-          Ett intyg kan ersättas om det innehåller felaktiga uppgifter eller om ny information tillkommit efter att intyget utfärdades. När
-          ett intyg ersätts med ett nytt skapas ett utkast, med samma information som i det ursprungliga intyget, som du kan redigera innan
-          du signerar intyget.{' '}
-        </p>
+        {!isDB ? (
+          <p>
+            Ett intyg kan ersättas om det innehåller felaktiga uppgifter eller om ny information tillkommit efter att intyget utfärdades.
+            När ett intyg ersätts med ett nytt skapas ett utkast, med samma information som i det ursprungliga intyget, som du kan redigera
+            innan du signerar intyget.
+          </p>
+        ) : (
+          <>
+            <p>
+              Ett intyg kan ersättas om det innehåller felaktiga uppgifter eller om ny information tillkommit efter att intyget utfärdades.
+              När ett intyg ersätts med ett nytt skapas ett utkast, med samma information som i det ursprungliga intyget, som du kan
+              redigera innan du signerar intyget.
+            </p>
+            <p>
+              Senast skapade dödsbevis är det som gäller. Om du ersätter det tidigare dödsbeviset och lämnar in det nya så blir det därför
+              detta dödsbevis som gäller.
+            </p>
+            <p>Det nya utkastet skapas på den enhet du är inloggad på.</p>
+          </>
+        )}
       </>
     </ButtonWithConfirmModal>
   )
