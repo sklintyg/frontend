b388d181a5024ca1d917bb186b13ae6be8e8e718
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2022-11-01 10:36:00 +0100

    INTYGFV-14855: Extract handleValidateCertificateInFrontend to separate file. Updated its tests to use fake-functions to create the test data.

--- packages/webcert/src/store/certificate/certificateMiddleware.ts
+++ packages/webcert/src/store/certificate/certificateMiddleware.ts
@@ -131,6 +131,7 @@ import { gotoComplement, updateComplements } from '../question/questionActions'
 
 import { createConcurrencyErrorRequestFromApiError, createErrorRequestFromApiError } from '../error/errorCreator'
 import { ErrorCode, ErrorType } from '../error/errorReducer'
+import {handleValidateCertificateInFrontEnd} from "./validateCertificateInFrontend";
 
 const handleGetCertificate: Middleware<Dispatch> = ({ dispatch }: MiddlewareAPI) => () => (action: AnyAction): void => {
   dispatch(showSpinner('Laddar...'))
@@ -752,17 +753,6 @@ const handlePrintCertificate: Middleware<Dispatch> = () => () => (action: AnyAct
   }
 }
 
-const handleValidateCertificateInFrontEnd: Middleware<Dispatch> = ({ dispatch, getState }: MiddlewareAPI) => () => (
-  action: AnyAction
-): void => {
-  const questionIdsToValidate = validate(getState().ui.uiCertificate.certificate, dispatch, action.payload)
-  dispatch(validateCertificateInFrontEndCompleted())
-
-  questionIdsToValidate.forEach((questionId) =>
-    dispatch(validateCertificateInFrontEnd(getState().ui.uiCertificate.certificate.data[questionId]))
-  )
-}
-
 const isSameValidationError = (savedValidationError: ValidationError, payloadValidationError: ValidationError) => {
   return (
     savedValidationError.type === payloadValidationError.type &&
@@ -790,83 +780,6 @@ const handleUpdateClientValidationError: Middleware<Dispatch> = ({ dispatch, get
   } else if (!action.payload.shouldBeRemoved) {
     dispatch(addClientValidationError(action.payload.validationError))
   }
-}
-
-function validate(certificate: Certificate, dispatch: Dispatch, update: CertificateDataElement): string[] {
-  if (!certificate) {
-    return []
-  }
-
-  const questionIdsToValidate = [] as string[]
-  const validationResults = validateExpressions(certificate, update)
-  validationResults.forEach((validationResult) => {
-    const { result, type, id } = validationResult
-    switch (type) {
-      case CertificateDataValidationType.MANDATORY_VALIDATION:
-        if (result) {
-          dispatch(hideCertificateDataElementMandatory(id))
-        } else {
-          dispatch(showCertificateDataElementMandatory(id))
-        }
-        break
-      case CertificateDataValidationType.HIDE_VALIDATION:
-        if (result) {
-          dispatch(hideCertificateDataElement(id))
-          if (certificate.data[id].visible) {
-            questionIdsToValidate.push(id)
-          }
-        } else {
-          dispatch(showCertificateDataElement(id))
-          if (!certificate.data[id].visible) {
-            questionIdsToValidate.push(id)
-          }
-        }
-        break
-      case CertificateDataValidationType.SHOW_VALIDATION:
-        if (result) {
-          dispatch(showCertificateDataElement(id))
-          if (!certificate.data[id].visible) {
-            questionIdsToValidate.push(id)
-          }
-        } else {
-          dispatch(hideCertificateDataElement(id))
-          if (certificate.data[id].visible) {
-            questionIdsToValidate.push(id)
-          }
-        }
-        break
-      case CertificateDataValidationType.DISABLE_VALIDATION:
-        if (result) {
-          dispatch(disableCertificateDataElement(id))
-        } else {
-          dispatch(enableCertificateDataElement(id))
-        }
-        break
-      case CertificateDataValidationType.DISABLE_SUB_ELEMENT_VALIDATION:
-        dispatch(setDisabledCertificateDataChild(validationResult))
-        break
-      case CertificateDataValidationType.ENABLE_VALIDATION:
-        if (result) {
-          dispatch(enableCertificateDataElement(id))
-        } else {
-          dispatch(disableCertificateDataElement(id))
-        }
-        break
-      case CertificateDataValidationType.HIGHLIGHT_VALIDATION:
-        if (result) {
-          dispatch(highlightCertificateDataElement(id))
-        } else {
-          dispatch(unstyleCertificateDataElement(id))
-        }
-        break
-      case CertificateDataValidationType.AUTO_FILL_VALIDATION:
-        if (result) {
-          dispatch(applyCertificateDataElementAutoFill(validationResult))
-        }
-        break
-    }
-  })
-  return questionIdsToValidate
 }
 
 const handleCreateNewCertificate: Middleware<Dispatch> = ({ dispatch }: MiddlewareAPI) => () => (action: AnyAction): void => {
