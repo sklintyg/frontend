64cb0db67ad4a05115cb9e632beaacd8c0f63286
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2022-11-03 11:06:56 +0100

    Add end to end test github action

--- packages/webcert/src/setupProxy.js
+++ packages/webcert/src/setupProxy.js
@@ -1,12 +1,16 @@
 /* eslint-disable */
 const { createProxyMiddleware } = require('http-proxy-middleware')
+const { env } = require('process')
 
 module.exports = function(app) {
   app.use(
     ['/fake', '/api', '/moduleapi', '/testability', '/visa', '/saml'],
     createProxyMiddleware({
-      target: 'http://localhost:8020',
+      target: env.API_TARGET,
+      cookieDomainRewrite: { '*': '' },
+      protocolRewrite: env.HTTPS === 'true' ? 'https' : 'http',
       changeOrigin: true,
+      autoRewrite: true,
     })
   )
 }
