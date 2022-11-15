82bfe811edf6fad4f370e0b1cd9041dd31a9fb76
melindalindgrensopra <112075517+melindalindgrensopra@users.noreply.github.com>
2022-11-15 13:12:33 +0100

    added text when canceling a DOI certificate

---
+++ packages/webcert/src/feature/certificate/Buttons/RevokeDBandDoiModalContent.tsx
@@ -0,0 +1,56 @@
+import { InfoBox } from '@frontend/common'
+import _ from 'lodash'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { getCertificateMetaData, getIsLocked } from '../../../store/certificate/certificateSelectors'
+
+export const RevokeDBandDoiModalContent: React.FC = () => {
+  const locked = useSelector(getIsLocked)
+  const metadata = useSelector(getCertificateMetaData, _.isEqual)
+  const isDodsbevis = metadata?.type === 'db'
+  const isDodsorsaksIntyg = metadata?.type === 'doi'
+
+  return (
+    <>
+      <InfoBox type="info" activateIconWrap>
+        <p>
+          {locked
+            ? 'Om du behöver korrigera innehållet i utkastet, ska du istället kopiera utkastet och skapa ett nytt intyg.'
+            : 'Du ska endast makulera om intyget är utfärdat på fel person. Om du behöver ändra innehållet i intyget ska du välja ”Avbryt” och därefter välja ”Ersätt”.'}
+        </p>
+      </InfoBox>
+      {(isDodsbevis || isDodsorsaksIntyg) && locked ? (
+        <p>Genom att trycka på "Makulera" makulerar du det låsta utkastet i Webcert. </p>
+      ) : isDodsbevis ? (
+        <>
+          <p>För att makulera ett dödsbevis som är skickat på fel person måste du genomföra följande två steg:</p>
+          <ol>
+            <li>
+              Tryck på ”Makulera”, detta leder till att dödsbeviset makuleras i Webcert men återkallar{' '}
+              <span className="iu-fw-bold">inte</span> dödsbeviset hos Skatteverket.
+            </li>
+            <li>
+              <span className="iu-fw-bold">Kontakta omedelbart Skatteverket</span> på telefon{' '}
+              <span className="iu-fw-bold">0771-567 567</span> (Skatteupplysningen) och ange "folkbokföring - dödsfall", för att begära att
+              intyget även makuleras hos dem.
+            </li>
+          </ol>
+        </>
+      ) : (
+        <>
+          <p>För att makulera ett dödsorsaksintyg som är skickat på fel person måste du genomföra följande två steg:</p>
+          <ol>
+            <li>
+              Tryck på ”Makulera”, detta leder till att dödsorsaksintyget makuleras i Webcert men återkallar{' '}
+              <span className="iu-fw-bold">inte</span> dödsbeviset hos Skatteverket.
+            </li>
+            <li>
+              <span className="iu-fw-bold">Kontakta omedelbart Socialstyrelsen</span> (Dödsorsaksproduktionen) på telefon{' '}
+              <span className="iu-fw-bold">075-247 39 00 </span>för att begära att intyget även makuleras hos dem.
+            </li>
+          </ol>
+        </>
+      )}
+    </>
+  )
+}
