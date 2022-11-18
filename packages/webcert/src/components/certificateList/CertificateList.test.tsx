f16962f2dbf8aaf9c7564e40d262d00c3f6a4019
Helena Edblad <helena.edblad@soprasteria.com>
2022-11-18 07:43:55 +0100

    INTYGFV-14903: Add tests

--- packages/webcert/src/components/certificateList/CertificateList.test.tsx
+++ packages/webcert/src/components/certificateList/CertificateList.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import { configureStore, EnhancedStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import React from 'react'
-import { CertificateType, ResourceLink, User } from '@frontend/common'
+import { CertificateType, fakePatient, fakeResourceLink, ResourceLink, ResourceLinkType, User } from '@frontend/common'
 import reducer from '../../store/reducers'
 import dispatchHelperMiddleware, { clearDispatchedActions } from '../../store/test/dispatchHelperMiddleware'
 import CertificateList from './CertificateList'
@@ -12,7 +12,8 @@ import userEvent from '@testing-library/user-event'
 import { Router } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import { updateCreatedCertificateId } from '../../store/certificate/certificateActions'
-import { updateCertificateTypes } from '../../store/patient/patientActions'
+import { setPatient, updateCertificateTypes } from '../../store/patient/patientActions'
+import { patientMiddleware } from '../../store/patient/patientMiddleware'
 
 const createType = ({
   description = '',
@@ -61,12 +62,27 @@ describe('CertificateList', () => {
   beforeEach(() => {
     testStore = configureStore({
       reducer,
-      middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(dispatchHelperMiddleware, userMiddleware),
+      middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(dispatchHelperMiddleware, userMiddleware, patientMiddleware),
     })
 
     types = [
-      createType({ id: 'typ1', label: 'Typ 1', message: 'Meddelande' }),
-      createType({ id: 'typ2', label: 'Typ 2' }),
+      createType({
+        id: 'typ1',
+        label: 'Typ 1',
+        message: 'Meddelande',
+        links: [
+          fakeResourceLink({ type: ResourceLinkType.CREATE_CERTIFICATE, name: 'Skapa intyg' }),
+          fakeResourceLink({ type: ResourceLinkType.MISSING_RELATED_CERTIFICATE_CONFIRMATION }),
+        ],
+      }),
+      createType({
+        id: 'typ2',
+        label: 'Typ 2',
+        links: [
+          fakeResourceLink({ type: ResourceLinkType.CREATE_CERTIFICATE, name: 'Skapa intyg' }),
+          fakeResourceLink({ type: ResourceLinkType.CREATE_DODSBEVIS_CONFIRMATION, name: 'Dödsbevis saknas' }),
+        ],
+      }),
       createType({ id: 'typ3', label: 'Typ 3' }),
       createType({ id: 'typ4', label: 'Typ 4' }),
       createType({ id: 'typ5', label: 'Typ 5' }),
@@ -157,5 +173,31 @@ describe('CertificateList', () => {
     renderComponent()
 
     expect(screen.getByText('Meddelande')).toBeInTheDocument()
+  })
+
+  it('should show confirm modal when CREATE_DODSBEVIS_CONFIRMATION resource link exists', () => {
+    renderComponent()
+
+    const button = screen.getAllByRole('button', {
+      name: /Skapa intyg/,
+    }) as HTMLElement[]
+    userEvent.click(button[1])
+
+    testStore.dispatch(setPatient(fakePatient()))
+
+    expect(screen.getByText('Du är på väg att utfärda ett dödsbevis för', { exact: false })).toBeInTheDocument()
+  })
+
+  it('should show modal when MISSING_RELATED_CERTIFICATE_CONFIRMATION resource link exists', () => {
+    renderComponent()
+
+    const button = screen.getAllByRole('button', {
+      name: /Skapa intyg/,
+    }) as HTMLElement[]
+    userEvent.click(button[0])
+
+    testStore.dispatch(setPatient(fakePatient()))
+    screen.debug()
+    expect(screen.getByRole('dialog')).toBeInTheDocument()
   })
 })
