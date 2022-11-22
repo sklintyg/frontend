bd0ed2308b5a9813654e9c42a3812ac7df00f501
Erik Sylwan <erik.sylwan@soprasteria.com>
2022-11-22 13:25:43 +0100

    added space between paragraph and bullet list

--- packages/webcert/src/components/notification/ProtectedPatientStatus.tsx
+++ packages/webcert/src/components/notification/ProtectedPatientStatus.tsx
@@ -17,6 +17,8 @@ const ProtectedPatientStatus: React.FC<Props> = ({ isProtectedPatient }) => {
       <p>
         Att en patient har skyddade personuppgifter betyder att Skatteverket har bedömt att patientens personuppgifter är extra viktiga att
         skydda. Det finns speciella riktlinjer för hur personuppgifter för de invånarna ska hanteras. I Webcert innebär det att:
+      </p>
+      <p>
         <ul>
           <li>
             Du som användare av Webcert ska behandla personuppgifterna med försiktighet. Samtliga personuppgifter rörande patienten är
