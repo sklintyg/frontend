56a8788cdca09cb9a2006d685c4123a4b9e22acc
Helena Edblad <helena.edblad@soprasteria.com>
2022-12-06 17:47:38 +0100

    INTYGFV-14970: Refactor Dropdown

--- packages/common/src/components/Inputs/Dropdown.tsx
+++ packages/common/src/components/Inputs/Dropdown.tsx
@@ -2,6 +2,14 @@ import React, { ChangeEvent } from 'react'
 import classNames from 'classnames'
 import styled from 'styled-components'
 
+const DropdownDiv = styled.div`
+  min-height: 2.956rem !important;
+`
+
+const DropdownSelect = styled.select`
+  height: 2.956rem !important;
+`
+
 interface Props {
   label?: string
   name?: string
@@ -13,30 +21,22 @@ interface Props {
   id: string
 }
 
-const StyledDiv = styled.div`
-  min-height: 2.956rem !important;
-`
-
-const StyledSelect = styled.select`
-  height: 2.956rem !important;
-`
-
 const Dropdown: React.FC<Props> = (props) => {
   const { onChange, label, name, id, value, hasValidationError, options, disabled } = props
 
   return (
     <>
       {label !== 'undefined' ? <label htmlFor={id}>{label}</label> : null}
-      <StyledDiv
+      <DropdownDiv
         className={classNames('ic-forms__select', {
           'iu-border-error dropdown': hasValidationError,
           dropdown: !hasValidationError,
           'ic-forms__select--disabled': disabled,
         })}>
-        <StyledSelect value={value} name={name} id={id} disabled={disabled} onChange={onChange}>
+        <DropdownSelect value={value} name={name} id={id} disabled={disabled} onChange={onChange}>
           {options}
-        </StyledSelect>
-      </StyledDiv>
+        </DropdownSelect>
+      </DropdownDiv>
     </>
   )
 }
