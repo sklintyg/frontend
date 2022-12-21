f079f4d389265c0e50fcc7661eaa8cbc34d28f1b
Ulrika <79509664+UlrikaSopra@users.noreply.github.com>
2022-12-21 13:47:46 +0100

    INTYGFV-14948: Fixed fonts and positions in certificate (#599)

--- packages/webcert/src/feature/certificate/Question/QuestionHeaderAccordion.tsx
+++ packages/webcert/src/feature/certificate/Question/QuestionHeaderAccordion.tsx
@@ -18,7 +18,7 @@ const QuestionHeaderAccordion: React.FC<QuestionHeaderAccordionProps> = ({ confi
         <AccordionHeader>
           {config.icon && <Icon iconType={config.icon} includeTooltip={true} />}
           {displayMandatory && <MandatoryIcon additionalStyles={mandatoryIconAdditionalStyles} display={true} />}
-          <h4 className={'iu-fs-300'}>{config.text}</h4>
+          {config.header ? <h5 className={'iu-fs-200 iu-lh-h4'}>{config.text}</h5> : <h4 className={'iu-fs-300'}>{config.text}</h4>}
         </AccordionHeader>
         <Text className={'iu-mb-300'} dangerouslySetInnerHTML={sanitizeText(config.description)}></Text>
       </Accordion>
