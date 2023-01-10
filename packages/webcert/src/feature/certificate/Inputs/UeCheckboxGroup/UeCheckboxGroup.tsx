331d201876765077eb8fdafcd854ecb2ae011e3e
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-01-10 15:39:33 +0100

    Move Ue components, tests and stories to correct folders

---
+++ packages/webcert/src/feature/certificate/Inputs/UeCheckboxGroup/UeCheckboxGroup.tsx
@@ -0,0 +1,49 @@
+import { CertificateDataElement, ConfigLayout, ConfigUeCheckboxMultipleCodes, QuestionValidationTexts } from '@frontend/common'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { getQuestionHasValidationError, getShowValidationErrors } from '../../../../store/certificate/certificateSelectors'
+import { GroupWrapper, ItemWrapper } from '../GroupWrappers'
+import UeCheckbox from '../UeCheckbox/UeCheckbox'
+
+export interface Props {
+  disabled: boolean
+  question: CertificateDataElement
+}
+
+const UeCheckboxGroup: React.FC<Props> = ({ question, disabled }) => {
+  const config = question.config as ConfigUeCheckboxMultipleCodes
+  const checkboxes = config.list
+  const isShowValidationError = useSelector(getShowValidationErrors)
+  const shouldDisplayValidationError = useSelector(getQuestionHasValidationError(question.id))
+
+  const noItems = checkboxes.length
+
+  function shouldHaveItemPadding(index: number) {
+    return (config.layout === ConfigLayout.ROWS || config.layout === ConfigLayout.COLUMN) && index < checkboxes.length - 1
+  }
+
+  return (
+    checkboxes && (
+      <div className="checkbox-group-wrapper">
+        <GroupWrapper layout={config.layout}>
+          {checkboxes.map((checkbox, index) => (
+            <ItemWrapper key={index} layout={config.layout} index={index} noItems={noItems}>
+              <UeCheckbox
+                id={checkbox.id}
+                label={checkbox.label}
+                disabled={disabled || checkbox.disabled}
+                hasValidationError={shouldDisplayValidationError}
+                question={question}
+                wrapperAdditionalStyles={shouldHaveItemPadding(index) ? 'iu-pb-400' : ''}
+              />
+            </ItemWrapper>
+          ))}
+
+          {isShowValidationError && <QuestionValidationTexts validationErrors={question.validationErrors}></QuestionValidationTexts>}
+        </GroupWrapper>
+      </div>
+    )
+  )
+}
+
+export default UeCheckboxGroup
