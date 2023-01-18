a00dfdd2bfd02db69a1664c76e6ece35a77bd799
Erik Sylwan <erik.sylwan@soprasteria.com>
2023-01-18 09:21:58 +0100

    added support for multiple links

--- packages/webcert/src/feature/certificate/CertificateSidePanel/AboutCertificatePanel.tsx
+++ packages/webcert/src/feature/certificate/CertificateSidePanel/AboutCertificatePanel.tsx
@@ -67,9 +67,7 @@ const AboutCertificatePanel: React.FC<Props> = ({ headerHeight }) => {
     if (!text) {
       return ''
     }
-    console.log(text)
     const splitText = text.split('<LINK:')
-    console.log(splitText.length)
     if (splitText.length > 1) {
       let returnValue = (
         <>
@@ -78,13 +76,8 @@ const AboutCertificatePanel: React.FC<Props> = ({ headerHeight }) => {
       )
 
       for (let i = 0; i < splitText.length - 1; i++) {
-        console.log(i)
-        console.log(`splitText i: ${splitText[i]}`)
-        console.log(`splitText i+1: ${splitText[i + 1]}`)
         const dynamicLinkKey = splitText[i + 1].split('>')[0]
-        console.log(`dynamicLinkKey: ${dynamicLinkKey}`)
         const textAfterLink = splitText[i + 1].substring(splitText[i + 1].indexOf('>') + 1)
-        console.log(`textAfterLink: ${textAfterLink}`)
         returnValue = (
           <>
             {returnValue}
