274725f8e2f5534efe5fbd994c53f6d8111de438
Ulrika <ulrika.sjoberg@soprasteria.com>
2023-01-23 13:36:59 +0100

    INTYGFV-15189: fixed bug in signed certificate view

--- packages/common/src/components/UnifiedView/UvMedicalInvestigationList/UvMedicalInvestigationList.tsx
+++ packages/common/src/components/UnifiedView/UvMedicalInvestigationList/UvMedicalInvestigationList.tsx
@@ -5,33 +5,46 @@ import { Table, TableHeader, TableRow, TableCell } from '../../Table'
 export const UvMedicalInvestigationList: React.FC<{
   value: ValueMedicalInvestigationList
   config: ConfigUeMedicalInvestigationList
-}> = ({ value, config }) => (
-  <div className={'iu-p-none'}>
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableCell>{config.typeText}</TableCell>
-          <TableCell>{config.dateText}</TableCell>
-          <TableCell>{config.informationSourceText}</TableCell>
-        </TableRow>
-      </TableHeader>
-      <tbody>
-        {config.list.map((medicalConfig) => {
-          const medicalValue = value.list.find((item) => item.investigationType.id === medicalConfig.investigationTypeId)
-          const codeValue = medicalConfig.typeOptions.find((value) => value.code === medicalValue?.investigationType.code)
-          return (
-            codeValue &&
-            medicalValue &&
-            medicalValue.informationSource.text && (
-              <TableRow key={medicalValue.investigationType.id}>
-                <TableCell style={{ minWidth: '8rem' }}>{codeValue.label}</TableCell>
-                <TableCell style={{ minWidth: '8rem' }}>{medicalValue.date.date}</TableCell>
-                <TableCell>{medicalValue.informationSource.text}</TableCell>
-              </TableRow>
-            )
-          )
-        })}
-      </tbody>
-    </Table>
-  </div>
-)
+}> = ({ value, config }) => {
+  let hasCells = false
+  return (
+    <div className={'iu-p-none'}>
+      {config.list.map((medicalConfig) => {
+        const medicalValue = value.list.find((item) => item.investigationType.id === medicalConfig.investigationTypeId)
+        const codeValue = medicalConfig.typeOptions.find((value) => value.code === medicalValue?.investigationType.code)
+        if (codeValue && medicalValue && medicalValue.informationSource.text) {
+          hasCells = true
+        }
+      })}
+
+      {hasCells && (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableCell>{config.typeText}</TableCell>
+              <TableCell>{config.dateText}</TableCell>
+              <TableCell>{config.informationSourceText}</TableCell>
+            </TableRow>
+          </TableHeader>
+          <tbody>
+            {config.list.map((medicalConfig) => {
+              const medicalValue = value.list.find((item) => item.investigationType.id === medicalConfig.investigationTypeId)
+              const codeValue = medicalConfig.typeOptions.find((value) => value.code === medicalValue?.investigationType.code)
+              return (
+                codeValue &&
+                medicalValue &&
+                medicalValue.informationSource.text && (
+                  <TableRow key={medicalValue.investigationType.id}>
+                    {codeValue.label && <TableCell style={{ minWidth: '8rem' }}>{codeValue.label}</TableCell>}
+                    {medicalValue.date.date && <TableCell style={{ minWidth: '8rem' }}>{medicalValue.date.date}</TableCell>}
+                    {medicalValue.informationSource.text && <TableCell>{medicalValue.informationSource.text}</TableCell>}
+                  </TableRow>
+                )
+              )
+            })}
+          </tbody>
+        </Table>
+      )}
+    </div>
+  )
+}
