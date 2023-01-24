c558f60429bbf4a694ee63b95f2e2a8a67dff2ed
Erik Sylwan <erik.sylwan@soprasteria.com>
2023-01-24 08:41:47 +0100

    read only component for UeYear

--- packages/common/src/components/UnifiedView/UvYear/UvYear.tsx
+++ packages/common/src/components/UnifiedView/UvYear/UvYear.tsx
@@ -2,4 +2,6 @@ import React from 'react'
 import { Value } from '../../../types/certificate'
 import { Badge } from '../Badge'
 
-export const UvYear: React.FC<{ value: Value }> = ({ value }) => <Badge>{value.year ? value.year.toString() : 'Ej angivet'}</Badge>
+export const UvYear: React.FC<{ value: Value }> = ({ value }) => (
+  <Badge>{typeof value.year === 'number' && value.year ? value.year.toString() : 'Ej angivet'}</Badge>
+)
