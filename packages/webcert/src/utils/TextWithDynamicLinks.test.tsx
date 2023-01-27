3285a9d04ec0119e2120565ab065d3825382e1be
Erik Sylwan <erik.sylwan@soprasteria.com>
2023-01-27 14:01:27 +0100

    added tests that dynamic link is rendered

--- packages/webcert/src/utils/TextWithDynamicLinks.test.tsx
+++ packages/webcert/src/utils/TextWithDynamicLinks.test.tsx
@@ -7,7 +7,9 @@ import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom'
 import { certificateMiddleware } from '../store/certificate/certificateMiddleware'
 import reducer from '../store/reducers'
-import dispatchHelperMiddleware from '../store/test/dispatchHelperMiddleware'
+import dispatchHelperMiddleware, { clearDispatchedActions } from '../store/test/dispatchHelperMiddleware'
+import { updateDynamicLinks } from '../store/utils/utilsActions'
+import { utilsMiddleware } from '../store/utils/utilsMiddleware'
 import TextWithDynamicLinks from './TextWithDynamicLinks'
 let testStore: EnhancedStore
 
@@ -16,8 +18,8 @@ const history = createMemoryHistory()
 const text1 = faker.lorem.sentence(10)
 const text2 = faker.lorem.sentence(10)
 const text3 = faker.lorem.sentence(10)
-const link1 = faker.lorem.word()
-const link2 = faker.lorem.word()
+const link1 = 'link1'
+const link2 = 'link2'
 
 const textWithLinks = `${text1} <LINK:${link1}> ${text2} <LINK:${link2}> ${text3}`
 
@@ -31,19 +33,34 @@ const renderComponent = () => {
   )
 }
 describe('TextWithDynamicLinks', () => {
-  testStore = configureStore({
-    reducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(dispatchHelperMiddleware, certificateMiddleware),
+  beforeEach(() => {
+    testStore = configureStore({
+      reducer,
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().prepend(dispatchHelperMiddleware, certificateMiddleware, utilsMiddleware),
+    })
+
+    testStore.dispatch(
+      updateDynamicLinks({
+        link1: { key: 'link1', url: 'https://www.inera.se', text: 'Inera', target: '_self', tooltip: 'inera' },
+        link2: { key: 'link2', url: 'https://www.soprasteria.se', text: 'Sopra Steria', target: '_self', tooltip: 'sopra' },
+      })
+    )
+  })
+
+  afterEach(() => {
+    clearDispatchedActions()
   })
 
   it('renders without crashing', () => {
     renderComponent()
   })
-
   it('renders with description', () => {
     renderComponent()
     expect(screen.getByText(text1)).toBeInTheDocument()
     expect(screen.getByText(text2)).toBeInTheDocument()
     expect(screen.getByText(text3)).toBeInTheDocument()
+    expect(screen.getByText('Inera')).toBeInTheDocument()
+    expect(screen.getByText('Sopra Steria')).toBeInTheDocument()
   })
 })
