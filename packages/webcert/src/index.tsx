b279b6269a1ec009ce07b47b64de6485ccf480b7
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-02-08 14:09:25 +0100

    WIP

--- packages/webcert/src/index.tsx
+++ packages/webcert/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
+import App from './App'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
 import store from './store/store'
 
 ReactDOM.render(
--- packages/webcert/vite.config.ts
+++ packages/webcert/vite.config.ts
@@ -1,6 +1,23 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, ProxyOptions } from 'vite'
+
+const proxy = ['/fake', '/api', '/moduleapi', '/testability', '/visa', '/saml', '/error.jsp'].reduce<Record<string, string | ProxyOptions>>(
+  (result, route) => ({
+    ...result,
+    [route]: {
+      target: 'https://wc2.webcert-devtest.intyg.nordicmedtest.se',
+      cookieDomainRewrite: { '*': '' },
+      protocolRewrite: 'http',
+      changeOrigin: true,
+      autoRewrite: true,
+    },
+  }),
+  {}
+)
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy,
+  },
 })
