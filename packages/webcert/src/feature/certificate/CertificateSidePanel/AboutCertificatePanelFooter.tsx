5a3a527dbf65a744ee9272cf11fd26ef431b2b04
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-03-01 15:18:10 +0100

    INTYGFV-15389: Removed links to Intygsskolan

--- packages/webcert/src/feature/certificate/CertificateSidePanel/AboutCertificatePanelFooter.tsx
+++ packages/webcert/src/feature/certificate/CertificateSidePanel/AboutCertificatePanelFooter.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
-import WCDynamicLink from '../../../utils/WCDynamicLink'
-import SidePanelFooter from './Footer/SidePanelFooter'
 
 const AboutCertificatePanelFooter: React.FC = () => {
-  return (
-    <SidePanelFooter additionalStyles="iu-m-none">
-      <WCDynamicLink linkKey="ineraIntygsskola" />
-    </SidePanelFooter>
-  )
+  return <></>
 }
 
 export default AboutCertificatePanelFooter
