cd7a68b0cc03d11dca6f622d7a51d2042472860f
Ulrika <ulrika.sjoberg@soprasteria.com>
2023-03-03 14:25:11 +0100

    .

--- packages/common/src/components/utils/Accordion.tsx
+++ packages/common/src/components/utils/Accordion.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const Accordion = styled.details.attrs({
-  className: 'ic-card ic-card--expandable ic-inner ic-card--sm-unset-style ic-expandable ic-card--inspiration-large iu-bg-white',
+  className: 'ic-card ic-card--expandable ic-card--sm-unset-style ic-expandable ic-card--inspiration-large iu-bg-white',
 })`
   padding: 0 !important;
   overflow: visible;
