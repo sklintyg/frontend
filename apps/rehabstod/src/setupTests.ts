743ceabb4f63b1da7fa97c0d95f8f58586a8a9ad
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-03-06 10:07:29 +0100

    rehabstod skeleton

---
+++ apps/rehabstod/src/setupTests.ts
@@ -0,0 +1,17 @@
+import matchers from '@testing-library/jest-dom/matchers'
+import { cleanup } from '@testing-library/react'
+
+// extends Vitest's expect method with methods from react-testing-library
+expect.extend(matchers)
+
+// Add modalRool container for ModalBase containers
+beforeEach(() => {
+  document.body.innerHTML = `
+        <div id="modalRoot"></div>
+    `
+})
+
+// runs a cleanup after each test case (e.g. clearing jsdom)
+afterEach(() => {
+  cleanup()
+})
