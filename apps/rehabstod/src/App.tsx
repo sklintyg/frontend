398f168e02566081f1d4aa0d26b969196ec63799
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-03-06 11:32:23 +0100

    install inera react components and fonts

--- apps/rehabstod/src/App.tsx
+++ apps/rehabstod/src/App.tsx
@@ -1,31 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
+import { IDSContainer, IDSFooter, IDSHeader } from '@inera/ids-react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((num) => num + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+    <>
+      <IDSHeader type="inera-admin" className="ids-hide ids-show-inera-admin" brandText="Rehabstöd" />
+      <main>
+        <IDSContainer>
+          <p className="ids-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </IDSContainer>
+      </main>
+      <IDSFooter type="inera-admin" headline="Rehabstöd" />
+    </>
   )
 }
 
