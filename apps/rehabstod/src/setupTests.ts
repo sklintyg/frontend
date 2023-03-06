90088afd2f2dc542734484772d1ae3c0c5c6f338
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-03-06 20:34:48 +0100

    cleanup

--- apps/rehabstod/src/setupTests.ts
+++ apps/rehabstod/src/setupTests.ts
@@ -4,13 +4,6 @@ import { cleanup } from '@testing-library/react'
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers)
 
-// Add modalRool container for ModalBase containers
-beforeEach(() => {
-  document.body.innerHTML = `
-        <div id="modalRoot"></div>
-    `
-})
-
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup()
--- apps/rehabstod/vite.config.ts
+++ apps/rehabstod/vite.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-default-export */
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
