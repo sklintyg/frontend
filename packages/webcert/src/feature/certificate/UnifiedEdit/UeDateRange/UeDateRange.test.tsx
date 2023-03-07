133de7d52b9e0973eae5f6239ad3cb8abcd836ae
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-03-07 10:24:10 +0100

    remove range validation test from UeDateRange

--- packages/webcert/src/feature/certificate/UnifiedEdit/UeDateRange/UeDateRange.test.tsx
+++ packages/webcert/src/feature/certificate/UnifiedEdit/UeDateRange/UeDateRange.test.tsx
@@ -1,6 +1,5 @@
 import { fakeCertificate, fakeDateRangeElement, ValidationError } from '@frontend/common'
-import { act, render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
 import { ComponentProps } from 'react'
 import { Provider } from 'react-redux'
 import { showValidationErrors, updateCertificate } from '../../../../store/certificate/certificateActions'
@@ -46,24 +45,6 @@ describe('Date range picker', () => {
   })
 
   describe('Validation error', () => {
-    it('shows date range error when end date is before start date', async () => {
-      const question = getQuestion([])
-      renderDefaultComponent({ disabled: false, question })
-      store.dispatch(showValidationErrors())
-
-      await act(async () => {
-        userEvent.type(screen.getByLabelText('Fr.o.m'), '20210202')
-        userEvent.type(screen.getByLabelText('t.o.m'), '20210102')
-        userEvent.click(screen.getByText('t.o.m'))
-      })
-
-      await act(async () => {
-        userEvent.type(screen.getByLabelText('t.o.m'), '20210302')
-        userEvent.click(screen.getByText('t.o.m'))
-      })
-      expect(screen.queryByText('Ange ett slutdatum som infaller efter startdatumet.')).not.toBeInTheDocument()
-    })
-
     it('should show validation error of type id.field', () => {
       const validationErrors = getValidationErrors('jsonid.tom')
       const question = getQuestion(validationErrors)
