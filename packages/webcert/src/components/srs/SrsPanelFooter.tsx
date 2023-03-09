34e74f1a63c36ecfd25547bf20cf1fd9df95c551
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-03-09 16:32:59 +0100

    INTYGFV-15397: Added external link icon

--- packages/webcert/src/components/srs/SrsPanelFooter.tsx
+++ packages/webcert/src/components/srs/SrsPanelFooter.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SidePanelFooter from '../../feature/certificate/CertificateSidePanel/Footer/SidePanelFooter'
 import { getDiagnosisCode, getDiagnosisDescription } from '../../store/srs/srsSelectors'
 import { useSelector } from 'react-redux'
+import { ExternalLinkIcon } from '@frontend/common'
 
 const FMBPanelFooter: React.FC = () => {
   const diagnosisDescription = useSelector(getDiagnosisDescription)
@@ -13,6 +14,7 @@ const FMBPanelFooter: React.FC = () => {
       <div className="iu-flex-column">
         <p className="iu-fw-bold">Mer information</p>
         <a href={link}>Information om {diagnosisDescription} hos Rätt Sjukskrivning</a>
+        <ExternalLinkIcon className="iu-ml-200 iu-fs-100" />
       </div>
     </SidePanelFooter>
   )
