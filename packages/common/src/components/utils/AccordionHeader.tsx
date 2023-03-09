44de3ebd0cac52878248285395ac7e351affc6db
Ulrika <ulrika.sjoberg@soprasteria.com>
2023-03-09 14:44:34 +0100

    Change padding on accordian header

--- packages/common/src/components/utils/AccordionHeader.tsx
+++ packages/common/src/components/utils/AccordionHeader.tsx
@@ -2,12 +2,13 @@ import styled from 'styled-components'
 
 const AccordionHeader = styled.summary.attrs({
   tabIndex: 0,
-  className: 'ic-expandable-button  ic-inner iu-focus ic-expandable-button--chevron iu-fs-400 iu-mb-300',
+  className: 'ic-expandable-button iu-focus ic-expandable-button--chevron iu-fs-400 iu-mb-300',
 })`
   justify-content: flex-start;
   align-items: flex-start;
   padding-left: 0 !important;
   margin-bottom: 0.625rem !important;
+  padding-right: 20px !important;
 
   & ::after {
     top: 20px !important;
