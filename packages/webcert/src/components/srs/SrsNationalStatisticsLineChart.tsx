43f873fbd4aa803bcdbd6ea34d071067ec8ecf22
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-03-13 10:56:22 +0100

    INTYGFV-15398: Refactored components

--- packages/webcert/src/components/srs/SrsNationalStatisticsLineChart.tsx
+++ packages/webcert/src/components/srs/SrsNationalStatisticsLineChart.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { getSrsInfo } from '../../store/srs/srsSelectors'
-import { LineChart, Line, XAxis, CartesianGrid, YAxis, Tooltip, TooltipProps } from 'recharts'
+import { LineChart, Line, XAxis, CartesianGrid, YAxis, Tooltip, TooltipProps, LabelList, LabelProps } from 'recharts'
 
 const CustomTooltip = ({ active, payload, label }: TooltipProps<number, string>) => {
   if (active && payload && payload.length) {
@@ -17,20 +17,29 @@ const CustomTooltip = ({ active, payload, label }: TooltipProps<number, string>)
   return null
 }
 
+const CustomizedLabel = (props: LabelProps) => {
+  const { x, y, stroke, value } = props
+
+  return (
+    <text x={x} y={y} dy={-8} dx={-8} fill={stroke} fontSize={12} textAnchor="middle">
+      {value}%
+    </text>
+  )
+}
+
 const SrsNationalStatistics: React.FC = () => {
   const srsInfo = useSelector(getSrsInfo)
   const statistics = srsInfo && srsInfo.statistikNationellStatistik.length > 0 ? srsInfo.statistikNationellStatistik : []
   const xLabels = [30, 90, 180, 365]
   const statisticsTotalNumber = statistics[statistics.length - 1]
-
-  const data = xLabels.map((xLabel, index) => {
-    return { name: xLabel, value: Math.round((statistics[index] / statisticsTotalNumber) * 100) }
-  })
+  const data = xLabels.map((xLabel, index) => ({ name: xLabel, value: Math.round((statistics[index] / statisticsTotalNumber) * 100) }))
 
   return (
     <LineChart width={500} height={300} data={data}>
       <CartesianGrid stroke="#ccc" vertical={false} />
-      <Line type="monotone" dataKey="value" stroke="#00706e" strokeWidth={3} />
+      <Line type="monotone" dataKey="value" stroke="#00706e" strokeWidth={3} isAnimationActive={false}>
+        <LabelList content={<CustomizedLabel />} />
+      </Line>
       <XAxis
         type="number"
         dataKey="name"
