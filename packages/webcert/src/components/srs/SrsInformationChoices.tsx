8bed1ac9cf0ceb0ff2d64dbb7395b4046c792ffa
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-03-13 10:23:23 +0100

    INTYGFV-15398: Refactored components

--- packages/webcert/src/components/srs/SrsInformationChoices.tsx
+++ packages/webcert/src/components/srs/SrsInformationChoices.tsx
@@ -21,25 +21,18 @@ interface Props {
   currentChoice: SrsInformationChoice
 }
 
-const SrsInformationChoices: React.FC<Props> = ({ onChange, currentChoice }) => {
-  const handleOnChange = (choice: SrsInformationChoice) => {
-    onChange(choice)
-  }
+const SrsInformationButton: React.FC<Props & {
+  text: string
+  choice: SrsInformationChoice
+}> = ({ text, choice, currentChoice, onChange }) => (
+  <StyledButton rounded text={text} onClick={() => onChange(choice)} buttonStyle={currentChoice === choice ? 'primary' : 'secondary'} />
+)
 
-  const getStyling = (choice: SrsInformationChoice) => {
-    return currentChoice === choice ? 'primary' : 'secondary'
-  }
-
-  const getButton = (choice: SrsInformationChoice, text: string) => {
-    return <StyledButton rounded text={text} onClick={() => handleOnChange(choice)} buttonStyle={getStyling(choice)} />
-  }
-
-  return (
-    <Root className="iu-mb-300">
-      {getButton(SrsInformationChoice.RECOMMENDATIONS, SRS_RECOMMENDATIONS_BUTTON_TEXT)}
-      {getButton(SrsInformationChoice.STATISTICS, SRS_STATISTICS_BUTTON_TEXT)}
-    </Root>
-  )
-}
+const SrsInformationChoices: React.FC<Props> = (props) => (
+  <Root className="iu-mb-300">
+    <SrsInformationButton text={SRS_RECOMMENDATIONS_BUTTON_TEXT} choice={SrsInformationChoice.RECOMMENDATIONS} {...props} />
+    <SrsInformationButton text={SRS_STATISTICS_BUTTON_TEXT} choice={SrsInformationChoice.STATISTICS} {...props} />
+  </Root>
+)
 
 export default SrsInformationChoices
