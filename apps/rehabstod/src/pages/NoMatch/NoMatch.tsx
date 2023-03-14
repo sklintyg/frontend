b48f355e817c64e1af28f824269319443dfea53c
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-03-14 09:51:48 +0100

    INTYGFV-15329: Get links from API (#770)

--- apps/rehabstod/src/pages/NoMatch/NoMatch.tsx
+++ apps/rehabstod/src/pages/NoMatch/NoMatch.tsx
@@ -6,21 +6,12 @@ export function NoMatch() {
   return (
     <PageHero type="error" icon="attention">
       <h1 className="ids-heading-1">Den här sidan hittades inte</h1>
-      <p className="ids-preamble">
-        Sidan du söker har fått en ny adress eller är borttagen. Inera arbetar löpande med att förbättra struktur och innehåll på
-        webbplatsen.
-      </p>
+      <p className="ids-preamble">Sidan du söker har fått en ny adress eller är borttagen.</p>
       <div className="bg-neutral-40 mb-5 block h-px w-12 md:hidden" />
-      <div className="grid grid-cols-1 gap-2 text-left md:inline-grid md:grid-cols-2 md:gap-8">
+      <div className="text-center">
         <IDSLink>
           <IDSIcon name="chevron" />
           <Link to="/">Till startsidan</Link>
-        </IDSLink>
-        <IDSLink>
-          <IDSIcon name="chevron" />
-          <a href="//www.inera.se/om-inera/" target="_blank" rel="noreferrer">
-            Om Inera.se
-          </a>
         </IDSLink>
       </div>
     </PageHero>
