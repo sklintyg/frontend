8683e02ca0922332826ec5f06068247f25500283
Ulrika <ulrika.sjoberg@soprasteria.com>
2023-03-14 11:45:53 +0100

    INTYGFV-15413: Update the value when the component goes from disabled to enabled

--- packages/common/src/components/Inputs/Dropdown.tsx
+++ packages/common/src/components/Inputs/Dropdown.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useEffect } from 'react'
 import styled from 'styled-components'
 
 const DropdownDiv = styled.div`
@@ -19,21 +19,32 @@ interface Props extends React.SelectHTMLAttributes<HTMLSelectElement> {
   label?: string
 }
 
-const Dropdown = forwardRef<HTMLSelectElement, Props>(({ children, id, error, disabled, title, label, value, ...props }, ref) => (
-  <>
-    {label !== null ? <label htmlFor={id}>{label}</label> : null}
-    <DropdownDiv
-      title={title}
-      className={classNames('ic-forms__select', {
-        'iu-border-error dropdown': error,
-        dropdown: !error,
-        'ic-forms__select--disabled': disabled,
-      })}>
-      <DropdownSelect ref={ref} id={id} disabled={disabled} value={value ?? ''} {...props}>
-        {children}
-      </DropdownSelect>
-    </DropdownDiv>
-  </>
-))
+const Dropdown = forwardRef<HTMLSelectElement, Props>(({ children, id, error, disabled, title, label, value, ...props }) => {
+  const selectRef = React.useRef<HTMLSelectElement>(null)
+
+  useEffect(() => {
+    if (!disabled && selectRef.current && selectRef.current.value !== '') {
+      const event = new Event('change', { bubbles: true })
+      selectRef.current.dispatchEvent(event)
+    }
+  }, [disabled])
+
+  return (
+    <>
+      {label !== null ? <label htmlFor={id}>{label}</label> : null}
+      <DropdownDiv
+        title={title}
+        className={classNames('ic-forms__select', {
+          'iu-border-error dropdown': error,
+          dropdown: !error,
+          'ic-forms__select--disabled': disabled,
+        })}>
+        <DropdownSelect ref={selectRef} id={id} disabled={disabled} value={value ?? ''} {...props}>
+          {children}
+        </DropdownSelect>
+      </DropdownDiv>
+    </>
+  )
+})
 
 export default Dropdown
