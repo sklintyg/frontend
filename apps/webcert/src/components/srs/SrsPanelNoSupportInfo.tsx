7d2f2520d42204cde132cb064006d3767c59eca0
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-03-16 16:23:14 +0100

    fix error after merge

--- apps/webcert/src/components/srs/SrsPanelNoSupportInfo.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { InfoBox } from '@frontend/common'
-
-const SrsPanelNoSupportInfo: React.FC = () => {
-  return <InfoBox type="info">För den angivna diagnosen finns för tillfället inget stöd för sjukskrivning.</InfoBox>
-}
-
-export default SrsPanelNoSupportInfo
