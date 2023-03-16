058a641a4e0080b19b559f212ece27f65fd17761
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-03-16 13:58:24 +0100

    INTYGFV-15399: Fixed test

--- packages/webcert/src/components/srs/risk/SrsRiskOpinion.test.tsx
+++ packages/webcert/src/components/srs/risk/SrsRiskOpinion.test.tsx
@@ -3,6 +3,8 @@ import { Provider } from 'react-redux'
 import store from '../../../store/store'
 import SrsRiskOpinion from './SrsRiskOpinion'
 import { SRS_OPINION_LABELS } from '../srsUtils'
+import { updateSrsPredictions } from '../../../store/srs/srsActions'
+import { fakeSrsPrediction } from '@frontend/common'
 
 const renderComponent = () => {
   render(
@@ -18,12 +20,20 @@ describe('SrsRiskOpinion', () => {
   })
 
   it('should render four radio buttons', () => {
+    store.dispatch(updateSrsPredictions([fakeSrsPrediction(), fakeSrsPrediction()]))
     renderComponent()
     expect(screen.getAllByRole('radio')).toHaveLength(4)
   })
 
   it.each(SRS_OPINION_LABELS)('should render radio button label', (label) => {
+    store.dispatch(updateSrsPredictions([fakeSrsPrediction(), fakeSrsPrediction()]))
     renderComponent()
     expect(screen.getByText(label)).toBeInTheDocument()
+  })
+
+  it('should not render radio buttons if current risk is not calculated', () => {
+    store.dispatch(updateSrsPredictions([]))
+    renderComponent()
+    expect(screen.queryAllByRole('radio')).toHaveLength(0)
   })
 })
