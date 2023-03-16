fab92901f62c54809a8f96dd2072cbf7eb4a68e7
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-03-16 11:11:55 +0100

    INTYGFV-15399: Updated footer

--- packages/webcert/src/components/srs/panel/SrsPanelFooter.tsx
+++ packages/webcert/src/components/srs/panel/SrsPanelFooter.tsx
@@ -1,32 +1,22 @@
 import React from 'react'
 import SidePanelFooter from '../../../feature/certificate/CertificateSidePanel/Footer/SidePanelFooter'
 import { getDiagnosisCode, getDiagnosisDescription } from '../../../store/srs/srsSelectors'
-import { useDispatch, useSelector } from 'react-redux'
-import { ExternalLinkIcon, SrsEvent, SrsInformationChoice } from '@frontend/common'
-import { logSrsInteraction } from '../../../store/srs/srsActions'
+import { useSelector } from 'react-redux'
+import { ExternalLinkIcon, SrsInformationChoice } from '@frontend/common'
 
 interface Props {
   informationChoice: SrsInformationChoice
 }
 const SrsPanelFooter: React.FC<Props> = ({ informationChoice }) => {
-  const dispatch = useDispatch()
   const diagnosisDescription = useSelector(getDiagnosisDescription(informationChoice))
   const diagnosisCode = useSelector(getDiagnosisCode(informationChoice))
   const link = `https://skr.se/${diagnosisCode ? diagnosisCode.replace('.', '').toLowerCase() : ''}`
-
-  const onClick = () => {
-    if (informationChoice === SrsInformationChoice.STATISTICS) {
-      dispatch(logSrsInteraction(SrsEvent.SRS_STATISTICS_LINK_CLICKED))
-    } else {
-      dispatch(logSrsInteraction(SrsEvent.SRS_MEASURES_LINK_CLICKED))
-    }
-  }
 
   return (
     <SidePanelFooter additionalStyles="iu-m-none">
       <div className="iu-flex-column">
         <p className="iu-fw-bold">Mer information</p>
-        <a href={link} onClick={onClick}>
+        <a href={link} target="_blank">
           Information om {diagnosisDescription} hos Rätt Sjukskrivning
         </a>
         <ExternalLinkIcon className="iu-ml-200 iu-fs-100" />
