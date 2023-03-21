e54b1bad4e6f2898ffc135faf784716046bae8d8
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2023-03-21 14:47:03 +0100

    INTYGFV-15400: Added SRS logging (#782)

--- packages/webcert/src/components/srs/panel/SrsPanel.test.tsx
+++ packages/webcert/src/components/srs/panel/SrsPanel.test.tsx
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import SrsPanel, { SRS_TITLE } from './SrsPanel'
 import { Provider } from 'react-redux'
-import store from '../../../store/store'
-import { setDiagnosisCodes, updateError, updateSrsInfo } from '../../../store/srs/srsActions'
+import { logSrsInteraction, setDiagnosisCodes, updateError, updateSrsInfo } from '../../../store/srs/srsActions'
 import { updateCertificate } from '../../../store/certificate/certificateActions'
 import { fakeCertificate, fakeDiagnosesElement, fakeSrsInfo } from '@frontend/common'
 import { SRS_OBSERVE_TITLE, SRS_RECOMMENDATIONS_TITLE } from '../recommendations/SrsRecommendations'
@@ -10,6 +9,10 @@ import { SRS_RECOMMENDATIONS_BUTTON_TEXT, SRS_STATISTICS_BUTTON_TEXT } from '../
 import userEvent from '@testing-library/user-event'
 import { SRS_STATISTICS_TITLE } from '../statistics/SrsNationalStatistics'
 import { SICKLEAVE_CHOICES_TEXTS } from '../srsUtils'
+import dispatchHelperMiddleware, { clearDispatchedActions, dispatchedActions } from '../../../store/test/dispatchHelperMiddleware'
+import { configureApplicationStore } from '../../../store/configureApplicationStore'
+import { EnhancedStore } from '@reduxjs/toolkit'
+import { srsMiddleware } from '../../../store/srs/srsMiddleware'
 
 const renderComponent = () => {
   render(
@@ -19,7 +22,17 @@ const renderComponent = () => {
   )
 }
 
+let store: EnhancedStore
+
 describe('SrsPanel', () => {
+  beforeEach(() => {
+    store = configureApplicationStore([dispatchHelperMiddleware, srsMiddleware])
+  })
+
+  afterEach(() => {
+    clearDispatchedActions()
+  })
+
   it('should render without problems', () => {
     expect(() => renderComponent()).not.toThrow()
   })
@@ -173,6 +186,12 @@ describe('SrsPanel', () => {
       userEvent.click(button)
       expect(screen.queryByText(SRS_RECOMMENDATIONS_TITLE)).not.toBeInTheDocument()
       expect(screen.getByText(SRS_STATISTICS_TITLE)).toBeInTheDocument()
+    })
+
+    it('should log when pressing statistics button', () => {
+      renderComponent()
+      userEvent.click(screen.getByText(SRS_STATISTICS_BUTTON_TEXT))
+      expect(dispatchedActions.find((a) => a.type === logSrsInteraction.type)).not.toBeUndefined()
     })
   })
 })
