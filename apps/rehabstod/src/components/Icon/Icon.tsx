2f1c974bf60e2956359deca8bf14e35885b62b45
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2023-03-23 09:17:39 +0100

    Update apps/rehabstod/src/components/Icon/Icon.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/rehabstod/src/components/Icon/Icon.tsx
+++ apps/rehabstod/src/components/Icon/Icon.tsx
@@ -3,7 +3,7 @@ import { IDSIcon, IDSTooltip } from '@frontend/ids-react-ts'
 export function Icon({ description, type, size }: { description: string; type: string; size: 'xs' | 's' | 'm' | undefined }) {
   return (
     <IDSTooltip>
-      <IDSIcon slot="trigger" name={type} size={size} colorpreset={1}></IDSIcon>
+      <IDSIcon slot="trigger" name={type} size={size} colorpreset={1} />
       <div slot="tooltip">{description}</div>
     </IDSTooltip>
   )
