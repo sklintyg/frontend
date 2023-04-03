c80c56f166c0198aea0ae404f4c51f539251d5cd
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-04-03 16:09:50 +0200

    INTYGFV-15340: Merge

--- apps/rehabstod/src/pages/CurrentSickLeaves/components/Filters.tsx
+++ apps/rehabstod/src/pages/CurrentSickLeaves/components/Filters.tsx
@@ -3,30 +3,6 @@ import { useState } from 'react'
 
 export function Filters({ onSearch, onReset, isDoctor }: { onSearch: () => void; onReset: () => void; isDoctor: boolean }) {
   const [expanded, setExpanded] = useState(true)
-  const [filterRequest, setFilterRequest] = useState<ActiveSickLeavesRequest>(defaultFilterRequest)
-  const { data: doctors } = useGetDoctorsWithActiveSickLeavesQuery()
-
-  const filterDescriptions = [
-    'Filtrerar på den läkare som har utfärdat det aktiva intyget. Endast läkare som utfärdat aktiva intyg visas i listan.',
-    'Filtrerar på total längd för det sjukfall som det aktiva intyget ingår i.',
-  ]
-
-  const handleReset = () => {
-    onReset()
-    setFilterRequest(defaultFilterRequest)
-  }
-
-  const onFromTimeChange = (value: number) => {
-    setFilterRequest({ ...filterRequest, from: value })
-  }
-
-  const onToTimeChange = (value: number) => {
-    setFilterRequest({ ...filterRequest, to: value })
-  }
-
-  const onDoctorChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setFilterRequest({ ...filterRequest, doctor: event.currentTarget.value })
-  }
 
   return (
     <>
@@ -36,28 +12,12 @@ export function Filters({ onSearch, onReset, isDoctor }: { onSearch: () => void;
       </IDSButton>
       {expanded && (
         <div>
-          {isDoctor && (
-            <DoctorFilter
-              onChange={onDoctorChange}
-              doctors={doctors || []}
-              selected={filterRequest ? filterRequest.doctor : ''}
-              description={filterDescriptions[0]}
-            />
-          )}
-          <TimePeriodFilter
-            title="Välj sjukskrivningslängd"
-            onFromChange={onFromTimeChange}
-            onToChange={onToTimeChange}
-            to={filterRequest.to}
-            from={filterRequest.from}
-            description={filterDescriptions[1]}
-          />
           <div className="flex justify-end">
             <IDSButtonGroup className="my-4 flex" style={{ justifyContent: 'flex-end' }}>
-              <IDSButton secondary onClick={handleReset}>
+              <IDSButton secondary onClick={onReset}>
                 Återställ
               </IDSButton>
-              <IDSButton onClick={() => onSearch(filterRequest)}>Sök</IDSButton>
+              <IDSButton onClick={onSearch}>Sök</IDSButton>
             </IDSButtonGroup>
           </div>
           <hr className="mb-10 opacity-40" />
