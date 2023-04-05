b641e40571ee8f0c729bd15fb0646a0a049bfdb3
Ulrika <ulrika.sjoberg@soprasteria.com>
2023-04-05 12:37:26 +0200

    Updated test

--- apps/rehabstod/src/pages/CareProvider/CareProvider.test.tsx
+++ apps/rehabstod/src/pages/CareProvider/CareProvider.test.tsx
@@ -15,15 +15,11 @@ it('Should render Välj enhet', async () => {
 it('displays an alert when user.roleSwitchPossible is true', async () => {
   fakeUser({ roleSwitchPossible: true })
   renderWithRouter(<CareProvider />)
-  const alertText =
-    'Du har behörigheten Rehabkoordinator på någon/några av dina enheter. Var uppmärksam om att din roll kommer skifta från Läkare till Rehabkoordinator när du väljer att logga in på en sådan enhet.'
-  expect(await screen.findByText(alertText)).toBeInTheDocument()
+  expect(await screen.findByText(/du har behörigheten Rehabkoordinator på någon/i)).toBeInTheDocument()
 })
 
 it('Should not display an alert when user.roleSwitchPossible is false', async () => {
   fakeUser({ roleSwitchPossible: false })
   renderWithRouter(<CareProvider />)
-  const alertText =
-    'Du har behörigheten Rehabkoordinator på någon/några av dina enheter. Var uppmärksam om att din roll kommer skifta från Läkare till Rehabkoordinator när du väljer att logga in på en sådan enhet.'
-  expect(await screen.queryByText(alertText)).not.toBeInTheDocument()
+  expect(await screen.queryByText(/du har behörigheten Rehabkoordinator på någon/i)).not.toBeInTheDocument()
 })
