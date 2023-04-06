6a187761289a09e1b7c8b595af1124f9f2ea907c
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-04-06 09:08:43 +0200

    revert test changes

--- apps/rehabstod/src/pages/CurrentSickLeaves/components/Filters.test.tsx
+++ apps/rehabstod/src/pages/CurrentSickLeaves/components/Filters.test.tsx
@@ -4,37 +4,45 @@ import { vi } from 'vitest'
 import { renderWithRouter } from '../../../utils/renderWithRouter'
 import { Filters } from './Filters'
 
-it('should render without problems', () => {
-  expect(() => renderWithRouter(<Filters onSearch={vi.fn()} onReset={vi.fn()} isDoctor={false} />)).not.toThrow()
-})
+let onSearch: () => void
+let onReset: () => void
+const renderComponent = () => {
+  onSearch = vi.fn()
+  onReset = vi.fn()
 
-it('should show hide filters button', () => {
-  renderWithRouter(<Filters onSearch={vi.fn()} onReset={vi.fn()} isDoctor={false} />)
-  expect(screen.getByText('Dölj sökfilter')).toBeInTheDocument()
-})
+  renderWithRouter(<Filters onSearch={onSearch} onReset={onReset} isDoctor={false} />)
+}
 
-it('should show buttons when filters are open', () => {
-  renderWithRouter(<Filters onSearch={vi.fn()} onReset={vi.fn()} isDoctor={false} />)
-  expect(screen.getByText('Återställ')).toBeInTheDocument()
-  expect(screen.getByText('Sök')).toBeInTheDocument()
-})
+describe('CurrentSickLeavesFilters', () => {
+  beforeEach(() => {
+    renderComponent()
+  })
 
-it('should show show filters button after pressing hide', async () => {
-  renderWithRouter(<Filters onSearch={vi.fn()} onReset={vi.fn()} isDoctor={false} />)
-  await userEvent.click(screen.getByText('Dölj sökfilter'))
-  expect(screen.getByText('Visa sökfilter')).toBeInTheDocument()
-})
+  it('should render without problems', () => {
+    expect(() => renderComponent()).not.toThrow()
+  })
 
-it('should call on search when pressing search button', async () => {
-  const onSearch = vi.fn()
-  renderWithRouter(<Filters onSearch={onSearch} onReset={vi.fn()} isDoctor={false} />)
-  await userEvent.click(screen.getByText('Sök'))
-  expect(onSearch).toHaveBeenCalledTimes(1)
-})
+  it('should show hide filters button', () => {
+    expect(screen.getByText('Dölj sökfilter')).toBeInTheDocument()
+  })
 
-it('should call on reset when pressing search button', async () => {
-  const onReset = vi.fn()
-  renderWithRouter(<Filters onSearch={vi.fn()} onReset={onReset} isDoctor={false} />)
-  await userEvent.click(screen.getByText('Återställ'))
-  expect(onReset).toHaveBeenCalledTimes(1)
+  it('should show buttons when filters are open', () => {
+    expect(screen.getByText('Återställ')).toBeInTheDocument()
+    expect(screen.getByText('Sök')).toBeInTheDocument()
+  })
+
+  it('should show show filters button after pressing hide', async () => {
+    await userEvent.click(screen.getByText('Dölj sökfilter'))
+    expect(screen.getByText('Visa sökfilter')).toBeInTheDocument()
+  })
+
+  it('should call on search when pressing search button', async () => {
+    await userEvent.click(screen.getByText('Sök'))
+    expect(onSearch).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call on reset when pressing search button', async () => {
+    await userEvent.click(screen.getByText('Återställ'))
+    expect(onReset).toHaveBeenCalledTimes(1)
+  })
 })
