3216e495e1a9686e0fae78545f98d7d49b12a56b
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-04-13 13:13:00 +0200

    Fix minor issues causing noisy test output

--- apps/rehabstod/src/pages/CareProvider/CareProvider.test.tsx
+++ apps/rehabstod/src/pages/CareProvider/CareProvider.test.tsx
@@ -1,4 +1,6 @@
 import { screen } from '@testing-library/react'
+import { rest } from 'msw'
+import { server } from '../../mocks/server'
 import { fakeUser } from '../../utils/fake'
 import { renderWithRouter } from '../../utils/renderWithRouter'
 import { CareProvider } from './CareProvider'
@@ -13,13 +15,13 @@ it('Should render Välj enhet', async () => {
 })
 
 it('displays an alert when user.roleSwitchPossible is true', async () => {
-  fakeUser({ roleSwitchPossible: true })
+  server.use(rest.get('/api/user', (_, res, ctx) => res(ctx.status(200), ctx.json(fakeUser({ roleSwitchPossible: true })))))
   renderWithRouter(<CareProvider />)
   expect(await screen.findByText(/du har behörigheten Rehabkoordinator på någon/i)).toBeInTheDocument()
 })
 
 it('Should not display an alert when user.roleSwitchPossible is false', () => {
-  fakeUser({ roleSwitchPossible: false })
+  server.use(rest.get('/api/user', (_, res, ctx) => res(ctx.status(200), ctx.json(fakeUser({ roleSwitchPossible: false })))))
   renderWithRouter(<CareProvider />)
   expect(screen.queryByText(/du har behörigheten Rehabkoordinator på någon/i)).not.toBeInTheDocument()
 })
