80b1581f69ebe98dbcc9aa659076bba8d5b50dcd
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-04-17 09:22:19 +0200

    INTYGFV-15349: Removed test

--- apps/rehabstod/src/pages/Home/Home.test.tsx
+++ apps/rehabstod/src/pages/Home/Home.test.tsx
@@ -1,15 +1,6 @@
-import { screen, waitForElementToBeRemoved } from '@testing-library/react'
 import { renderWithRouter } from '../../utils/renderWithRouter'
 import { Home } from './Home'
 
 it('Should render without error', () => {
   expect(() => renderWithRouter(<Home />)).not.toThrow()
-})
-
-it('Should present user once logged in', async () => {
-  renderWithRouter(<Home />)
-
-  await waitForElementToBeRemoved(document.querySelector('ids-spinner'))
-
-  expect(screen.getByText('Hej Karolina Ek PhD')).toBeInTheDocument()
 })
