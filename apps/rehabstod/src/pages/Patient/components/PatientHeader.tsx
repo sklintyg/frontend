5bdb82b8269c8d8d0e39c9a4d7c13ecd9ae8ac74
Ulrika <ulrika.sjoberg@soprasteria.com>
2023-04-18 13:02:52 +0200

    INTYGFV-15500: show first certificate patient information

--- apps/rehabstod/src/pages/Patient/components/PatientHeader.tsx
+++ apps/rehabstod/src/pages/Patient/components/PatientHeader.tsx
@@ -1,41 +1,49 @@
-import { IDSIcon, IDSLink } from '@frontend/ids-react-ts'
-import { Link } from 'react-router-dom'
+import { IDSIcon, IDSButton } from '@frontend/ids-react-ts'
+import { useNavigate } from 'react-router-dom'
 import { Patient } from '../../../schemas/patientSchema'
 
-function countSickDays(startDate: Date, endDate: Date): number {
-  const today = new Date()
-  const daysSinceStart = Math.floor((today.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24))
-  const daysBeyondEnd = Math.max(0, Math.floor((today.getTime() - endDate.getTime()) / (1000 * 60 * 60 * 24)))
-  const daysSick = Math.max(0, daysSinceStart - daysBeyondEnd)
-  return daysSick
-}
-
 export function PatientHeader({ patient }: { patient: Patient }) {
+  const navigate = useNavigate()
+  const firstSickLeave = patient.sjukfallList[0]
+  const lastSickLeave = patient.sjukfallList[patient.sjukfallList.length - 1]
+  const firstCertificate = firstSickLeave ? firstSickLeave.intyg[0] : null
+
+  const countSickDays = (startDate: Date, endDate: Date): number => {
+    const today = new Date()
+    const daysSinceStart = Math.floor((today.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24))
+    const daysBeyondEnd = Math.max(0, Math.floor((today.getTime() - endDate.getTime()) / (1000 * 60 * 60 * 24)))
+    const daysSick = Math.max(0, daysSinceStart - daysBeyondEnd)
+    return daysSick
+  }
+
+  const handleClick = () => {
+    navigate('/pagaende-sjukfall')
+  }
+
   return (
     <div className=" bg-neutral-99 sticky top-0 z-50">
       <div className="flex items-center space-x-2 p-4">
         <IDSIcon size="s" name="user" />
-        {patient.sjukfallList.map((sjukfall) =>
-          sjukfall.intyg.map((intyg) => (
-            <div key={intyg.patient.id} className="flex items-center space-x-2">
-              <span className="font-bold">{intyg.patient.namn},</span>
-              <span>{intyg.patient.id},</span>
-              <span>{intyg.patient.alder},</span>
-              <span>{intyg.patient.kon}</span>
-              <span color="neutral-20" className="space-x-2">
-                |
-              </span>
-              <span>
-                Uppskattad dag i sjukfallet: <span className="font-bold">{countSickDays(new Date(intyg.start), new Date(intyg.slut))}</span>
-              </span>
-            </div>
-          ))
+
+        {firstCertificate && (
+          <div key={firstCertificate.patient.id} className="flex items-center space-x-2">
+            <span className="font-bold">{firstCertificate.patient.namn},</span> <span>{firstCertificate.patient.id},</span>
+            <span>{firstCertificate.patient.alder},</span> <span>{firstCertificate.patient.kon}</span>
+            <span color="neutral-20" className="space-x-2">
+              |
+            </span>
+            <span>
+              Uppskattad dag i sjukfallet:
+              <span className="font-bold">{countSickDays(new Date(firstSickLeave.start), new Date(lastSickLeave.slut))}</span>dagar
+            </span>
+          </div>
         )}
+
         <div className="grow" />
         <div>
-          <IDSLink>
-            <Link to="/pagaende-sjukfall">STÄNG PATIENTVYN</Link>
-          </IDSLink>
+          <IDSButton onClick={handleClick} tertiary>
+            STÄNG PATIENTVYN
+          </IDSButton>
         </div>
       </div>
     </div>
