2d35a5d70c9c259eb628c497cfe497d4a066d9a6
Ulrika <ulrika.sjoberg@soprasteria.com>
2023-04-19 12:39:23 +0200

    INTYGFV-15374: Added control for personalInformaiton i patientview

--- apps/rehabstod/src/pages/Patient/components/PatientHeader.tsx
+++ apps/rehabstod/src/pages/Patient/components/PatientHeader.tsx
@@ -1,16 +1,23 @@
 import { IDSIcon, IDSButton, IDSContainer } from '@frontend/ids-react-ts'
 import { useNavigate } from 'react-router-dom'
 import { formatDuration, intervalToDuration, isBefore, subDays } from 'date-fns'
+import { useSelector } from 'react-redux'
 import { Patient } from '../../../schemas/patientSchema'
+import { RootState } from '../../../store/store'
 
 export function PatientHeader({ patient }: { patient: Patient }) {
   const navigate = useNavigate()
   const isDateBeforeToday = (date: string) => isBefore(new Date(date), subDays(Date.now(), 1))
   const currentSickness = patient.sjukfallList.find(({ slut }) => !isDateBeforeToday(slut))
   const firstCertificate = currentSickness ? currentSickness.intyg[0] : null
+  const { showPersonalInformation } = useSelector((state: RootState) => state.sickLeave)
 
   const handleClick = () => {
     navigate('/pagaende-sjukfall')
+  }
+
+  if (!firstCertificate || !currentSickness) {
+    return null
   }
 
   return (
@@ -18,10 +25,11 @@ export function PatientHeader({ patient }: { patient: Patient }) {
       <IDSContainer>
         <div className="flex items-center space-x-2 py-4">
           <IDSIcon size="s" name="user" />
-          {firstCertificate && currentSickness && (
+
+          {showPersonalInformation ? (
             <div key={firstCertificate.patient.id} className="flex items-center space-x-2">
               <span className="font-bold">{firstCertificate.patient.namn},</span> <span>{firstCertificate.patient.id},</span>
-              <span>{firstCertificate.patient.alder},</span> <span>{firstCertificate.patient.kon}</span>
+              <span>{firstCertificate.patient.alder} år,</span> <span>{firstCertificate.patient.kon}</span>
               <span color="neutral-20" className="space-x-2">
                 |
               </span>
@@ -39,6 +47,10 @@ export function PatientHeader({ patient }: { patient: Patient }) {
                   )}{' '}
                 </span>
               </span>
+            </div>
+          ) : (
+            <div key={firstCertificate.patient.id} className="flex items-center space-x-2">
+              <span>{firstCertificate.patient.alder} år,</span> <span>{firstCertificate.patient.kon}</span>
             </div>
           )}
 
