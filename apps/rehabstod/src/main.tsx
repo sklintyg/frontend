07d24293ea628b1f2a1e5b7add40f3bb3fb1c78a
Ulrika <ulrika.sjoberg@soprasteria.com>
2023-04-19 09:34:15 +0200

    INTYGFV-15500: fixed PR requests

--- apps/rehabstod/src/main.tsx
+++ apps/rehabstod/src/main.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/order */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
@@ -5,6 +6,10 @@ import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
 import './index.css'
 import { store } from './store/store'
+import { setDefaultOptions } from 'date-fns'
+import { sv } from 'date-fns/locale'
+
+setDefaultOptions({ locale: sv })
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- apps/rehabstod/src/pages/Patient/components/PatientHeader.tsx
+++ apps/rehabstod/src/pages/Patient/components/PatientHeader.tsx
@@ -1,50 +1,55 @@
-import { IDSIcon, IDSButton } from '@frontend/ids-react-ts'
+import { IDSIcon, IDSButton, IDSContainer } from '@frontend/ids-react-ts'
 import { useNavigate } from 'react-router-dom'
+import { formatDuration, intervalToDuration, isBefore, subDays } from 'date-fns'
 import { Patient } from '../../../schemas/patientSchema'
 
 export function PatientHeader({ patient }: { patient: Patient }) {
   const navigate = useNavigate()
-  const firstSickLeave = patient.sjukfallList[0]
-  const lastSickLeave = patient.sjukfallList[patient.sjukfallList.length - 1]
-  const firstCertificate = firstSickLeave ? firstSickLeave.intyg[0] : null
-
-  const countSickDays = (startDate: Date, endDate: Date): number => {
-    const today = new Date()
-    const daysSinceStart = Math.floor((today.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24))
-    const daysBeyondEnd = Math.max(0, Math.floor((today.getTime() - endDate.getTime()) / (1000 * 60 * 60 * 24)))
-    const daysSick = Math.max(0, daysSinceStart - daysBeyondEnd)
-    return daysSick
-  }
+  const isDateBeforeToday = (date: string) => isBefore(new Date(date), subDays(Date.now(), 1))
+  const currentSickness = patient.sjukfallList.find(({ slut }) => !isDateBeforeToday(slut))
+  const firstCertificate = currentSickness ? currentSickness.intyg[0] : null
 
   const handleClick = () => {
     navigate('/pagaende-sjukfall')
   }
 
   return (
-    <div className=" bg-neutral-99 sticky top-0">
-      <div className="flex items-center space-x-2 p-4">
-        <IDSIcon size="s" name="user" />
-        {firstCertificate && (
-          <div key={firstCertificate.patient.id} className="flex items-center space-x-2">
-            <span className="font-bold">{firstCertificate.patient.namn},</span> <span>{firstCertificate.patient.id},</span>
-            <span>{firstCertificate.patient.alder},</span> <span>{firstCertificate.patient.kon}</span>
-            <span color="neutral-20" className="space-x-2">
-              |
-            </span>
-            <span>
-              Uppskattad dag i sjukfallet:{' '}
-              <span className="font-bold">{countSickDays(new Date(firstSickLeave.start), new Date(lastSickLeave.slut))} dagar</span>
-            </span>
-          </div>
-        )}
+    <div className="bg-secondary-95 sticky top-0">
+      <IDSContainer>
+        <div className="flex items-center space-x-2 py-4">
+          <IDSIcon size="s" name="user" />
+          {firstCertificate && currentSickness && (
+            <div key={firstCertificate.patient.id} className="flex items-center space-x-2">
+              <span className="font-bold">{firstCertificate.patient.namn},</span> <span>{firstCertificate.patient.id},</span>
+              <span>{firstCertificate.patient.alder},</span> <span>{firstCertificate.patient.kon}</span>
+              <span color="neutral-20" className="space-x-2">
+                |
+              </span>
+              <span>
+                Uppskattad dag i sjukfallet:{' '}
+                <span className="font-bold">
+                  {formatDuration(
+                    intervalToDuration({
+                      start: new Date(currentSickness.start),
+                      end: new Date(),
+                    }),
+                    {
+                      format: ['years', 'days'],
+                    }
+                  )}{' '}
+                </span>
+              </span>
+            </div>
+          )}
 
-        <div className="grow" />
-        <div>
-          <IDSButton onClick={handleClick} tertiary>
-            STÄNG PATIENTVYN
-          </IDSButton>
+          <div className="grow" />
+          <div>
+            <IDSButton onClick={handleClick} tertiary>
+              STÄNG PATIENTVYN
+            </IDSButton>
+          </div>
         </div>
-      </div>
+      </IDSContainer>
     </div>
   )
 }
