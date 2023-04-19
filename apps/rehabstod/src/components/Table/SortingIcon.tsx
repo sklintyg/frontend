7cde3df8bfe8d58143f6116c9af1cb860e2e9dd9
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-04-19 07:57:20 +0200

    correct sorting icons

--- apps/rehabstod/src/components/Table/SortingIcon.tsx
+++ apps/rehabstod/src/components/Table/SortingIcon.tsx
@@ -7,25 +7,26 @@ export function SortingIcon({ column }: { column: string }) {
   const sorting = currentColumn === column
 
   return (
-    <div className="relative ml-1 inline-block h-3 w-6">
-      <IDSIcon
-        className={classNames('absolute', !sorting && 'right-0', sorting && !ascending && 'hidden')}
-        name="arrow"
-        rotate="270"
-        width="14"
-        height="14"
-        color="currentColor"
-        color2="currentColor"
-      />
-      <IDSIcon
-        className={classNames('absolute', !sorting && 'left-0', sorting && ascending && 'hidden')}
-        name="arrow"
-        rotate="90"
-        width="14"
-        height="14"
-        color="currentColor"
-        color2="currentColor"
-      />
+    <div className="relative ml-1 inline-block h-3">
+      {!sorting && <IDSIcon name="swap" rotate="90" width="14" height="14" className="absolute inset-y-0" />}
+      {sorting && (
+        <>
+          <IDSIcon
+            className={classNames('absolute inset-y-0', sorting && !ascending && 'hidden')}
+            name="arrow"
+            rotate="270"
+            width="14"
+            height="14"
+          />
+          <IDSIcon
+            className={classNames('absolute inset-y-0', sorting && ascending && 'hidden')}
+            name="arrow"
+            rotate="90"
+            width="14"
+            height="14"
+          />
+        </>
+      )}
     </div>
   )
 }
