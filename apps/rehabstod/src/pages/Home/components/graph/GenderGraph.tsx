503964d46529ade44c1b9bdbd603e22ffa935a2d
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-04-20 09:03:30 +0200

    INTYGFV-15349: Moved icons to separate components

--- apps/rehabstod/src/pages/Home/components/graph/GenderGraph.tsx
+++ apps/rehabstod/src/pages/Home/components/graph/GenderGraph.tsx
@@ -2,6 +2,8 @@ import { Gender, GenderSummary } from '../../../../schemas/sickLeaveSchema'
 import { Tooltip } from '../../../../components/Tooltip/Tooltip'
 import { TooltipTrigger } from '../../../../components/Tooltip/TooltipTrigger'
 import { TooltipContent } from '../../../../components/Tooltip/TooltipContent'
+import { FemaleIcon } from '../../assets/FemaleIcon'
+import { MaleIcon } from '../../assets/MaleIcon'
 
 export function GenderGraph({ gender }: { gender: GenderSummary | undefined }) {
   if (!gender) {
@@ -25,29 +27,7 @@ export function GenderGraph({ gender }: { gender: GenderSummary | undefined }) {
               {`${Math.round(gender ? gender.percentage : 0)} %`}
             </text>
           </svg>
-          {!isFemale ? (
-            <svg
-              width="35"
-              height="100"
-              viewBox="0 0 35 100"
-              fill="var(--IDS-COLOR-PRIMARY-30)"
-              xmlns="http://www.w3.org/2000/svg"
-              className="mx-auto"
-              data-testid="iconFemale">
-              <path d="M10 100V65H0V33.375C0 31.2917 0.729167 29.5208 2.1875 28.0625C3.64583 26.6042 5.41667 25.875 7.5 25.875H27.5C29.5833 25.875 31.3542 26.6042 32.8125 28.0625C34.2708 29.5208 35 31.2917 35 33.375V65H25V100H10ZM17.5 18.25C15 18.25 12.8542 17.3542 11.0625 15.5625C9.27083 13.7708 8.375 11.625 8.375 9.125C8.375 6.625 9.27083 4.47917 11.0625 2.6875C12.8542 0.895833 15 0 17.5 0C20 0 22.1458 0.895833 23.9375 2.6875C25.7292 4.47917 26.625 6.625 26.625 9.125C26.625 11.625 25.7292 13.7708 23.9375 15.5625C22.1458 17.3542 20 18.25 17.5 18.25Z" />
-            </svg>
-          ) : (
-            <svg
-              width="44"
-              height="100"
-              viewBox="0 0 44 100"
-              fill="var(--IDS-COLOR-ACCENT-40)"
-              xmlns="http://www.w3.org/2000/svg"
-              className="mx-auto"
-              data-testid="iconMale">
-              <path d="M14.6667 100V72.5H0L12.4667 31.5C13.1185 29.4167 14.3407 27.8125 16.1333 26.6875C17.9259 25.5625 19.8815 25 22 25C24.1185 25 26.0741 25.5625 27.8667 26.6875C29.6593 27.8125 30.8815 29.4167 31.5333 31.5L44 72.5H29.3333V100H14.6667ZM22 18.25C19.5556 18.25 17.4574 17.3542 15.7056 15.5625C13.9537 13.7708 13.0778 11.625 13.0778 9.125C13.0778 6.625 13.9537 4.47917 15.7056 2.6875C17.4574 0.895833 19.5556 0 22 0C24.4444 0 26.5426 0.895833 28.2944 2.6875C30.0463 4.47917 30.9222 6.625 30.9222 9.125C30.9222 11.625 30.0463 13.7708 28.2944 15.5625C26.5426 17.3542 24.4444 18.25 22 18.25Z" />
-            </svg>
-          )}
+          {isFemale ? <FemaleIcon /> : <MaleIcon />}
         </div>
         <TooltipContent>{`${isFemale ? 'Kvinnor' : 'Män'} (${gender.count} st, ${Math.round(gender.percentage)} %)`}</TooltipContent>
       </TooltipTrigger>
