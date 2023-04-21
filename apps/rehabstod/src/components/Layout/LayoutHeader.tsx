ddb36ec52812089f9b441df05dadc08a97bb8e05
Ulrika <ulrika.sjoberg@soprasteria.com>
2023-04-21 07:57:11 +0200

    INTYGFV-15548: Fixed bug Fixed bug with patient header that should lie above the tablewith patientheader to overlap table

--- apps/rehabstod/src/components/Layout/LayoutHeader.tsx
+++ apps/rehabstod/src/components/Layout/LayoutHeader.tsx
@@ -14,7 +14,7 @@ export function LayoutHeader() {
   const ref = useRef<IDSDialogElement>(null)
 
   return (
-    <IDSHeader type="inera-admin" unresponsive>
+    <IDSHeader type="inera-admin" unresponsive className="z-50">
       <Link className="text-primary-40" slot="brand-text" to="/">
         Rehabstöd
       </Link>
