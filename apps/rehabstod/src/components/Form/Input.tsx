0f425602ee17c7bbe4344670475d312b6c39f6e1
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-04-21 15:42:58 +0200

    Styling issues after ids-core update

--- apps/rehabstod/src/components/Form/Input.tsx
+++ apps/rehabstod/src/components/Form/Input.tsx
@@ -5,16 +5,35 @@ import { classNames } from '../../utils/classNames'
 interface InputProps {
   hasIcon?: boolean
   error?: boolean
+  disabled?: boolean
+}
+
+const styles = {
+  default: 'bg-secondary-95 border-accent-40',
+  error: 'bg-error-99 border-error-40',
+  disabled: 'bg-white border-neutral-40',
+}
+
+const getStyle = ({ error, disabled }: { error: boolean; disabled: boolean }) => {
+  if (error) {
+    return styles.error
+  }
+
+  if (disabled) {
+    return styles.disabled
+  }
+
+  return styles.default
 }
 
 export const Input = React.forwardRef<HTMLInputElement, React.HTMLProps<HTMLInputElement> & InputProps>(
-  ({ hasIcon, error = false, ...props }, ref) => (
+  ({ hasIcon, error = false, disabled = false, ...props }, ref) => (
     <input
       ref={ref}
       className={classNames(
-        'border-accent-40 text-neutral-20 bg-secondary-95 box-border w-full rounded border py-3 text-left my-3',
+        'text-neutral-20 box-border w-full rounded border py-3 text-left my-3',
         hasIcon ? 'pl-5 pr-12' : 'px-5',
-        error && 'bg-error-99 border-error-40'
+        getStyle({ error, disabled })
       )}
       {...props}
     />
