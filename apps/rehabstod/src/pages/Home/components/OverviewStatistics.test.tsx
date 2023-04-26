81967ce16f60b9224ce768fa5957b32436e0087f
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-04-26 08:40:48 +0200

    INTYGFV-15554: Removed test

--- apps/rehabstod/src/pages/Home/components/OverviewStatistics.test.tsx
+++ apps/rehabstod/src/pages/Home/components/OverviewStatistics.test.tsx
@@ -1,4 +1,3 @@
-import { screen } from '@testing-library/react'
 import { OverviewStatistics } from './OverviewStatistics'
 import { renderWithRouter } from '../../../utils/renderWithRouter'
 
@@ -9,10 +8,5 @@ const renderComponent = () => {
 describe('OverviewStatistics', () => {
   it('should render without errors', () => {
     expect(() => renderComponent()).not.toThrow()
-  })
-
-  it('should show title', () => {
-    renderComponent()
-    expect(screen.getByText('Översikt över pågående sjukfall just nu')).toBeInTheDocument()
   })
 })
