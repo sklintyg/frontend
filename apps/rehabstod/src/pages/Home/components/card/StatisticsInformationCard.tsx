2079412def70ecafe054e3c5ba9af32726e27660
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-04-27 11:44:07 +0200

    INTYGFV-15567: Fixed test

--- apps/rehabstod/src/pages/Home/components/card/StatisticsInformationCard.tsx
+++ apps/rehabstod/src/pages/Home/components/card/StatisticsInformationCard.tsx
@@ -7,10 +7,6 @@ import { Tooltip } from '../../../../components/Tooltip/Tooltip'
 export function StatisticsInformationCard() {
   const { data: links } = useGetLinksQuery()
 
-  if (!links || !links.statistiktjanstenTooltip) {
-    return null
-  }
-
   return (
     <>
       <h3 className="ids-heading-4">Här kan du hitta mer statistik</h3>
@@ -20,16 +16,18 @@ export function StatisticsInformationCard() {
           öppnas Intygsstatistik i en ny flik, och du blir automatiskt inloggad om du har giltig behörighet till Intygsstatistik.
         </p>
         <div className="flex pt-5 pb-14">
-          <Tooltip>
-            <TooltipTrigger>
-              <IDSLink>
-                <IDSIcon name="arrow" />
-                <a href={links.statistiktjanstenTooltip.url}>{links.statistiktjanstenTooltip.text}</a>
-                <IDSIcon slot="append-icon" name="external" size="xs" className="my-auto" />
-              </IDSLink>
-            </TooltipTrigger>
-            <TooltipContent>{links.statistiktjanstenTooltip.tooltip}</TooltipContent>
-          </Tooltip>
+          {links && links.statistiktjanstenTooltip && (
+            <Tooltip>
+              <TooltipTrigger>
+                <IDSLink>
+                  <IDSIcon name="arrow" />
+                  <a href={links.statistiktjanstenTooltip.url}>{links.statistiktjanstenTooltip.text}</a>
+                  <IDSIcon slot="append-icon" name="external" size="xs" className="my-auto" />
+                </IDSLink>
+              </TooltipTrigger>
+              {links.statistiktjanstenTooltip.tooltip && <TooltipContent>{links.statistiktjanstenTooltip.tooltip}</TooltipContent>}
+            </Tooltip>
+          )}
         </div>
       </div>
     </>
