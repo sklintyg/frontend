bcd2dbd662a4b0171e2fd4dd3e48bc3d9ed849e2
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-04-27 14:47:03 +0200

    INTYGFV-15568: Added text for when there is filtration but no results

--- apps/rehabstod/src/pages/CurrentSickLeaves/sickLeaveSlice.ts
+++ apps/rehabstod/src/pages/CurrentSickLeaves/sickLeaveSlice.ts
@@ -1,15 +1,19 @@
 /* eslint-disable no-param-reassign */
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import _ from 'lodash'
 import { SickLeaveFilter } from '../../schemas/sickLeaveSchema'
 
 export interface SickLeaveState {
   showPersonalInformation: boolean
   filter: SickLeaveFilter
+
+  hasAppliedFilters: boolean
 }
 
 const initialState: SickLeaveState = {
   showPersonalInformation: true,
   filter: { doctorIds: [], diagnosisChapters: [], fromSickLeaveLength: 1, toSickLeaveLength: 10000 },
+  hasAppliedFilters: false,
 }
 
 const sickLeaveSlice = createSlice({
@@ -24,6 +28,7 @@ const sickLeaveSlice = createSlice({
     },
     updateFilter(state, { payload }: PayloadAction<Partial<SickLeaveFilter>>) {
       Object.assign(state.filter, payload)
+      state.hasAppliedFilters = !_.isEqual(initialState.filter, state.filter)
     },
     updateShowPersonalInformation(state, { payload }: PayloadAction<boolean>) {
       state.showPersonalInformation = payload
