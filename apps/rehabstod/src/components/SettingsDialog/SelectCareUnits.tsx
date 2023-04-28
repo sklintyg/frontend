e1c58bdbe4fba07331e6b9b8a7b2cec1951238d7
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-04-28 14:30:03 +0200

    fixed padding on select and changed to Valj enhet label

--- apps/rehabstod/src/components/SettingsDialog/SelectCareUnits.tsx
+++ apps/rehabstod/src/components/SettingsDialog/SelectCareUnits.tsx
@@ -13,22 +13,24 @@ export function SelectCareUnits({ onChange, preferences }: { onChange: (value: s
 
   return (
     <IDSSelect>
-      <label htmlFor={id}>Förvald enhet</label>
-      <select
-        id={id}
-        name="options"
-        className="text-neutral-20 box-border w-full truncate rounded border py-3 px-5 text-left"
-        onChange={(event) => onChange(event.currentTarget.value)}
-        value={preferences.standardenhet ?? 'Ingen förvald enhet'}>
-        <option className="ml-2" value="Ingen förvald enhet">
-          Ingen förvald enhet
-        </option>
-        {getUnitsForUser(user).map((item) => (
-          <option key={item.id} value={item.id}>
-            {item.namn}
+      <label htmlFor={id}>Välj enhet</label>
+      <div>
+        <select
+          id={id}
+          name="options"
+          className="text-neutral-20 box-border w-full appearance-none truncate rounded border py-3 px-4 pl-5 pr-12 text-left"
+          onChange={(event) => onChange(event.currentTarget.value)}
+          value={preferences.standardenhet ?? 'Ingen förvald enhet'}>
+          <option className="ml-2" value="Ingen förvald enhet">
+            Ingen förvald enhet
           </option>
-        ))}
-      </select>
+          {getUnitsForUser(user).map((item) => (
+            <option key={item.id} value={item.id}>
+              {item.namn}
+            </option>
+          ))}
+        </select>
+      </div>
     </IDSSelect>
   )
 }
