da3fb5539104d0750fb9bcc81ce9e072128eb567
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-05-02 11:18:57 +0200

    simplified tests

--- apps/rehabstod/src/components/SettingsDialog/SelectCareUnits.test.tsx
+++ apps/rehabstod/src/components/SettingsDialog/SelectCareUnits.test.tsx
@@ -1,48 +1,45 @@
 import { screen } from '@testing-library/react'
-import { beforeEach, expect, vi } from 'vitest'
-import { rest } from 'msw'
 import userEvent from '@testing-library/user-event'
-import { SelectCareUnits } from './SelectCareUnits'
-import { fakeUser, fakeUserPreferences } from '../../utils/fake'
+import { rest } from 'msw'
+import { beforeEach, expect, vi } from 'vitest'
 import { server } from '../../mocks/server'
-import { renderWithRouter } from '../../utils/renderWithRouter'
 import { User } from '../../schemas'
+import { fakeUser } from '../../utils/fake'
 import { getUnitsForUser } from '../../utils/getUnitsForUser'
+import { renderWithRouter } from '../../utils/renderWithRouter'
+import { SelectCareUnits } from './SelectCareUnits'
 
-const preferences = fakeUserPreferences()
-let onChange: (value: string) => void
 let user: User
 beforeEach(() => {
-  onChange = vi.fn()
   user = fakeUser()
   server.use(rest.get('/api/user', (_, res, ctx) => res(ctx.status(200), ctx.json(user))))
 })
 
 it('should display Ingen forvald enhet option', async () => {
-  renderWithRouter(<SelectCareUnits onChange={onChange} preferences={preferences} />)
+  renderWithRouter(<SelectCareUnits onChange={vi.fn()} />)
 
   expect(await screen.findByText('Ingen förvald enhet')).toBeInTheDocument()
 })
 
 it('should display options with care units related to user', async () => {
-  renderWithRouter(<SelectCareUnits onChange={onChange} preferences={preferences} />)
+  renderWithRouter(<SelectCareUnits onChange={vi.fn()} />)
 
   expect(await screen.findAllByRole('option')).toHaveLength(getUnitsForUser(user).length + 1)
 })
 
 it('should display standardenhet from preferences in select as default value', async () => {
   const unit = getUnitsForUser(user)[0]
-  const userPreferences = fakeUserPreferences({ standardenhet: unit.id })
 
-  renderWithRouter(<SelectCareUnits onChange={onChange} preferences={userPreferences} />)
+  renderWithRouter(<SelectCareUnits onChange={vi.fn()} standardenhet={unit.id} />)
 
   expect(await screen.findByDisplayValue(unit.namn)).toBeInTheDocument()
 })
 
 it('should call onChange if option is selected', async () => {
   const unit = getUnitsForUser(user)[0]
+  const onChange = vi.fn()
 
-  renderWithRouter(<SelectCareUnits onChange={onChange} preferences={preferences} />)
+  renderWithRouter(<SelectCareUnits onChange={onChange} />)
   const options = await screen.findAllByText(unit.namn)
   const option = options[0]
 
