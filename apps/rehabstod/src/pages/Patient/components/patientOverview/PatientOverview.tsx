0ea31dbe99a8b6d65f56c31aa6800b2e8e55f467
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-05-05 10:43:14 +0200

    INTYGFV-15362: Fixed test

--- apps/rehabstod/src/pages/Patient/components/patientOverview/PatientOverview.tsx
+++ apps/rehabstod/src/pages/Patient/components/patientOverview/PatientOverview.tsx
@@ -45,7 +45,7 @@ export function PatientOverview({
   }
 
   return (
-    <div className="grid grid-cols-2 gap-4">
+    <div className="grid grid-cols-2 gap-4 py-5">
       <OpenInformationCard
         title="Ospärrad information inom vårdgivare"
         subTitle="Vårdenhet att hämta information från"
