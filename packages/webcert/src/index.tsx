bb8c1677ba63dcd4b4d4c57cdf0ba5202109efaf
Samuel Tilly <samuel.tilly@gmail.com>
2023-05-25 14:11:14 +0200

    add missing dependencies

--- packages/webcert/src/index.tsx
+++ packages/webcert/src/index.tsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react'
-import 'react-datepicker/dist/react-datepicker.css'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
