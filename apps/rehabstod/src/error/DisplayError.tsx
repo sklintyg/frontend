6ab965191a910092667aeabcd67c2aa51af449e4
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-05-26 14:57:10 +0200

    INTYGFV-15623: Added tooltipIcon that enables copy of error id.

--- apps/rehabstod/src/error/DisplayError.tsx
+++ apps/rehabstod/src/error/DisplayError.tsx
@@ -1,7 +1,9 @@
 import { IDSAlert } from '@frontend/ids-react-ts'
+import { useState } from 'react'
 import { useAppSelector } from '../store/hooks'
 import { DynamicLink } from '../components/DynamicLink/DynamicLink'
 import { useGetLinksQuery } from '../store/api'
+import { TooltipIcon } from '../components/TooltipIcon/TooltipIcon'
 
 export function DisplayError({
   heading,
@@ -16,6 +18,12 @@ export function DisplayError({
 }) {
   const { data: links } = useGetLinksQuery()
   const { errorId } = useAppSelector((state) => state.error)
+  const [displayCopyMessage, setDisplayCopyMessage] = useState(false)
+
+  const handleCopyClick = () => {
+    setDisplayCopyMessage(true)
+    navigator.clipboard.writeText(errorId)
+  }
   return (
     <div className="mt-5">
       <IDSAlert type={errorType} headline={heading}>
@@ -27,7 +35,16 @@ export function DisplayError({
         <div className="mt-5 flex">
           <p className="mr-2 font-bold">FEL-ID:</p>
           {errorId}
+          <TooltipIcon
+            description="Kopiera fel-id"
+            name="copy-link"
+            size="s"
+            colorpreset={1}
+            className="ml-2 inline cursor-pointer"
+            onClick={handleCopyClick}
+          />
         </div>
+        {displayCopyMessage && 'Fel-id kopierat till urklipp.'}
       </IDSAlert>
     </div>
   )
