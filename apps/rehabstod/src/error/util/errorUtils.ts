5b661ea2f3fd60b26b0062b8db4c99dddd14acc9
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-05-26 15:22:32 +0200

    INTYGFV-15623: Fixed lint errors.

--- apps/rehabstod/src/error/util/errorUtils.ts
+++ apps/rehabstod/src/error/util/errorUtils.ts
@@ -1,6 +1,17 @@
-export const uuidv4 = (): string =>
-  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0
-    const v = c === 'x' ? r : (r & 0x3) | 0x8
-    return v.toString(16)
-  })
+export const uuidv4 = (): string => {
+  const characters = '0123456789abcdef'
+  let uuid = ''
+
+  for (let i = 0; i < 36; i++) {
+    if (i === 8 || i === 13 || i === 18 || i === 23) {
+      uuid += '-'
+    } else if (i === 14) {
+      uuid += '4'
+    } else {
+      const randomChar = characters[Math.floor(Math.random() * characters.length)]
+      uuid += randomChar
+    }
+  }
+
+  return uuid
+}
