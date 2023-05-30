bc0ddd7d9dd2ecf0cea8ce14e4d754599e46bb8e
Samuel Tilly <samuel.tilly@gmail.com>
2023-05-30 08:40:42 +0200

    fix issues with common tests and unecessary packages

--- packages/common/vite.config.ts
+++ packages/common/vite.config.ts
@@ -1,14 +1,6 @@
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
   test: {
     globals: true,
     environment: 'jsdom',
