91d34f2037c1119fa7e1c47d5693a59c2170d3e4
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-05-31 13:19:33 +0200

    INTYGFV-15659: Added tests

--- apps/rehabstod/src/pages/CurrentSickLeaves/components/TableHeaderRow.test.tsx
+++ apps/rehabstod/src/pages/CurrentSickLeaves/components/TableHeaderRow.test.tsx
@@ -1,10 +1,12 @@
 import { act, screen } from '@testing-library/react'
+import { rest } from 'msw'
 import { Table } from '../../../components/Table/Table'
 import { api } from '../../../store/api'
 import { hideColumn, SickLeaveColumn } from '../../../store/slices/sickLeaveTableColumns.slice'
 import { store } from '../../../store/store'
 import { renderWithRouter } from '../../../utils/renderWithRouter'
 import { TableHeaderRow } from './TableHeaderRow'
+import { server } from '../../../mocks/server'
 
 beforeEach(() => {
   store.dispatch(api.endpoints.getUser.initiate())
@@ -35,17 +37,44 @@ it('Should render all but doctor column if user is doctor', async () => {
   expect(screen.queryByRole('columnheader', { name: 'Läkare' })).not.toBeInTheDocument()
 })
 
-it.each(Object.values(SickLeaveColumn))('Should render and hide %s column', async (column) => {
-  renderWithRouter(
-    <Table>
-      <thead>
-        <TableHeaderRow showPersonalInformation isDoctor={false} />
-      </thead>
-    </Table>
-  )
-  expect(await screen.findByRole('columnheader', { name: column })).toBeInTheDocument()
+describe('Risk column activated', () => {
+  beforeEach(() => {
+    server.use(
+      rest.get('/api/sickleaves/filters', (_, res, ctx) =>
+        res(
+          ctx.status(200),
+          ctx.json({
+            srsActivated: true,
+          })
+        )
+      )
+    )
+  })
 
-  await act(() => store.dispatch(hideColumn(column)))
+  it.skip('Should render risk column if feature is activated', async () => {
+    renderWithRouter(
+      <Table>
+        <thead>
+          <TableHeaderRow showPersonalInformation isDoctor={false} />
+        </thead>
+      </Table>
+    )
 
-  expect(screen.queryByRole('columnheader', { name: column })).not.toBeInTheDocument()
+    expect(await screen.findAllByRole('columnheader')).toHaveLength(14)
+  })
+
+  it.each(Object.values(SickLeaveColumn))('Should render and hide %s column', async (column) => {
+    renderWithRouter(
+      <Table>
+        <thead>
+          <TableHeaderRow showPersonalInformation isDoctor={false} />
+        </thead>
+      </Table>
+    )
+    expect(await screen.findByRole('columnheader', { name: column })).toBeInTheDocument()
+
+    await act(() => store.dispatch(hideColumn(column)))
+
+    expect(screen.queryByRole('columnheader', { name: column })).not.toBeInTheDocument()
+  })
 })
