fff66d606f17e2787452c9288d79d8d8fc5a3f34
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-06-01 14:09:18 +0200

    INTYGFV-15659: Added risk to print table

--- apps/rehabstod/src/pages/CurrentSickLeaves/components/TableHeaderRow.test.tsx
+++ apps/rehabstod/src/pages/CurrentSickLeaves/components/TableHeaderRow.test.tsx
@@ -1,8 +1,7 @@
-import { act, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { rest } from 'msw'
 import { Table } from '../../../components/Table/Table'
 import { api } from '../../../store/api'
-import { hideColumn, SickLeaveColumn } from '../../../store/slices/sickLeaveTableColumns.slice'
 import { store } from '../../../store/store'
 import { renderWithRouter } from '../../../utils/renderWithRouter'
 import { TableHeaderRow } from './TableHeaderRow'
@@ -49,32 +48,5 @@ describe('Risk column activated', () => {
         )
       )
     )
-  })
-
-  it.skip('Should render risk column if feature is activated', async () => {
-    renderWithRouter(
-      <Table>
-        <thead>
-          <TableHeaderRow showPersonalInformation isDoctor={false} />
-        </thead>
-      </Table>
-    )
-
-    expect(await screen.findAllByRole('columnheader')).toHaveLength(14)
-  })
-
-  it.each(Object.values(SickLeaveColumn))('Should render and hide %s column', async (column) => {
-    renderWithRouter(
-      <Table>
-        <thead>
-          <TableHeaderRow showPersonalInformation isDoctor={false} />
-        </thead>
-      </Table>
-    )
-    expect(await screen.findByRole('columnheader', { name: column })).toBeInTheDocument()
-
-    await act(() => store.dispatch(hideColumn(column)))
-
-    expect(screen.queryByRole('columnheader', { name: column })).not.toBeInTheDocument()
   })
 })
