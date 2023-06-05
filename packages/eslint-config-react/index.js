d96c08fcf6e99774e46f5a8d5d21ee242cc27814
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-06-05 15:40:03 +0200

    fix merge issues

---
+++ packages/eslint-config-react/index.js
@@ -0,0 +1,16 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'custom',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  rules: {
+    'react/require-default-props': 'off',
+  },
+}
