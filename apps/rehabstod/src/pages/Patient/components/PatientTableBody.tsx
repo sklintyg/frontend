1b67eba3c6e847f2731cde5d5b3ab2a2fd535228
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-06-12 11:04:52 +0200

    INTYGFV-15653: Fixed format?

--- apps/rehabstod/src/pages/Patient/components/PatientTableBody.tsx
+++ apps/rehabstod/src/pages/Patient/components/PatientTableBody.tsx
@@ -1,19 +1,19 @@
-import { IDSButton, IDSColumn, IDSIcon, IDSRow } from '@frontend/ids-react-ts'
-import { DiagnosisDescription } from '../../../components/SickLeave/DiagnosisDescription'
-import { DiagnosisInfo } from '../../../components/SickLeave/DiagnosisInfo'
-import { SickLeaveDegreeInfo } from '../../../components/SickLeave/SickLeaveDegreeInfo'
-import { TableCell } from '../../../components/Table/TableCell'
-import { useTableContext } from '../../../components/Table/hooks/useTableContext'
-import { Tooltip } from '../../../components/Tooltip/Tooltip'
-import { TooltipContent } from '../../../components/Tooltip/TooltipContent'
-import { TooltipTrigger } from '../../../components/Tooltip/TooltipTrigger'
-import { PatientSjukfallIntyg } from '../../../schemas/patientSchema'
-import { useGetUserQuery } from '../../../store/api'
-import { useAppSelector } from '../../../store/hooks'
-import { allPatientColumns } from '../../../store/slices/patientTableColumns.selector'
-import { PatientColumn } from '../../../store/slices/patientTableColumns.slice'
-import { usePatient } from '../hooks/usePatient'
-import { getCertificateColumnData } from '../utils/getCertificateColumnData'
+import {IDSButton, IDSColumn, IDSIcon, IDSRow} from '@frontend/ids-react-ts'
+import {DiagnosisDescription} from '../../../components/SickLeave/DiagnosisDescription'
+import {DiagnosisInfo} from '../../../components/SickLeave/DiagnosisInfo'
+import {SickLeaveDegreeInfo} from '../../../components/SickLeave/SickLeaveDegreeInfo'
+import {TableCell} from '../../../components/Table/TableCell'
+import {useTableContext} from '../../../components/Table/hooks/useTableContext'
+import {Tooltip} from '../../../components/Tooltip/Tooltip'
+import {TooltipContent} from '../../../components/Tooltip/TooltipContent'
+import {TooltipTrigger} from '../../../components/Tooltip/TooltipTrigger'
+import {PatientSjukfallIntyg} from '../../../schemas/patientSchema'
+import {useGetUserQuery} from '../../../store/api'
+import {useAppSelector} from '../../../store/hooks'
+import {allPatientColumns} from '../../../store/slices/patientTableColumns.selector'
+import {PatientColumn} from '../../../store/slices/patientTableColumns.slice'
+import {usePatient} from '../hooks/usePatient'
+import {getCertificateColumnData} from '../utils/getCertificateColumnData'
 
 function OtherUnitInformation() {
   return (
@@ -27,23 +27,23 @@ function OtherUnitInformation() {
 }
 
 function PatientTableCellResolver({
-  column,
-  list,
-  certificate,
-  belongsToOtherUnit,
-}: {
+                                    column,
+                                    list,
+                                    certificate,
+                                    belongsToOtherUnit,
+                                  }: {
   column: string
   list: PatientSjukfallIntyg[]
   certificate: PatientSjukfallIntyg
   belongsToOtherUnit: boolean
 }) {
-  const { navigateToWebcert } = usePatient()
+  const {navigateToWebcert} = usePatient()
+
   switch (column) {
     case PatientColumn.Diagnos:
       return (
         <TableCell
-          description={certificate.diagnos && <DiagnosisDescription diagnos={certificate.diagnos} biDiagnoser={certificate.bidiagnoser} />}
-        >
+          description={certificate.diagnos && <DiagnosisDescription diagnos={certificate.diagnos} biDiagnoser={certificate.bidiagnoser} />}>
           <DiagnosisInfo diagnos={certificate.diagnos} biDiagnoser={certificate.bidiagnoser} />
         </TableCell>
       )
@@ -59,12 +59,9 @@ function PatientTableCellResolver({
           {belongsToOtherUnit ? (
             <OtherUnitInformation />
           ) : (
-            <IDSButton
-              tertiary
-              onClick={() => {
-                navigateToWebcert(certificate.intygsId)
-              }}
-            >
+            <IDSButton tertiary onClick={() => {
+              navigateToWebcert(certificate.intygsId)
+            }}>
               <IDSRow align="center">
                 <IDSColumn cols="auto">Visa </IDSColumn>
                 <IDSColumn cols="auto" className="ml-2">
@@ -86,33 +83,26 @@ function PatientTableCellResolver({
   }
 }
 
-export function PatientTableBody({ certificates, isDoctor }: { certificates: PatientSjukfallIntyg[]; isDoctor: boolean }) {
-  const { sortTableList } = useTableContext()
+export function PatientTableBody({certificates, isDoctor}: { certificates: PatientSjukfallIntyg[]; isDoctor: boolean }) {
+  const {sortTableList} = useTableContext()
   const columns = useAppSelector(allPatientColumns)
-  const { data: user } = useGetUserQuery()
+  const {data: user} = useGetUserQuery()
   return (
     <tbody className="whitespace-normal break-words">
-      {sortTableList(certificates, getCertificateColumnData).map(
-        (certificate) =>
-          columns.length > 0 && (
-            <tr
-              key={`${certificate.start}${certificate.slut}`}
-              className={user?.valdVardenhet?.id !== certificate.vardenhetId ? 'italic' : ''}>
-              {columns
-                .filter(({ visible }) => visible)
-                .filter(({ name }) => !(isDoctor && name === PatientColumn.Läkare))
-                .map(({ name }) => (
-                  <PatientTableCellResolver
-                    key={name}
-                    column={name}
-                    certificate={certificate}
-                    belongsToOtherUnit={user?.valdVardenhet?.id !== certificate.vardenhetId}
-                    list={certificates}
-                  />
-                ))}
-            </tr>
-          )
-      )}
+    {sortTableList(certificates, getCertificateColumnData).map(
+      (certificate) =>
+        columns.length > 0 && (
+          <tr key={`${certificate.start}${certificate.slut}`} className={user?.valdVardenhet?.id !== certificate.vardenhetId ? 'italic' : ''}>
+            {columns
+            .filter(({visible}) => visible)
+            .filter(({name}) => !(isDoctor && name === PatientColumn.Läkare))
+            .map(({name}) => (
+              <PatientTableCellResolver key={name} column={name} certificate={certificate}
+                                        belongsToOtherUnit={user?.valdVardenhet?.id !== certificate.vardenhetId} list={certificates} />
+            ))}
+          </tr>
+        )
+    )}
     </tbody>
   )
 }
