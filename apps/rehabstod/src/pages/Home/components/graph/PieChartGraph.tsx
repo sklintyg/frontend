e8e6104fbbf96f05c4fb432269728de8c3f32db6
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-06-12 13:39:40 +0200

    INTYGFV-15594: Added tooltips to pie chart

--- apps/rehabstod/src/pages/Home/components/graph/PieChartGraph.tsx
+++ apps/rehabstod/src/pages/Home/components/graph/PieChartGraph.tsx
@@ -1,8 +1,16 @@
-import { useEffect, useState } from 'react'
-import { Legend, Pie, PieChart, ResponsiveContainer } from 'recharts'
+import {useEffect, useState} from 'react'
+import {Legend, Pie, PieChart, ResponsiveContainer, Tooltip, TooltipProps} from 'recharts'
 import classes from './PieChartGraph.css'
 
-export function PieChartGraph({ data }: { data: { id: string; value: number; name: string; fill: string }[] }) {
+function CustomTooltip({payload}: TooltipProps<string, string>) {
+  if (payload && payload.length > 0) {
+    return <p className="border-none bg-white p-2">{payload[0].name}</p>
+  }
+
+  return null
+}
+
+export function PieChartGraph({data}: { data: { id: string; value: number; name: string; fill: string }[] }) {
   const [, setLoaded] = useState(false)
   const getLegend = (name: string) => <span className="text-neutral-20 text-sm">{name}</span>
 
@@ -16,6 +24,7 @@ export function PieChartGraph({ data }: { data: { id: string; value: number; nam
   return (
     <ResponsiveContainer width={500} height="100%" minHeight="150px" className={classes}>
       <PieChart>
+        <Tooltip content={<CustomTooltip/>} wrapperStyle={{outline: "none"}}/>
         <Pie
           isAnimationActive={false}
           cx="100"
