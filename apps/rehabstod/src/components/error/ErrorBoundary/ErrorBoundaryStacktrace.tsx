a10bb88e3e9565cee3d45c5b70fb3f6416420dc5
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-06-13 12:13:29 +0200

    cleanup

--- apps/rehabstod/src/components/error/ErrorBoundary/ErrorBoundaryStacktrace.tsx
+++ apps/rehabstod/src/components/error/ErrorBoundary/ErrorBoundaryStacktrace.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-array-index-key */
 export function ErrorBoundryStacktrace({ stackTrace }: { stackTrace: string }) {
   return <pre className="bg-secondary-90 overflow-auto rounded p-5 text-left [&:not(:last-child)]:mb-5">{stackTrace}</pre>
 }
