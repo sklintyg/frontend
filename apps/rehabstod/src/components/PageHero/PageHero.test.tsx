996a34a2341676cd07a5517d0e053937ec6e1044
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-06-14 09:41:51 +0200

    fix typescript issue

--- apps/rehabstod/src/components/PageHero/PageHero.test.tsx
+++ apps/rehabstod/src/components/PageHero/PageHero.test.tsx
@@ -2,5 +2,5 @@ import { render } from '@testing-library/react'
 import { PageHero } from './PageHero'
 
 it('Should render without error', () => {
-  expect(() => render(<PageHero icon="user" />)).not.toThrow()
+  expect(() => render(<PageHero />)).not.toThrow()
 })
