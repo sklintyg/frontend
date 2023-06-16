9ba15127768f461709ddd460b35e03e60acd1edf
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-06-16 08:41:28 +0200

    Added one more test

--- apps/rehabstod/src/components/Calendar/CalendarHeader.test.tsx
+++ apps/rehabstod/src/components/Calendar/CalendarHeader.test.tsx
@@ -3,6 +3,9 @@ import { CalendarHeader } from './CalendarHeader'
 
 it('Should render without crashing', () => {
   expect(() => render(<CalendarHeader title="Hello" prevButtonProps={{}} nextButtonProps={{}} />)).not.toThrow()
+})
 
+it('Should display title', () => {
+  render(<CalendarHeader title="Hello" prevButtonProps={{}} nextButtonProps={{}} />)
   expect(screen.queryByText('Hello')).toBeInTheDocument()
 })
