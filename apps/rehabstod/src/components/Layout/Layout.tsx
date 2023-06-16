275995b748e052cd0a58da7f0ed9cfd2f3473bf6
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-06-16 10:20:58 +0200

    INTYGFV-15687: After navigation back from patient view the scrolling position should resume in current sickleave view.

--- apps/rehabstod/src/components/Layout/Layout.tsx
+++ apps/rehabstod/src/components/Layout/Layout.tsx
@@ -1,17 +1,11 @@
-import { useLayoutEffect } from 'react'
-import { Outlet, useLocation } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { useSession } from '../../hooks/useSession'
 import { SettingsDialog } from '../SettingsDialog/SettingsDialog'
 import { LayoutFooter } from './LayoutFooter'
 import { LayoutHeader } from './LayoutHeader'
 
 export function Layout() {
-  const location = useLocation()
   useSession()
-
-  useLayoutEffect(() => {
-    window.scrollTo(0, 0)
-  }, [location.pathname])
 
   return (
     <div className="flex min-h-screen flex-col">
