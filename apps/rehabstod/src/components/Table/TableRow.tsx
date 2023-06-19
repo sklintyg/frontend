ad550e3bf147fcc763e1f74e2cc414cdf9db8939
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-06-19 16:15:36 +0200

    INTYGFV-15388: TS fix

--- apps/rehabstod/src/components/Table/TableRow.tsx
+++ apps/rehabstod/src/components/Table/TableRow.tsx
@@ -11,22 +11,20 @@ export function TableRow({
   id: string
   children: ReactNode
 }) {
-  const handleOnKeyDown = (event: KeyboardEvent) => {
-    if (['Enter', 'Space'].includes(event.code)) {
-      onClick(id)
-    }
-    if (event.code === 'ArrowUp' && event.currentTarget.previousElementSibling) {
-      ;(event.currentTarget.previousElementSibling as HTMLElement).focus()
-    }
-    if (event.code === 'ArrowDown' && event.currentTarget.nextElementSibling) {
-      ;(event.currentTarget.nextElementSibling as HTMLElement).focus()
-    }
-  }
-
   return (
     <tr
       tabIndex={0}
-      onKeyDown={(event) => handleOnKeyDown(event)}
+      onKeyDown={({ code, currentTarget }) => {
+        if (['Enter', 'Space'].includes(code)) {
+          onClick(id)
+        }
+        if (code === 'ArrowUp' && currentTarget.previousElementSibling) {
+          ;(currentTarget.previousElementSibling as HTMLElement).focus()
+        }
+        if (code === 'ArrowDown' && currentTarget.nextElementSibling) {
+          ;(currentTarget.nextElementSibling as HTMLElement).focus()
+        }
+      }}
       onClick={() => onClick(id)}
       key={id}
       className={`hover:scale-100 hover:cursor-pointer hover:shadow-[0_0_10px_rgba(0,0,0,0.3)] ${italic ? 'italic' : ''} print:hidden`}
