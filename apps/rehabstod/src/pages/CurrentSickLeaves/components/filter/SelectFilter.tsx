511d29dd9bfb41f903e914558778e64295eaf017
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-06-19 14:24:59 +0200

    INTYGFV-15644: Ärendestatus filter not persisting (#977)

--- apps/rehabstod/src/pages/CurrentSickLeaves/components/filter/SelectFilter.tsx
+++ apps/rehabstod/src/pages/CurrentSickLeaves/components/filter/SelectFilter.tsx
@@ -7,29 +7,35 @@ export function SelectFilter({
   onChange,
   options,
   description,
+  value,
   label,
 }: {
   onChange: (option: string) => void
   options: { id: string; name: string }[]
   description: string
+  value?: string
   label: string
 }) {
   const id = useId()
 
   return (
-    <div className="flex-1">
-      <IDSSelect className="m-0">
-        <label htmlFor={id}>
-          {label}
-          {description && <TooltipIcon description={description} icon={<IDSIconQuestion size="s" className="relative top-1 ml-2" />} />}
-        </label>
-        <Select id={id} onChange={(event) => onChange(event.currentTarget.value)}>
-          <option className="ml-2" id="" value="">
-            Visa alla
-          </option>
-          {options ? options.map((option) => <option value={option.id} key={option.id} label={option.name} />) : null}
-        </Select>
-      </IDSSelect>
-    </div>
+    <IDSSelect className="m-0">
+      <label htmlFor={id}>
+        {label}
+        {description && <TooltipIcon description={description} icon={<IDSIconQuestion size="s" className="relative top-1 ml-2" />} />}
+      </label>
+      <Select id={id} value={value} onChange={(event) => onChange(event.currentTarget.value)}>
+        <option className="ml-2" id="" value="">
+          Visa alla
+        </option>
+        {options
+          ? options.map((option) => (
+              <option value={option.id} key={option.id}>
+                {option.name}
+              </option>
+            ))
+          : null}
+      </Select>
+    </IDSSelect>
   )
 }
