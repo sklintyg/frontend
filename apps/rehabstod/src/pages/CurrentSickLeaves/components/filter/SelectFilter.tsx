70745b428c6bc4c71c793ffaabdb0f5fd74107e8
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-06-19 10:49:18 +0200

    Add test for selected value

--- apps/rehabstod/src/pages/CurrentSickLeaves/components/filter/SelectFilter.tsx
+++ apps/rehabstod/src/pages/CurrentSickLeaves/components/filter/SelectFilter.tsx
@@ -19,19 +19,23 @@ export function SelectFilter({
   const id = useId()
 
   return (
-    <div className="flex-1">
-      <IDSSelect className="m-0">
-        <label htmlFor={id}>
-          {label}
-          {description && <TooltipIcon description={description} icon={<IDSIconQuestion size="s" className="relative top-1 ml-2" />} />}
-        </label>
-        <Select id={id} value={value} onChange={(event) => onChange(event.currentTarget.value)}>
-          <option className="ml-2" id="" value="">
-            Visa alla
-          </option>
-          {options ? options.map((option) => <option value={option.id} key={option.id} label={option.name} />) : null}
-        </Select>
-      </IDSSelect>
-    </div>
+    <IDSSelect className="m-0">
+      <label htmlFor={id}>
+        {label}
+        {description && <TooltipIcon description={description} icon={<IDSIconQuestion size="s" className="relative top-1 ml-2" />} />}
+      </label>
+      <Select id={id} value={value} onChange={(event) => onChange(event.currentTarget.value)}>
+        <option className="ml-2" id="" value="">
+          Visa alla
+        </option>
+        {options
+          ? options.map((option) => (
+              <option value={option.id} key={option.id}>
+                {option.name}
+              </option>
+            ))
+          : null}
+      </Select>
+    </IDSSelect>
   )
 }
