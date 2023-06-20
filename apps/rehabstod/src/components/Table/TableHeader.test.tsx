27c65ee7fcc5bceb9201e98ff0b6eb77c07be7a3
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-06-20 15:35:06 +0200

    INTYGFV-15388: Added tests

---
+++ apps/rehabstod/src/components/Table/TableHeader.test.tsx
@@ -0,0 +1,33 @@
+import { Column } from 'react-stately'
+import { render, screen } from '@testing-library/react'
+import { TableHeader } from './TableHeader'
+import { Table } from './Table'
+
+const columns: Column[] = [
+  { name: 'Personnummer', width: 50 },
+  { name: 'Ålder', width: 100, description: 'Namn beskrivning' },
+]
+
+const renderComponent = () => {
+  render(
+    <Table>
+      <TableHeader columns={columns} />
+    </Table>
+  )
+}
+
+describe('TableHeader', () => {
+  it('should render component without throwing error', () => {
+    expect(() => renderComponent()).not.toThrow()
+  })
+
+  it('should present column name', () => {
+    renderComponent()
+    expect(screen.getByText('Personnummer')).toBeInTheDocument()
+  })
+
+  it('should present column name with swedish letters', () => {
+    renderComponent()
+    expect(screen.getByText('Ålder')).toBeInTheDocument()
+  })
+})
