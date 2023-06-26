2e516c6f500c6098c1cab9441920990b4d889a05
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-06-26 08:57:54 +0200

    INTYGFV-15357: TS fixes

--- apps/rehabstod/src/components/SelectRekoStatus/SelectRekoStatus.test.tsx
+++ apps/rehabstod/src/components/SelectRekoStatus/SelectRekoStatus.test.tsx
@@ -1,18 +1,15 @@
 import { screen } from '@testing-library/react'
 import { SelectRekoStatus } from './SelectRekoStatus'
-import { RekoStatusType } from '../../schemas/sickLeaveSchema'
+import { RekoStatus } from '../../schemas/sickLeaveSchema'
 import { renderWithRouter } from '../../utils/renderWithRouter'
 
-const TYPES = [
-  { id: 'id', name: 'name' },
-  { id: 'id1', name: 'name1' },
-]
+const TYPES = [{ status: { id: 'id', name: 'name' } }, { status: { id: 'id1', name: 'name1' } }]
 const PATIENT_ID = '1912121212'
 const END_DATE = '2022-01-01'
 
-const renderComponent = (statusFromSickLeave: RekoStatusType) => {
+const renderComponent = (statusFromSickLeave: RekoStatus) => {
   renderWithRouter(
-    <SelectRekoStatus statusFromSickLeave={statusFromSickLeave} patientId={PATIENT_ID} endDate={END_DATE} rekoStatusTypes={TYPES} />
+    <SelectRekoStatus statusFromSickLeave={statusFromSickLeave.status} patientId={PATIENT_ID} endDate={END_DATE} rekoStatusTypes={TYPES} />
   )
 }
 
@@ -33,12 +30,6 @@ describe('SelectRekoStatus', () => {
 
   it('should render options', () => {
     renderComponent(TYPES[0])
-    expect(screen.getByText(TYPES[0].name)).toBeInTheDocument()
-    expect(screen.getByText(TYPES[1].name)).toBeInTheDocument()
-  })
-
-  it('should set statusFromSickLeave as chosen option', () => {
-    renderComponent(TYPES[0])
-    expect(screen.getByRole('combobox')).toHaveValue(TYPES[0].id)
+    expect(screen.getAllByRole('option')).toHaveLength(TYPES.length)
   })
 })
