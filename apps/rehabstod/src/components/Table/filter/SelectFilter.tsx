d761107c8d025300f364af9c24b4c0f74713b7f9
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-06-29 09:11:52 +0200

    INTYGFV-15388: Added missing truncate from Filter Arendestatus.

--- apps/rehabstod/src/components/Table/filter/SelectFilter.tsx
+++ apps/rehabstod/src/components/Table/filter/SelectFilter.tsx
@@ -1,5 +1,5 @@
 import { IDSIconQuestion, IDSSelect } from '@frontend/ids-react-ts'
-import { useId } from 'react'
+import React, { useId, useState } from 'react'
 import { Select } from '../../Form/Select'
 import { TooltipIcon } from '../../TooltipIcon/TooltipIcon'
 import { PrintTitle } from '../print/PrintTitle'
@@ -20,27 +20,31 @@ export function SelectFilter({
   hideDefaultValue?: boolean
 }) {
   const id = useId()
+  const [open, setOpen] = useState(false)
 
   const chosenOption = options ? options.find((option) => option.id === value) : undefined
 
   return (
     <>
       <div className="flex-1 print:hidden">
+        <label htmlFor={id}>
+          {label}
+          {description && <TooltipIcon description={description} icon={<IDSIconQuestion size="s" className="relative top-1 ml-2" />} />}
+        </label>
         <IDSSelect className="m-0">
-          <label htmlFor={id}>
-            {label}
-            {description && <TooltipIcon description={description} icon={<IDSIconQuestion size="s" className="relative top-1 ml-2" />} />}
-          </label>
-          <Select value={value} id={id} onChange={(event) => onChange(event.currentTarget.value)}>
-            {!hideDefaultValue && (
-              <option className="ml-2" id="" value="">
-                Visa alla
-              </option>
-            )}
-            {options ? options.map((option) => <option value={option.id} key={option.id} label={option.name} />) : null}
-          </Select>
+          <div className="relative">
+            <Select onClick={() => setOpen(!open)} value={value} id={id} onChange={(event) => onChange(event.currentTarget.value)}>
+              {!hideDefaultValue && (
+                <option className="ml-2" id="" value="">
+                  Visa alla{' '}
+                </option>
+              )}
+              {options ? options.map((option) => <option value={option.id} key={option.id} label={option.name} />) : null}
+            </Select>
+          </div>
         </IDSSelect>
       </div>
+
       <div className="hidden print:block">
         <PrintTitle title={label} />
         {chosenOption ? chosenOption.name : '-'}
