d882e52ea2df160bb3ea2c9fa7eb774ca7310dc9
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-06-30 13:19:25 +0200

    remove files with wrong casing

--- apps/rehabstod/src/store/slices/LUUnitTableColumns.selector.ts
+++
@@ -1,6 +0,0 @@
-import { RootState } from '../store'
-import { getLUUnitTableColumnsSelector } from './luUnitTableColumns.slice'
-
-export const { selectAll: allLUUnitTableColumns, selectColumnString: luUnitTableColumnsString } = getLUUnitTableColumnsSelector(
-  (state: RootState) => state.lakarutlatandeUnitTableColumns
-)
