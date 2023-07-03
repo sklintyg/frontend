cb01cca10dfe78547036c2c7b9811cce2b36f4b4
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-07-03 16:03:34 +0200

    INTYGFV-15762: Fixed test

--- apps/rehabstod/src/pages/Patient/utils/getCertificateColumnData.test.ts
+++ apps/rehabstod/src/pages/Patient/utils/getCertificateColumnData.test.ts
@@ -3,7 +3,10 @@ import { patientSjukfallIntygSchema } from '../../../schemas/patientSchema'
 import { PatientColumn } from '../../../store/slices/patientTableColumns.slice'
 import { getCertificateColumnData } from './getCertificateColumnData'
 
-it.each(Object.values(PatientColumn).filter((name) => name !== PatientColumn.Intyg))('Should return value on column %s', (column) => {
-  const certificate = fakerFromSchema(patientSjukfallIntygSchema)()
-  expect(getCertificateColumnData(column, certificate, [certificate])?.toString()).toBeTruthy()
-})
+it.each(Object.values(PatientColumn).filter((name) => name !== PatientColumn.Intyg && name !== PatientColumn.Intygstyp))(
+  'Should return value on column %s',
+  (column) => {
+    const certificate = fakerFromSchema(patientSjukfallIntygSchema)()
+    expect(getCertificateColumnData(column, certificate, [certificate])?.toString()).toBeTruthy()
+  }
+)
