8a7b0b9b3885044ab948a0bdc25798b106beea4d
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-07-03 09:48:52 +0200

    INTYGFV-15764: Remove react error for DatePickerButton (#1012)

--- apps/rehabstod/src/components/Form/Date/DatePickerButton.tsx
+++ apps/rehabstod/src/components/Form/Date/DatePickerButton.tsx
@@ -1,14 +1,15 @@
 import { IDSIconCalendar } from '@frontend/ids-react-ts'
 import { useRef } from 'react'
 import { AriaButtonProps, useButton } from 'react-aria'
+import { PopoverTrigger } from '../../Popover/PopoverTrigger'
 
 export function DatePickerButton(props: AriaButtonProps) {
   const ref = useRef<HTMLButtonElement>(null)
   const { buttonProps } = useButton(props, ref)
 
   return (
-    <button type="button" {...buttonProps} ref={ref} className="h-full w-full py-3 px-5">
+    <PopoverTrigger ref={ref} {...buttonProps} className="py-3 px-5">
       <IDSIconCalendar width="1.25em" height="1.25em" />
-    </button>
+    </PopoverTrigger>
   )
 }
