a5d769755572987d39d1adb7b6f5ee94756ff001
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-07-04 14:49:34 +0200

    Listen to resize for FixedTable

--- apps/rehabstod/src/components/Table/FixedTable.tsx
+++ apps/rehabstod/src/components/Table/FixedTable.tsx
@@ -1,19 +1,30 @@
-import { ReactNode, RefObject, useEffect, useRef, useState } from 'react'
+import { ReactNode, RefObject, useEffect, useState } from 'react'
+import { mergeRefs } from 'react-merge-refs'
+import useResizeObserver from 'use-resize-observer'
 import { classNames } from '../../utils/classNames'
 
 export function FixedTable({ children, scrollRef }: { children: ReactNode; scrollRef: RefObject<HTMLDivElement> }) {
-  const [tableRect, setTableRect] = useState<DOMRect>()
   const [scrollLeft, setScrollLeft] = useState(0)
+  const [tableWidth, setTableWidth] = useState(0)
   const [fixed, setFixed] = useState(false)
-  const outerDiv = useRef<HTMLDivElement>(null)
-  const tableWidth = tableRect?.width ?? 0
+
+  const { ref } = useResizeObserver<HTMLDivElement>()
+  mergeRefs([
+    ref,
+    scrollRef,
+    (element: HTMLDivElement) => {
+      if (element) {
+        setTableWidth(element.getBoundingClientRect().width)
+      }
+    },
+  ])
 
   useEffect(() => {
     const handleScroll = () => {
-      const parentRect = outerDiv?.current?.parentElement?.getBoundingClientRect()
-      if (parentRect) {
-        setFixed(parentRect.top < 0 && parentRect.bottom > 0)
-        setTableRect(parentRect)
+      const scrollElementRect = scrollRef.current?.getBoundingClientRect()
+      if (scrollElementRect) {
+        setFixed(scrollElementRect.top < 0 && scrollElementRect.bottom > 0)
+        setTableWidth(scrollElementRect.width)
       }
       setScrollLeft(scrollRef.current?.scrollLeft ?? 0)
     }
@@ -25,11 +36,7 @@ export function FixedTable({ children, scrollRef }: { children: ReactNode; scrol
   })
 
   return (
-    <div
-      ref={outerDiv}
-      style={{ width: `${tableWidth}px` }}
-      className={classNames('fixed top-0 z-20 overflow-hidden', fixed ? 'block' : 'hidden')}
-    >
+    <div style={{ width: `${tableWidth}px` }} className={classNames('fixed top-0 z-20 overflow-hidden', fixed ? 'block' : 'hidden')}>
       <div style={{ width: `${tableWidth + scrollLeft}px` }} className="float-right">
         <table className="ids-table whitespace-nowrap border-none text-sm" style={{ width: `${tableWidth}px` }}>
           {children}
