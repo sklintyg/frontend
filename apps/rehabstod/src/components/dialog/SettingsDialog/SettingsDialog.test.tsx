d6cacbd177b60b10fc38f6e65ee933d40ef0e435
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-07-04 08:40:38 +0200

    fix ts issue

--- apps/rehabstod/src/components/dialog/SettingsDialog/SettingsDialog.test.tsx
+++ apps/rehabstod/src/components/dialog/SettingsDialog/SettingsDialog.test.tsx
@@ -1,7 +1,6 @@
 import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { rest } from 'msw'
-import { vi } from 'vitest'
 import { server } from '../../../mocks/server'
 import { updateSettings } from '../../../store/slices/settings.slice'
 import { store } from '../../../store/store'
@@ -10,27 +9,27 @@ import { renderWithRouter } from '../../../utils/renderWithRouter'
 import { SettingsDialog } from './SettingsDialog'
 
 it('should render without errors', () => {
-  expect(() => renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)).not.toThrow()
+  expect(() => renderWithRouter(<SettingsDialog />)).not.toThrow()
 })
 
 it('should render save button', async () => {
-  renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+  renderWithRouter(<SettingsDialog />)
   expect(await screen.findByText('Spara')).toBeInTheDocument()
 })
 
 it('should render cancel button', async () => {
-  renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+  renderWithRouter(<SettingsDialog />)
   expect(await screen.findByText('Avbryt')).toBeInTheDocument()
 })
 
 it('should close when clicking cancel button', async () => {
-  renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+  renderWithRouter(<SettingsDialog />)
   await userEvent.click(await screen.findByText('Avbryt'))
   expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
 })
 
 it('should close when clicking save button', async () => {
-  renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+  renderWithRouter(<SettingsDialog />)
   await userEvent.click(await screen.findByText('Spara'))
   expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
 })
@@ -39,18 +38,18 @@ it('should not have save button disabled as default', async () => {
   const preferences = { maxAntalDagarMellanIntyg: '12', maxAntalDagarSedanSjukfallAvslut: '6' }
   server.use(rest.get('/api/user', (_, res, ctx) => res(ctx.status(200), ctx.json(fakeUser({ preferences })))))
   store.dispatch(updateSettings(preferences))
-  renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+  renderWithRouter(<SettingsDialog />)
   expect(await screen.findByText('Spara')).not.toBeDisabled()
 })
 
 describe('days after finished sick leaves', () => {
   it('should render title', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     expect(await screen.findByText('Visa nyligen avslutade sjukfall')).toBeInTheDocument()
   })
 
   it('should render text', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     expect(
       await screen.findByText(
         'Välj maximalt antal dagar som får ha passerat efter ett sjukfalls slutdatum för att sjukfallet ska visas upp i sjukfallstabellen. Med denna funktion kan du bevaka de sjukfall som är nyligen avslutade.'
@@ -59,24 +58,24 @@ describe('days after finished sick leaves', () => {
   })
 
   it('should render input', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     expect(await screen.findByText('Max antal dagar sedan avslut (0-14 dagar)')).toBeInTheDocument()
   })
 
   it('should disable save button if input is over limit', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     await userEvent.type(await screen.findByLabelText('Max antal dagar sedan avslut (0-14 dagar)'), '800')
     expect(screen.getByText('Spara')).toBeDisabled()
   })
 
   it('should disable save button if input is under limit', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     await userEvent.type(await screen.findByLabelText('Max antal dagar sedan avslut (0-14 dagar)'), '-6')
     expect(screen.getByText('Spara')).toBeDisabled()
   })
 
   it('should disable save button if input is empty limit', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     await userEvent.clear(await screen.findByLabelText('Max antal dagar sedan avslut (0-14 dagar)'))
     expect(screen.getByLabelText('Max antal dagar sedan avslut (0-14 dagar)')).toHaveValue(null)
     expect(screen.getByText('Spara')).toBeDisabled()
@@ -85,12 +84,12 @@ describe('days after finished sick leaves', () => {
 
 describe('days between sick leaves', () => {
   it('should render title', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     expect(await screen.findByText('Antal dagar mellan intyg')).toBeInTheDocument()
   })
 
   it('should render text', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     expect(
       await screen.findByText(
         'Välj hur många dagars uppehåll det maximalt får vara mellan två intyg för att de ska räknas till samma sjukfall.'
@@ -99,24 +98,24 @@ describe('days between sick leaves', () => {
   })
 
   it('should render input', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     expect(await screen.findByLabelText(/dagar mellan intyg/i)).toBeInTheDocument()
   })
 
   it('should disable save button if input is over limit', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     await userEvent.type(await screen.findByLabelText(/dagar mellan intyg/i), '800')
     expect(await screen.findByText('Spara')).toBeDisabled()
   })
 
   it('should disable save button if input is under limit', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     await userEvent.type(await screen.findByLabelText(/dagar mellan intyg/i), '-5')
     expect(screen.getByText('Spara')).toBeDisabled()
   })
 
   it('should disable save button if input is empty limit', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     await userEvent.clear(await screen.findByLabelText(/dagar mellan intyg/i))
     expect(screen.getByLabelText(/dagar mellan intyg/i)).toHaveValue(null)
     expect(screen.getByText('Spara')).toBeDisabled()
@@ -125,11 +124,11 @@ describe('days between sick leaves', () => {
 
 describe('selected care unit', () => {
   it('should render title', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     expect(await screen.findByText('Förvald enhet')).toBeInTheDocument()
   })
   it('should render text', async () => {
-    renderWithRouter(<SettingsDialog onVisibilityChanged={vi.fn()} />)
+    renderWithRouter(<SettingsDialog />)
     expect(await screen.findByText(/du kan välja en enhet som du automatiskt loggas in på när rehabstöd startas/i)).toBeInTheDocument()
   })
 })
