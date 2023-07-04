4d7dcd9f1f8b20d09613451ce1fa5ae2990ac7a1
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-07-04 09:59:15 +0200

    cleanup

--- apps/rehabstod/src/components/Table/Table.tsx
+++ apps/rehabstod/src/components/Table/Table.tsx
@@ -1,8 +1,28 @@
 import { IDSContainer } from '@frontend/ids-react-ts'
 import { ReactNode } from 'react'
+import { mergeRefs } from 'react-merge-refs'
+import useResizeObserver from 'use-resize-observer'
 import { FloatingTableScroll } from '../FloatingScroll/FloatingTableScroll'
-import { TableContentDiv } from './TableContentDiv'
 import { TableContext, TableOptions, useTable } from './hooks/useTable'
+import { useTableContext } from './hooks/useTableContext'
+
+function TableWrapper({ children }: { children: ReactNode }) {
+  const { ref } = useResizeObserver<HTMLDivElement>()
+  const tableContext = useTableContext()
+  const mergedRefCallback = mergeRefs([
+    ref,
+    (element: HTMLDivElement) => {
+      if (element) {
+        tableContext.setTableWidth(element.getBoundingClientRect().width)
+      }
+    },
+  ])
+  return (
+    <div ref={mergedRefCallback} className="relative pb-4 pt-1">
+      {children}
+    </div>
+  )
+}
 
 export function Table({ children, print, ...options }: { children?: ReactNode; print?: ReactNode } & TableOptions) {
   const table = useTable(options)
@@ -11,9 +31,9 @@ export function Table({ children, print, ...options }: { children?: ReactNode; p
     <TableContext.Provider value={table}>
       <IDSContainer gutterless className="print:hidden">
         <FloatingTableScroll>
-          <TableContentDiv>
+          <TableWrapper>
             <table className="ids-table w-full overflow-visible whitespace-nowrap border-none text-sm">{children}</table>
-          </TableContentDiv>
+          </TableWrapper>
         </FloatingTableScroll>
       </IDSContainer>
       <div className="hidden print:block">
