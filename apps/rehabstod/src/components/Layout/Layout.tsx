3a387bfa69ea4fabb5c184f8ef879dde33f9407d
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-07-05 10:46:53 +0200

    Fix import issue

--- apps/rehabstod/src/components/Layout/Layout.tsx
+++ apps/rehabstod/src/components/Layout/Layout.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import { useSession } from '../../hooks/useSession'
-import { SettingsDialog } from '../SettingsDialog/SettingsDialog'
-import { LayoutFooter } from './LayoutFooter'
-import { LayoutHeader } from './LayoutHeader'
 import { useGetConfigQuery } from '../../store/api'
 import { GlobalAlert } from '../GlobalAlert/GlobalAlert'
+import { AboutDialog } from '../dialog/AboutDialog'
+import { SettingsDialog } from '../dialog/SettingsDialog/SettingsDialog'
+import { LayoutFooter } from './LayoutFooter'
+import { LayoutHeader } from './LayoutHeader/LayoutHeader'
 
 export function Layout() {
   useSession()
@@ -16,6 +17,7 @@ export function Layout() {
       <main className="flex-1">
         {config && config.banners.length > 0 && <GlobalAlert>{config.banners[0].message}</GlobalAlert>}
         <SettingsDialog />
+        <AboutDialog />
         <Outlet />
       </main>
       <LayoutFooter />
