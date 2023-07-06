54ffb2c1d263c8d0dd59d95cbf45618415ea929f
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-07-06 14:49:06 +0200

    fix validateDOMNesting error in test

--- apps/rehabstod/src/components/Table/FloatingTableScroll/FloatingTableScroll.test.tsx
+++ apps/rehabstod/src/components/Table/FloatingTableScroll/FloatingTableScroll.test.tsx
@@ -1,22 +1,11 @@
 import { render } from '@testing-library/react'
-import { Table } from '../Table'
 import { FloatingTableScroll } from './FloatingTableScroll'
 
 it('Should render without issues', () => {
-  expect(() =>
-    render(
-      <Table>
-        <FloatingTableScroll>Hello</FloatingTableScroll>
-      </Table>
-    )
-  ).not.toThrow()
+  expect(() => render(<FloatingTableScroll>Hello</FloatingTableScroll>)).not.toThrow()
 })
 
 it('Should add handyscroll to element', async () => {
-  const { container } = render(
-    <Table>
-      <FloatingTableScroll>Hello</FloatingTableScroll>
-    </Table>
-  )
+  const { container } = render(<FloatingTableScroll>Hello</FloatingTableScroll>)
   expect(container.querySelector('.handy-scroll')).toBeInTheDocument()
 })
