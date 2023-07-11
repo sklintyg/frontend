67878dc8702f63b68df67c3930d8acbef227b98f
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-07-11 14:15:05 +0200

    INTYGFV-15774: Expandables should be closed as default

--- apps/rehabstod/src/components/PatientAccordion/PatientAccordion.tsx
+++ apps/rehabstod/src/components/PatientAccordion/PatientAccordion.tsx
@@ -1,9 +1,9 @@
 import { IDSIconChevron } from '@frontend/ids-react-ts'
 import { ReactNode } from 'react'
 
-export function PatientAccordion({ title, children }: { title: string; children: ReactNode }) {
+export function PatientAccordion({ title, children, open = true }: { title: string; children: ReactNode; open: boolean }) {
   return (
-    <details open className="group [&:not(:last-child)]:mb-5">
+    <details open={open} className="group [&:not(:last-child)]:mb-5">
       <summary role="button" className="border-neutral-40 mb-2.5 flex cursor-pointer items-center space-x-2 border-b py-5">
         <h3 className="ids-heading-4 text-accent-40 m-0 grow">{title}</h3>
         <span className="inline-block h-5 w-5 origin-center rotate-90 justify-self-end group-open:-rotate-90">
