09cd6450864617dac1651ad4088067ecda48d622
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2023-07-12 09:56:10 +0200

    DEVENV: Attempt to fix flaky tests (#1034)

--- apps/rehabstod/src/components/Layout/LayoutFooter.test.tsx
+++ apps/rehabstod/src/components/Layout/LayoutFooter.test.tsx
@@ -5,6 +5,12 @@ import { LayoutFooter } from './LayoutFooter'
 it('Should display links', async () => {
   renderWithRouter(<LayoutFooter />)
 
+  expect(
+    screen.getByText(
+      'Rehabstöd används av rehabkoordinatorer och läkare för att samordna och följa upp sjukskrivna patienters rehabilitering.'
+    )
+  ).toBeInTheDocument()
+
   expect(await screen.findByText('ineraManualRehabstod')).toBeInTheDocument()
   expect(await screen.findByText('ineraNationellKundservice')).toBeInTheDocument()
   expect(await screen.findByText('ineraMainPage')).toBeInTheDocument()
