03205c5c9166a09a0ae8570a3a0149c57aa97b8f
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2023-08-02 08:31:05 +0200

    INTYGFV-15732: Moved check if initial state has loaded (#1059)

--- apps/webcert/src/App.tsx
+++ apps/webcert/src/App.tsx
@@ -1,10 +1,8 @@
-import { SpinnerBackdrop } from '@frontend/common'
 import { ConnectedRouter } from 'connected-react-router'
 import 'inera-core-css/dist/inera-master.css'
 import { useEffect } from 'react'
 import 'react-datepicker/dist/react-datepicker.css'
 import { ErrorBoundary, FallbackProps } from 'react-error-boundary'
-import { useSelector } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
 import ErrorComponent from './components/error/ErrorComponent'
 import CareProviderModal from './feature/careProvider/CareProviderModal'
@@ -26,13 +24,11 @@ import { ErrorCode, ErrorType } from './store/error/errorReducer'
 import { useAppDispatch } from './store/store'
 import { cancelLogout, getUser, getUserStatistics, triggerLogout } from './store/user/userActions'
 import { getAllDynamicLinks, getConfig } from './store/utils/utilsActions'
-import { selectIsLoadingInitialState } from './store/utils/utilsSelectors'
 
 const ErrorMessage = ({ error }: FallbackProps) => <>Ett fel har inträffat: {error.message}</>
 
 function App(): JSX.Element {
   const dispatch = useAppDispatch()
-  const isLoadingInitialState = useSelector(selectIsLoadingInitialState)
 
   useEffect(() => {
     const handleWindowBeforeUnload = () => dispatch(triggerLogout())
@@ -57,28 +53,26 @@ function App(): JSX.Element {
   }
 
   return (
-    <SpinnerBackdrop open={isLoadingInitialState} spinnerText="Laddar...">
-      <ConnectedRouter history={history}>
-        <ErrorBoundary fallbackRender={ErrorMessage} onError={onError}>
-          <ErrorComponent />
-          <CareProviderModal />
-          <SubscriptionWarningModal />
-          <WarningNormalOriginModal />
-          <Switch>
-            <Route path="/" exact render={() => <StartPageWithRedirect />} />
-            <Route path="/certificate/:certificateId/sign/:error" render={() => <CertificatePage />} />
-            <Route path="/certificate/:certificateId" render={() => <CertificatePage />} />
-            <Route path="/welcome(.html)?" render={() => <Welcome />} />
-            <Route path="/error(.jsp)?" render={() => <ErrorPage />} />
-            <Route path="/create/:patientId?" render={() => <CreatePageWithRedirect />} />
-            <Route path="/search" render={() => <SearchPageWithRedirect />} />
-            <Route path="/list/draft" render={() => <CertificateDraftsPage />} />
-            <Route path="/list/certificate" render={() => <SignedCertificatesPage />} />
-            <Route path="/list/unhandledcertificates" render={() => <UnhandledCertificatesPage />} />
-          </Switch>
-        </ErrorBoundary>
-      </ConnectedRouter>
-    </SpinnerBackdrop>
+    <ConnectedRouter history={history}>
+      <ErrorBoundary fallbackRender={ErrorMessage} onError={onError}>
+        <ErrorComponent />
+        <CareProviderModal />
+        <SubscriptionWarningModal />
+        <WarningNormalOriginModal />
+        <Switch>
+          <Route path="/" exact render={() => <StartPageWithRedirect />} />
+          <Route path="/certificate/:certificateId/sign/:error" render={() => <CertificatePage />} />
+          <Route path="/certificate/:certificateId" render={() => <CertificatePage />} />
+          <Route path="/welcome(.html)?" render={() => <Welcome />} />
+          <Route path="/error(.jsp)?" render={() => <ErrorPage />} />
+          <Route path="/create/:patientId?" render={() => <CreatePageWithRedirect />} />
+          <Route path="/search" render={() => <SearchPageWithRedirect />} />
+          <Route path="/list/draft" render={() => <CertificateDraftsPage />} />
+          <Route path="/list/certificate" render={() => <SignedCertificatesPage />} />
+          <Route path="/list/unhandledcertificates" render={() => <UnhandledCertificatesPage />} />
+        </Switch>
+      </ErrorBoundary>
+    </ConnectedRouter>
   )
 }
 
