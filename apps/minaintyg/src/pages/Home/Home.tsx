8698656a56d04333fa81a79f1001391d4510441b
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-08-22 09:53:28 +0200

    Revert "add config api and schemas"
    This reverts commit d35a86d61a5debcda3c8a057ad359407c6998459.

--- apps/minaintyg/src/pages/Home/Home.tsx
+++ apps/minaintyg/src/pages/Home/Home.tsx
@@ -1,7 +1,3 @@
-import { useGetAppConfigQuery, useGetCertificateDescriptionQuery } from '../../store/configApi'
-
 export function Home() {
-  const { data: config } = useGetAppConfigQuery()
-  const { data: certificateDescription } = useGetCertificateDescriptionQuery()
   return <p>Start</p>
 }
--- apps/minaintyg/src/store/store.ts
+++ apps/minaintyg/src/store/store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { api } from './api'
-import { configApi } from './configApi'
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
-    [configApi.reducerPath]: configApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([api.middleware, configApi.middleware]),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([api.middleware]),
 })
 
 export type RootState = ReturnType<typeof store.getState>
