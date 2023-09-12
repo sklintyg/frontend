7f7919eba6c0b6e94f7035f8477d059b63ad1ca9
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-09-12 14:09:01 +0200

    lower coverage requirements for rs

--- apps/rehabstod/vitest.config.ts
+++ apps/rehabstod/vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       reporter: ['text', 'json', 'lcov'],
       branches: 80,
       lines: 80,
-      functions: 80,
+      functions: 70,
       statements: 80,
     },
   },
