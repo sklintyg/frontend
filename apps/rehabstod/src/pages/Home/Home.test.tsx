5f90552038868523a030e86e6cbe82e958874e08
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-09-15 13:44:22 +0200

    INTYGFV-15867: Use siths url from config instead of hardcoded in Home.tsx.

--- apps/rehabstod/src/pages/Home/Home.test.tsx
+++ apps/rehabstod/src/pages/Home/Home.test.tsx
@@ -1,6 +1,22 @@
+import { rest } from 'msw'
+import { screen, waitFor } from '@testing-library/react'
+import { vi } from 'vitest'
 import { renderWithRouter } from '../../utils/renderWithRouter'
 import { Home } from './Home'
+import { server, waitForRequest } from '../../mocks/server'
 
 it('Should render without error', () => {
   expect(() => renderWithRouter(<Home />)).not.toThrow()
+})
+
+it('Should use siths url provided by config request', async () => {
+  const expectedUrl = 'expectedUrl'
+  const openSpy = vi.spyOn(window, 'open')
+  server.use(rest.get('/api/config', (_, res, ctx) => res(ctx.status(200), ctx.json({ sithsIdpUrl: expectedUrl }))))
+  await waitForRequest('GET', '/api/config')
+  const { user } = renderWithRouter(<Home />)
+  await user.click(screen.getByText('Logga in'))
+  await waitFor(() => {
+    expect(openSpy).toHaveBeenCalledWith(expectedUrl, '_self')
+  })
 })
