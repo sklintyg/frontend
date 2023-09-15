af3b73a90215e64ad0bdebd0e11e1d31ba10a621
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-09-15 14:14:14 +0200

    INTYGFV-15867: Updated failing test.

--- apps/rehabstod/src/pages/Home/Home.test.tsx
+++ apps/rehabstod/src/pages/Home/Home.test.tsx
@@ -4,6 +4,8 @@ import { vi } from 'vitest'
 import { renderWithRouter } from '../../utils/renderWithRouter'
 import { Home } from './Home'
 import { server, waitForRequest } from '../../mocks/server'
+import { store } from '../../store/store'
+import { api } from '../../store/api'
 
 it('Should render without error', () => {
   expect(() => renderWithRouter(<Home />)).not.toThrow()
@@ -13,8 +15,9 @@ it('Should use siths url provided by config request', async () => {
   const expectedUrl = 'expectedUrl'
   const openSpy = vi.spyOn(window, 'open')
   server.use(rest.get('/api/config', (_, res, ctx) => res(ctx.status(200), ctx.json({ sithsIdpUrl: expectedUrl }))))
-  await waitForRequest('GET', '/api/config')
+  store.dispatch(api.endpoints.getConfig.initiate())
   const { user } = renderWithRouter(<Home />)
+  await waitForRequest('GET', '/api/config')
   await user.click(screen.getByText('Logga in'))
   await waitFor(() => {
     expect(openSpy).toHaveBeenCalledWith(expectedUrl, '_self')
