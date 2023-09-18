9a3aa3e8239b38ae7b714611bea3fa190f7b44e4
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-09-18 12:01:22 +0200

    Use new endpoint for fake login

--- apps/minaintyg/src/pages/Welcome/Welcome.tsx
+++ apps/minaintyg/src/pages/Welcome/Welcome.tsx
@@ -1,6 +1,6 @@
 import { IDSButton, IDSContainer, IDSSpinner } from '@frontend/ids-react-ts'
-import { useEffect, useRef, useState } from 'react'
-import { useGetPersonsQuery } from '../../store/testabilityApi'
+import { useEffect, useState } from 'react'
+import { useFakeLoginMutation, useGetPersonsQuery } from '../../store/testabilityApi'
 import { ResultTextArea } from './components/ResultTextArea'
 import { SelectProfile } from './components/SelectProfile'
 
@@ -8,7 +8,7 @@ export function Welcome() {
   const [profile, setProfile] = useState<string>()
   const [freeText, setFreeText] = useState<string>()
   const { data: persons, isLoading } = useGetPersonsQuery()
-  const formRef = useRef<HTMLFormElement>(null)
+  const [login] = useFakeLoginMutation()
 
   useEffect(() => {
     if (!freeText && persons) {
@@ -20,33 +20,40 @@ export function Welcome() {
     <IDSContainer>
       <div className="ids-content py-4">
         <h1 className="ids-heading-2">Testinloggningar Mina Intyg</h1>
-        <form ref={formRef} action={`/fake/sso?personId=${profile}`} method="POST" acceptCharset="UTF-8">
-          <div className="flex space-x-7 pb-4">
-            {isLoading && <IDSSpinner />}
-            {persons && (
-              <>
-                <div className="w-6/12 flex-auto">
-                  <SelectProfile
-                    value={profile}
-                    persons={persons}
-                    onChange={({ target }) => {
-                      setProfile(target.value)
-                      setFreeText(undefined)
-                    }}
-                  />
-                </div>
-                <div className="w-6/12 flex-auto">
-                  <ResultTextArea
-                    person={persons.find(({ personId }) => personId === profile)}
-                    freeText={freeText}
-                    onChange={(event) => setFreeText(event.target.value)}
-                  />
-                </div>
-              </>
-            )}
-          </div>
-          <IDSButton type="submit">Logga in</IDSButton>
-        </form>
+        <div className="flex space-x-7 pb-4">
+          {isLoading && <IDSSpinner />}
+          {persons && (
+            <>
+              <div className="w-6/12 flex-auto">
+                <SelectProfile
+                  value={profile}
+                  persons={persons}
+                  onChange={({ target }) => {
+                    setProfile(target.value)
+                    setFreeText(undefined)
+                  }}
+                />
+              </div>
+              <div className="w-6/12 flex-auto">
+                <ResultTextArea
+                  person={persons.find(({ personId }) => personId === profile)}
+                  freeText={freeText}
+                  onChange={(event) => setFreeText(event.target.value)}
+                />
+              </div>
+            </>
+          )}
+        </div>
+        <IDSButton
+          type="submit"
+          onClick={() => {
+            if (profile) {
+              login({ personId: profile })
+            }
+          }}
+        >
+          Logga in
+        </IDSButton>
       </div>
     </IDSContainer>
   )
