31d129cff43712ecb7bc8db6e392a0b1863f9ba6
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-09-18 08:35:41 +0200

    INTYGFV-15842: TS fixes

--- apps/webcert/src/components/fmb/FMBPanelGuidanceInfo.test.tsx
+++ apps/webcert/src/components/fmb/FMBPanelGuidanceInfo.test.tsx
@@ -1,15 +1,16 @@
 import { render, screen } from '@testing-library/react'
 import FMBPanelGuidanceInfo from './FMBPanelGuidanceInfo'
+import { FMBDiagnosisCodeInfoFormContentHeading } from '@frontend/common'
 
 describe('FMBPanelGuidanceInfo', () => {
   it('should show text if avaialble', () => {
-    render(<FMBPanelGuidanceInfo info={{ heading: 'heading', text: 'text' }} />)
+    render(<FMBPanelGuidanceInfo info={{ heading: FMBDiagnosisCodeInfoFormContentHeading.FMB_GENERAL_INFO, text: 'text' }} />)
 
     expect(screen.getByText('text')).toBeInTheDocument()
   })
 
   it('should show list text if avaialble', () => {
-    render(<FMBPanelGuidanceInfo info={{ heading: 'heading', list: ['1', '2'] }} />)
+    render(<FMBPanelGuidanceInfo info={{ heading: FMBDiagnosisCodeInfoFormContentHeading.FMB_GENERAL_INFO, list: ['1', '2'] }} />)
 
     expect(screen.getByText('1')).toBeInTheDocument()
     expect(screen.getByText('2')).toBeInTheDocument()
