c900ae2db6058c24febbababaf4c30153f659738
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-09-18 09:51:10 +0200

    simplify certificate list tests

--- apps/minaintyg/src/pages/Certificates/components/CertificateList.test.tsx
+++ apps/minaintyg/src/pages/Certificates/components/CertificateList.test.tsx
@@ -14,40 +14,35 @@ function renderComponent(props: ComponentProps<typeof CertificateList>) {
     </MemoryRouter>
   )
 }
-
-it('Should sort certificates by year in descending order', () => {
+describe('Group and sort by year', () => {
   const certificates = [
     fakeCertificate({ timestamp: '2021-01-01' }),
     fakeCertificate({ timestamp: '2022-01-01' }),
     fakeCertificate({ timestamp: '2023-01-01' }),
   ]
 
-  renderComponent({ certificates })
+  it('Should sort certificates by year in descending order', () => {
+    renderComponent({ certificates })
 
-  const yearHeadings = screen.getAllByRole('heading', { level: 2 })
-  expect(yearHeadings).toHaveLength(3)
-  expect(yearHeadings[0]).toHaveTextContent('2023')
-  expect(yearHeadings[1]).toHaveTextContent('2022')
-  expect(yearHeadings[2]).toHaveTextContent('2021')
+    const yearHeadings = screen.getAllByRole('heading', { level: 2 })
+    expect(yearHeadings).toHaveLength(3)
+    expect(yearHeadings[0]).toHaveTextContent('2023')
+    expect(yearHeadings[1]).toHaveTextContent('2022')
+    expect(yearHeadings[2]).toHaveTextContent('2021')
+  })
+
+  it('Should sort certificates by year in ascending order', () => {
+    renderComponent({ certificates, order: 'ascending' })
+
+    const yearHeadings = screen.getAllByRole('heading', { level: 2 })
+    expect(yearHeadings).toHaveLength(3)
+    expect(yearHeadings[0]).toHaveTextContent('2021')
+    expect(yearHeadings[1]).toHaveTextContent('2022')
+    expect(yearHeadings[2]).toHaveTextContent('2023')
+  })
 })
 
-it('Should sort certificates by year in ascending order', () => {
-  const certificates = [
-    fakeCertificate({ timestamp: '2021-01-01' }),
-    fakeCertificate({ timestamp: '2022-01-01' }),
-    fakeCertificate({ timestamp: '2023-01-01' }),
-  ]
-
-  renderComponent({ certificates, order: 'ascending' })
-
-  const yearHeadings = screen.getAllByRole('heading', { level: 2 })
-  expect(yearHeadings).toHaveLength(3)
-  expect(yearHeadings[0]).toHaveTextContent('2021')
-  expect(yearHeadings[1]).toHaveTextContent('2022')
-  expect(yearHeadings[2]).toHaveTextContent('2023')
-})
-
-it('Should sort certificates in descending order per year', () => {
+describe('Sort certificates', () => {
   const certificates = [
     fakeCertificate({ timestamp: '2022-01-01', title: 'Certificate A' }),
     fakeCertificate({ timestamp: '2022-02-02', title: 'Certificate B' }),
@@ -55,36 +50,31 @@ it('Should sort certificates in descending order per year', () => {
     fakeCertificate({ timestamp: '2023-01-01', title: 'Certificate D' }),
   ]
 
-  renderComponent({ certificates })
+  it('Should sort certificates in descending order per year', () => {
+    renderComponent({ certificates })
 
-  const container = screen.getByTestId('2022-certificates')
-  expect(container).toBeInTheDocument()
-  expect(screen.getByTestId('2023-certificates')).toBeInTheDocument()
-  expect(within(screen.getByTestId('2023-certificates')).getAllByRole('heading', { level: 3 })).toHaveLength(1)
+    const container = screen.getByTestId('2022-certificates')
+    expect(container).toBeInTheDocument()
+    expect(screen.getByTestId('2023-certificates')).toBeInTheDocument()
+    expect(within(screen.getByTestId('2023-certificates')).getAllByRole('heading', { level: 3 })).toHaveLength(1)
 
-  const certificateHeadings = within(container).getAllByRole('heading', { level: 3 })
-  expect(certificateHeadings).toHaveLength(3)
-  expect(certificateHeadings[0]).toHaveTextContent('Certificate B')
-  expect(certificateHeadings[1]).toHaveTextContent('Certificate C')
-  expect(certificateHeadings[2]).toHaveTextContent('Certificate A')
-})
+    const certificateHeadings = within(container).getAllByRole('heading', { level: 3 })
+    expect(certificateHeadings).toHaveLength(3)
+    expect(certificateHeadings[0]).toHaveTextContent('Certificate B')
+    expect(certificateHeadings[1]).toHaveTextContent('Certificate C')
+    expect(certificateHeadings[2]).toHaveTextContent('Certificate A')
+  })
 
-it('Should sort certificates in ascending order per year', () => {
-  const certificates = [
-    fakeCertificate({ timestamp: '2022-01-01', title: 'Certificate A' }),
-    fakeCertificate({ timestamp: '2022-02-02', title: 'Certificate B' }),
-    fakeCertificate({ timestamp: '2022-02-01', title: 'Certificate C' }),
-    fakeCertificate({ timestamp: '2023-01-01', title: 'Certificate D' }),
-  ]
+  it('Should sort certificates in ascending order per year', () => {
+    renderComponent({ certificates, order: 'ascending' })
 
-  renderComponent({ certificates, order: 'ascending' })
+    const container = screen.getByTestId('2022-certificates')
+    expect(container).toBeInTheDocument()
 
-  const container = screen.getByTestId('2022-certificates')
-  expect(container).toBeInTheDocument()
-
-  const certificateHeadings = within(container).getAllByRole('heading', { level: 3 })
-  expect(certificateHeadings).toHaveLength(3)
-  expect(certificateHeadings[0]).toHaveTextContent('Certificate A')
-  expect(certificateHeadings[1]).toHaveTextContent('Certificate C')
-  expect(certificateHeadings[2]).toHaveTextContent('Certificate B')
+    const certificateHeadings = within(container).getAllByRole('heading', { level: 3 })
+    expect(certificateHeadings).toHaveLength(3)
+    expect(certificateHeadings[0]).toHaveTextContent('Certificate A')
+    expect(certificateHeadings[1]).toHaveTextContent('Certificate C')
+    expect(certificateHeadings[2]).toHaveTextContent('Certificate B')
+  })
 })
