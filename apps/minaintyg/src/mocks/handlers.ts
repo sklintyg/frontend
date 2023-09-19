64fe8552d70b82270726756a3e568653bf031079
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-09-19 23:12:08 +0200

    update mocks

--- apps/minaintyg/src/mocks/handlers.ts
+++ apps/minaintyg/src/mocks/handlers.ts
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { fakeCertificateLabel, faker, fakerFromSchema } from '@frontend/fake'
+import { format, parseISO, subDays } from 'date-fns'
 import { rest } from 'msw'
-import { certificateListItemSchema } from '../schema/certificateList.schema'
+import { CertificateStatus, certificateListItemSchema } from '../schema/certificateList.schema'
 import { testabilityPersonSchema } from '../schema/testability/person.schema'
 import { userSchema } from '../schema/user.schema'
 
@@ -18,20 +19,30 @@ export const handlers = [
       )
     )
   ),
+
   rest.post('/api/certificate', (_, res, ctx) =>
     res(
       ctx.status(200),
       ctx.json({
-        content: Array.from(
-          { length: 5 },
-          fakerFromSchema(certificateListItemSchema, {
+        content: Array.from({ length: 5 }, () => {
+          const timestamp = faker.date.between('2021-01-01T00:00:00.000Z', new Date().toISOString()).toISOString()
+          const startDate = subDays(parseISO(timestamp), faker.datatype.number({ min: 1, max: 120 }))
+          const endDate = parseISO(timestamp)
+
+          return fakerFromSchema(certificateListItemSchema, {
             stringMap: {
               title: fakeCertificateLabel,
               certificateId: faker.datatype.uuid,
-              timestamp: () => faker.date.between('2021-01-01T00:00:00.000Z', new Date().toISOString()).toISOString(),
+              timestamp: () => timestamp,
             },
+          })({
+            statuses: faker.helpers.arrayElements(Object.values(CertificateStatus), faker.datatype.number({ min: 1, max: 2 })),
+            summary: faker.helpers.arrayElements([
+              ['intygsperiod', `${format(startDate, 'yyyy-MM-dd')} - ${format(endDate, 'yyyy-MM-dd')}`],
+              ['diagnos', 'Downs syndrom'],
+            ]),
           })
-        ),
+        }),
       })
     )
   ),
