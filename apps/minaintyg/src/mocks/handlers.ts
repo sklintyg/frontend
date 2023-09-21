c7339aa3cdae0b3a58c641d9cc8c9a0b891c0a18
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-09-21 08:24:58 +0200

    update mocks

--- apps/minaintyg/src/mocks/handlers.ts
+++ apps/minaintyg/src/mocks/handlers.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-import { fakeCertificate, fakeCertificateId, fakeCertificateLabel, faker, fakerFromSchema } from '@frontend/fake'
+import { fakeCertificate, fakeCertificateId, faker, fakerFromSchema } from '@frontend/fake'
 import { format, getYear, parseISO, subDays } from 'date-fns'
 import { rest } from 'msw'
 import { CertificateStatus, CertificateStatusEnum, certificateListItemSchema } from '../schema/certificateList.schema'
@@ -8,16 +8,22 @@ import { certificateFilterOptionsSchema } from '../schema/certificateListFilter.
 import { testabilityPersonSchema } from '../schema/testability/person.schema'
 import { userSchema } from '../schema/user.schema'
 
-const certificates = Array.from(
-  { length: 5 },
-  fakerFromSchema(certificateListItemSchema, {
-    stringMap: {
-      title: fakeCertificateLabel,
-      certificateId: faker.datatype.uuid,
-      timestamp: () => faker.date.between('2021-01-01T00:00:00.000Z', new Date().toISOString()).toISOString(),
-    },
+const certificates = Array.from({ length: 5 }, () => {
+  const timestamp = faker.date.between('2021-01-01T00:00:00.000Z', new Date().toISOString()).toISOString()
+  const startDate = subDays(parseISO(timestamp), faker.datatype.number({ min: 1, max: 120 }))
+  const endDate = parseISO(timestamp)
+  const certificate = fakeCertificate()
+
+  return fakerFromSchema(certificateListItemSchema)({
+    issued: timestamp,
+    type: { id: certificate.id, name: certificate.label, version: '1' },
+    statuses: faker.helpers.arrayElements(CertificateStatusEnum.options, faker.datatype.number({ min: 1, max: 2 })),
+    summary: faker.helpers.arrayElement([
+      { label: 'Gäller intygsperiod', value: `${format(startDate, 'yyyy-MM-dd')} - ${format(endDate, 'yyyy-MM-dd')}` },
+      { label: 'Avser diagnos', value: 'Downs syndrom' },
+    ]),
   })
-)
+})
 
 export const handlers = [
   rest.get('/api/user', (_, res, ctx) =>
@@ -33,29 +39,7 @@ export const handlers = [
     )
   ),
 
-  rest.post('/api/certificate', (_, res, ctx) =>
-    res(
-      ctx.status(200),
-      ctx.json({
-        content: Array.from({ length: 5 }, () => {
-          const timestamp = faker.date.between('2021-01-01T00:00:00.000Z', new Date().toISOString()).toISOString()
-          const startDate = subDays(parseISO(timestamp), faker.datatype.number({ min: 1, max: 120 }))
-          const endDate = parseISO(timestamp)
-          const certificate = fakeCertificate()
-
-          return fakerFromSchema(certificateListItemSchema)({
-            issued: timestamp,
-            type: { id: certificate.id, name: certificate.label, version: '1' },
-            statuses: faker.helpers.arrayElements(CertificateStatusEnum.options, faker.datatype.number({ min: 1, max: 2 })),
-            summary: faker.helpers.arrayElement([
-              { label: 'Gäller intygsperiod', value: `${format(startDate, 'yyyy-MM-dd')} - ${format(endDate, 'yyyy-MM-dd')}` },
-              { label: 'Avser diagnos', value: 'Downs syndrom' },
-            ]),
-          })
-        }),
-      })
-    )
-  ),
+  rest.post('/api/certificate', (_, res, ctx) => res(ctx.status(200), ctx.json({ content: certificates }))),
 
   rest.get('/api/filter-certificate', (_, res, ctx) =>
     res(
