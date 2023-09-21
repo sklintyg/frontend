dfa737c0a28b9b482d2fb0cfdb6e3443a3fc5137
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-09-21 08:33:17 +0200

    fix pixel issue

--- apps/minaintyg/src/components/FilterAccordion/FilterAccordion.tsx
+++ apps/minaintyg/src/components/FilterAccordion/FilterAccordion.tsx
@@ -6,7 +6,7 @@ export function FilterAccordion({ title, children }: { title: string; children: 
     <details open className="group mb-5">
       <summary className="flex cursor-pointer select-none flex-col md:flex-row">
         <div className="border-stone-clear grow border-y py-2.5 md:pl-5">{title}</div>
-        <div className="border-stone-clear -mt-px flex justify-between gap-5 border-y py-2.5 md:border-l md:px-5">
+        <div className="border-stone-clear -mt-px flex justify-between gap-5 border-y py-2.5 md:mt-0 md:border-l md:px-5">
           Filtrera lista
           <IDSIconChevron width="100%" height="100%" className="h-2.5 w-2.5 rotate-90 self-center group-open:-rotate-90" />
         </div>
