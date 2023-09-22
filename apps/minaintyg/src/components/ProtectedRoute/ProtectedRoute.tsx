8add6e9f029254dde8c0f0e29ca27131e0447e44
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-09-22 10:21:50 +0200

    remove use of useEffect

--- apps/minaintyg/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ apps/minaintyg/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,16 +1,11 @@
-import { ReactNode, useEffect } from 'react'
+import { ReactNode } from 'react'
 import { useGetUserQuery } from '../../store/api'
 
 export function ProtectedRoute({ children }: { children: ReactNode }): JSX.Element | null {
   const { isError } = useGetUserQuery()
 
-  useEffect(() => {
-    if (isError) {
-      window.open('/saml2/authenticate/eleg', '_self')
-    }
-  }, [isError])
-
   if (isError) {
+    window.open('/saml2/authenticate/eleg', '_self')
     return null
   }
 
