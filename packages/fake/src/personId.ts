7dd4bc110a32f8af82c8f4ee44868f6393cf40ee
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-09-22 10:00:58 +0200

    simplify personId function

--- packages/fake/src/personId.ts
+++ packages/fake/src/personId.ts
@@ -1,14 +1,8 @@
 import { faker } from '@faker-js/faker'
 
 export const fakePersonId = () => {
-  const currentYear = new Date().getUTCFullYear()
-  const year = faker.datatype.number({ min: currentYear - 100, max: currentYear - 20 })
-  const month = faker.datatype.number({ min: 1, max: 12 }).toString().padStart(2, '0')
-  const day = faker.datatype.number({ min: 1, max: 31 }).toString().padStart(2, '0')
-  const id = faker.datatype
-    .number({ min: 1, max: 1e4 - 1 })
-    .toString()
-    .padStart(2, '0')
+  const birthdate = faker.date.birthdate({ min: 18, max: 86, mode: 'age' })
+  const id = `${faker.datatype.number({ min: 1, max: 1e4 - 1 })}`.padStart(4, '0')
 
-  return `${year}${month}${day}${id}`
+  return `${birthdate.toISOString().replace(/T.*/g, '').replace(/-/g, '')}${id}`
 }
