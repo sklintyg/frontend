66b93fe1a27c32555dd1b9f3cc7f130a8a56e699
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-09-22 08:43:15 +0200

    add ProtectedRoute component and SAML redirect

---
+++ apps/minaintyg/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import { ReactNode, useEffect } from 'react'
+import { useGetUserQuery } from '../../store/api'
+
+export function ProtectedRoute({ children }: { children: ReactNode }): JSX.Element | null {
+  const { isError } = useGetUserQuery()
+
+  useEffect(() => {
+    if (isError) {
+      window.open('/saml2/authenticate/eleg', '_self')
+    }
+  })
+
+  if (isError) {
+    return null
+  }
+
+  return <>{children}</>
+}
