30e41d6e2fb6df41ad244979feb59c0d672ea7eb
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-09-26 14:16:28 +0200

    add test for jsx element as breadcrumb

--- apps/minaintyg/src/components/Layout/Breadcrumbs.test.tsx
+++ apps/minaintyg/src/components/Layout/Breadcrumbs.test.tsx
@@ -1,46 +1,79 @@
+import { fakerFromSchema } from '@frontend/fake'
 import { render, screen } from '@testing-library/react'
+import { rest } from 'msw'
+import { Provider } from 'react-redux'
 import { Outlet, Route, RouterProvider, createMemoryRouter, createRoutesFromChildren } from 'react-router-dom'
+import { server } from '../../mocks/server'
+import { CertificateCrumb } from '../../pages/Certificate/CertificateCrumb'
+import { certificateSchema } from '../../schema/certificate.schema'
+import { store } from '../../store/store'
 import { Breadcrumbs } from './Breadcrumbs'
 
-beforeEach(() => {
+const renderComponent = (initialEntries = ['/']) =>
   render(
-    <RouterProvider
-      router={createMemoryRouter(
-        createRoutesFromChildren([
-          <Route
-            key="root"
-            path="/"
-            handle={{ crumb: () => 'Start' }}
-            element={
-              <div>
-                <Breadcrumbs />
-                <Outlet />
-              </div>
-            }
-          >
-            <Route index element={<p>Start</p>} />
-            <Route path="/intyg" handle={{ crumb: () => 'Intyg' }}>
-              <Route index element={<p>Certificates List</p>} />
-              <Route
-                path=":id"
-                handle={{
-                  crumb: ({ id }: { id: string }) => id,
-                }}
-                element={<p>Intyget</p>}
-              />
-            </Route>
-          </Route>,
-        ]),
-        { initialEntries: ['/intyg/1234'] }
-      )}
-    />
+    <Provider store={store}>
+      <RouterProvider
+        router={createMemoryRouter(
+          createRoutesFromChildren([
+            <Route
+              key="root"
+              path="/"
+              handle={{ crumb: () => 'Start' }}
+              element={
+                <div>
+                  <Breadcrumbs />
+                  <Outlet />
+                </div>
+              }
+            >
+              <Route index element={<p>Start</p>} />
+              <Route path="/intyg" handle={{ crumb: () => 'Intyg' }}>
+                <Route index element={<p>Certificates List</p>} />
+                <Route
+                  path=":id"
+                  handle={{
+                    crumb: ({ id }: { id: string }) => <CertificateCrumb id={id} />,
+                  }}
+                  element={<p>Intyget</p>}
+                />
+              </Route>
+            </Route>,
+          ]),
+          { initialEntries }
+        )}
+      />
+    </Provider>
   )
+
+it('Should render as expected with one level', () => {
+  const { baseElement } = renderComponent(['/'])
+  expect(baseElement).toMatchSnapshot()
 })
 
-it('Should render starting link', () => {
-  expect(screen.getByRole('link', { name: 'Start' })).toBeInTheDocument()
+it('Should render as expected with two level', () => {
+  const { baseElement } = renderComponent(['/intyg'])
+  expect(baseElement).toMatchSnapshot()
 })
 
-it('Should render intyg link twice (mobile and desktop)', () => {
-  expect(screen.getAllByRole('link', { name: 'Intyg' })).toHaveLength(2)
+it('Should render as expected with three levels', async () => {
+  const certificateName = 'läkares anmälan till transportstyrelsen'
+  server.use(
+    rest.get('/api/certificate/:id', (_, res, ctx) =>
+      res(
+        ctx.status(200),
+        ctx.json(
+          fakerFromSchema(certificateSchema)({
+            metadata: {
+              type: {
+                name: certificateName,
+              },
+            },
+          })
+        )
+      )
+    )
+  )
+  const { baseElement } = renderComponent(['/intyg/12345'])
+  expect(await screen.findByText(certificateName)).toBeInTheDocument()
+  expect(baseElement).toMatchSnapshot()
 })
