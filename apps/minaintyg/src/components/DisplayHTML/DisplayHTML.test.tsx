f275fc582bfc6b2ec211d2ffce3ccb89a31d822f
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-09-28 11:31:23 +0200

    Added more tests

---
+++ apps/minaintyg/src/components/DisplayHTML/DisplayHTML.test.tsx
@@ -0,0 +1,87 @@
+import { render, screen } from '@testing-library/react'
+import { DisplayHTML } from './DisplayHTML'
+
+describe('tables', () => {
+  it('Should add class to tables', () => {
+    render(<DisplayHTML html="<table></table>" />)
+    expect(screen.getByRole('table')).toHaveAttribute('class', 'ids-table')
+  })
+
+  it('Should render mobile table', () => {
+    const { baseElement } = render(
+      <DisplayHTML
+        html="`
+          <table>
+            <thead>
+              <tr>
+                <th>First cell</th>
+                <th>Second cell</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>123</td>
+                <td>Lorem ipsum</td>
+              </tr>
+              <tr>
+                <td>456</td>
+                <td>dolor sit amet</td>
+              </tr>
+            </tbody>
+          </table>
+        `"
+        mobile
+      />
+    )
+    expect(baseElement).toMatchSnapshot()
+  })
+
+  it('Should render mobile table with row headers', () => {
+    const { baseElement } = render(
+      <DisplayHTML
+        html="`
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>First cell</th>
+                <th>Second cell</th>
+                <th>Third cell</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>Row heading</th>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+        `"
+        mobile
+      />
+    )
+    expect(baseElement).toMatchSnapshot()
+  })
+})
+
+describe('link', () => {
+  it('Should render link as ids-link', () => {
+    const { baseElement } = render(<DisplayHTML html="<a href='http://some-place.com'>Some link</a>" />)
+    expect(baseElement).toMatchSnapshot()
+  })
+
+  it('Should render link with external icon when target is _blank', () => {
+    const { baseElement } = render(<DisplayHTML html="<a href='http://some-place.com' target='_blank'>Some link</a>" />)
+    expect(baseElement).toMatchSnapshot()
+  })
+})
+
+describe('headings', () => {
+  it.each(Array.from({ length: 6 }, (_, index) => index + 1))('Should render %s heading as expected', (level) => {
+    const tag = `h${level}`
+    render(<DisplayHTML html={`<${tag}>text</${tag}>`} />)
+    expect(screen.getByRole('heading')).toHaveAttribute('class', `ids-heading-${level}`)
+  })
+})
