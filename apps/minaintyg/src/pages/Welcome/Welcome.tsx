853ac93561d440cf751d43c6385c1f8d587e42ce
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-10-11 14:46:31 +0200

    INTYGFV-15532: parse and use textfield for login profile (#1151)
    Co-authored-by: Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>

--- apps/minaintyg/src/pages/Welcome/Welcome.tsx
+++ apps/minaintyg/src/pages/Welcome/Welcome.tsx
@@ -1,60 +1,14 @@
-import { IDSButton, IDSContainer, IDSSpinner } from '@frontend/ids-react-ts'
-import { useEffect, useState } from 'react'
-import { useFakeLoginMutation, useGetPersonsQuery } from '../../store/testabilityApi'
-import { ResultTextArea } from './components/ResultTextArea'
-import { SelectProfile } from './components/SelectProfile'
+import { IDSContainer, IDSSpinner } from '@frontend/ids-react-ts'
+import { useGetPersonsQuery } from '../../store/testabilityApi'
+import { FakeLoginForm } from './components/FakeLoginForm'
 
 export function Welcome() {
-  const [profile, setProfile] = useState<string>()
-  const [freeText, setFreeText] = useState<string>()
   const { data: persons, isLoading } = useGetPersonsQuery()
-  const [login] = useFakeLoginMutation()
-
-  useEffect(() => {
-    if (!freeText && persons) {
-      setProfile(persons.at(0)?.personId)
-    }
-  }, [freeText, persons])
 
   return (
     <IDSContainer>
-      <div className="ids-content py-4">
-        <h1 className="ids-heading-2">Testinloggningar Mina Intyg</h1>
-        <div className="flex space-x-7 pb-4">
-          {isLoading && <IDSSpinner />}
-          {persons && (
-            <>
-              <div className="w-6/12 flex-auto">
-                <SelectProfile
-                  value={profile}
-                  persons={persons}
-                  onChange={({ target }) => {
-                    setProfile(target.value)
-                    setFreeText(undefined)
-                  }}
-                />
-              </div>
-              <div className="w-6/12 flex-auto">
-                <ResultTextArea
-                  person={persons.find(({ personId }) => personId === profile)}
-                  freeText={freeText}
-                  onChange={(event) => setFreeText(event.target.value)}
-                />
-              </div>
-            </>
-          )}
-        </div>
-        <IDSButton
-          type="submit"
-          onClick={() => {
-            if (profile) {
-              login({ personId: profile })
-            }
-          }}
-        >
-          Logga in
-        </IDSButton>
-      </div>
+      {isLoading && <IDSSpinner />}
+      {persons && <FakeLoginForm persons={persons} />}
     </IDSContainer>
   )
 }
