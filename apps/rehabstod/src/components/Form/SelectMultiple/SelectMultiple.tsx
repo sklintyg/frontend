e85fba9546fe82bcbb6c2cf3276998d6cdf542d4
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-10-12 13:48:54 +0200

    move hasNoChildren to components

--- apps/rehabstod/src/components/Form/SelectMultiple/SelectMultiple.tsx
+++ apps/rehabstod/src/components/Form/SelectMultiple/SelectMultiple.tsx
@@ -10,10 +10,9 @@ import {
   useInteractions,
   useRole,
 } from '@floating-ui/react'
-import { classNames, Input, TooltipIcon } from '@frontend/components'
+import { classNames, hasNoChildren, Input, TooltipIcon } from '@frontend/components'
 import { IDSIconChevronBold, IDSIconQuestion } from '@frontend/ids-react-ts'
 import { ReactNode, useId, useState } from 'react'
-import { hasNoChildren } from '../../../utils/hasNoChildren'
 
 export function SelectMultiple({
   children,
