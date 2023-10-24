8961a7717a562fd61e4390021bca2120e4d7742f
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-10-24 15:57:45 +0200

    Add tests for sickLeaveColumnFilter

---
+++ apps/rehabstod/src/pages/CurrentSickLeaves/utils/sickLeaveColumnFilter.test.ts
@@ -0,0 +1,31 @@
+import { fakerFromSchema } from '@frontend/fake'
+import { tableColumnSchema } from '../../../schemas/tableSchema'
+import { SickLeaveColumn } from '../../../store/slices/sickLeaveTableColumns.slice'
+import { sickLeaveColumnFilter } from './sickLeaveColumnFilter'
+
+it('Should show columns', () => {
+  const column = fakerFromSchema(tableColumnSchema)({ visible: true })
+  expect(sickLeaveColumnFilter(true, false, true)(column)).toBe(true)
+})
+
+it('Hide invisible columns', () => {
+  const column = fakerFromSchema(tableColumnSchema)({ visible: false })
+  expect(sickLeaveColumnFilter(true, false, true)(column)).toBe(false)
+})
+
+describe('Hide personal columns', () => {
+  it.each([SickLeaveColumn.Personnummer, SickLeaveColumn.Namn])('Hide %s column', (name) => {
+    const column = fakerFromSchema(tableColumnSchema)({ visible: true, name })
+    expect(sickLeaveColumnFilter(false, false, true)(column)).toBe(false)
+  })
+})
+
+it('Should hide doctor column', () => {
+  const column = fakerFromSchema(tableColumnSchema)({ visible: true, name: SickLeaveColumn.Läkare })
+  expect(sickLeaveColumnFilter(true, true, true)(column)).toBe(false)
+})
+
+it('Should hide srs column', () => {
+  const column = fakerFromSchema(tableColumnSchema)({ visible: true, name: SickLeaveColumn.Risk })
+  expect(sickLeaveColumnFilter(true, false, false)(column)).toBe(false)
+})
