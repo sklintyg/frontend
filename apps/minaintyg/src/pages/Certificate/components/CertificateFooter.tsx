806b6434ee600bf9a2acf02f61b65535c7fbeb2b
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-10-24 10:18:16 +0200

    INTYGFV-15990: Made requested PR changes.

--- apps/minaintyg/src/pages/Certificate/components/CertificateFooter.tsx
+++ apps/minaintyg/src/pages/Certificate/components/CertificateFooter.tsx
@@ -1,16 +1,9 @@
 import { PageDivider } from '../../../components/PageDivider/PageDivider'
-import { CertificateMetadata, CertificateUnit } from '../../../schema/certificate.schema'
+import { CertificateMetadata } from '../../../schema/certificate.schema'
 
 export function CertificateFooter({ issuer, unit, careUnit }: CertificateMetadata) {
-  const buildUnitAddress = (unit: CertificateUnit) => {
-    return `${[unit.address, unit.zipCode, unit.city].join(', ')}`
-  }
-  const buildUnitName = (unit: CertificateUnit, careUnit: CertificateUnit) => {
-    if (unit.name === careUnit.name) {
-      return unit.name
-    }
-    return `${[unit.name, careUnit.name].join(', ')}`
-  }
+  const unitName = unit.name === careUnit.name ? unit.name : [unit.name, careUnit.name].join(', ')
+  const unitAddress = [unit.address, unit.zipCode, unit.city].join(', ')
   return (
     <footer>
       <PageDivider />
@@ -21,8 +14,8 @@ export function CertificateFooter({ issuer, unit, careUnit }: CertificateMetadat
           <p>{unit.phoneNumber}</p>
         </div>
         <div>
-          <strong>{buildUnitName(unit, careUnit)}</strong>
-          <p>{buildUnitAddress(unit)}</p>
+          <strong>{unitName}</strong>
+          <p>{unitAddress}</p>
         </div>
       </address>
       <PageDivider />
