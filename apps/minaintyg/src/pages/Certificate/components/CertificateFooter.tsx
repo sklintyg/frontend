62cf6983e3e19e85ee8fa198347146832b1ec34d
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-10-25 11:27:48 +0200

    INTYGFV-15990: Changed to use isTruthy instead.

--- apps/minaintyg/src/pages/Certificate/components/CertificateFooter.tsx
+++ apps/minaintyg/src/pages/Certificate/components/CertificateFooter.tsx
@@ -1,14 +1,11 @@
+import { isTruthy } from '@frontend/utils/src'
 import { PageDivider } from '../../../components/PageDivider/PageDivider'
 import { CertificateMetadata } from '../../../schema/certificate.schema'
 
 export function CertificateFooter({ issuer, unit, careUnit }: CertificateMetadata) {
   const unitName = unit.name === careUnit.name ? unit.name : [unit.name, careUnit.name].join(', ')
+  const unitAddress = [unit.address, unit.zipCode, unit.city].filter((field) => isTruthy(field)).join(', ')
 
-  function isUndefinedOrEmpty(field: string) {
-    return field && field.length > 0
-  }
-
-  const unitAddress = [unit.address, unit.zipCode, unit.city].filter((field) => isUndefinedOrEmpty(field)).join(', ')
   return (
     <footer>
       <PageDivider />
