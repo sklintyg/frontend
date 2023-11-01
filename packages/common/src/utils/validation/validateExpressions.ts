144570d350fe7836f7d71cd18f4d3d12f4747919
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-11-01 15:44:26 +0100

    cleanup

--- packages/common/src/utils/validation/validateExpressions.ts
+++ packages/common/src/utils/validation/validateExpressions.ts
@@ -1,17 +1,15 @@
-import { CertificateData, CertificateDataElement } from '../../types'
+import { CertificateData, CertificateDataElement, CertificateDataValidation } from '../../types'
 import { filterValidationResults } from './filterValidationResults'
 import { getValidationResults } from './getValidationResults'
 import { ValidationResult } from './types'
 
+const isAffected = (element: CertificateDataElement) => (validation: CertificateDataValidation) =>
+  validation.questionId === element.id || validation.questions?.some(isAffected(element))
+
 export function validateExpressions(data: CertificateData, updated: CertificateDataElement): ValidationResult[] {
   return filterValidationResults(
     Object.values(data)
-      .filter((element) =>
-        (element.validation ?? []).some(
-          (validation) =>
-            validation.questionId === updated.id || validation.questions?.some((validation) => validation.questionId === updated.id)
-        )
-      )
+      .filter((element) => (element.validation ?? []).some(isAffected(updated)))
       .map((element) => getValidationResults(data, element))
       .flat()
   )
