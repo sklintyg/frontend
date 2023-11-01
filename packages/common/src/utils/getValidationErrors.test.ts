63966ae0db4d113f9ed8612b42ce916770833f9f
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-11-01 15:50:24 +0100

    cleanup

--- packages/common/src/utils/getValidationErrors.test.ts
+++ packages/common/src/utils/getValidationErrors.test.ts
@@ -1,23 +1,15 @@
-import { ValidationError } from '../../types'
-import { getValidationErrors } from '../getValidationErrors'
+import { fakeCertificateValidationError } from './faker'
+import { getValidationErrors } from './getValidationErrors'
 
 it('Should return validation errors from field', () => {
-  const validationError: ValidationError = { id: '', category: '', field: 'grunddata.skapadAv.vardenhet.postadress', type: '', text: '' }
-  const validationErrors: ValidationError[] = []
-  validationErrors.push(validationError)
-
-  const result = getValidationErrors(validationErrors, 'grunddata.skapadAv.vardenhet.postadress')
+  const result = getValidationErrors([fakeCertificateValidationError({ field: 'FIELD_IDENTIFIER' })], 'FIELD_IDENTIFIER')
 
   expect(result.length).toBe(1)
-  expect(result[0].field).toBe('grunddata.skapadAv.vardenhet.postadress')
+  expect(result[0].field).toBe('FIELD_IDENTIFIER')
 })
 
 it('Should return empty array on non existing field', () => {
-  const validationError: ValidationError = { id: '', category: '', field: 'grunddata.skapadAv.vardenhet.postadress', type: '', text: '' }
-  const validationErrors: ValidationError[] = []
-  validationErrors.push(validationError)
-
-  const result = getValidationErrors(validationErrors, 'NON_EXISTING_FIELD')
+  const result = getValidationErrors([fakeCertificateValidationError({ field: 'FIELD_IDENTIFIER' })], 'NON_EXISTING_FIELD')
 
   expect(result.length).toBe(0)
 })
