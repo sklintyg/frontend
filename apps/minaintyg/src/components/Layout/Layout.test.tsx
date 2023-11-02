08d502ccc6d6f0c7e399adc5b1fdc376a669400a
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-11-02 15:27:02 +0100

    INTYGFV-15989: Add footer links (#1186)

--- apps/minaintyg/src/components/Layout/Layout.test.tsx
+++ apps/minaintyg/src/components/Layout/Layout.test.tsx
@@ -1,36 +1,11 @@
 import { act, render } from '@testing-library/react'
-import { rest } from 'msw'
 import { Provider } from 'react-redux'
 import { Route, RouterProvider, createMemoryRouter, createRoutesFromChildren } from 'react-router-dom'
-import { server, waitForRequest } from '../../mocks/server'
+import { waitForRequest } from '../../mocks/server'
 import { store } from '../../store/store'
 import { Layout } from './Layout'
 
 it('Should render as expected', async () => {
-  const userRequest = waitForRequest('GET', '/api/user')
-
-  const { container } = render(
-    <Provider store={store}>
-      <RouterProvider
-        router={createMemoryRouter(
-          createRoutesFromChildren([
-            <Route key="root" path="/" element={<Layout />}>
-              <Route index element={<p>FooBar</p>} />
-            </Route>,
-          ]),
-          { initialEntries: ['/'] }
-        )}
-      />
-    </Provider>
-  )
-
-  await act(async () => userRequest)
-
-  expect(container).toMatchSnapshot()
-})
-
-it('Should render without header when user is not logged in', async () => {
-  server.use(rest.get('/api/user', (_, res, ctx) => res(ctx.status(400))))
   const userRequest = waitForRequest('GET', '/api/user')
 
   const { container } = render(
---
+++ apps/minaintyg/src/components/Layout/LayoutFooter/LayoutFooter.tsx
@@ -0,0 +1,50 @@
+import { IDSFooter, IDSIconChevronBold, IDSIconExternal, IDSLink } from '@frontend/ids-react-ts'
+import navigation from '../../../data/1177-navbar-services.json'
+import footerLinks from '../../../data/footer-links.json'
+import { resolveNavigationUrl } from '../../../utils/resolveNavigationUrl'
+import { LayoutFooterLink } from './LayoutFooterLink'
+import { LayoutFooterMobile } from './LayoutFooterMobile'
+
+export function LayoutFooter() {
+  const startLinkItem = navigation.menu.items.find(({ name }) => name === 'Start')
+  return (
+    <IDSFooter type="1177" headline="1177" subheadline="– tryggt om din hälsa och vård" className="print:hidden">
+      <p>1177 är en tjänst från Sveriges regioner. Vi finns alltid med dig när du vill må bättre.</p>
+
+      {footerLinks.map(({ col, links }) =>
+        links.map(({ title, url, target }) => (
+          <IDSLink key={`${col}-${title}`} slot={`link-col-${col}`}>
+            <IDSIconChevronBold />
+            <a href={url} target={target}>
+              {title}
+            </a>
+            {target === '_blank' && <IDSIconExternal slot="append-icon" />}
+          </IDSLink>
+        ))
+      )}
+
+      <span slot="sub-footer-right" className="relative">
+        <LayoutFooterLink href="https://www.1177.se/om-1177/1177.se/hantering-av-personuppgifter-pa-1177.se/" target="_blank">
+          Behandling av personuppgifter
+        </LayoutFooterLink>
+      </span>
+      <span slot="sub-footer-right" className="relative">
+        <LayoutFooterLink href="https://www.1177.se/om-1177/1177.se/hantering-av-kakor-cookies-pa-1177.se/" target="_blank">
+          Hantering av kakor
+        </LayoutFooterLink>
+      </span>
+
+      {startLinkItem &&
+        ['left', 'mobile'].map((slot) => (
+          <span key={slot} slot={`sub-footer-${slot}`} className="relative">
+            1177 drivs av{' '}
+            <LayoutFooterLink href={resolveNavigationUrl(startLinkItem.url)} target="_blank">
+              Inera AB
+            </LayoutFooterLink>{' '}
+            på uppdrag av Sveriges regioner.
+          </span>
+        ))}
+      <LayoutFooterMobile />
+    </IDSFooter>
+  )
+}
