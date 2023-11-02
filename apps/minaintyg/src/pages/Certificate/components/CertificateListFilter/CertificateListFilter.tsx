404f853c753458c6a75de76138f0dea76163442f
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-11-02 10:46:07 +0100

    INTYGFV-16043: Store certificate filters between views (#1183)

--- apps/minaintyg/src/pages/Certificate/components/CertificateListFilter/CertificateListFilter.tsx
+++ apps/minaintyg/src/pages/Certificate/components/CertificateListFilter/CertificateListFilter.tsx
@@ -1,33 +1,41 @@
 import { IDSButton } from '@frontend/ids-react-ts'
+import { omit } from '@frontend/utils'
 import { FilterAccordion } from '../../../../components/FilterAccordion/FilterAccordion'
 import { useGetCertificatesFilterQuery } from '../../../../store/api'
-import { useAppDispatch } from '../../../../store/hooks'
-import { reset } from '../../../../store/slice/certificateFilter.slice'
+import { useAppDispatch, useAppSelector } from '../../../../store/hooks'
+import { reset, submit } from '../../../../store/slice/certificateFilter.slice'
 import { CertificateStatusFilter } from './CertificateStatusFilter'
 import { CertificateTypeFilter } from './CertificateTypeFilter'
 import { CertificateUnitFilter } from './CertificateUnitFilter'
 import { CertificateYearFilter } from './CertificateYearFilter'
 
-export function CertificateListFilter({ listed, onSubmit }: { listed: number; onSubmit: () => void }) {
-  const { data: filter } = useGetCertificatesFilterQuery()
+export function CertificateListFilter({ listed }: { listed: number }) {
+  const { data: filterOptions } = useGetCertificatesFilterQuery()
+  const { ...filter } = useAppSelector((state) => state.certificateFilter)
   const dispatch = useAppDispatch()
 
-  if (!filter) {
+  if (!filterOptions) {
     return null
   }
 
   return (
-    <FilterAccordion listed={listed} total={filter.total} noun="intyg">
+    <FilterAccordion listed={listed} total={filterOptions.total} noun="intyg">
       <div className="grid grid-cols-1 gap-5 md:grid-cols-4 md:gap-10">
-        <CertificateStatusFilter options={filter.statuses} />
-        <CertificateUnitFilter options={filter.units} />
-        <CertificateTypeFilter options={filter.certificateTypes} />
-        <CertificateYearFilter options={filter.years} />
+        <CertificateStatusFilter options={filterOptions.statuses} />
+        <CertificateUnitFilter options={filterOptions.units} />
+        <CertificateTypeFilter options={filterOptions.certificateTypes} />
+        <CertificateYearFilter options={filterOptions.years} />
         <div className="flex flex-col gap-5 md:col-span-4 md:flex-row">
           <IDSButton secondary sblock onClick={() => dispatch(reset())} aria-label="Återställ filter">
             Återställ filter
           </IDSButton>
-          <IDSButton sblock onClick={onSubmit} aria-label="Filtrera">
+          <IDSButton
+            sblock
+            onClick={() => {
+              dispatch(submit(omit(filter, ['submitFilters'])))
+            }}
+            aria-label="Filtrera"
+          >
             Filtrera
           </IDSButton>
         </div>
