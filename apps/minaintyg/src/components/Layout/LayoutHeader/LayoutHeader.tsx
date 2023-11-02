11df8f4a948942f9c1df994dd01c8b288eeb2810
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-11-02 14:07:26 +0100

    Add link to header logo (#1185)

--- apps/minaintyg/src/components/Layout/LayoutHeader/LayoutHeader.tsx
+++ apps/minaintyg/src/components/Layout/LayoutHeader/LayoutHeader.tsx
@@ -1,13 +1,21 @@
 import { IDSHeader } from '@frontend/ids-react-ts'
+import navigation from '../../../data/1177-navbar-services.json'
 import { useGetUserQuery } from '../../../store/api'
+import { resolveNavigationUrl } from '../../../utils/resolveNavigationUrl'
 import { LayoutHeaderAvatar } from './LayoutHeaderAvatar'
 import { LayoutHeaderNavigation } from './LayoutHeaderNavigation'
 
 export function LayoutHeader() {
   const { data: user } = useGetUserQuery()
+  const linkItem = navigation.menu.items.find(({ name }) => name === 'Start')
 
   return (
-    <IDSHeader type="1177" hideregionpicker className="z-40 bg-white print:hidden">
+    <IDSHeader
+      type="1177"
+      logohref={linkItem && resolveNavigationUrl(linkItem.url)}
+      hideregionpicker
+      className="z-40 bg-white print:hidden"
+    >
       {user && (
         <>
           <LayoutHeaderAvatar />
