a9e5852e7164aea1b2a2477c55b59b683ba3d214
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-11-09 07:26:16 +0100

    INTYGFV-16073: Alert messages on failed requests (#1197)

--- apps/minaintyg/src/pages/Certificate/CertificateListPage.tsx
+++ apps/minaintyg/src/pages/Certificate/CertificateListPage.tsx
@@ -3,6 +3,8 @@ import { useSessionStorage } from '@react-hooks-library/core'
 import { SortDirection } from 'react-stately'
 import { PageHeading } from '../../components/PageHeading/PageHeading'
 import { PageHeadingDescription } from '../../components/PageHeading/PageHeadingDescription'
+import { SupportLink } from '../../components/error/SupportLink/SupportLink'
+import { TechnicalIssueAlert } from '../../components/error/TechnicalIssueAlert'
 import { useGetCertificatesFilterQuery, useGetCertificatesQuery } from '../../store/api'
 import { useAppSelector } from '../../store/hooks'
 import { CertificateList } from './components/CertificateList'
@@ -13,7 +15,7 @@ import { EmptyCertificateListInfo } from './components/EmptyCertificateListInfo'
 export function CertificateListPage() {
   const [order, setOrder] = useSessionStorage<SortDirection>('certificate-list-order', 'descending')
   const { submitFilters } = useAppSelector((state) => state.certificateFilter)
-  const { isLoading: isLoadingList, data: list } = useGetCertificatesQuery(submitFilters, { refetchOnMountOrArgChange: true })
+  const { isLoading: isLoadingList, data: list, isError } = useGetCertificatesQuery(submitFilters, { refetchOnMountOrArgChange: true })
   const { isLoading: isLoadingFilters, data: filter } = useGetCertificatesFilterQuery()
   const isLoading = isLoadingList || isLoadingFilters
 
@@ -28,10 +30,13 @@ export function CertificateListPage() {
       </PageHeading>
       <CertificateListFilter listed={list?.content.length ?? 0} />
       <CertificateListOrder setOrder={setOrder} order={order} />
-      {isLoading && (
-        <div data-testid="certificate-list-spinner">
-          <IDSSpinner />
-        </div>
+      {isLoading && <IDSSpinner data-testid="certificate-list-spinner" />}
+      {isError && (
+        <TechnicalIssueAlert>
+          <p>
+            Intygen kunde inte visas på grund av ett tekniskt fel. Försök igen senare. Om felet kvarstår kontakta <SupportLink />.
+          </p>
+        </TechnicalIssueAlert>
       )}
       {list && list.content.length === 0 && <EmptyCertificateListInfo total={filter?.total ?? 0} />}
       {list && <CertificateList certificates={list.content} order={order} />}
--- apps/minaintyg/src/pages/Certificate/CertificatePage.tsx
+++ apps/minaintyg/src/pages/Certificate/CertificatePage.tsx
@@ -5,6 +5,8 @@ import { DisplayHTML } from '../../components/DisplayHTML/DisplayHTML'
 import { PageDivider } from '../../components/PageDivider/PageDivider'
 import { PageHeading } from '../../components/PageHeading/PageHeading'
 import { PageHeadingDescription } from '../../components/PageHeading/PageHeadingDescription'
+import { SupportLink } from '../../components/error/SupportLink/SupportLink'
+import { TechnicalIssueAlert } from '../../components/error/TechnicalIssueAlert'
 import { useGetCertificateQuery } from '../../store/api'
 import { CertificateActions } from './components/CertificateActions'
 import { CertificateBody } from './components/CertificateBody/CertificateBody'
@@ -16,55 +18,65 @@ import { CertificateStatusBadge } from './components/CertificateStatusBadge'
 
 export function CertificatePage() {
   const { id } = useParams()
-  const { data: certificate, isLoading } = useGetCertificateQuery(id ? { id } : skipToken)
-
-  if (isLoading) {
-    return <IDSSpinner data-testid="spinner" />
-  }
-
-  if (!certificate) {
-    return null
-  }
+  const { data: certificate, isLoading, isError } = useGetCertificateQuery(id ? { id } : skipToken)
 
   return (
     <>
       <PageHeading heading="Läs och hantera ditt intyg">
-        {certificate.metadata.type.description && (
-          <PageHeadingDescription>
-            <DisplayHTML html={certificate.metadata.type.description} />
-          </PageHeadingDescription>
+        {certificate && (
+          <>
+            {certificate.metadata.type.description && (
+              <PageHeadingDescription>
+                <DisplayHTML html={certificate.metadata.type.description} />
+              </PageHeadingDescription>
+            )}
+            {certificate.metadata.statuses.includes('REPLACED') && <CertificateReplacedAlert />}
+          </>
         )}
-        {certificate.metadata.statuses.includes('REPLACED') && <CertificateReplacedAlert />}
       </PageHeading>
 
-      <div className="mb-5">
-        <CertificateActions recipient={certificate.metadata.recipient} />
-      </div>
+      {isLoading && <IDSSpinner data-testid="spinner" />}
+      {isError && (
+        <TechnicalIssueAlert>
+          <p>
+            Intyget kunde inte visas på grund av ett tekniskt fel. Försök igen senare. Om felet kvarstår kontakta <SupportLink /> och ange
+            intygs-id: {id}.
+          </p>
+        </TechnicalIssueAlert>
+      )}
 
-      <div className="md:hidden">
-        <PageDivider />
-      </div>
-
-      <div className="rounded-[10px] border-stone-line md:border md:p-7 md:shadow-[0_0_2px_0px_rgba(0,0,0,0.3)]">
-        <div className="mb-5 flex flex-col justify-between gap-2.5 md:flex-row md:gap-5">
-          <h2 className="ids-heading-2">{certificate.metadata.type.name}</h2>
-          <div className="flex gap-1">
-            {certificate.metadata.statuses.map((status) => (
-              <CertificateStatusBadge key={status} status={status} />
-            ))}
+      {certificate && (
+        <>
+          <div className="mb-5">
+            <CertificateActions recipient={certificate.metadata.recipient} />
           </div>
-        </div>
-        <div className="mb-5">
-          <CertificateInformation {...certificate.metadata} />
-        </div>
-        <h3 className="ids-heading-4 mb-0">Händelser</h3>
-        <CertificateEventsInfo events={certificate.metadata.events} />
 
-        <article className="ids-certificate">
-          <CertificateBody content={certificate.content} />
-          <CertificateFooter {...certificate.metadata} />
-        </article>
-      </div>
+          <div className="md:hidden">
+            <PageDivider />
+          </div>
+
+          <div className="rounded-[10px] border-stone-line md:border md:p-7 md:shadow-[0_0_2px_0px_rgba(0,0,0,0.3)]">
+            <div className="mb-5 flex flex-col justify-between gap-2.5 md:flex-row md:gap-5">
+              <h2 className="ids-heading-2">{certificate.metadata.type.name}</h2>
+              <div className="flex gap-1">
+                {certificate.metadata.statuses.map((status) => (
+                  <CertificateStatusBadge key={status} status={status} />
+                ))}
+              </div>
+            </div>
+            <div className="mb-5">
+              <CertificateInformation {...certificate.metadata} />
+            </div>
+            <h3 className="ids-heading-4 mb-0">Händelser</h3>
+            <CertificateEventsInfo events={certificate.metadata.events} />
+
+            <article className="ids-certificate">
+              <CertificateBody content={certificate.content} />
+              <CertificateFooter {...certificate.metadata} />
+            </article>
+          </div>
+        </>
+      )}
     </>
   )
 }
