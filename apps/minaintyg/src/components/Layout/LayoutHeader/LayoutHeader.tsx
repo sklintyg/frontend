bbda8931ab2ec4b9d345893e31bf7021042a4e4e
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-11-10 15:15:38 +0100

    Hide navigation when no user is loaded

--- apps/minaintyg/src/components/Layout/LayoutHeader/LayoutHeader.tsx
+++ apps/minaintyg/src/components/Layout/LayoutHeader/LayoutHeader.tsx
@@ -1,11 +1,13 @@
 import { IDSHeader } from '@frontend/ids-react-ts'
 import navigation from '../../../data/1177-navbar-services.json'
+import { useGetUserQuery } from '../../../store/hooks'
 import { resolveNavigationUrl } from '../../../utils/resolveNavigationUrl'
 import { LayoutHeaderAvatar } from './LayoutHeaderAvatar'
 import { LayoutHeaderNavigation } from './LayoutHeaderNavigation'
 
 export function LayoutHeader() {
   const linkItem = navigation.menu.items.find(({ name }) => name === 'Start')
+  const { data: user } = useGetUserQuery()
 
   return (
     <IDSHeader
@@ -14,8 +16,12 @@ export function LayoutHeader() {
       hideregionpicker
       className="z-40 bg-white print:hidden"
     >
-      <LayoutHeaderAvatar />
-      <LayoutHeaderNavigation />
+      {user && (
+        <>
+          <LayoutHeaderAvatar />
+          <LayoutHeaderNavigation />
+        </>
+      )}
     </IDSHeader>
   )
 }
