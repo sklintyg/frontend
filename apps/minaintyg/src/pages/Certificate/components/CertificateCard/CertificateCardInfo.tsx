e0dfdb518ce47a6501f69d08a3ecb52b96f756f2
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2023-11-14 11:07:52 +0100

    INTYGFV-16019: Updated border color for mobile view (#1204)

--- apps/minaintyg/src/pages/Certificate/components/CertificateCard/CertificateCardInfo.tsx
+++ apps/minaintyg/src/pages/Certificate/components/CertificateCard/CertificateCardInfo.tsx
@@ -2,7 +2,7 @@ import { CertificateIssuer, CertificateUnit } from '../../../../schema/certifica
 
 export function CertificateCardInfo({ issuer, unit }: { issuer: CertificateIssuer; unit: CertificateUnit }) {
   return (
-    <div className="mb-2.5 border-b pb-5 md:flex-row md:border-0 md:pb-0">
+    <div className="mb-2.5 border-b border-stone-line pb-5 md:flex-row md:border-0 md:pb-0">
       <div>
         <h4 className="ids-heading-4 mb-0">Skrivet av</h4>
         <p>
