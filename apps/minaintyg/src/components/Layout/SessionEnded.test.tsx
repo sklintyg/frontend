c228a1651e109cae5a4d40bd44e327ccf84b17e8
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-11-14 11:01:05 +0100

    simplify test

--- apps/minaintyg/src/components/Layout/SessionEnded.test.tsx
+++ apps/minaintyg/src/components/Layout/SessionEnded.test.tsx
@@ -5,15 +5,19 @@ import { store } from '../../store/store'
 import { withRouter } from '../../utils/withRouter'
 import { SessionEnded } from './SessionEnded'
 
+function renderComponent() {
+  return render(<Provider store={store}>{withRouter(<SessionEnded />)}</Provider>)
+}
+
 it('Should render as expected', () => {
-  const { container } = render(<Provider store={store}>{withRouter(<SessionEnded />)}</Provider>)
+  const { container } = renderComponent()
   expect(screen.getByRole('heading', { level: 1, name: 'Du är utloggad' })).toBeInTheDocument()
   expect(container).toMatchSnapshot()
 })
 
 it('Should display unavailable service error', () => {
   store.dispatch(updateServiceAvailability({ ...new Error('unavailable'), id: '152336e7-41a5-4ef7-abae-c2663c143591' }))
-  const { container } = render(<Provider store={store}>{withRouter(<SessionEnded />)}</Provider>)
+  const { container } = renderComponent()
   expect(screen.getByRole('heading', { level: 1, name: 'Tjänsten är inte tillgänglig just nu' })).toBeInTheDocument()
   expect(container).toMatchSnapshot()
 })
