875763e08d96230cac985170a9ac47204d7d6378
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-11-15 16:10:15 +0100

    INTYGFV-16069: Added Print button to CertificateActions.tsx

--- apps/minaintyg/src/pages/Certificate/components/CertificateActions.tsx
+++ apps/minaintyg/src/pages/Certificate/components/CertificateActions.tsx
@@ -1,5 +1,5 @@
-import { IDSButton } from '@frontend/ids-react-ts'
-import { useState } from 'react'
+import { IDSButton, IDSButtonGroup, IDSColumn, IDSIconFilePdf, IDSRow } from '@frontend/ids-react-ts'
+import { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AvailableFunction, AvailableFunctionsTypeEnum, CertificateRecipient } from '../../../schema/certificate.schema'
 import { CertificateSentDialog } from './CertificateSentDialog/CertificateSentDialog'
@@ -7,18 +7,37 @@ import { CertificateSentDialog } from './CertificateSentDialog/CertificateSentDi
 export function CertificateActions({
   recipient,
   availableFunctions,
+  id,
 }: {
   recipient?: CertificateRecipient | null
   availableFunctions: AvailableFunction[]
+  id: string
 }) {
   const [showCertificateSentDialog, updateShowCertificateSentDialog] = useState(false)
   const navigate = useNavigate()
   const sendFunction = availableFunctions.find(
     (availableFunction) => availableFunction.type === AvailableFunctionsTypeEnum.enum.SEND_CERTIFICATE
   )
+  const printFunction = availableFunctions.find(
+    (availableFunction) => availableFunction.type === AvailableFunctionsTypeEnum.enum.PRINT_CERTIFICATE
+  )
+  const iframeRef = useRef<HTMLIFrameElement>(null)
+
+  const printCertificate = (customizeId: string | undefined) => {
+    if (!iframeRef || !iframeRef.current) {
+      return
+    }
+    iframeRef.current.onload = () => {
+      setTimeout(() => {
+        iframeRef.current?.focus()
+        iframeRef.current?.contentWindow?.print()
+      }, 1)
+    }
+    iframeRef.current.src = `/api/certificate/${id}/pdf/${customizeId && ''}`
+  }
 
   return (
-    <div className="flex justify-end">
+    <IDSButtonGroup rtl>
       {sendFunction && recipient && (
         <>
           <IDSButton
@@ -37,6 +56,19 @@ export function CertificateActions({
           <CertificateSentDialog recipient={recipient} open={showCertificateSentDialog} onOpenChange={updateShowCertificateSentDialog} />
         </>
       )}
-    </div>
+      {printFunction && (
+        <>
+          <IDSButton secondary sblock role="button" onClick={() => printCertificate(undefined)}>
+            <IDSRow className="justify-center" align="center">
+              <IDSColumn cols="auto" className="ids-mr-3">
+                <IDSIconFilePdf />
+              </IDSColumn>
+              <IDSColumn cols="auto">Skriv ut</IDSColumn>
+            </IDSRow>
+          </IDSButton>
+          <iframe ref={iframeRef} title="title" hidden />
+        </>
+      )}
+    </IDSButtonGroup>
   )
 }
