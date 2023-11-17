70e262d67c7ce130d96aa3e1587b557594f449aa
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-11-17 08:55:07 +0100

    INTYGFV-16086: Add 404 page (#1213)

--- apps/minaintyg/src/schema/error.schema.ts
+++ apps/minaintyg/src/schema/error.schema.ts
@@ -1,4 +1,4 @@
 import { z } from 'zod'
 
-export const ErrorType = z.enum(['unknown', 'login-failed', 'unavailable', 'logged-out'])
+export const ErrorType = z.enum(['unknown', 'login-failed', 'unavailable', 'logged-out', 'not-found'])
 export type ErrorTypeEnum = z.infer<typeof ErrorType>
