8dc768ddc52ccb17d282df53904d510eeb7a0164
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-11-20 09:23:22 +0100

    Add log to routing tests

--- apps/minaintyg/src/routes.test.tsx
+++ apps/minaintyg/src/routes.test.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { RouterProvider, createMemoryRouter } from 'react-router-dom'
 import { server, waitForRequest } from './mocks/server'
 import { routes } from './routes'
+import { startSession } from './store/slice/session.slice'
 import { store } from './store/store'
 
 it.each([
@@ -11,17 +12,27 @@ it.each([
   ['Tjänsten är inte tillgänglig just nu', 503],
   ['Någonting gick fel', 504],
 ] as const)('Should display %s when request status is %s', async (headline, status) => {
+  store.dispatch(startSession())
   server.use(rest.post('/api/certificate', (_, res, ctx) => res(ctx.status(status))))
-  const userRequest = waitForRequest('GET', '/api/user')
-  const certificateListRequest = waitForRequest('POST', '/api/certificate')
+  const pendingUserRequest = waitForRequest('GET', '/api/user')
+  const pendingCertificateListRequest = waitForRequest('POST', '/api/certificate')
+  const pendingLogRequest = waitForRequest('POST', '/api/log/error')
+
   render(
     <Provider store={store}>
       <RouterProvider router={createMemoryRouter(routes, { initialEntries: ['/intyg'] })} />
     </Provider>
   )
 
-  await act(async () => userRequest)
-  await act(async () => certificateListRequest)
+  await act(async () => pendingUserRequest)
+  await act(async () => pendingCertificateListRequest)
+
+  const logRequest = await pendingLogRequest
+  expect(await logRequest.json()).toMatchObject({
+    code: status,
+    id: '5f92e947-e2ee-4238-bf29-4cdc6b6c4b54',
+    message: "'Rejected' method 'POST' url '/api/certificate'",
+  })
 
   expect(store.getState().sessionSlice.hasSession).toBe(false)
   expect(store.getState().sessionSlice.hasSessionEnded).toBe(true)
