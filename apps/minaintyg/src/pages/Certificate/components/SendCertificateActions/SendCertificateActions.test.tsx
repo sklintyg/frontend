75806f96134aef36e9e17b5ab1f7bbacb36d2348
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2023-11-21 15:49:10 +0100

    INTYGFV-16034: Fixed tests.

--- apps/minaintyg/src/pages/Certificate/components/SendCertificateActions/SendCertificateActions.test.tsx
+++ apps/minaintyg/src/pages/Certificate/components/SendCertificateActions/SendCertificateActions.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { rest } from 'msw'
 import { Provider } from 'react-redux'
-import { Outlet, Route, RouterProvider, createMemoryRouter, createRoutesFromChildren } from 'react-router-dom'
+import { createMemoryRouter, createRoutesFromChildren, Outlet, Route, RouterProvider } from 'react-router-dom'
 import { server } from '../../../../mocks/server'
 import { CertificateRecipient, certificateRecipientSchema } from '../../../../schema/certificate.schema'
 import { store } from '../../../../store/store'
@@ -32,14 +32,12 @@ function renderComponent(recipient: CertificateRecipient) {
 it('Should display message when certificate is sent', async () => {
   renderComponent(fakerFromSchema(certificateRecipientSchema)({ sent: '2023-10-23T11:13:37.000Z' }))
 
-  await userEvent.click(screen.getByRole('button', { name: 'Skicka' }))
-
   expect(screen.getByText(/ditt intyg har skickats till följande mottagare:/i)).toBeInTheDocument()
 })
 
 it('Should display error-message when certificate was unable to be sent', async () => {
   server.use(rest.post('/api/certificate/:id/send', (req, res, ctx) => res(ctx.status(500))))
-  renderComponent(fakerFromSchema(certificateRecipientSchema)({ sent: undefined }))
+  renderComponent(fakerFromSchema(certificateRecipientSchema)({ sent: null }))
 
   await userEvent.click(screen.getByRole('button', { name: 'Skicka' }))
 
