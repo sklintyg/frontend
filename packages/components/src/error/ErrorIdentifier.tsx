fa832d3cef70b495bb52abd97896731bdfda432a
Mikaela Romild <mikaela.romild@soprasteria.com>
2023-11-22 16:15:28 +0100

    INTYGFV-16126: Should also copy when error-id is clicked

--- packages/components/src/error/ErrorIdentifier.tsx
+++ packages/components/src/error/ErrorIdentifier.tsx
@@ -8,26 +8,28 @@ export function ErrorIdentifier({ id, showTitle = true }: { id: string; showTitl
 
   return (
     <div className="text-center">
-      <p className="flex justify-center gap-1 align-bottom font-bold">
-        {showTitle && <span>FEL-ID: </span>} <span className={classNames(showTitle ? 'font-normal' : 'font-bold')}>{id}</span>
+      <button
+        type="button"
+        aria-label="Kopiera fel-id"
+        value={id}
+        tabIndex={0}
+        onClick={() => {
+          setDisplayCopyMessage(true)
+          navigator.clipboard.writeText(id)
+        }}
+      >
         <Tooltip>
           <TooltipTrigger asChild>
-            <button
-              type="button"
-              value={id}
-              tabIndex={0}
-              onClick={() => {
-                setDisplayCopyMessage(true)
-                navigator.clipboard.writeText(id)
-              }}
-            >
+            <p className="flex justify-center gap-1 align-bottom font-bold">
+              {showTitle && <span>FEL-ID: </span>} <span className={classNames(showTitle ? 'font-normal' : 'font-bold')}>{id}</span>
               <IDSIconCopyFile size="s" colorpreset={1} />
-            </button>
+            </p>
           </TooltipTrigger>
           <TooltipContent>Kopiera fel-id</TooltipContent>
         </Tooltip>
-      </p>
-      {displayCopyMessage && <strong className="text-xs">Fel-id kopierat till urklipp.</strong>}
+
+        {displayCopyMessage && <strong className="text-xs">Fel-id kopierat till urklipp.</strong>}
+      </button>
     </div>
   )
 }
