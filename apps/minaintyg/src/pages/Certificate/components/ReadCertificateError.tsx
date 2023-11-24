110a4ab9abbd04cfb35f11b4fd3cf394d851a881
Mikaela Romild <mikaela.romild@soprasteria.com>
2023-11-24 15:10:14 +0100

    INTYGFV-16126-fix: Updated tests

--- apps/minaintyg/src/pages/Certificate/components/ReadCertificateError.tsx
+++ apps/minaintyg/src/pages/Certificate/components/ReadCertificateError.tsx
@@ -6,7 +6,7 @@ export function ReadCertificateError({ error }: { error: QueryError }) {
   return (
     <TechnicalIssueAlert
       error={error}
-      additionalInfo={<p>Om du inte kan nå ditt intyg, kontakta din läkare för att få en kopia av intyget.</p>}
+      additionalInfo={<p>Om du inte kan nå ditt intyg, kontakta din mottagning för att få en kopia av intyget.</p>}
     >
       <p>
         Intyget kunde inte visas på grund av ett tekniskt fel. Försök igen senare. Om felet kvarstår kontakta <SupportLink />
