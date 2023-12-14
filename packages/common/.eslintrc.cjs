a3e497fe6daa11bcae96a1a8d440c6db29aff29a
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2023-12-14 08:57:18 +0100

    cleanup

--- packages/common/.eslintrc.cjs
+++ packages/common/.eslintrc.cjs
@@ -31,7 +31,6 @@ module.exports = {
     'no-continue': 'off',
     'no-else-return': 'off',
     'no-nested-ternary': 'off',
-    // 'no-param-reassign': 'off',
     'no-plusplus': 'off',
     'no-restricted-globals': 'off',
     'no-restricted-syntax': 'off',
