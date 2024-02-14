ea347a7f5caedfe3692f91f02b2aab69255889c3
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2024-02-14 08:21:50 +0100

    use randomUUID for UUID4

--- apps/webcert/src/components/error/modals/errorUtils.ts
+++ apps/webcert/src/components/error/modals/errorUtils.ts
@@ -13,15 +13,6 @@ export const reloadPage = (activeError: ErrorData, dispatch: Dispatch<any>) => {
   }
 }
 
-// https://stackoverflow.com/questions/105034/how-to-create-guid-uuid
-export const uuidv4 = (): string => {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    const r = (Math.random() * 16) | 0,
-      v = c === 'x' ? r : (r & 0x3) | 0x8
-    return v.toString(16)
-  })
-}
-
 export const messageSubstring = (activeError: { message?: string }): string | undefined => {
   return activeError?.message?.substring(0, activeError.message?.indexOf(' -'))
 }
