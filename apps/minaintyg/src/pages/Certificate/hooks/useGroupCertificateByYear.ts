c89d989e910b0ad97867f0bba71bbeadf1dfc3f7
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2024-03-13 11:17:40 +0100

    Property testing POC

--- apps/minaintyg/src/pages/Certificate/hooks/useGroupCertificateByYear.ts
+++ apps/minaintyg/src/pages/Certificate/hooks/useGroupCertificateByYear.ts
@@ -1,22 +1,15 @@
-import { sortBy } from '@frontend/utils'
+import { groupBy, sortBy } from '@frontend/utils'
 import { getYear, parseISO } from 'date-fns'
 import { useMemo } from 'react'
 import { SortDirection } from 'react-stately'
 import { CertificateMetadata } from '../../../schema/certificate.schema'
 
-const getCertificateYear = ({ issued }: CertificateMetadata) => getYear(parseISO(issued))
-
 export function useGroupCertificateByYear(certificates: CertificateMetadata[], order: SortDirection = 'descending') {
-  return useMemo(() => {
-    // Could be replaced with Object.groupBy in the future (currently experimental)
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/groupBy#browser_compatibility
-    const groupByYear = [...certificates]
-      .sort(sortBy(order, ({ issued }) => new Date(issued).getTime()))
-      .reduce<Record<string, CertificateMetadata[]>>((result, certificate) => {
-        const year = getCertificateYear(certificate)
-        return { ...result, [year]: [...(result[year] ?? []), certificate] }
-      }, {})
-
-    return Object.entries(groupByYear).sort(sortBy(order, ([year]) => parseInt(year, 10)))
-  }, [certificates, order])
+  return useMemo(
+    () =>
+      Object.entries(groupBy(certificates, ({ issued }: CertificateMetadata) => getYear(parseISO(issued)))).sort(
+        sortBy(order, ([year]) => parseInt(year, 10))
+      ),
+    [certificates, order]
+  )
 }
