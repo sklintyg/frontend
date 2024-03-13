ccd84aac7ee996bda30d99913039017073f891a8
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2024-03-13 11:29:52 +0100

    INTYGFV-16134: Replace links for Mina intyg to Intyg

--- apps/webcert/src/components/notification/PatientTestIndicatedStatus.tsx
+++ apps/webcert/src/components/notification/PatientTestIndicatedStatus.tsx
@@ -12,7 +12,7 @@ const PatientTestIndicatedStatus: React.FC<Props> = ({ isTestIndicated }) => {
       <p>
         En valideringsperson är en fingerad person som används i syfte att validera funktion, felsöka och säkerställa kvalitet i tjänsten.
         Intyg utfärdade på valideringsperson kan inte skickas till intygsmottagare. De kommer inte heller visas i Intygsstatistik, Rehabstöd
-        eller i Mina intyg.{' '}
+        eller i Intyg.{' '}
       </p>
     </PatientStatusNotificationWithModal>
   )
