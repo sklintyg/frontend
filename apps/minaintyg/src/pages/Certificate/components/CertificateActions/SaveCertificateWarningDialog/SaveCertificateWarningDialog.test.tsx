af6d51be301db0d8b2898fb878c44444d5d90de2
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2024-03-26 13:40:44 +0100

    fix flaky test

--- apps/minaintyg/src/pages/Certificate/components/CertificateActions/SaveCertificateWarningDialog/SaveCertificateWarningDialog.test.tsx
+++ apps/minaintyg/src/pages/Certificate/components/CertificateActions/SaveCertificateWarningDialog/SaveCertificateWarningDialog.test.tsx
@@ -1,11 +1,13 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { SaveCertificateWarningDialog } from './SaveCertificateWarningDialog'
 
 it('Should render as expected', () => {
-  const { container } = render(
+  render(
     <SaveCertificateWarningDialog open onOpenChange={vi.fn()}>
       Test
     </SaveCertificateWarningDialog>
   )
-  expect(container).toMatchSnapshot()
+  expect(screen.getByRole('dialog', { name: 'Spara intyg som PDF' })).toBeInTheDocument()
+  expect(screen.getByText(/viktigt! Ditt intyg har information om dig, som ditt namn och andra personuppgifter/i)).toBeInTheDocument()
+  expect(screen.getByText(/när du sparar intyget som en PDF-fil, sparas filen på den enhet du använder/i)).toBeInTheDocument()
 })
