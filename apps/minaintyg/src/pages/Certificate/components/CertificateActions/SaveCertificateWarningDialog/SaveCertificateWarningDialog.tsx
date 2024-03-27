f21c3258635daf7051ad69c73af778e389c218e8
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2024-03-27 13:01:49 +0100

    set max width

--- apps/minaintyg/src/pages/Certificate/components/CertificateActions/SaveCertificateWarningDialog/SaveCertificateWarningDialog.tsx
+++ apps/minaintyg/src/pages/Certificate/components/CertificateActions/SaveCertificateWarningDialog/SaveCertificateWarningDialog.tsx
@@ -12,11 +12,13 @@ export function SaveCertificateWarningDialog({
 }) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange} headline="Spara intyg som PDF">
-      <p className="mb-5">Viktigt! Ditt intyg har information om dig, som ditt namn och andra personuppgifter.</p>
-      <p>
-        När du sparar intyget som en PDF-fil, sparas filen på den enhet du använder. Om du använder en dator som andra också kan använda,
-        till exempel på ett bibliotek, måste du själv ta bort filen från datorn innan du går därifrån.
-      </p>
+      <div className="max-w-3xl">
+        <p className="mb-5">Viktigt! Ditt intyg har information om dig, som ditt namn och andra personuppgifter.</p>
+        <p>
+          När du sparar intyget som en PDF-fil, sparas filen på den enhet du använder. Om du använder en dator som andra också kan använda,
+          till exempel på ett bibliotek, måste du själv ta bort filen från datorn innan du går därifrån.
+        </p>
+      </div>
       {children}
     </Dialog>
   )
