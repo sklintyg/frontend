25f0e91de67a05721ec42e8fc84c8ca343545a32
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-06-11 16:55:59 +0200

    DEVENV: Removed unused import

--- apps/maintenance/src/intyg.tsx
+++ apps/maintenance/src/intyg.tsx
@@ -1,5 +1,5 @@
 import { LayoutFooter, LayoutHeader, PageHero, PageHeroActions } from '@frontend/components/1177'
-import { IDSIconAttention, IDSIconChevron, IDSIconExternal, IDSLink } from '@frontend/ids-react-ts'
+import { IDSIconChevron, IDSIconExternal, IDSLink } from '@frontend/ids-react-ts'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './intyg.css'
