e70fe24d1d7ef5a6057f35ae578b722520135593
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-06-11 13:54:44 +0200

    INTYGFV-16519: Webcert maintenance page

--- apps/maintenance/src/intyg.tsx
+++ apps/maintenance/src/intyg.tsx
@@ -1,37 +1,32 @@
-import { LayoutFooter, LayoutHeader, PageHero, PageHeroActions } from '@frontend/components/1177'
-import { IDSIconChevron, IDSLink } from '@frontend/ids-react-ts'
+import { IDSFooter, IDSHeader, IDSIconAttention, IDSIconChevron, IDSLink } from '@frontend/ids-react-ts'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div className="flex min-h-screen flex-col">
-      <LayoutHeader mode={import.meta.env.MODE} />
-      <main className="relative flex-1">
-        <div className="ids-content m-auto max-w-screen-xl overflow-hidden px-2.5 py-5">
-          <PageHero heading="Välkommen till Intyg" type="none">
-            <p className="ids-preamble">
-              I Intyg kan du läsa och skriva ut dina digitala läkarintyg. Du kan också skicka dem digitalt till antingen Försäkringskassan
-              eller Transportstyrelsen. För att kunna logga in behöver du en e-legitimation.
-            </p>
-            <p className="ids-preamble">Intyg är en del av 1177 och du når Intyg från huvudmenyn när du är inloggad.</p>
-            <PageHeroActions>
-              <IDSLink>
-                <IDSIconChevron />
-                <a href={import.meta.env.VITE_LOGIN_URL}>Till inloggning</a>
-              </IDSLink>
-              <IDSLink>
-                <IDSIconChevron />
-                <a href="https://www.1177.se/om-1177/nar-du-loggar-in-pa-1177.se/det-har-kan-du-gora-nar-du-loggat-in/hantera-intyg/hantera-dina-lakarintyg-och-lakarutlatanden/">
-                  Om Intyg på 1177.se
-                </a>
-              </IDSLink>
-            </PageHeroActions>
-          </PageHero>
+    <IDSHeader type="inera-admin" className="z-40 bg-white print:hidden">
+      <a className="ids-heading-2 text-legacy-main no-underline" slot="brand-text" href="/">
+        Webcert
+      </a>
+    </IDSHeader>
+    <div className="px-5 md:p-0">
+      <div className="ids-content mx-auto max-w-screen-md py-24 text-center">
+        <div className="mb-10 inline-block">
+          <IDSIconAttention height="4.375rem" width="4.375rem" color="#00706e" color2="#00706e" />
         </div>
-      </main>
-      <LayoutFooter hasSession={false} />
+        <div className="mb-5">
+          <h1 className="ids-heading-1 text-legacy-main">Planerat underhåll</h1>
+          <p className="ids-preamble">Webcert är stängd för tekniskt underhåll. Välkommen åter!</p>
+          <IDSLink>
+            <IDSIconChevron />
+            <a href="https://wc2.webcert.intygstjanster.se/">Till startsidan</a>
+          </IDSLink>
+        </div>
+      </div>
     </div>
+    <IDSFooter type="inera-legacy" headline="Webcert">
+      <p>Webcert är en tjänst som drivs av Inera AB.</p>
+    </IDSFooter>
   </React.StrictMode>
 )
