0cae8700021e0ee8ec2466da10644bdfca35b576
Samuel Tilly <samuel.tilly@gmail.com>
2024-06-12 10:11:10 +0200

    Fix footer issue

--- apps/maintenance/src/general.tsx
+++ apps/maintenance/src/general.tsx
@@ -5,40 +5,42 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <IDSHeader type="inera-admin" className="z-40 bg-white print:hidden">
-      <a className="ids-heading-2 no-underline" slot="brand-text" href="/">
-        Intygstjänster
-      </a>
-    </IDSHeader>
-    <div className="px-5 md:p-0">
-      <div className="ids-content mx-auto max-w-screen-md py-24 text-center">
-        <div className="mb-10 inline-block">
-          <IDSIconAttention height="4.375rem" width="4.375rem" />
+    <div className="flex min-h-screen flex-col">
+      <IDSHeader type="inera-admin" className="z-40 bg-white print:hidden">
+        <a className="ids-heading-2 no-underline" slot="brand-text" href="/">
+          Intygstjänster
+        </a>
+      </IDSHeader>
+      <main className="relative flex-1">
+        <div className="ids-content mx-auto max-w-screen-md py-24 text-center">
+          <div className="mb-10 inline-block">
+            <IDSIconAttention height="4.375rem" width="4.375rem" />
+          </div>
+          <div className="mb-5">
+            <h1 className="ids-heading-1">Planerat underhåll</h1>
+            <p className="ids-preamble">
+              Intygstjänster är stängd för tekniskt underhåll. <br />
+              <br />
+              Torsdagen den 13 juni mellan kl. 07:00 - 08:00 görs tekniskt underhåll av Intygstjänster. Tjänsterna kommer inte vara
+              tillgängliga under tiden.
+              <br />
+              <br /> Välkommen åter!
+            </p>
+            <IDSLink>
+              <IDSIconChevron />
+              <a href="https://www.inera.se">Till Inera.se</a> <IDSIconExternal slot="append-icon" />
+            </IDSLink>
+            <IDSLink className="pl-3">
+              <IDSIconChevron />
+              <a href="https://www.inera.se/driftstatus/kommande-atgarder/">Se driftstatus på Inera.se</a>{' '}
+              <IDSIconExternal slot="append-icon" />
+            </IDSLink>
+          </div>
         </div>
-        <div className="mb-5">
-          <h1 className="ids-heading-1">Planerat underhåll</h1>
-          <p className="ids-preamble">
-            Intygstjänster är stängd för tekniskt underhåll. <br />
-            <br />
-            Torsdagen den 13 juni mellan kl. 07:00 - 08:00 görs tekniskt underhåll av Intygstjänster. Tjänsterna kommer inte vara
-            tillgängliga under tiden.
-            <br />
-            <br /> Välkommen åter!
-          </p>
-          <IDSLink>
-            <IDSIconChevron />
-            <a href="https://www.inera.se">Till Inera.se</a> <IDSIconExternal slot="append-icon" />
-          </IDSLink>
-          <IDSLink className="pl-3">
-            <IDSIconChevron />
-            <a href="https://www.inera.se/driftstatus/kommande-atgarder/">Se driftstatus på Inera.se</a>{' '}
-            <IDSIconExternal slot="append-icon" />
-          </IDSLink>
-        </div>
-      </div>
+      </main>
+      <IDSFooter type="inera-admin" headline="Intygstjänster">
+        <p>Intygsjänster är en grupp tjänster som drivs av Inera AB.</p>
+      </IDSFooter>
     </div>
-    <IDSFooter type="inera-admin" headline="Intygstjänster">
-      <p>Intygsjänster är en grupp tjänster som drivs av Inera AB.</p>
-    </IDSFooter>
   </React.StrictMode>
 )
