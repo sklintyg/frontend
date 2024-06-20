5e6cf38ec8f0525ddaafd88da21d63810c251fa8
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2024-06-20 15:22:40 +0200

    INTYGFV-16688: Redesign UeMedicalInvestigation (#1337)

--- apps/webcert/src/feature/certificate/UnifiedEdit/UeMedicalInvestigation/UeMedicalInvestigation.tsx
+++ apps/webcert/src/feature/certificate/UnifiedEdit/UeMedicalInvestigation/UeMedicalInvestigation.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import styled from 'styled-components'
 import DatePickerCustom from '../../../../components/Inputs/DatePickerCustom/DatePickerCustom'
 import Dropdown from '../../../../components/Inputs/Dropdown'
 import TextInput from '../../../../components/Inputs/TextInput'
@@ -8,39 +8,52 @@ import {
   CertificateDataValidationType,
   ConfigUeCodeItem,
   ConfigUeMedicalInvestigation,
+  ConfigUeMedicalInvestigationList,
   TextValidation,
   ValidationError,
   ValueMedicalInvestigation,
 } from '../../../../types'
-import { UeMedicalInvestigationGrid } from './UeMedicalInvestigationGrid'
 
-export interface Props {
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+  &:not(:last-child) {
+    padding-bottom: 0.9375rem;
+  }
+`
+
+const Root = styled.div`
+  &:not(:last-child) {
+    padding-bottom: 1.25rem;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  }
+`
+
+export function UeMedicalInvestigation({
+  disabled,
+  config,
+  questionConfig,
+  value,
+  validation,
+  validationErrors,
+  error,
+  onChange,
+}: {
   disabled?: boolean
   config: ConfigUeMedicalInvestigation
+  questionConfig: ConfigUeMedicalInvestigationList
   value: ValueMedicalInvestigation
   validation: CertificateDataValidation[]
   validationErrors: ValidationError[]
   error: boolean
   onChange: (value: ValueMedicalInvestigation) => void
-}
-
-const UeMedicalInvestigation: React.FC<Props> = ({ disabled, config, value, validation, validationErrors, error, onChange }) => {
+}) {
   const textValidation = validation
     ? (validation.find((v) => v.type === CertificateDataValidationType.TEXT_VALIDATION) as TextValidation)
     : undefined
 
   const typeOptions: ConfigUeCodeItem[] = [{ id: '', label: 'Välj...', code: '' }, ...config.typeOptions]
-
-  const handleInvestigationTypeChange = (code: string) => {
-    onChange({
-      ...value,
-      investigationType: {
-        ...value.investigationType,
-        id: config.investigationTypeId,
-        code: code,
-      },
-    })
-  }
 
   const handleDateChange = (date: string) => {
     onChange({
@@ -53,65 +66,80 @@ const UeMedicalInvestigation: React.FC<Props> = ({ disabled, config, value, vali
     })
   }
 
-  const handleInformationSourceChange = (text: string) => {
-    onChange({
-      ...value,
-      informationSource: {
-        ...value.informationSource,
-        id: config.informationSourceId,
-        text: text || null,
-      },
-    })
-  }
+  const typeErrors = validationErrors.filter((v) => v.field === config.investigationTypeId)
+  const dateErrors = validationErrors.filter((v) => v.field === config.dateId)
+  const informationSourceErrors = validationErrors.filter((v) => v.field === config.informationSourceId)
+
   return (
-    <>
-      <UeMedicalInvestigationGrid>
-        <div>
-          <Dropdown
-            id={config.investigationTypeId}
-            value={value.investigationType.code ?? ''}
-            title={typeOptions.find((item) => item.code === value.investigationType.code)?.label}
-            disabled={disabled}
-            onChange={(event) => {
-              handleInvestigationTypeChange(event.currentTarget.value)
-            }}
-            error={error || validationErrors.some((v) => v.field === config.investigationTypeId)}
-          >
-            {typeOptions.map((item) => (
-              <option key={item.id} value={item.id}>
-                {item.label}
-              </option>
-            ))}
-          </Dropdown>
-        </div>
-        <div>
-          <DatePickerCustom
-            id={config.dateId}
-            max={config.maxDate}
-            min={config.minDate}
-            inputString={value.date.date ?? ''}
-            textInputOnChange={handleDateChange}
-            disabled={disabled}
-            setDate={handleDateChange}
-            displayValidationErrorOutline={error || validationErrors.some((v) => v.field === config.dateId)}
-          />
-        </div>
+    <Root>
+      <div>
+        <Row>
+          <div style={{ width: '100%' }}>
+            <Dropdown
+              label={questionConfig.typeText}
+              id={config.investigationTypeId}
+              value={value.investigationType.code ?? ''}
+              title={typeOptions.find((item) => item.code === value.investigationType.code)?.label}
+              disabled={disabled}
+              onChange={(event) => {
+                onChange({
+                  ...value,
+                  investigationType: {
+                    ...value.investigationType,
+                    id: config.investigationTypeId,
+                    code: event.currentTarget.value,
+                  },
+                })
+              }}
+              error={error || typeErrors.length > 0}
+              fullWidth={true}
+            >
+              {typeOptions.map((item) => (
+                <option key={item.id} value={item.id}>
+                  {item.label}
+                </option>
+              ))}
+            </Dropdown>
+            <QuestionValidationTexts validationErrors={typeErrors} />
+          </div>
+          <div style={{ minWidth: '201px' }}>
+            <label htmlFor={config.dateId}>{questionConfig.dateText}</label>
+            <DatePickerCustom
+              id={config.dateId}
+              max={config.maxDate}
+              min={config.minDate}
+              inputString={value.date.date ?? ''}
+              textInputOnChange={handleDateChange}
+              disabled={disabled}
+              setDate={handleDateChange}
+              displayValidationErrorOutline={error || dateErrors.length > 0}
+            />
+            <QuestionValidationTexts validationErrors={dateErrors} />
+          </div>
+        </Row>
         <div>
           <TextInput
+            label={questionConfig.informationSourceText}
             onChange={(event) => {
-              handleInformationSourceChange(event.currentTarget.value)
+              onChange({
+                ...value,
+                informationSource: {
+                  ...value.informationSource,
+                  id: config.informationSourceId,
+                  text: event.currentTarget.value || null,
+                },
+              })
             }}
             id={config.informationSourceId}
-            hasValidationError={error || validationErrors.some((v) => v.field === config.informationSourceId)}
+            hasValidationError={error || informationSourceErrors.length > 0}
             value={value.informationSource.text ?? ''}
             limit={textValidation ? textValidation.limit : 100}
             disabled={disabled}
+            tooltip={questionConfig.informationSourceDescription}
           />
+          <QuestionValidationTexts validationErrors={informationSourceErrors} />
         </div>
-      </UeMedicalInvestigationGrid>
-      <QuestionValidationTexts validationErrors={validationErrors} />
-    </>
+      </div>
+    </Root>
   )
 }
-
-export default UeMedicalInvestigation
