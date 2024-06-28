5edc69271bb414886d6773183ee7c556f9b3972e
Samuel Tilly <samuel.tilly@gmail.com>
2024-06-28 08:35:53 +0200

    INTYGFV-16508: Add margin to messages

--- apps/webcert/src/feature/certificate/Question/Question.tsx
+++ apps/webcert/src/feature/certificate/Question/Question.tsx
@@ -43,7 +43,11 @@ const Question: React.FC<QuestionProps> = ({ id, className }) => {
           {<QuestionHeading question={question} />}
         </>
       )}
-      {question.config.message && !question.readOnly && <QuestionMessage message={question.config.message} />}
+      {question.config.message && !question.readOnly && (
+        <div className="iu-mb-200">
+          <QuestionMessage message={question.config.message} />
+        </div>
+      )}
       <div>
         {isReadOnly ? <QuestionUvResolve question={question} /> : <QuestionEditComponent question={question} disabled={disabled} />}
       </div>
