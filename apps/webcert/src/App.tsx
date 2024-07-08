cfc77b5f926d031fe8ca4b5f5b3853222f47056b
Samuel Tilly <samuel.tilly@gmail.com>
2024-07-08 14:26:26 +0200

    make sure gotoCertificate hook is alwaays available

--- apps/webcert/src/App.tsx
+++ apps/webcert/src/App.tsx
@@ -2,27 +2,18 @@ import 'inera-core-css/dist/inera-master.css'
 import { useEffect } from 'react'
 import 'react-datepicker/dist/react-datepicker.css'
 import { ErrorBoundary } from 'react-error-boundary'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Routes } from './Routes'
 import { ErrorMessage } from './components/ErrorMessage/ErrorMessage'
 import ErrorComponent from './components/error/ErrorComponent'
 import CareProviderModal from './feature/careProvider/CareProviderModal'
 import SubscriptionWarningModal from './feature/subscription/SubscriptionWarningModal'
-import CertificateDraftsPage from './page/CertificateDraftsPage'
-import CertificatePage from './page/CertificatePage'
-import { CreatePageWithRedirect } from './page/CreatePage'
-import ErrorPage from './page/ErrorPage'
-import { SearchPageWithRedirect } from './page/SearchPage'
-import SignedCertificatesPage from './page/SignedCertificatesPage'
-import { StartPage } from './page/StartPage'
-import UnhandledCertificatesPage from './page/UnhandledCertificatesPage'
-import Welcome from './page/Welcome'
 import { throwError } from './store/error/errorActions'
 import { createErrorRequest } from './store/error/errorCreator'
 import { ErrorCode, ErrorType } from './store/error/errorReducer'
 import { useAppDispatch } from './store/store'
 import { cancelLogout, triggerLogout } from './store/user/userActions'
 import { initateApplication } from './store/welcome/welcomeActions'
-import { LoggedInUserRedirect } from './utils/LoggedInUserRedirect'
 
 function App(): JSX.Element {
   const dispatch = useAppDispatch()
@@ -52,26 +43,7 @@ function App(): JSX.Element {
         <ErrorComponent />
         <CareProviderModal />
         <SubscriptionWarningModal />
-        <Switch>
-          <Route
-            path="/"
-            exact
-            render={() => (
-              <LoggedInUserRedirect>
-                <StartPage />
-              </LoggedInUserRedirect>
-            )}
-          />
-          <Route path="/certificate/:certificateId/sign/:error" render={() => <CertificatePage />} />
-          <Route path="/certificate/:certificateId" render={() => <CertificatePage />} />
-          <Route path="/welcome(.html)?" render={() => <Welcome />} />
-          <Route path="/error(.jsp)?" render={() => <ErrorPage />} />
-          <Route path="/create/:patientId?" render={() => <CreatePageWithRedirect />} />
-          <Route path="/search" render={() => <SearchPageWithRedirect />} />
-          <Route path="/list/draft" render={() => <CertificateDraftsPage />} />
-          <Route path="/list/certificate" render={() => <SignedCertificatesPage />} />
-          <Route path="/list/unhandledcertificates" render={() => <UnhandledCertificatesPage />} />
-        </Switch>
+        <Routes />
       </ErrorBoundary>
     </BrowserRouter>
   )
---
+++ apps/webcert/src/Routes.tsx
@@ -0,0 +1,39 @@
+import { Route, Switch } from 'react-router-dom'
+import { useGotoCertificate } from './hooks/useGotoCertificate'
+import CertificateDraftsPage from './page/CertificateDraftsPage'
+import CertificatePage from './page/CertificatePage'
+import { CreatePageWithRedirect } from './page/CreatePage'
+import ErrorPage from './page/ErrorPage'
+import { SearchPageWithRedirect } from './page/SearchPage'
+import SignedCertificatesPage from './page/SignedCertificatesPage'
+import { StartPage } from './page/StartPage'
+import UnhandledCertificatesPage from './page/UnhandledCertificatesPage'
+import Welcome from './page/Welcome'
+import { LoggedInUserRedirect } from './utils/LoggedInUserRedirect'
+
+export function Routes() {
+  useGotoCertificate()
+
+  return (
+    <Switch>
+      <Route
+        path="/"
+        exact
+        render={() => (
+          <LoggedInUserRedirect>
+            <StartPage />
+          </LoggedInUserRedirect>
+        )}
+      />
+      <Route path="/certificate/:certificateId/sign/:error" render={() => <CertificatePage />} />
+      <Route path="/certificate/:certificateId" render={() => <CertificatePage />} />
+      <Route path="/welcome(.html)?" render={() => <Welcome />} />
+      <Route path="/error(.jsp)?" render={() => <ErrorPage />} />
+      <Route path="/create/:patientId?" render={() => <CreatePageWithRedirect />} />
+      <Route path="/search" render={() => <SearchPageWithRedirect />} />
+      <Route path="/list/draft" render={() => <CertificateDraftsPage />} />
+      <Route path="/list/certificate" render={() => <SignedCertificatesPage />} />
+      <Route path="/list/unhandledcertificates" render={() => <UnhandledCertificatesPage />} />
+    </Switch>
+  )
+}
