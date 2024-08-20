73c3510d1a29ab3a3e4235ab47f981fd731f14d9
Samuel Tilly <samuel.tilly@gmail.com>
2024-08-20 09:21:46 +0200

    Added confirmation modal tests

--- apps/webcert/src/faker/certificate/fakeCertificateConfirmationModal.ts
+++ apps/webcert/src/faker/certificate/fakeCertificateConfirmationModal.ts
@@ -1,7 +1,7 @@
 import faker from 'faker'
 import { CertificateConfirmationModal } from '../../types/confirmModal'
 
-export function fakeCertificateConfirmationModal(value: Partial<CertificateConfirmationModal>): CertificateConfirmationModal {
+export function fakeCertificateConfirmationModal(value?: Partial<CertificateConfirmationModal>): CertificateConfirmationModal {
   return {
     title: faker.lorem.sentence(),
     text: faker.lorem.paragraphs(),
