2b3446e3ada1560e83d615c8495ff0f52059b1e0
Samuel Tilly <samuel.tilly@gmail.com>
2024-08-27 09:15:06 +0200

    update error page content

--- apps/webcert/src/components/error/errorPageContent/FetchQuestionsProblem.tsx
+++ apps/webcert/src/components/error/errorPageContent/FetchQuestionsProblem.tsx
@@ -1,12 +1,8 @@
-import type React from 'react'
 import styled from 'styled-components'
 import errorImage from '../../../images/fel-1.svg'
-import WCDynamicLink from '../../../utils/WCDynamicLink'
 import CenteredImageWithContent from '../../image/CenteredImageWithContent'
+import { ContactSupportMessage } from '../ContactSupportMessage'
 import ErrorCopyText from '../ErrorCopyText'
-
-export const TITLE = 'Ärenden kunde inte visas'
-export const MESSAGE = 'Prova att ladda om sidan. Om problemet kvarstår, kontakta i första hand din lokala IT-avdelning och i andra hand'
 
 const Wrapper = styled.div`
   background-color: white;
@@ -14,11 +10,7 @@ const Wrapper = styled.div`
   text-align: center;
 `
 
-interface Props {
-  errorId: string
-}
-
-const FetchQuestionsProblem: React.FC<Props> = ({ errorId }) => {
+export function FetchQuestionsProblem({ errorId }: { errorId: string }) {
   if (!errorId) {
     return null
   }
@@ -26,14 +18,12 @@ const FetchQuestionsProblem: React.FC<Props> = ({ errorId }) => {
   return (
     <Wrapper>
       <CenteredImageWithContent imgSrc={errorImage}>
-        <h4>{TITLE}</h4>
+        <h4>Ärenden kunde inte visas</h4>
         <p>
-          {MESSAGE} <WCDynamicLink linkKey={'ineraKundserviceAnmalFel'} />.
+          Prova att ladda om sidan. <ContactSupportMessage />.
         </p>
         <ErrorCopyText errorId={errorId} />
       </CenteredImageWithContent>
     </Wrapper>
   )
 }
-
-export default FetchQuestionsProblem
