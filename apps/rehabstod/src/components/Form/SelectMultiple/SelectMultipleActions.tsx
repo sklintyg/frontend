a913f7ad23dc5677cf63fdf805d9bb28681b6c65
Samuel Tilly <samuel.tilly@gmail.com>
2024-09-05 11:11:19 +0200

    fix styling issues for ModifyTableColumns

--- apps/rehabstod/src/components/Form/SelectMultiple/SelectMultipleActions.tsx
+++ apps/rehabstod/src/components/Form/SelectMultiple/SelectMultipleActions.tsx
@@ -3,7 +3,7 @@ import { forwardRef } from 'react'
 
 export const SelectMultipleActions = forwardRef<HTMLDivElement, HTMLProps<HTMLDivElement>>(({ children }, ref) => (
   <div ref={ref} className="pt-2">
-    <div className="flex grow items-center border-t border-neutral-90 pt-3">{children}</div>
+    <div className="flex grow items-center border-t border-neutral-90 py-3">{children}</div>
   </div>
 ))
 
