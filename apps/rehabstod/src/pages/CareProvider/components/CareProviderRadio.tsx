bc0a3c40cfd2d99ceaf5b85f55d87fc509e1361b
Samuel Tilly <samuel.tilly@gmail.com>
2024-09-10 08:37:12 +0200

    styling fixes to CareProvider page

---
+++ apps/rehabstod/src/pages/CareProvider/components/CareProviderRadio.tsx
@@ -0,0 +1,28 @@
+import { classNames } from '@frontend/components'
+import { IDSRadio } from '@frontend/ids-react-ts'
+import type { ChangeEventHandler, MouseEventHandler } from 'react'
+
+export function CareProviderRadioButton({
+  id,
+  value,
+  label,
+  checked,
+  onChange,
+  onClick,
+}: {
+  id: string
+  value: string
+  checked: boolean
+  label: string
+  onChange: ChangeEventHandler<HTMLInputElement>
+  onClick?: MouseEventHandler<HTMLInputElement>
+}) {
+  return (
+    <IDSRadio>
+      <input type="radio" name="selectedUnit" value={value} id={id} checked={checked} onClick={onClick} onChange={onChange} />
+      <label htmlFor={id} className={classNames('ids-label ids-label--radio', checked && 'font-bold')}>
+        <span>{label}</span>
+      </label>
+    </IDSRadio>
+  )
+}
