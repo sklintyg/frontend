e0a25dc5e867e3698dbe020d61f95b6142e310ea
Mikael Heglind <mikael.heglind@soprasteria.com>
2024-09-12 15:24:15 +0200

    K1J-156: Changed redirect from back button to use history.action POP

--- apps/webcert/src/feature/certificate/CertificateHeader/NavigateBackButton.tsx
+++ apps/webcert/src/feature/certificate/CertificateHeader/NavigateBackButton.tsx
@@ -2,16 +2,24 @@ import StatusWithIcon from '../../../components/utils/StatusWithIcon'
 import { WithUserResourceLink } from '../../../components/utils/WithResourceLink'
 import { LinkButton } from '../../../styles'
 import { ResourceLinkType } from '../../../types'
-import { useGoBackEffect } from '../../../hooks/useGoBackEffect'
+import { useHistory } from 'react-router-dom'
 
 function NavigateBackButton() {
-  const goBack = useGoBackEffect()
+  const history = useHistory()
+
+  const handleGoBack = () => {
+    if (history.action === 'POP') {
+      history.push('/')
+    } else {
+      history.goBack()
+    }
+  }
 
   return (
     <WithUserResourceLink type={ResourceLinkType.NAVIGATE_BACK_BUTTON}>
       {(link) => (
         <StatusWithIcon icon="ArrowLeft">
-          <LinkButton className="ic-link" onClick={goBack}>
+          <LinkButton className="ic-link" onClick={handleGoBack}>
             {link.name}
           </LinkButton>
         </StatusWithIcon>
