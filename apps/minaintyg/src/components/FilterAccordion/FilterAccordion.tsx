d1116db544cbc517517ed72580c329bdc727dab0
Samuel Tilly <samuel.tilly@gmail.com>
2024-09-12 10:36:31 +0200

    Fix missing colors

--- apps/minaintyg/src/components/FilterAccordion/FilterAccordion.tsx
+++ apps/minaintyg/src/components/FilterAccordion/FilterAccordion.tsx
@@ -5,15 +5,15 @@ export function FilterAccordion({ total, listed, noun, children }: { noun: strin
   return (
     <details className="group mb-5">
       <summary className="flex cursor-pointer select-none flex-col md:flex-row">
-        <div className="grow border-y border-stone-clear py-2.5 md:pl-5">
+        <div className="grow border-y border-neutral-90  py-2.5 md:pl-5">
           Visar {listed} av {total} {noun}
         </div>
-        <div className="-mt-px flex justify-between gap-5 border-y border-stone-clear py-2.5 md:mt-0 md:border-l md:px-5">
+        <div className="-mt-px flex justify-between gap-5 border-y border-neutral-90  py-2.5 md:mt-0 md:border-l md:px-5">
           Filtrera lista
           <IDSIconChevron width="100%" height="100%" className="h-2.5 w-2.5 rotate-90 self-center group-open:-rotate-90" />
         </div>
       </summary>
-      <div className="border-b border-stone-clear py-5">{children}</div>
+      <div className="border-b border-neutral-90  py-5">{children}</div>
     </details>
   )
 }
--- apps/minaintyg/src/pages/Certificate/components/CertificateReplacedAlert.tsx
+++ apps/minaintyg/src/pages/Certificate/components/CertificateReplacedAlert.tsx
@@ -2,7 +2,7 @@ import { IDSAlert } from '@frontend/ids-react-ts'
 
 export function CertificateReplacedAlert() {
   return (
-    <IDSAlert headline="Detta intyg har ersatts" type="attention">
+    <IDSAlert headline="Detta intyg har ersatts" type="attention" className="mb-2">
       Läkaren kan ersätta ett intyg om till exempel intyget innehåller fel information eller om ny information tillkommit.
     </IDSAlert>
   )
