fcf625358cd56df840fb6d5a76b4682cb624606f
Mikael Heglind <mikael.heglind@soprasteria.com>
2024-09-13 12:12:28 +0200

    K1J-156: Change to always goback to /search after sig with siths

--- apps/webcert/src/hooks/useGoBack.ts
+++ apps/webcert/src/hooks/useGoBack.ts
@@ -4,16 +4,11 @@ import { getPatientId } from '../store/srs/srsSelectors'
 
 export const useGoBack = () => {
   const history = useHistory()
-  const patientId = useAppSelector((state) => getPatientId(state))
   const isSithsSignReferrer = document.referrer.startsWith('https://esign')
 
   return () => {
     if (isSithsSignReferrer) {
-      if (patientId) {
-        window.location.replace(`/create/${encodeURIComponent(btoa(patientId))}`)
-      } else {
-        window.location.replace('/search')
-      }
+      window.location.replace('/search')
     } else {
       history.goBack()
     }
