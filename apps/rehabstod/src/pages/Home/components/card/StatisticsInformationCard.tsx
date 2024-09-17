28a6dafdbc8970aa6e02170f40d70d73b674ff71
Samuel Tilly <samuel.tilly@gmail.com>
2024-09-17 10:06:14 +0200

    fix icon and color in mobile menu

--- apps/rehabstod/src/pages/Home/components/card/StatisticsInformationCard.tsx
+++ apps/rehabstod/src/pages/Home/components/card/StatisticsInformationCard.tsx
@@ -1,5 +1,5 @@
 import { Tooltip, TooltipContent, TooltipTrigger } from '@frontend/components'
-import { IDSIconArrow, IDSIconExternal, IDSLink } from '@frontend/ids-react-ts'
+import { IDSIconExternal, IDSLink } from '@frontend/ids-react-ts'
 import { useGetLinksQuery } from '../../../../store/api'
 
 export function StatisticsInformationCard() {
@@ -18,7 +18,6 @@ export function StatisticsInformationCard() {
             <Tooltip>
               <TooltipTrigger>
                 <IDSLink>
-                  <IDSIconArrow />
                   <a target="_blank" href={links.statistiktjanstenTooltip.url} rel="noreferrer">
                     {links.statistiktjanstenTooltip.text}
                   </a>
