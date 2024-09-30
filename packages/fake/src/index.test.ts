b63f0997c24102452779d281533880c6c5504d71
Samuel Tilly <samuel.tilly@gmail.com>
2024-09-30 08:55:08 +0200

    Handle seed option with fake from schema

--- packages/fake/src/index.test.ts
+++ packages/fake/src/index.test.ts
@@ -10,7 +10,7 @@ it('Should generate a faker function from schema', () => {
 
   const fakePerson = fakerFromSchema(schema, { seed: 1 })
 
-  expect(fakePerson()).toEqual({ firstName: 'Rosemarie', lastName: 'Stamm' })
+  expect(fakePerson()).toEqual({ firstName: 'Mina', lastName: 'Champlin' })
 })
 
 it('Should be able to write partial data', () => {
@@ -21,7 +21,7 @@ it('Should be able to write partial data', () => {
 
   const fakePerson = fakerFromSchema(schema, { seed: 1 })
 
-  expect(fakePerson({ lastName: 'Smith' })).toEqual({ firstName: 'Rosemarie', lastName: 'Smith' })
+  expect(fakePerson({ lastName: 'Smith' })).toEqual({ firstName: 'Mina', lastName: 'Smith' })
 })
 
 it('Should be possible to override mock', () => {
@@ -37,5 +37,17 @@ it('Should be possible to override mock', () => {
     },
   })
 
-  expect(fakePerson()).toEqual({ firstName: 'Rosemarie', lastName: 'Andersson' })
+  expect(fakePerson()).toEqual({ firstName: 'Mina', lastName: 'Andersson' })
+})
+
+it('Should be able to handle seed when called multiple times', () => {
+  const schema = z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+  })
+
+  const fakePerson = fakerFromSchema(schema, { seed: 1 })
+
+  expect(fakePerson({ lastName: 'Smith' })).toEqual({ firstName: 'Mina', lastName: 'Smith' })
+  expect(fakePerson({ lastName: 'Smith' })).toEqual({ firstName: 'Caesar', lastName: 'Smith' })
 })
