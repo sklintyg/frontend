7c5c884b6e72acff21ac0522d24334cedd1bca97
Samuel Tilly <samuel.tilly@gmail.com>
2024-10-04 13:35:10 +0200

    update SelectProfile test

--- apps/minaintyg/src/pages/Welcome/components/SelectProfile.test.tsx
+++ apps/minaintyg/src/pages/Welcome/components/SelectProfile.test.tsx
@@ -1,12 +1,11 @@
-import { faker, fakerFromSchema } from '@frontend/fake'
+import { fakerFromSchema } from '@frontend/fake'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { vi } from 'vitest'
 import { testabilityPersonSchema } from '../../../schema/testability/person.schema'
 import { SelectProfile } from './SelectProfile'
 
-faker.seed(1234)
-const persons = Array.from({ length: 5 }, fakerFromSchema(testabilityPersonSchema))
+const persons = Array.from({ length: 5 }, fakerFromSchema(testabilityPersonSchema, { seed: 1 }))
 
 it('Should render persons as options', () => {
   render(<SelectProfile persons={persons} onChange={vi.fn()} />)
@@ -17,6 +16,6 @@ it('Should call onChange when clicking on option', async () => {
   const onChange = vi.fn()
   render(<SelectProfile persons={persons} onChange={onChange} />)
 
-  await userEvent.selectOptions(screen.getByRole('listbox'), screen.getByRole('option', { name: 'Lula Schmitt V (197105016834)' }))
+  await userEvent.selectOptions(screen.getByRole('listbox'), screen.getByRole('option', { name: 'Alberto Stamm (198312213968)' }))
   expect(onChange).toHaveBeenCalledTimes(1)
 })
