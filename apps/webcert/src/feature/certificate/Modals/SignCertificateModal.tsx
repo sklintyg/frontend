eab33da8c2c3be613fce659fe2caea3ed86c4243
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-10-11 14:42:41 +0200

    POC: Added error modal for status unknown

--- apps/webcert/src/feature/certificate/Modals/SignCertificateModal.tsx
+++ apps/webcert/src/feature/certificate/Modals/SignCertificateModal.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import type React from 'react'
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 import { getQrCodeForElegSignature, getSigningStatus } from '../../../store/certificate/certificateSelectors'
@@ -34,7 +35,7 @@ export const SignCertificateModal: React.FC = () => {
     return
   }
 
-  if (signStatus == CertificateSignStatus.FAILED) {
+  if (signStatus == CertificateSignStatus.FAILED || signStatus == CertificateSignStatus.UNKNOWN) {
     return (
       <ModalBase
         open={open}
