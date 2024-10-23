6fd42689fd20e9672391bfc6ba399b4e51a5c1d0
Samuel Tilly <samuel.tilly@gmail.com>
2024-10-23 15:01:54 +0200

    cleanup

--- apps/rehabstod/src/components/SickLeave/RiskSignalCircle.tsx
+++ apps/rehabstod/src/components/SickLeave/RiskSignalCircle.tsx
@@ -7,7 +7,7 @@ export function RiskSignalCircle({ riskCategory, index }: { riskCategory: number
       className={`block h-2.5 w-2.5 rounded-full border border-solid ${
         riskCategory <= index
           ? 'bg-white'
-          : classNames(riskCategory === 3 && 'bg-error-40', riskCategory === 2 && 'bg-graphic', riskCategory === 1 && 'bg-attention-40')
+          : classNames(riskCategory === 3 && 'bg-error-40', riskCategory === 2 && 'bg-error-10', riskCategory === 1 && 'bg-attention-40')
       }`}
     />
   )
