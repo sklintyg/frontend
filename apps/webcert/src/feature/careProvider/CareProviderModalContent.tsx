2cd3a056b5a6a673788013f16d0fcf541fbe46bb
Samuel Tilly <samuel.tilly@gmail.com>
2024-10-25 11:17:15 +0200

    K1J-450: Hide columns when statistics fails to load

--- apps/webcert/src/feature/careProvider/CareProviderModalContent.tsx
+++ apps/webcert/src/feature/careProvider/CareProviderModalContent.tsx
@@ -1,6 +1,5 @@
 import type React from 'react'
 import { useHistory } from 'react-router-dom'
-import styled from 'styled-components'
 import ExpandableTableRow from '../../components/Table/ExpandableTableRow'
 import SimpleTable from '../../components/Table/SimpleTable'
 import { START_URL, START_URL_FOR_ADMINISTRATORS } from '../../constants'
@@ -14,23 +13,21 @@ import {
   getUnitStatistics as selectUnitStatistics,
 } from '../../store/user/userSelectors'
 import type { CareUnit, Unit } from '../../types'
+import { CareProviderModalContentRow } from './CareProviderModalContentRow'
 
-const StyledButton = styled.button`
-  text-indent: 1.2em;
-`
+function getUnitStatisticsLiteral(amountOnUnit: number, amountOnOtherUnit = 0, careUnit?: CareUnit) {
+  const showTotal = careUnit && careUnit?.units.length > 0 && amountOnOtherUnit > 0
+  return `${amountOnUnit} ${showTotal ? `(totalt ${amountOnUnit + amountOnOtherUnit})` : ''}`
+}
 
-export const CareProviderModalContent: React.FC = () => {
+export function CareProviderModalContent() {
   const dispatch = useAppDispatch()
   const history = useHistory()
   const careProviders = useAppSelector(getCareProviders)
   const isCareAdministrator = useAppSelector(selectIsCareAdministrator)
   const unitStatistics = useAppSelector(selectUnitStatistics)
   const loggedInUnit = useAppSelector(getLoggedInUnit)
-
-  const getUnitStatisticsLiteral = (amountOnUnit: number, amountOnOtherUnit = 0, careUnit?: CareUnit) => {
-    const showTotal = careUnit && careUnit?.units.length > 0 && amountOnOtherUnit > 0
-    return `${amountOnUnit} ${showTotal ? `(totalt ${amountOnUnit + amountOnOtherUnit})` : ''}`
-  }
+  const hasStatistics = Object.keys(unitStatistics).length > 0
 
   const handleChooseUnit = (event: React.MouseEvent) => {
     const unitId = event.currentTarget.id
@@ -44,42 +41,15 @@ export const CareProviderModalContent: React.FC = () => {
     return unit.unitId === loggedInUnit?.unitId
   }
 
-  const getExpandedRows = (units: Unit[]) => {
-    return units.map((unit) => {
-      const questionsOnUnit = unitStatistics[unit.unitId]?.questionsOnUnit
-      const draftsOnUnit = unitStatistics[unit.unitId]?.draftsOnUnit
-
-      if (questionsOnUnit == null || draftsOnUnit == null) {
-        return null
-      }
-
-      return (
-        <tr key={unit.unitId}>
-          <td>
-            <button
-              className={`ic-link iu-ml-700 iu-text-left iu-border-white ${isLoggedInUnit(unit) && 'iu-color-muted ic-button--disabled'}`}
-              type="button"
-              id={unit.unitId}
-              onClick={handleChooseUnit}
-              disabled={isLoggedInUnit(unit)}
-            >
-              {getUnitName(unit)}
-            </button>
-          </td>
-          <td>{getUnitStatisticsLiteral(questionsOnUnit)}</td>
-          <td>{getUnitStatisticsLiteral(draftsOnUnit)}</td>
-        </tr>
-      )
-    })
-  }
-
   const getStatistics = (id: string) => {
-    return {
-      questionsOnUnit: unitStatistics[id]?.questionsOnUnit ?? 0,
-      questionsOnSubUnits: unitStatistics[id]?.questionsOnSubUnits ?? 0,
-      draftsOnUnit: unitStatistics[id]?.draftsOnUnit ?? 0,
-      draftsOnSubUnits: unitStatistics[id]?.draftsOnSubUnits ?? 0,
-    }
+    return hasStatistics
+      ? {
+          questionsOnUnit: unitStatistics[id]?.questionsOnUnit ?? 0,
+          questionsOnSubUnits: unitStatistics[id]?.questionsOnSubUnits ?? 0,
+          draftsOnUnit: unitStatistics[id]?.draftsOnUnit ?? 0,
+          draftsOnSubUnits: unitStatistics[id]?.draftsOnSubUnits ?? 0,
+        }
+      : undefined
   }
 
   const getUnitName = (unit: Unit) => {
@@ -88,43 +58,6 @@ export const CareProviderModalContent: React.FC = () => {
     }
 
     return unit.unitName
-  }
-
-  const renderRows = (careUnit: CareUnit) => {
-    const statistics = getStatistics(careUnit.unitId)
-    const careUnitHasUnits = careUnit.units.length > 0
-
-    return careUnitHasUnits ? (
-      <ExpandableTableRow
-        rowContent={[
-          getUnitName(careUnit),
-          getUnitStatisticsLiteral(statistics.questionsOnUnit, statistics.questionsOnSubUnits, careUnit),
-          getUnitStatisticsLiteral(statistics.draftsOnUnit, statistics.draftsOnSubUnits, careUnit),
-        ]}
-        id={careUnit.unitId}
-        handleClick={handleChooseUnit}
-        key={careUnit.unitId}
-        disabled={isLoggedInUnit(careUnit)}
-      >
-        {getExpandedRows(careUnit.units)}
-      </ExpandableTableRow>
-    ) : (
-      <tr key={careUnit.unitId}>
-        <td>
-          <StyledButton
-            className={`ic-link iu-text-left iu-border-white ${isLoggedInUnit(careUnit) && 'iu-color-muted ic-button--disabled'}`}
-            type="button"
-            id={careUnit.unitId}
-            onClick={handleChooseUnit}
-            disabled={isLoggedInUnit(careUnit)}
-          >
-            {getUnitName(careUnit)}
-          </StyledButton>
-        </td>
-        <td>{getUnitStatisticsLiteral(statistics.questionsOnUnit, statistics.questionsOnSubUnits, careUnit)}</td>
-        <td>{getUnitStatisticsLiteral(statistics.draftsOnUnit, statistics.draftsOnSubUnits, careUnit)}</td>
-      </tr>
-    )
   }
 
   return (
@@ -138,12 +71,65 @@ export const CareProviderModalContent: React.FC = () => {
                   title: careProvider.missingSubscription ? `${careProvider.name} (Abonnemang saknas)` : careProvider.name,
                   adjustCellToText: false,
                 },
-                { title: 'Ej hanterade ärenden', adjustCellToText: true },
-                { title: 'Ej signerade utkast', adjustCellToText: true },
+                ...(hasStatistics
+                  ? [
+                      { title: 'Ej hanterade ärenden', adjustCellToText: true },
+                      { title: 'Ej signerade utkast', adjustCellToText: true },
+                    ]
+                  : []),
               ]}
               key={careProvider.id}
             >
-              {careProvider.careUnits.map((careUnit) => renderRows(careUnit))}
+              {careProvider.careUnits.map((careUnit: CareUnit) => {
+                const statistics = getStatistics(careUnit.unitId)
+                const careUnitHasUnits = careUnit.units.length > 0
+
+                return careUnitHasUnits ? (
+                  <ExpandableTableRow
+                    rowContent={[
+                      getUnitName(careUnit),
+                      ...(statistics
+                        ? [
+                            getUnitStatisticsLiteral(statistics.questionsOnUnit, statistics.questionsOnSubUnits, careUnit),
+                            getUnitStatisticsLiteral(statistics.draftsOnUnit, statistics.draftsOnSubUnits, careUnit),
+                          ]
+                        : []),
+                    ]}
+                    id={careUnit.unitId}
+                    handleClick={handleChooseUnit}
+                    key={careUnit.unitId}
+                    disabled={isLoggedInUnit(careUnit)}
+                  >
+                    {careUnit.units.map((unit) => {
+                      const unitStatistics = getStatistics(unit.unitId)
+                      return (
+                        <CareProviderModalContentRow
+                          key={unit.unitId}
+                          unit={unit}
+                          name={getUnitName(unit)}
+                          questionsOnUnit={unitStatistics && getUnitStatisticsLiteral(unitStatistics.questionsOnUnit)}
+                          draftsOnUnit={unitStatistics && getUnitStatisticsLiteral(unitStatistics.draftsOnUnit)}
+                          loggedIn={isLoggedInUnit(unit)}
+                          expanded
+                          onChoose={handleChooseUnit}
+                        />
+                      )
+                    })}
+                  </ExpandableTableRow>
+                ) : (
+                  <CareProviderModalContentRow
+                    key={careUnit.unitId}
+                    unit={careUnit}
+                    name={getUnitName(careUnit)}
+                    loggedIn={isLoggedInUnit(careUnit)}
+                    onChoose={handleChooseUnit}
+                    questionsOnUnit={
+                      statistics && getUnitStatisticsLiteral(statistics.questionsOnUnit, statistics.questionsOnSubUnits, careUnit)
+                    }
+                    draftsOnUnit={statistics && getUnitStatisticsLiteral(statistics.draftsOnUnit, statistics.draftsOnSubUnits, careUnit)}
+                  />
+                )
+              })}
             </SimpleTable>
           </div>
         )
