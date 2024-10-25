99a1f3e8b649395ccc2894bd5f3ca29823081c47
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2024-10-25 15:15:25 +0200

    K1J-442: Extra rehabstod welcome fixes (#1421)
    Co-authored-by: Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>

--- packages/components/src/form/Select.tsx
+++ packages/components/src/form/Select.tsx
@@ -14,15 +14,16 @@ interface SelectProps {
   description?: string
   disabled?: boolean
   defaultOption?: SelectOption
+  light?: boolean
 }
 
 export const Select = forwardRef<HTMLSelectElement, React.SelectHTMLAttributes<HTMLSelectElement> & SelectProps>(
-  ({ children, id: controlledId, label, description, disabled, options, defaultOption, ...props }, ref) => {
+  ({ children, id: controlledId, label, description, disabled, light, options, defaultOption, ...props }, ref) => {
     const uncontrolledId = useId()
     const id = controlledId ?? uncontrolledId
 
     return (
-      <IDSSelect isDisabled={disabled} className="my-0">
+      <IDSSelect light={light} isDisabled={disabled} className="my-0">
         {label && (
           <InputLabel htmlFor={id} description={description}>
             {label}
@@ -30,7 +31,7 @@ export const Select = forwardRef<HTMLSelectElement, React.SelectHTMLAttributes<H
         )}
         <select ref={ref} id={id} disabled={disabled} {...props}>
           {defaultOption && (
-            <option key={defaultOption.label} value={defaultOption.value}>
+            <option key={`${defaultOption.label}_${defaultOption.value}`} value={defaultOption.value}>
               {defaultOption.label}
             </option>
           )}
