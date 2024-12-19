df92cef16e51fcd42fb30239f35d825bed9d5f30
cigerhed <christoffer.sigerhed@soprasteria.com>
2024-12-19 08:26:44 +0100

    K1J-632: Dont display MajorVersionNotification if certificate is inactive type.

--- apps/webcert/src/feature/certificate/NotificationBanners/MajorVersionNotification.test.tsx
+++ apps/webcert/src/feature/certificate/NotificationBanners/MajorVersionNotification.test.tsx
@@ -18,8 +18,10 @@ const renderDefaultComponent = () => {
 
 const INFO_TEXT = 'Du kan inte använda alla funktioner, intyget är av en äldre version.'
 
-const setState = (latestMajorVersion: boolean) => {
-  testStore.dispatch(updateCertificate(fakeCertificate({ metadata: fakeCertificateMetaData({ latestMajorVersion }) })))
+const setState = (latestMajorVersion: boolean, inactiveCertificateType: boolean) => {
+  testStore.dispatch(
+    updateCertificate(fakeCertificate({ metadata: fakeCertificateMetaData({ latestMajorVersion, inactiveCertificateType }) }))
+  )
 }
 
 describe('MajorVersionNotification', () => {
@@ -28,13 +30,19 @@ describe('MajorVersionNotification', () => {
   })
 
   it('shall render a banner if not latest major version', () => {
-    setState(false)
+    setState(false, false)
+    renderDefaultComponent()
+    expect(screen.getByText(INFO_TEXT)).toBeInTheDocument()
+  })
+
+  it('shall not render a banner if certificate type is inactive', () => {
+    setState(false, true)
     renderDefaultComponent()
     expect(screen.getByText(INFO_TEXT)).toBeInTheDocument()
   })
 
   it('shall not render a banner if latest major version', () => {
-    setState(true)
+    setState(true, false)
     renderDefaultComponent()
     expect(screen.queryByText(INFO_TEXT)).not.toBeInTheDocument()
   })
