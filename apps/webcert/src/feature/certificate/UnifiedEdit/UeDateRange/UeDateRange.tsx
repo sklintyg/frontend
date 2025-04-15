6f82ae6ac2b359b1e8634558bb3aa08d17c41c84
cigerhed <christoffer.sigerhed@soprasteria.com>
2025-04-15 16:11:11 +0200

    K1J-969: Refactor UeDateRange component and tests to improve validation error handling from CS.

--- apps/webcert/src/feature/certificate/UnifiedEdit/UeDateRange/UeDateRange.tsx
+++ apps/webcert/src/feature/certificate/UnifiedEdit/UeDateRange/UeDateRange.tsx
@@ -7,16 +7,16 @@ import { updateCertificateDataElement } from '../../../../store/certificate/cert
 import { getVisibleValidationErrors } from '../../../../store/certificate/certificateSelectors'
 import DatePickerCustom from '../../../../components/Inputs/DatePickerCustom/DatePickerCustom'
 import QuestionValidationTexts from '../../../../components/Validation/QuestionValidationTexts'
-import type { CertificateDataElement, ConfigUeDateRange, ValueDateRange, ValidationError } from '../../../../types'
+import type { CertificateDataElement, ConfigUeDateRange, ValidationError, ValueDateRange } from '../../../../types'
 import {
-  dayCodeReg,
-  weekCodeReg,
-  monthCodeReg,
-  getValidDate,
-  parseDayCodes,
-  formatDateToString,
   _dateReg,
   _dateRegDashesOptional,
+  dayCodeReg,
+  formatDateToString,
+  getValidDate,
+  monthCodeReg,
+  parseDayCodes,
+  weekCodeReg,
 } from '../../../../utils'
 
 const regexArray = [dayCodeReg, weekCodeReg, monthCodeReg]
@@ -165,7 +165,9 @@ const UeDateRange: React.FC<Props> = ({ question, disabled }) => {
             textInputOnBlur={handleToTextInputOnBlur}
             textInputOnKeyDown={handleToTextInputOnKeyDown}
             textInputDataTestId={`tom${config.id}`}
-            displayValidationErrorOutline={getShouldDisplayValidationErrorOutline(config.id, 'tom')}
+            displayValidationErrorOutline={
+              getShouldDisplayValidationErrorOutline(config.id, 'tom') || getShouldDisplayValidationErrorOutline(config.id, 'to')
+            }
           />
         </DatesWrapper>
       </DateGrid>
