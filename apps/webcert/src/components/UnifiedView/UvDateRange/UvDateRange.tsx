321a8fdd5214ce90561aed46b17fdcd77f7c514a
cigerhed <christoffer.sigerhed@soprasteria.com>
2025-04-15 13:49:15 +0200

    K1J-969: Refactor UvDateRange component and corresponding tests

--- apps/webcert/src/components/UnifiedView/UvDateRange/UvDateRange.tsx
+++ apps/webcert/src/components/UnifiedView/UvDateRange/UvDateRange.tsx
@@ -1,7 +1,11 @@
 import type React from 'react'
 import styled from 'styled-components'
 import type { ConfigUeDateRange, ValueDateRange } from '../../../types'
-import { Badge } from '../Badge'
+import { Table } from '../../Table/Table'
+import { TableHeader } from '../../Table/TableHeader'
+import { TableRow } from '../../Table/TableRow'
+import { TableCell } from '../../Table/TableCell'
+import { TableBody } from '../../Table/TableBody'
 
 const DateRangeWrapper = styled.div`
   display: inline-block;
@@ -9,15 +13,21 @@ const DateRangeWrapper = styled.div`
 
 export const UvDateRange: React.FC<{ value: ValueDateRange; config: ConfigUeDateRange }> = ({ value, config }) => {
   return (
-    <DateRangeWrapper>
-      <div>
-        <p className={'iu-fs-200 iu-fw-bold iu-pb-200 iu-pt-300'}>{config.fromLabel}</p>
-        <Badge>{typeof value.from === 'string' && value.from.length > 0 ? value.from : 'Ej angivet'}</Badge>
-      </div>
-      <div className={'iu-ml-600'}>
-        <p className={'iu-fs-200 iu-fw-bold iu-pb-200 iu-pt-300'}>{config.toLabel}</p>
-        <Badge>{typeof value.to === 'string' && value.to.length > 0 ? value.to : 'Ej angivet'}</Badge>
-      </div>
-    </DateRangeWrapper>
+    <div className={'iu-p-none'}>
+      <Table style={{ tableLayout: 'fixed', width: '100%' }}>
+        <TableHeader>
+          <TableRow>
+            <TableCell style={{ width: '50%' }}>Från och med</TableCell>
+            <TableCell style={{ width: '50%' }}>Till och med</TableCell>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow key={config.id}>
+            <TableCell>{typeof value.from === 'string' && value.from.length > 0 ? value.from : 'Ej angivet'}</TableCell>
+            <TableCell>{typeof value.to === 'string' && value.to.length > 0 ? value.to : 'Ej angivet'}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   )
 }
