82fb9ec6eed084699ad0aefd3a4507e90ad1d7e9
cigerhed <christoffer.sigerhed@soprasteria.com>
2025-04-16 16:43:31 +0200

    K1J-270: Fix test.

--- apps/minaintyg/src/components/ProtectedRoute/ProtectedRoute.test.tsx
+++ apps/minaintyg/src/components/ProtectedRoute/ProtectedRoute.test.tsx
@@ -5,7 +5,6 @@ import { server } from '../../mocks/server'
 import { store } from '../../store/store'
 import { withRouter } from '../../utils/withRouter'
 import { ProtectedRoute } from './ProtectedRoute'
-import { endSession } from '../../store/slice/session.slice'
 
 function renderComponent() {
   return render(<Provider store={store}>{withRouter(<ProtectedRoute>content</ProtectedRoute>)}</Provider>)
@@ -31,46 +30,21 @@ it('Should display dialog when there is 5 minutes left of the session', async ()
   expect(await screen.findByText('Vill du fortsätta vara inloggad?')).toBeInTheDocument()
 })
 
-it('Should redirect to saml login when unable to load user', async () => {
-  server.use(rest.get('/api/user', (_, res, ctx) => res(ctx.status(500))))
-  const openSpy = vi.spyOn(window, 'open')
-  vi.stubEnv('VITE_LOGIN_URL', '/saml2/authenticate/eleg')
+it('Should log out user and reset API state when session expires in less than 30 seconds', async () => {
+  const endSessionSpy = vi.spyOn(store, 'dispatch')
+  const formSubmitSpy = vi.spyOn(HTMLFormElement.prototype, 'submit')
+  server.use(
+    rest.get('/api/user', (_, res, ctx) => res(ctx.status(200), ctx.json({ loginMethod: 'SAML' }))),
+    rest.get('/api/session/ping', (_, res, ctx) => res(ctx.status(200), ctx.json({ hasSession: true, secondsUntilExpire: 10 })))
+  )
 
   renderComponent()
 
   await waitFor(() => {
-    expect(openSpy).toHaveBeenCalledWith('/saml2/authenticate/eleg', '_self')
-  })
-})
-
-it('Should redirect to saml logout when user has less than 30 seconds left in session', async () => {
-  const dispatchMock = vi.spyOn(store, 'dispatch')
-  const createElementSpy = vi.spyOn(document, 'createElement')
-  const formSubmitSpy = vi.fn()
-  createElementSpy.mockImplementation((tag) => {
-    if (tag === 'form') {
-      return {
-        method: '',
-        action: '',
-        appendChild: vi.fn(),
-        submit: formSubmitSpy,
-      } as unknown as HTMLFormElement
-    }
-    if (tag === 'input') {
-      return {} as HTMLInputElement
-    }
-    return document.createElement(tag)
-  })
-
-  renderComponent()
-
-  await waitFor(() => {
-    expect(dispatchMock).toHaveBeenCalledWith(endSession({ reason: 'logged-out' }))
-  })
-
-  await waitFor(() => {
+    expect(endSessionSpy).toHaveBeenCalledWith(endSession({ reason: 'logged-out' }))
+    // eslint-disable-next-line testing-library/no-wait-for-multiple-assertions
     expect(formSubmitSpy).toHaveBeenCalled()
+    // eslint-disable-next-line testing-library/no-wait-for-multiple-assertions
+    expect(endSessionSpy).toHaveBeenCalledWith(api.util.resetApiState())
   })
-
-  createElementSpy.mockRestore()
 })
