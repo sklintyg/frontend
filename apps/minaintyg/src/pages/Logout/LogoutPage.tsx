9df1749fcf2bf9fa17180c6407e42804cbced8fc
Samuel Tilly <samuel.tilly@gmail.com>
2025-04-23 09:00:50 +0200

    logout on page

--- apps/minaintyg/src/pages/Logout/LogoutPage.tsx
+++ apps/minaintyg/src/pages/Logout/LogoutPage.tsx
@@ -1,8 +1,14 @@
-import type { ReactNode } from 'react'
-import { useAppSelector } from '../../store/hooks'
+import { useEffect, type ReactNode } from 'react'
+import { useAppDispatch, useAppSelector } from '../../store/hooks'
+import { endSession } from '../../store/slice/session.slice'
 
 export function LogoutPage({ children }: { children?: ReactNode }) {
   const hasSessionEnded = useAppSelector((state) => state.sessionSlice.hasSessionEnded)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(endSession({ reason: 'logged-out' }))
+  }, [dispatch])
 
   if (hasSessionEnded) {
     return children
