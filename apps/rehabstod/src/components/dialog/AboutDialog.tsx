6944a7c7fb3c5ca6baddd17bbbb9c54c19c5d376
Samuel Tilly <samuel.tilly@gmail.com>
2025-05-07 14:43:13 +0200

    K1J-671: Update rehabstod texts

--- apps/rehabstod/src/components/dialog/AboutDialog.tsx
+++ apps/rehabstod/src/components/dialog/AboutDialog.tsx
@@ -17,30 +17,30 @@ export function AboutDialog() {
     <Dialog open={showAboutDialog} onOpenChange={(open) => dispatch(updateShowAboutDialog(open))} dismissible headline="Om Rehabstöd">
       <div className="ids-content text-base [&:not(:last-child)]:mb-8">
         <p className="ids-body">
-          Rehabstöd är en tjänst för dig som arbetar med att koordinera rehabiliteringsinsatser för sjukskrivna patienter.{' '}
+          Rehabstöd är en tjänst för dig som arbetar med att koordinera rehabiliteringsinsatser för sjukskrivna patienter.
         </p>
         {config && <p className="ids-body">Nuvarande version är {config.version}. </p>}
         <p className="ids-body">
           Rehabstöd är utvecklat för Chrome och Edge chromium. Andra webbläsare kan användas, men det finns risk att problem kan uppstå.
-          Tjänsten kräver att JavaScript är aktiverat.{' '}
+          Tjänsten kräver att JavaScript är aktiverat.
         </p>
 
         <h2 className="ids-heading-3">Vad kan jag se i Rehabstöd?</h2>
         <p className="ids-body">
-          Är du rehabkoordinator får du en överblick över vårdenhetens alla pågående sjukfall. Är du läkare ser du de pågående sjukfall där
-          du har skrivit det senaste intyget.{' '}
+          Arbetar du med koordineringsinsatser på en vårdenhet får du en överblick över vårdenhetens alla pågående sjukfall. Är du läkare
+          ser du de pågående sjukfall där du har skrivit det senaste intyget.
         </p>
         <p className="ids-body">
           När du klickar på fliken &quot;Pågående sjukfall&quot; visas pågående sjukfall för den enhet du har loggat in på. För varje
           sjukfall visas patientens personuppgifter, diagnos, sjukskrivningstid, sjukskrivningsgrad med mera. Som rehabkoordinator kan du
-          ange i vilken status sjukfallet befinner sig, till exempel kontaktad, aktiv, avslutad eller avböjt.{' '}
+          ange i vilken status sjukfallet befinner sig, till exempel kontaktad, aktiv, avslutad eller avböjt.
         </p>
         <p className="ids-body">
           När du klickar på fliken “Läkarutlåtanden” kan du se läkarutlåtanden (FK 7800, FK 7801 och FK 7802) som utfärdats på enheten de
-          senaste tre åren.{' '}
+          senaste tre åren.
         </p>
         <p className="ids-body">
-          Om du har tillgång till flera enheter kan du byta enhet i genom att klicka på ditt namn högst upp till höger.{' '}
+          Om du har tillgång till flera enheter kan du byta enhet i genom att klicka på ditt namn högst upp till höger.
         </p>
         <p className="ids-body">Informationen som visas loggas enligt Patientdatalagen (PDL).</p>
       </div>
@@ -50,7 +50,7 @@ export function AboutDialog() {
           <p className="ids-body" />
           <p>
             Om du vill se mer information om SRS, t.ex. hur prediktionen räknas ut, fler åtgärder, mer nationell statistik så kan du gå till
-            SRS webbplats. När du klickar på länken nedan öppnas SRS webbplats i ett nytt fönster.{' '}
+            SRS webbplats. När du klickar på länken nedan öppnas SRS webbplats i ett nytt fönster.
           </p>
           {links?.rattsjukskrivning && <DynamicLink link={links.rattsjukskrivning} />}
         </div>
