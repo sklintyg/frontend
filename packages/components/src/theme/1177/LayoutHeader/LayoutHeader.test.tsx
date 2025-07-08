34fecee6f5853a31ac802be19b61c577400c8d9c
Samuel Tilly <samuel.tilly@gmail.com>
2025-07-08 08:40:48 +0200

    fix tests and ts issue

--- packages/components/src/theme/1177/LayoutHeader/LayoutHeader.test.tsx
+++ packages/components/src/theme/1177/LayoutHeader/LayoutHeader.test.tsx
@@ -2,13 +2,7 @@ import { render } from '@testing-library/react'
 import { LayoutHeader } from './LayoutHeader'
 
 it('Should render links when user is loaded', async () => {
-  const { container } = render(<LayoutHeader mode="development" skipToContent="skip-to-content" />)
+  const { container } = render(<LayoutHeader avatar={undefined} mode="development" skipToContent="skip-to-content" />)
 
-  expect(container).toMatchInlineSnapshot(`
-    <div>
-      <ids-header
-        class="z-40 bg-white print:hidden"
-      />
-    </div>
-  `)
+  expect(container).toMatchSnapshot()
 })
