bd42acc031d8c0d4f6123c8d0777577810b9ca61
Samuel Tilly <samuel.tilly@gmail.com>
2025-07-14 09:33:22 +0200

    K1J-984: Fix layout issues

--- apps/rehabstod/src/components/DynamicLink/DynamicLink.tsx
+++ apps/rehabstod/src/components/DynamicLink/DynamicLink.tsx
@@ -1,14 +1,13 @@
-import { IDSIconExternal } from '@inera/ids-react'
 import type { Link } from '../../schemas'
 
 export type DynamicLinkType = 'regular' | 'footer' | 'sub-footer'
 
 export function DynamicLink({ type = 'regular', link }: { link?: Link; type?: DynamicLinkType }) {
-  const size = type === 'sub-footer' ? '15px' : '0.875em'
+  // const size = type === 'sub-footer' ? '15px' : '0.875em'
   const styles: Record<DynamicLinkType, string> = {
     regular: 'text-accent-40 decoration-accent-40',
     footer: 'text-neutral-20 decoration-neutral-20',
-    'sub-footer': 'text-white decoration-white',
+    'sub-footer': 'text-accent-40 decoration-accent-40',
   }
 
   if (link == null) {
@@ -20,16 +19,7 @@ export function DynamicLink({ type = 'regular', link }: { link?: Link; type?: Dy
   return (
     <a href={url} target={target} className={`${styles[type]} underline`} rel={target === '_blank' ? 'noreferrer' : undefined}>
       {text}
-      {target === '_blank' && (
-        <IDSIconExternal
-          className="inline pl-2 align-middle"
-          width={size}
-          height={size}
-          slot="append-icon"
-          color="currentColor"
-          color2="currentColor"
-        />
-      )}
+      {target === '_blank' && <span className="ids-icon-external-link-small ids-icon--text-end" />}
     </a>
   )
 }
