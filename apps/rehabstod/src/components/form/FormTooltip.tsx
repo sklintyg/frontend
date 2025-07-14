5bd8e8f4d124a3adce944f434f918d45c93b8c5b
Samuel Tilly <samuel.tilly@gmail.com>
2025-07-14 14:05:05 +0200

    fix tooltip label issue

--- apps/rehabstod/src/components/form/FormTooltip.tsx
+++ apps/rehabstod/src/components/form/FormTooltip.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
 import { IDSTooltip } from '@inera/ids-react'
-import type { ReactNode } from 'react'
+import { useId, type ReactNode } from 'react'
 
 export function FormTooltip({ children }: { children: ReactNode }) {
+  const id = useId()
+
   return (
     <IDSTooltip slot="tooltip" position="bottom-left">
-      <span className="ids-icon-information ids-icon--m ids-icon--color-preset-1" slot="trigger" tabIndex={0} />
-      <div className="max-w-md">{children}</div>
+      <span className="ids-icon-information ids-icon--m ids-icon--color-preset-1" aria-labelledby={id} slot="trigger" tabIndex={0} />
+      <div id={id} className="max-w-md">
+        {children}
+      </div>
     </IDSTooltip>
   )
 }
