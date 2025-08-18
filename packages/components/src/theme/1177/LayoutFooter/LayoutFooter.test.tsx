d14a28caf58ac8be40ed9e36c10740ab9a7afc78
Samuel Tilly <samuel.tilly@gmail.com>
2025-08-18 09:41:11 +0200

    remove unreliable snapshot test

--- packages/components/src/theme/1177/LayoutFooter/LayoutFooter.test.tsx
+++ packages/components/src/theme/1177/LayoutFooter/LayoutFooter.test.tsx
@@ -1,14 +1,21 @@
 import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 import { LayoutFooter } from './LayoutFooter'
 
-it('Should render correctly', () => {
-  const { container } = render(<LayoutFooter hasSession={false} />)
-  expect(container).toMatchSnapshot()
-  expect(screen.getAllByRole('link', { name: 'Webbkarta' })).toHaveLength(0)
+it('Should not have Webbkarta link when session is available', () => {
+  render(
+    <MemoryRouter>
+      <LayoutFooter hasSession={false} />
+    </MemoryRouter>
+  )
+  expect(screen.queryAllByRole('link', { name: 'Webbkarta' })).toHaveLength(0)
 })
 
 it('Should have Webbkarta link when session is available', () => {
-  render(<LayoutFooter hasSession />)
-  const links = screen.getAllByRole('link', { name: 'Webbkarta' })
-  expect(links).toHaveLength(2)
+  render(
+    <MemoryRouter>
+      <LayoutFooter hasSession />
+    </MemoryRouter>
+  )
+  expect(screen.queryAllByRole('link', { name: 'Webbkarta' })).toHaveLength(2)
 })
