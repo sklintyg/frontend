96f7e0cfa6b903a3a28c82b0b7c0ada1f147e54b
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2025-08-27 08:59:59 +0200

    Wrong selector function (#1507)

--- apps/webcert/src/page/SelectUnitPage.tsx
+++ apps/webcert/src/page/SelectUnitPage.tsx
@@ -7,7 +7,6 @@ import SimpleTable from '../components/Table/SimpleTable'
 import ModalBase from '../components/utils/Modal/ModalBase'
 import logo from '../images/webcert_logo.png'
 import { useAppSelector } from '../store/store'
-import { getUser } from '../store/user/userActions'
 import { getCareProviders, getSelectUnitHeading } from '../store/user/userSelectors'
 import type { CareUnit, Unit } from '../types'
 
@@ -31,11 +30,10 @@ function SelectUnitRow({ unit, certificateId }: { unit: CareUnit | Unit; certifi
 
 export function SelectUnitPage() {
   const { certificateId } = useParams<{ certificateId: string }>()
-  const user = useAppSelector(getUser)
   const modalTitle = useAppSelector(getSelectUnitHeading)
   const careProviders = useAppSelector(getCareProviders)
 
-  if (!user || !certificateId) {
+  if (!certificateId) {
     return null
   }
 
