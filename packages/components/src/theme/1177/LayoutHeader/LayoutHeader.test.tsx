8b3589d6113ee03b783acc59905700d83d5fc501
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-09-12 13:49:18 +0200

    K1J-1361: Add dummy links to LayoutHeader.test.tsx.

--- packages/components/src/theme/1177/LayoutHeader/LayoutHeader.test.tsx
+++ packages/components/src/theme/1177/LayoutHeader/LayoutHeader.test.tsx
@@ -2,7 +2,12 @@ import { render } from '@testing-library/react'
 import { LayoutHeader } from './LayoutHeader'
 
 it('Should render links when user is loaded', async () => {
-  const { container } = render(<LayoutHeader avatar={undefined} mode="development" skipToContent="skip-to-content" />)
+  const links = [
+    { id: '1', name: 'Start', url: '/' },
+    { id: '2', name: 'Om Mina intyg', url: '/about' },
+    { id: '3', name: 'Hjälp', url: '/help' },
+  ]
+  const { container } = render(<LayoutHeader avatar={undefined} links={links} skipToContent="skip-to-content" />)
 
   expect(container).toMatchSnapshot()
 })
