430d363f27ba829d96c9651c61c9e90b133fbfdd
Samuel Tilly <samuel.tilly@gmail.com>
2025-10-01 10:09:21 +0200

    fix issue with global mocks

--- packages/components/src/setupTests.ts
+++ packages/components/src/setupTests.ts
@@ -1,16 +1,6 @@
 import '@testing-library/jest-dom/vitest'
 import { cleanup } from '@testing-library/react'
-import { mockViewport } from 'jsdom-testing-mocks'
 import { afterEach } from 'vitest'
-
-Object.assign(global, global, {
-  open: vi.fn(),
-  scrollTo: vi.fn(),
-  visualViewport: {
-    ...mockViewport({ width: '1440px', height: '900px' }),
-    addEventListener: vi.fn(),
-  },
-})
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
