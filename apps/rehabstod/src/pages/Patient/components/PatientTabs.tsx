41b8f91052f52c146f53334c567461739167e5cb
Samuel Tilly <samuel.tilly@gmail.com>
2025-10-30 08:32:30 +0100

    fix issue with tabs on LUCertificate page

--- apps/rehabstod/src/pages/Patient/components/PatientTabs.tsx
+++ apps/rehabstod/src/pages/Patient/components/PatientTabs.tsx
@@ -7,7 +7,13 @@ export function PatientTabs({ active }: { active: number }) {
   const [activeTab, setActiveTab] = useState(active)
 
   return (
-    <IDSTabs onTabChange={setActiveTab} tabs={[<IDSTab key="tab-1" label="Sjukfall" />, <IDSTab key="tab-2" label="Läkarutlåtanden" />]}>
+    <IDSTabs
+      onTabChange={setActiveTab}
+      tabs={[
+        <IDSTab key="tab-1" label="Sjukfall" selected={activeTab === 0} />,
+        <IDSTab key="tab-2" label="Läkarutlåtanden" selected={activeTab === 1} />,
+      ]}
+    >
       <IDSTabPanel noFocus>{activeTab === 0 && <PatientSickLeaves />}</IDSTabPanel>
       <IDSTabPanel noFocus>{activeTab === 1 && <PatientLUCertificatesTable />}</IDSTabPanel>
     </IDSTabs>
