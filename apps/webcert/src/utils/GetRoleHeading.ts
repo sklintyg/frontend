2971fb4c4f034e5001eb0b718ac4b20d737d5166
Alexey Ryabov <alexey.ryabov@soprasteria.com>
2025-11-20 10:51:22 +0100

    K1J-1591: Add component to display heading role based on resourse links

---
+++ apps/webcert/src/utils/GetRoleHeading.ts
@@ -0,0 +1,20 @@
+import { getUser, getUserResourceLink } from '../store/user/userSelectors'
+import { type ResourceLink, ResourceLinkType } from '../types'
+import { useAppSelector } from '../store/store'
+
+export function GetRoleHeading(): string | ResourceLink | null {
+  const registerLink = useAppSelector(getUserResourceLink(ResourceLinkType.ACCESS_REGISTER_PRIVATE_PRACTITIONER))
+  const editLink = useAppSelector(getUserResourceLink(ResourceLinkType.ACCESS_EDIT_PRIVATE_PRACTITIONER))
+  const notAuthorizedLink = useAppSelector(getUserResourceLink(ResourceLinkType.NOT_AUTHORIZED_PRIVATE_PRACTITIONER))
+  const user = useAppSelector(getUser)
+
+  if (notAuthorizedLink && notAuthorizedLink.enabled) {
+    return 'Ej behörig'
+  } else if (registerLink && registerLink.enabled) {
+    return 'Ej registrerad'
+  } else if (editLink && editLink.enabled) {
+    return editLink // FIXME: Placeholder, should be replaced with a proper link
+  } else {
+    return user?.role ?? null
+  }
+}
