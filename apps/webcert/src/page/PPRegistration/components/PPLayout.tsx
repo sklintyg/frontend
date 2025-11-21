d41c4da79955b1aceb67a0cf5d1afc785db85013
Samuel Tilly <samuel.tilly@gmail.com>
2025-11-21 08:18:33 +0100

    navigate back to missing step

--- apps/webcert/src/page/PPRegistration/components/PPLayout.tsx
+++ apps/webcert/src/page/PPRegistration/components/PPLayout.tsx
@@ -1,12 +1,23 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import AppHeader from '../../../components/AppHeader/AppHeader'
 import { UserHeaderMenu, UserHeaderMenuItem } from '../../../components/AppHeader/UserHeaderMenu'
 import SystemBanners from '../../../components/notification/SystemBanners'
 import logo from '../../../images/webcert_logo.png'
 import { useGetHOSPInformationQuery } from '../../../store/pp/ppApi'
+import { useAppSelector } from '../../../store/store'
 
 export function PPLayout() {
   useGetHOSPInformationQuery()
+  const location = useLocation()
+  const navigate = useNavigate()
+  const hasSomeStepOneData = useAppSelector((state) => Object.values(state.ui.pp.step01.data).some(Boolean))
+
+  useEffect(() => {
+    if (location.pathname !== '/register/step-1' && !hasSomeStepOneData) {
+      navigate('/register/step-1')
+    }
+  })
 
   return (
     <>
