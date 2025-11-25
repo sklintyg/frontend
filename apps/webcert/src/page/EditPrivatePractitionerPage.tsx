9c02ce57833d5f06ae68a9f0c06f021fd5eb63e2
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-11-25 11:32:01 +0100

    K1J-1610: Refactor EditPrivatePractitionerPage to include redirect functionality

--- apps/webcert/src/page/EditPrivatePractitionerPage.tsx
+++ apps/webcert/src/page/EditPrivatePractitionerPage.tsx
@@ -1,3 +1,12 @@
+import { ResourceAccess } from '../utils/ResourceAccess'
+import { ResourceLinkType } from '../types'
+
 export function EditPrivatePractitionerPage() {
   return <div></div>
 }
+
+export const EditPrivatePractitionerPageWithRedirect = () => (
+  <ResourceAccess linkType={ResourceLinkType.ACCESS_EDIT_PRIVATE_PRACTITIONER}>
+    <EditPrivatePractitionerPage />
+  </ResourceAccess>
+)
