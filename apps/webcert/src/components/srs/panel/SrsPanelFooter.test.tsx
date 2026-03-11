527f3928210224bd0f0ef3edccc04955d9d1ab2f
mhornfeldt <michaela.hornfeldt@gmail.com>
2026-03-11 15:26:57 +0100

    K1J-1598: Fixed SRS links

--- apps/webcert/src/components/srs/panel/SrsPanelFooter.test.tsx
+++ apps/webcert/src/components/srs/panel/SrsPanelFooter.test.tsx
@@ -4,13 +4,14 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { Provider } from 'react-redux'
 import { fakeSrsInfo } from '../../../faker'
-import { configureApplicationStore } from '../../../store/configureApplicationStore'
 import { logSrsInteraction, updateSrsInfo } from '../../../store/srs/srsActions'
 import { srsMiddleware } from '../../../store/srs/srsMiddleware'
 import dispatchHelperMiddleware, { clearDispatchedActions, dispatchedActions } from '../../../store/test/dispatchHelperMiddleware'
 import type { SrsInfoForDiagnosis } from '../../../types'
 import { SrsInformationChoice } from '../../../types'
 import SrsPanelFooter from './SrsPanelFooter'
+import { SKR_DIAGNOSIS_LINK_MAP, SKR_FALLBACK_LINK } from '../srsUtils'
+import { configureApplicationStore } from '../../../store/configureApplicationStore'
 
 let store: EnhancedStore
 let srsInfo: SrsInfoForDiagnosis
@@ -25,7 +26,7 @@ const renderComponent = (informationChoice: SrsInformationChoice) => {
 describe('SrsPanelFooter', () => {
   beforeEach(() => {
     store = configureApplicationStore([dispatchHelperMiddleware, srsMiddleware])
-    srsInfo = fakeSrsInfo()
+    srsInfo = fakeSrsInfo({ atgarderDiagnosisCode: 'F32', statistikDiagnosisCode: 'M54.5' })
     store.dispatch(updateSrsInfo(srsInfo))
   })
 
@@ -35,7 +36,7 @@ describe('SrsPanelFooter', () => {
 
   it('should set correct link in footer for recommendations', async () => {
     renderComponent(SrsInformationChoice.RECOMMENDATIONS)
-    await expect(screen.getByRole('link')).toHaveAttribute('href', `https://skr.se/${srsInfo.atgarderDiagnosisCode}`)
+    await expect(screen.getByRole('link')).toHaveAttribute('href', SKR_DIAGNOSIS_LINK_MAP[srsInfo.atgarderDiagnosisCode])
   })
 
   it('should set correct text in footer for recommendations', () => {
@@ -45,7 +46,13 @@ describe('SrsPanelFooter', () => {
 
   it('should set correct link in footer for statistics', async () => {
     renderComponent(SrsInformationChoice.STATISTICS)
-    await expect(screen.getByRole('link')).toHaveAttribute('href', `https://skr.se/${srsInfo.statistikDiagnosisCode}`)
+    await expect(screen.getByRole('link')).toHaveAttribute('href', SKR_DIAGNOSIS_LINK_MAP[srsInfo.statistikDiagnosisCode])
+  })
+
+  it('should use fallback link for unknown diagnosis code', async () => {
+    store.dispatch(updateSrsInfo(fakeSrsInfo({ statistikDiagnosisCode: 'UNKNOWN' })))
+    renderComponent(SrsInformationChoice.STATISTICS)
+    await expect(screen.getByRole('link')).toHaveAttribute('href', SKR_FALLBACK_LINK)
   })
 
   it('should set correct text in footer for statistics', () => {
--- apps/webcert/src/components/srs/srsUtils.tsx
+++ apps/webcert/src/components/srs/srsUtils.tsx
@@ -2,6 +2,54 @@ import type { SrsPrediction, ValueDiagnosisList, CertificateMetadata } from '../
 import { SrsSickLeaveChoice, SrsUserClientContext, CertificateStatus } from '../../types'
 import { isRenewedChild } from '../../utils'
 
+export const SKR_DIAGNOSIS_LINK_MAP: Record<string, string> = {
+  R52: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/allmannasymtomochsjukhetstecken/r52smartaochvarksomejklassificerasannorstades.33589.html',
+  R53: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/allmannasymtomochsjukhetstecken/r53sjukdomskanslaochtrotthet.33591.html',
+  M16: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/artrosochandraledsjukdomar/m16hoftledsartros.33581.html',
+  M17: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/artrosochandraledsjukdomar/m17knaartros.33583.html',
+  M19: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/artrosochandraledsjukdomar/m19andraartroser.33585.html',
+  M23: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/artrosochandraledsjukdomar/m23andrasjukligaforandringariknaled.33587.html',
+  S52: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/frakturerochledskador/s52frakturpaunderarm.33593.html',
+  S62: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/frakturerochledskador/s62frakturpahandledochhand.33595.html',
+  S82: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/frakturerochledskador/s82frakturpaunderbeninklusivefotled.33597.html',
+  S83: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/frakturerochledskador/s83luxationochdistorsioniknaetslederochligament.33599.html',
+  F31: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/psykiskasjukdomarochsyndrom/f31bipolarsjukdom.33535.html',
+  F32: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/psykiskasjukdomarochsyndrom/f32depressivepisod.33537.html',
+  F33: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/psykiskasjukdomarochsyndrom/f33recidiverandedepression.33539.html',
+  F41: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/psykiskasjukdomarochsyndrom/f41andraangestsyndrom.33541.html',
+  F43: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/psykiskasjukdomarochsyndrom/f43anpassningsstorningarochreaktionpasvarstress.33543.html',
+  'F43.0': 'https://extra.skr.se/rattsjukskrivning/radochatgarder/psykiskasjukdomarochsyndrom/f430akutstressreaktion.33545.html',
+  'F43.1':
+    'https://extra.skr.se/rattsjukskrivning/radochatgarder/psykiskasjukdomarochsyndrom/f431posttraumatiskstressyndromptsd.33547.html',
+  'F43.2':
+    'https://extra.skr.se/rattsjukskrivning/radochatgarder/psykiskasjukdomarochsyndrom/f432anpassningsstorninglivskrissorgreaktion.33549.html',
+  'F43.8':
+    'https://extra.skr.se/rattsjukskrivning/radochatgarder/psykiskasjukdomarochsyndrom/f438andraspecificeradereaktionerpasvarstress.33551.html',
+  'F43.8A': 'https://extra.skr.se/rattsjukskrivning/radochatgarder/psykiskasjukdomarochsyndrom/f438autmattningssyndrom.33553.html',
+  I63: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/sjukdomarihjarnanskarl/i63cerebralinfarkt.33601.html',
+  G56: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/sjukdomarimuskuloskeletalasystemetochnerver/g56mononeuropatisjukdomienendaperifernerviovreextremitet.33555.html',
+  M51: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/sjukdomarimuskuloskeletalasystemetochnerver/m51diskbrackandrasjukdomarimellankotsskivorna.33557.html',
+  M54: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/sjukdomarimuskuloskeletalasystemetochnerver/m54ryggvark.33561.html',
+  'M54.2': 'https://extra.skr.se/rattsjukskrivning/radochatgarder/sjukdomarimuskuloskeletalasystemetochnerver/m542cervikalgi.33563.html',
+  'M54.4':
+    'https://extra.skr.se/rattsjukskrivning/radochatgarder/sjukdomarimuskuloskeletalasystemetochnerver/m544lumbagomedischias.33565.html',
+  'M54.5': 'https://extra.skr.se/rattsjukskrivning/radochatgarder/sjukdomarimuskuloskeletalasystemetochnerver/m545lumbago.33567.html',
+  M75: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/sjukdomarimuskuloskeletalasystemetochnerver/m75sjukdomstillstandiskulderled.33569.html',
+  'M75.4':
+    'https://extra.skr.se/rattsjukskrivning/radochatgarder/sjukdomarimuskuloskeletalasystemetochnerver/m754impingementsyndromiaxelled.33571.html',
+  M77: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/sjukdomarimuskuloskeletalasystemetochnerver/m77andraentesopatiersjukdomariperiferaligamentochmuskelfasten.33573.html',
+  M79: 'https://extra.skr.se/rattsjukskrivning/radochatgarder/sjukdomarimuskuloskeletalasystemetochnerver/m79andrasjukdomstillstandimjukvavnadersomejklassificerasannorstades.33575.html',
+  'M79.1': 'https://extra.skr.se/rattsjukskrivning/radochatgarder/sjukdomarimuskuloskeletalasystemetochnerver/m791myalgi.33577.html',
+  'M79.7': 'https://extra.skr.se/rattsjukskrivning/radochatgarder/sjukdomarimuskuloskeletalasystemetochnerver/m797fibromyalgi.33579.html',
+}
+
+export const SKR_FALLBACK_LINK = 'https://extra.skr.se/rattsjukskrivning/radochatgarder'
+
+export const getSKRLink = (diagnosisCode: string): string => {
+  const normalised = diagnosisCode.toUpperCase()
+  return SKR_DIAGNOSIS_LINK_MAP[normalised] ?? SKR_FALLBACK_LINK
+}
+
 export const SICKLEAVE_CHOICES_TEXTS = ['Ny sjukskrivning', 'Förlängning', 'Förlängning efter 60 dagar']
 export const SRS_OPINION_LABELS = ['Högre', 'Korrekt', 'Lägre', 'Kan ej bedöma']
 export const SRS_OPINION_IDS = ['HOGRE', 'KORREKT', 'LAGRE', 'KAN_EJ_BEDOMA']
