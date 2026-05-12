a567321a4340989d220922316cb303b877e7d4fb
cigerhed <christoffer.sigerhed@soprasteria.com>
2026-05-12 11:23:55 +0200

    K1J-1986: Handle lint.

--- apps/webcert/src/utils/textUtils.ts
+++ apps/webcert/src/utils/textUtils.ts
@@ -2,7 +2,7 @@ export const replaceDecimalSeparator = (text: string): string => {
   return text.replace(/\./gm, ',')
 }
 
-export interface FilterUnsupportedCharactersResult {
+interface FilterUnsupportedCharactersResult {
   sanitized: string
   hasRemovedCharacters: boolean
 }
