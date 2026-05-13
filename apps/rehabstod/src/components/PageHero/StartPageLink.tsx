9f143030bb35d280033f83e696aab35f8e1d7e74
Jacob Almqvist <jacob.almqvist@soprasteria.com>
2026-05-13 14:53:51 +0200

    K1J-1522: Fix error page problems (#1627)

--- apps/rehabstod/src/components/PageHero/StartPageLink.tsx
+++ apps/rehabstod/src/components/PageHero/StartPageLink.tsx
@@ -1,11 +1,16 @@
+import { Icon } from '@frontend/components'
+import { IDSLink } from '@inera/ids-react'
 import { Link } from 'react-router-dom'
 
 export function StartPageLink() {
   return (
     <div className="text-center">
-      <Link to="/" className="ids-icon-arrow-link ids-link--start-icon ids-link ids-link--color-1">
-        Till startsidan
-      </Link>
+      <IDSLink>
+        <Link to="/">
+          <Icon icon="arrow-right-small" textStart />
+          Till startsidan
+        </Link>
+      </IDSLink>
     </div>
   )
 }
