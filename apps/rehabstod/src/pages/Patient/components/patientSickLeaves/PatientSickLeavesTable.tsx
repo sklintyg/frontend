32a05046213a1fe0443887638e3121258855afdd
ChrilleSigerhed <christoffer.sigerhed@gmail.com>
2026-07-22 09:28:16 +0200

    K1J-1539: Fix the sorting order for patient tables.

--- apps/rehabstod/src/pages/Patient/components/patientSickLeaves/PatientSickLeavesTable.tsx
+++ apps/rehabstod/src/pages/Patient/components/patientSickLeaves/PatientSickLeavesTable.tsx
@@ -1,12 +1,13 @@
 import { Heading } from '@frontend/components'
 import type { ReactNode } from 'react'
+import { useState } from 'react'
 import { Table } from '../../../../components/Table/Table'
 import { TableHeader } from '../../../../components/Table/tableHeader/TableHeader'
 import type { PatientSjukfall } from '../../../../schemas/patientSchema'
-import { PatientColumn } from '../../../../store/slices/patientTableColumns.slice'
 import { PatientDiagnosisAccordion } from '../PatientAccordion/PatientDiagnosisAccordion'
 import { PatientTableBody } from './PatientTableBody'
 import { usePatientSickLeavesTableColumns } from './hooks/usePatientSickLeavesTableColumns'
+import { PatientColumn } from '../../../../store/slices/patientTableColumns.slice'
 
 export function PatientSickLeavesTable({
   sickLeaves,
@@ -22,6 +23,10 @@ export function PatientSickLeavesTable({
   open?: boolean
 }) {
   const columns = usePatientSickLeavesTableColumns()
+  const [tableState, setTableState] = useState<{ sortColumn: string; ascending: boolean }>({
+    sortColumn: PatientColumn.Startdatum,
+    ascending: false,
+  })
 
   return (
     <>
@@ -30,7 +35,7 @@ export function PatientSickLeavesTable({
       </Heading>
       {sickLeaves.map(({ start, slut, diagnos, dagar, intyg }) => (
         <PatientDiagnosisAccordion key={`${start}${slut}`} diagnos={diagnos} dagar={dagar} open={open}>
-          <Table header={<TableHeader columns={columns} />} sortColumn={PatientColumn.Num}>
+          <Table header={<TableHeader columns={columns} />} sortColumn={tableState.sortColumn} onSortChange={setTableState}>
             <PatientTableBody certificates={intyg} isDoctor={isDoctor} />
           </Table>
           {children}
